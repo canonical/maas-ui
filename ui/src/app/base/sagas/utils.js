fdc3176bbbf23f85b055c88b7736dd1e4b76a899
Kit Randel <kit.randel@canonical.com>
2019-07-24 17:44:43 +1200

    Open websocket connection with csrftoken from cookie. (#40)

---
+++ ui/src/app/base/sagas/utils.js
@@ -0,0 +1,11 @@
+/**
+ * Get cookie value by name.
+ *
+ * @param {string} n - cookie name.
+ */
+export const getCookie = n => {
+  const cookie = `; ${document.cookie}`.match(`;\\s*${n}=([^;]+)`);
+  return cookie ? cookie[1] : undefined;
+};
+
+export default getCookie;
---
+++ ui/src/app/base/sagas/utils.test.js
@@ -0,0 +1,17 @@
+import getCookie from "./utils";
+
+describe("saga utils", () => {
+  it("returns undefined if a cookie is not found", () => {
+    expect(getCookie("foo")).toEqual(undefined);
+  });
+
+  it("returns the value of a cookie by name", () => {
+    Object.defineProperty(document, "cookie", {
+      get: jest.fn().mockImplementation(() => {
+        return "a=foo; b=bar; c=baz";
+      })
+    });
+
+    expect(getCookie("b")).toEqual("bar");
+  });
+});
