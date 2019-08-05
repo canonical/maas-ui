1edcc3ddf88f80a33f9834c7440cd7366659c000
Steve Rydz <steve.rydz@canonical.com>
2019-08-05 14:32:23 +0100

    Add logic to configuration reducer

--- ui/src/app/settings/reducers/configuration.js
+++ ui/src/app/settings/reducers/configuration.js
@@ -1,3 +1,25 @@
-const configuration = {};
+import produce from "immer";
+
+const configuration = produce(
+  (draft, action) => {
+    switch (action.type) {
+      case "FETCH_CONFIGURATION_START":
+        draft.loading = true;
+        return;
+      case "FETCH_CONFIGURATION_SUCCESS":
+        draft.loading = false;
+        draft.loaded = true;
+        draft.items = action.payload;
+        return;
+      default:
+        return draft;
+    }
+  },
+  {
+    loading: false,
+    loaded: false,
+    items: []
+  }
+);
 
 export default configuration;
--- ui/src/app/settings/reducers/configuration.test.js
+++ ui/src/app/settings/reducers/configuration.test.js
@@ -2,6 +2,48 @@ import configuration from "./configuration";
 
 describe("status", () => {
   it("should return the initialState", () => {
+    expect(configuration(undefined, {})).toEqual({
+      loading: false,
+      loaded: false,
+      items: []
+    });
+  });
 
+  it("should correctly reduce FETCH_CONFIGURATION_START", () => {
+    expect(
+      configuration(undefined, {
+        type: "FETCH_CONFIGURATION_START"
+      })
+    ).toEqual({
+      loading: true,
+      loaded: false,
+      items: []
+    });
+  });
+
+  it("should correctly reduce FETCH_CONFIGURATION_SUCCESS", () => {
+    expect(
+      configuration(
+        {
+          loading: true,
+          loaded: false,
+          items: []
+        },
+        {
+          type: "FETCH_CONFIGURATION_SUCCESS",
+          payload: [
+            { name: "boot_images_no_proxy", value: false },
+            { name: "enable_third_party_drivers", value: true }
+          ]
+        }
+      )
+    ).toEqual({
+      loading: false,
+      loaded: true,
+      items: [
+        { name: "boot_images_no_proxy", value: false },
+        { name: "enable_third_party_drivers", value: true }
+      ]
+    });
   });
 });
