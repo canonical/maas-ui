5686fb4b1ee73d04db13b68595da184a74fbff07
Steve Rydz <steve.rydz@canonical.com>
2019-08-05 15:59:06 +0100

    Trying to render something

--- ui/src/app/settings/containers/Configuration/Configuration.js
+++ ui/src/app/settings/containers/Configuration/Configuration.js
@@ -1,5 +1,28 @@
-import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import PropTypes from "prop-types";
+import React, { useState, useEffect } from "react";
 
-const Configuration = () => <h4>Configuration</h4>;
+import actions from "app/settings/actions";
+import selectors from "app/settings/selectors";
+import baseSelectors from "app/base/selectors";
+import Loader from "app/base/components/Loader";
+
+const Configuration = () => {
+  const configuration = useSelector(state =>
+    selectors.configuration.all(state)
+  );
+  const loading = useSelector(selectors.configuration.loading);
+  const loaded = useSelector(selectors.configuration.loaded);
+
+  return (
+    <>
+      <h4>
+        Configuration
+        {loading && <Loader text="Loading..." inline />}
+      </h4>
+      {loaded && <div>{configuration[0].name}</div>}
+    </>
+  );
+};
 
 export default Configuration;
--- ui/src/app/settings/selectors/index.js
+++ ui/src/app/settings/selectors/index.js
@@ -1,7 +1,9 @@
+import configuration from "./configuration";
 import repositories from "./repositories";
 import users from "./users";
 
 export default {
+  configuration,
   repositories,
   users
 };
