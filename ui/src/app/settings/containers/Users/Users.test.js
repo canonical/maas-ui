4fd2823a8c51c33d34e9885bef5e0bedc34e00b3
Huw Wilkins <huwshimi@gmail.com>
2019-08-08 14:51:33 +1000

    Add a delete confirm to the user list.

--- ui/src/app/settings/containers/Users/Users.test.js
+++ ui/src/app/settings/containers/Users/Users.test.js
@@ -8,27 +8,46 @@ import Users from "./Users";
 const mockStore = configureStore();
 
 describe("Users", () => {
-  it("displays a loading component if loading", () => {
-    const user = {
-      email: "admin@example.com",
-      first_name: "",
-      global_permissions: ["machine_create"],
-      id: 1,
-      is_superuser: true,
-      last_name: "",
-      sshkeys_count: 0,
-      username: "admin"
-    };
-    const store = mockStore({
+  let defaultStore, users;
+
+  beforeEach(() => {
+    users = [
+      {
+        email: "admin@example.com",
+        first_name: "",
+        global_permissions: ["machine_create"],
+        id: 1,
+        is_superuser: true,
+        last_name: "",
+        sshkeys_count: 0,
+        username: "admin"
+      },
+      {
+        email: "user@example.com",
+        first_name: "",
+        global_permissions: ["machine_create"],
+        id: 2,
+        is_superuser: false,
+        last_name: "",
+        sshkeys_count: 0,
+        username: "user1"
+      }
+    ];
+    defaultStore = {
       auth: {
-        user
+        user: users[0]
       },
       users: {
-        loading: true,
-        items: []
+        loading: false,
+        loaded: true,
+        items: users
       }
-    });
+    };
+  });
 
+  it("displays a loading component if loading", () => {
+    defaultStore.users.loading = true;
+    const store = mockStore(defaultStore);
     const wrapper = mount(
       <Provider store={store}>
         <MemoryRouter
@@ -38,31 +57,12 @@ describe("Users", () => {
         </MemoryRouter>
       </Provider>
     );
-
     expect(wrapper.find("Loader").exists()).toBe(true);
   });
 
   it("hides the table if no users have loaded", () => {
-    const user = {
-      email: "admin@example.com",
-      first_name: "",
-      global_permissions: ["machine_create"],
-      id: 1,
-      is_superuser: true,
-      last_name: "",
-      sshkeys_count: 0,
-      username: "admin"
-    };
-    const store = mockStore({
-      auth: {
-        user
-      },
-      users: {
-        loading: false,
-        items: []
-      }
-    });
-
+    defaultStore.users.loaded = false;
+    const store = mockStore(defaultStore);
     const wrapper = mount(
       <Provider store={store}>
         <MemoryRouter
@@ -72,7 +72,44 @@ describe("Users", () => {
         </MemoryRouter>
       </Provider>
     );
-
     expect(wrapper.find("MainTable").exists()).toBe(false);
+  });
+
+  it("shows the table if there are users", () => {
+    const store = mockStore(defaultStore);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: "/settings/users", key: "testKey" }]}
+        >
+          <Users />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find("MainTable").exists()).toBe(true);
+  });
+
+  it("can show a delete confirmation", () => {
+    const store = mockStore(defaultStore);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: "/settings/users", key: "testKey" }]}
+        >
+          <Users />
+        </MemoryRouter>
+      </Provider>
+    );
+    let row = wrapper.find("MainTable").prop("rows")[1];
+    expect(row.expanded).toBe(false);
+    // Click on the delete button:
+    wrapper
+      .find("TableRow")
+      .at(2)
+      .find("Button")
+      .at(1)
+      .simulate("click");
+    row = wrapper.find("MainTable").prop("rows")[1];
+    expect(row.expanded).toBe(true);
   });
 });
