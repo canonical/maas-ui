173826cadc136703c5f93d75865a6090bbba7f22
Huw Wilkins <huwshimi@gmail.com>
2019-08-09 10:18:53 +1000

    Generate unique ids.

--- ui/src/app/base/components/Modal/Modal.js
+++ ui/src/app/base/components/Modal/Modal.js
@@ -1,31 +1,36 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useRef } from "react";
+import uuidv4 from "uuid/v4";
 
-export const Modal = ({ children, close, title }) => (
-  <div className="p-modal" onClick={close}>
-    <div
-      className="p-modal__dialog"
-      role="dialog"
-      aria-labelledby="modal-title"
-      aria-describedby="modal-description"
-      onClick={evt => evt.stopPropagation()}
-    >
-      <header className="p-modal__header">
-        <h2 className="p-modal__title" id="modal-title">
-          {title}
-        </h2>
-        <button
-          className="p-modal__close"
-          aria-label="Close active modal"
-          onClick={close}
-        >
-          Close
-        </button>
-      </header>
-      <div id="modal-description">{children}</div>
+export const Modal = ({ children, close, title }) => {
+  const descriptionId = useRef(uuidv4());
+  const titleId = useRef(uuidv4());
+  return (
+    <div className="p-modal" onClick={close}>
+      <div
+        className="p-modal__dialog"
+        role="dialog"
+        aria-labelledby={titleId.current}
+        aria-describedby={descriptionId.current}
+        onClick={evt => evt.stopPropagation()}
+      >
+        <header className="p-modal__header">
+          <h2 className="p-modal__title" id={titleId.current}>
+            {title}
+          </h2>
+          <button
+            className="p-modal__close"
+            aria-label="Close active modal"
+            onClick={close}
+          >
+            Close
+          </button>
+        </header>
+        <div id={descriptionId.current}>{children}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 Modal.propTypes = {
   children: PropTypes.node.isRequired,
