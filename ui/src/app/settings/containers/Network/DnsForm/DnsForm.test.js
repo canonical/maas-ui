eeeebe64ec13968f950bb344c4e5bd8a2c490538
Caleb Ellis <caleb.ellis@canonical.com>
2019-08-13 16:29:38 +0200

    Build DNS form

--- ui/src/app/settings/containers/Network/DnsForm/DnsForm.test.js
+++ ui/src/app/settings/containers/Network/DnsForm/DnsForm.test.js
@@ -14,7 +14,22 @@ describe("DnsForm", () => {
       config: {
         loading: false,
         loaded: true,
-        items: []
+        items: [
+          {
+            name: "dnssec_validation",
+            value: "auto",
+            choices: [
+              ["auto", "Automatic (use default root key)"],
+              ["yes", "Yes (manually configured root key)"],
+              [
+                "no",
+                "No (Disable DNSSEC; useful when upstream DNS is misconfigured)"
+              ]
+            ]
+          },
+          { name: "dns_trusted_acl", value: "" },
+          { name: "upstream_dns", value: "" }
+        ]
       }
     };
   });
@@ -31,5 +46,37 @@ describe("DnsForm", () => {
     );
 
     expect(wrapper.find("Loader").exists()).toBe(true);
+  });
+
+  it("dispatches an action to update config on save button click", done => {
+    const state = { ...initialState };
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <DnsForm />
+      </Provider>
+    );
+    wrapper.find("form").simulate("submit");
+
+    // since Formik handler is evaluated asynchronously we have to delay checking the assertion
+    window.setTimeout(() => {
+      expect(store.getActions()).toEqual([
+        {
+          type: "UPDATE_CONFIG",
+          payload: {
+            params: [
+              { name: "dnssec_validation", value: "auto" },
+              { name: "dns_trusted_acl", value: "" },
+              { name: "upstream_dns", value: "" }
+            ]
+          },
+          meta: {
+            method: "config.update",
+            type: 0
+          }
+        }
+      ]);
+      done();
+    }, 0);
   });
 });
