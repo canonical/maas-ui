89c6f8acd95757d494945e95dcc274c0927727d0
Huw Wilkins <huwshimi@gmail.com>
2019-08-23 13:16:43 +1000

    Add missing user details. Add toggle between username and real name.

--- ui/src/app/base/proptypes.js
+++ ui/src/app/base/proptypes.js
@@ -5,8 +5,11 @@ export const UserShape = PropTypes.shape({
   first_name: PropTypes.string,
   global_permissions: PropTypes.arrayOf(PropTypes.string),
   id: PropTypes.number.isRequired,
+  is_local: PropTypes.bool,
   is_superuser: PropTypes.bool.isRequired,
   last_name: PropTypes.string,
+  last_login: PropTypes.string,
+  machines_count: PropTypes.number,
   sshkeys_count: PropTypes.number,
   username: PropTypes.string.isRequired
 });
