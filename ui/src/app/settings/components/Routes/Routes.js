cf2dd5d63f92ade15a724769600f894840ceec49
Steve Rydz <steve.rydz@canonical.com>
2019-08-23 14:14:23 +0100

    Redirect settings base url to first form (#149)

--- ui/src/app/settings/components/Routes/Routes.js
+++ ui/src/app/settings/components/Routes/Routes.js
@@ -37,6 +37,7 @@ const Routes = () => (
       component={KernelParameters}
     />
     <Route exact path="/configuration/deploy" component={Deploy} />
+    <Redirect from="/" to="/configuration/general" />
     <Redirect from="/configuration" to="/configuration/general" />
     <Route exact path="/users" component={UsersList} />
     <Route exact path="/users/add" component={UserAdd} />
