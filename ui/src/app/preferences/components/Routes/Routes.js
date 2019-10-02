b799fb89eb4415c81a1528d5137780f177b016e8
Huw Wilkins <huwshimi@gmail.com>
2019-10-02 14:22:21 +1000

    Add the UI for dislaying a list of API keys.

--- ui/src/app/preferences/components/Routes/Routes.js
+++ ui/src/app/preferences/components/Routes/Routes.js
@@ -4,6 +4,7 @@ import { Route, Redirect, Switch } from "react-router-dom";
 import { useRouter } from "app/base/hooks";
 import AddSSHKey from "app/preferences/views/SSHKeys/AddSSHKey";
 import AddSSLKey from "app/preferences/views/SSLKeys/AddSSLKey";
+import APIKeyList from "app/preferences/views/APIKeys/APIKeyList";
 import Details from "app/preferences/views/Details";
 import SSHKeyList from "app/preferences/views/SSHKeys/SSHKeyList";
 import SSLKeyList from "app/preferences/views/SSLKeys/SSLKeyList";
@@ -14,6 +15,7 @@ const Routes = () => {
     <Switch>
       <Redirect exact from={`${match.path}/`} to={`${match.path}/details`} />
       <Route exact path={`${match.path}/details`} component={Details} />
+      <Route exact path={`${match.path}/api-keys`} component={APIKeyList} />
       <Route exact path={`${match.path}/ssh-keys`} component={SSHKeyList} />
       <Route exact path={`${match.path}/ssh-keys/add`} component={AddSSHKey} />
       <Route exact path={`${match.path}/ssl-keys`} component={SSLKeyList} />
---
+++ ui/src/app/preferences/views/APIKeys/APIKeyList/APIKeyList.test.js
@@ -0,0 +1,33 @@
+import { MemoryRouter } from "react-router-dom";
+import { mount } from "enzyme";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import React from "react";
+
+import APIKeyList from "./APIKeyList";
+
+const mockStore = configureStore();
+
+describe("APIKeyList", () => {
+  let state;
+
+  beforeEach(() => {
+    state = {};
+  });
+
+  it("can render the table", () => {
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[
+            { pathname: "/account/prefs/ssl-keys", key: "testKey" }
+          ]}
+        >
+          <APIKeyList />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find("MainTable").exists()).toBe(true);
+  });
+});
