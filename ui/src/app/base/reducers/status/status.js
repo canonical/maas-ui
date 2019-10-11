aac2e1ab5228d71f648664add9e0911b1e73d26d
Huw Wilkins <huwshimi@gmail.com>
2019-10-11 11:16:59 +1100

    Notify the user if the websocket disconnects.

--- ui/src/app/base/reducers/status/status.js
+++ ui/src/app/base/reducers/status/status.js
@@ -3,11 +3,13 @@ import produce from "immer";
 const status = produce(
   (draft, action) => {
     switch (action.type) {
+      case "WEBSOCKET_DISCONNECTED":
       case "WEBSOCKET_CONNECT":
         draft.connected = false;
         break;
       case "WEBSOCKET_CONNECTED":
         draft.connected = true;
+        draft.error = null;
         break;
       case "WEBSOCKET_ERROR":
         draft.error = action.error;
