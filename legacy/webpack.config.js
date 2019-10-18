c683654d4f7afd9630858138577a9505a77b6022
Kit Randel <kit.randel@canonical.com>
2019-10-18 13:23:38 +1300

    Remove react deps.

--- legacy/webpack.config.js
+++ legacy/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
         query: {
-          presets: ["@babel/preset-env", "@babel/preset-react"]
+          presets: ["@babel/preset-env"]
         }
       },
       {
