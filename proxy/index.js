1fec34aeb7061559e099fa91af17118537385b05
Huw Wilkins <huwshimi@gmail.com>
2019-10-18 11:50:33 +1100

    Set up proxy for the React app.

---
+++ proxy/index.js
@@ -0,0 +1,34 @@
+require("dotenv-flow").config();
+var express = require("express");
+var proxy = require("http-proxy-middleware");
+
+var app = express();
+
+app.use(
+  proxy(["/MAAS/api", "/MAAS/accounts"], {
+    target: process.env.MAAS_URL
+  })
+);
+
+app.use(
+  proxy("/MAAS/ws", {
+    target: process.env.MAAS_URL,
+    ws: true
+  })
+);
+
+app.use(
+  proxy(["/MAAS/r/", "/static/"], {
+    target: "http://localhost:8401/"
+  })
+);
+
+app.use(
+  proxy("/MAAS/", {
+    target: process.env.MAAS_URL
+  })
+);
+
+app.listen(8400);
+
+console.log("Serving on port 8400");
