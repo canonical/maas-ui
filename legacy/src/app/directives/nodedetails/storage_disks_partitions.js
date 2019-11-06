a1a62dd312ff40ef1e1dc806a7da0a1c3f188f4f
Steve Rydz <steverydz@gmail.com>
2019-11-06 14:37:52 +0000

    Fix broken link to settings in storage tab

--- legacy/src/app/directives/nodedetails/storage_disks_partitions.js
+++ legacy/src/app/directives/nodedetails/storage_disks_partitions.js
@@ -2,7 +2,11 @@ import disksPartitionsTmpl from "../../partials/nodedetails/storage/disks-partit
 
 const storageDisksPartitions = () => ({
   restrict: "E",
-  template: disksPartitionsTmpl
+  template: disksPartitionsTmpl,
+  link: scope => {
+    scope.BASENAME = process.env.BASENAME;
+    scope.REACT_BASENAME = process.env.REACT_BASENAME;
+  }
 });
 
 export default storageDisksPartitions;
