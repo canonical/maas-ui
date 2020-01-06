0af35335a92a4d1407aa8f0ba19ee3c469bc7533
Kit Randel <kit@nocturne.net.nz>
2020-01-06 13:27:28 +1300

    Mock Date for snapshots.

--- shared/src/components/Footer/Footer.test.js
+++ shared/src/components/Footer/Footer.test.js
@@ -4,6 +4,13 @@ import React from "react";
 import { Footer } from "./Footer";
 
 describe("Footer", () => {
+  beforeEach(() => {
+    const mockDate = new Date(2020, 1, 1, 0, 0, 0);
+    jest
+      .spyOn(global, 'Date')
+      .mockImplementation(() => mockDate)
+  });
+
   afterEach(() => {
     jest.resetModules();
   });
