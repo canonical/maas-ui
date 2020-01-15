51aa1ec3a540e18867ae69a072a5a1af81168555
Caleb Ellis <caleb.ellis@canonical.com>
2020-01-15 10:29:17 +0100

    Sort pod machine list by status by default.

--- legacy/src/app/directives/machines_table.js
+++ legacy/src/app/directives/machines_table.js
@@ -37,7 +37,8 @@ function maasMachinesTable(
       zones: "=",
       hideFailedTests: "<",
       metadata: "=",
-      truncate: "<"
+      displayLimit: "<",
+      defaultSort: "<"
     },
     template: machinesTableTmpl,
     link: function(scope) {
@@ -81,7 +82,7 @@ function maasMachinesTable(
     // Scope variables.
     $scope.table = {
       column: "fqdn",
-      predicate: "fqdn",
+      predicate: $scope.defaultSort || "fqdn",
       reverse: false,
       allViewableChecked: false,
       machines,
@@ -90,7 +91,7 @@ function maasMachinesTable(
       machineActions: GeneralManager.getData("machine_actions")
     };
 
-    $scope.DISPLAY_LIMIT = $scope.truncate ? 5 : 100;
+    $scope.DISPLAY_LIMIT = $scope.displayLimit || 5;
     $scope.displayLimits = {};
     const groupLabels = [
       "Failed",
