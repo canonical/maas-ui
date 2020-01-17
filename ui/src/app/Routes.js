86bb855319243cd58d94c3ddc084d43d41d084e8
Caleb Ellis <caleb.ellis@canonical.com>
2020-01-17 13:59:11 +0100

    Remove new machines route for maas 2.7 api release.

--- ui/src/app/Routes.js
+++ ui/src/app/Routes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-import Machines from "app/machines/views/Machines";
 import NotFound from "app/base/views/NotFound";
 import Preferences from "app/preferences/views/Preferences";
 import Settings from "app/settings/views/Settings";
@@ -17,7 +16,6 @@ const Routes = () => (
       }}
     />
     <Route path="/settings" component={Settings} />
-    <Route path="/machines" component={Machines} />
     <Route path="/account/prefs" component={Preferences} />
     <Route path="*" component={() => <NotFound includeSection />} />
   </Switch>
