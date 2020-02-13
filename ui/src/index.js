63f2cae84d6f88d3cb2487c95e3a18906951c314
Kit Randel <kit@nocturne.net.nz>
2020-02-13 22:10:50 +1300

    Simplify store configuration with redux-toolkit. (#790)

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,9 +1,8 @@
 import { Provider } from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
-import { applyMiddleware, createStore } from "redux";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { ConnectedRouter, routerMiddleware } from "connected-react-router";
-import { composeWithDevTools } from "redux-devtools-extension";
 import { createBrowserHistory } from "history";
 import createSagaMiddleware from "redux-saga";
 
@@ -13,14 +12,24 @@ import * as serviceWorker from "./serviceWorker";
 import App from "./app/App";
 import createRootReducer from "./root-reducer";
 
-const sagaMiddleware = createSagaMiddleware();
 export const history = createBrowserHistory({
   basename: `${process.env.REACT_APP_BASENAME}${process.env.REACT_APP_REACT_BASENAME}`
 });
-const composeEnhancers = composeWithDevTools({});
-const middleware = [sagaMiddleware, routerMiddleware(history)];
-const enhancers = composeEnhancers(applyMiddleware(...middleware));
-export const store = createStore(createRootReducer(history), enhancers);
+const reducer = createRootReducer(history);
+
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [
+  ...getDefaultMiddleware(),
+  sagaMiddleware,
+  routerMiddleware(history)
+];
+
+export const store = configureStore({
+  reducer,
+  middleware,
+  devTools: process.env.NODE_ENV !== "production"
+});
+
 sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
