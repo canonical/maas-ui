54a1a1dfdf412f344eb4d75a3d107bb669809ae3
Caleb Ellis <caleb.ellis@canonical.com>
2020-02-19 09:50:16 +0100

    Update http-proxy-middleware to use explicit require.

--- proxy/index.js
+++ proxy/index.js
@@ -1,19 +1,22 @@
 require("dotenv-flow").config();
 var express = require("express");
-var proxy = require("http-proxy-middleware");
+var { createProxyMiddleware } = require("http-proxy-middleware");
 
 var app = express();
 
 // Proxy API endpoints to the MAAS.
 app.use(
-  proxy([`${process.env.BASENAME}/api`, `${process.env.BASENAME}/accounts`], {
-    target: process.env.MAAS_URL
-  })
+  createProxyMiddleware(
+    [`${process.env.BASENAME}/api`, `${process.env.BASENAME}/accounts`],
+    {
+      target: process.env.MAAS_URL
+    }
+  )
 );
 
 // Proxy the WebSocket API endpoint to the MAAS.
 app.use(
-  proxy(`${process.env.BASENAME}/ws`, {
+  createProxyMiddleware(`${process.env.BASENAME}/ws`, {
     target: process.env.MAAS_URL,
     ws: true
   })
@@ -21,7 +24,7 @@ app.use(
 
 // Proxy the HMR endpoint to the React client.
 app.use(
-  proxy("/sockjs-node/", {
+  createProxyMiddleware("/sockjs-node/", {
     target: "http://localhost:8401/",
     ws: true
   })
@@ -29,7 +32,7 @@ app.use(
 
 // Proxy the HMR endpoint to the Angular client.
 app.use(
-  proxy("/sockjs-legacy/", {
+  createProxyMiddleware("/sockjs-legacy/", {
     target: "http://localhost:8402/",
     ws: true
   })
@@ -37,22 +40,35 @@ app.use(
 
 // Proxy URLs and assets to the React client.
 app.use(
-  proxy([`${process.env.BASENAME}${process.env.REACT_BASENAME}`, "/static/", "/maas-favicon-32px.png"], {
+  createProxyMiddleware(
+    [
+      `${process.env.BASENAME}${process.env.REACT_BASENAME}`,
+      "/static/",
+      "/maas-favicon-32px.png"
+    ],
+    {
+      target: "http://localhost:8401/"
+    }
+  )
+);
+
+// Proxy the HMR url to the React client.
+app.use(
+  createProxyMiddleware("/main.*.hot-update.js", {
     target: "http://localhost:8401/"
   })
 );
 
-// Proxy the HMR url to the React client.
-app.use(proxy("/main.*.hot-update.js", { target: "http://localhost:8401/" }));
-
 // Proxy the remaining URLs to the Angular client.
 app.use(
-  proxy(`${process.env.BASENAME}/`, {
+  createProxyMiddleware(`${process.env.BASENAME}/`, {
     target: "http://localhost:8402/"
   })
 );
 
-app.get(process.env.BASENAME, (req, res) => res.redirect(`${process.env.BASENAME}/`));
+app.get(process.env.BASENAME, (req, res) =>
+  res.redirect(`${process.env.BASENAME}/`)
+);
 app.get("/", (req, res) => res.redirect(`${process.env.BASENAME}/`));
 
 app.listen(8400);
