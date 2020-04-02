8555f2058167a8f9420bd0d6b74f8d2b91de44ef
Caleb Ellis <caleb.ellis@canonical.com>
2020-04-02 14:02:38 +1000

    Build set pool form in action menu. (#891)
    Co-authored-by: huwshimi <huwshimi@users.noreply.github.com>

---
+++ ui/src/app/machines/components/HeaderStrip/ActionFormWrapper/SetPoolForm/SetPoolFormFields/SetPoolFormFields.js
@@ -0,0 +1,68 @@
+import { Col, Row, Select } from "@canonical/react-components";
+import { useFormikContext } from "formik";
+import { useSelector } from "react-redux";
+import React from "react";
+
+import { resourcepool as resourcePoolSelectors } from "app/base/selectors";
+
+import FormikField from "app/base/components/FormikField";
+
+export const SetPoolFormFields = () => {
+  const resourcePools = useSelector(resourcePoolSelectors.all);
+  const { values } = useFormikContext();
+
+  const resourcePoolOptions = [
+    { label: "Select resource pool", value: "", disabled: true },
+    ...resourcePools.map((pool) => ({
+      key: `pool-${pool.id}`,
+      label: pool.name,
+      value: pool.name,
+    })),
+  ];
+
+  return (
+    <Row>
+      <Col size="6">
+        <ul className="p-inline-list u-equal-height u-no-margin--bottom">
+          <li className="p-inline-list__item">
+            <FormikField
+              data-test="select-pool"
+              label="Select pool"
+              name="poolSelection"
+              type="radio"
+              value="select"
+            />
+          </li>
+          <li className="p-inline-list__item">
+            {/* Disabled until we have a method for handling sequenced websocket requests.
+                https://github.com/canonical-web-and-design/maas-ui/issues/928 */}
+            <FormikField
+              data-test="create-pool"
+              disabled
+              label="Create pool"
+              name="poolSelection"
+              type="radio"
+              value="create"
+            />
+          </li>
+        </ul>
+        {values.poolSelection === "select" ? (
+          <FormikField
+            component={Select}
+            label="Pool"
+            name="name"
+            options={resourcePoolOptions}
+            required
+          />
+        ) : (
+          <>
+            <FormikField label="Name" name="name" required type="text" />
+            <FormikField label="Description" name="description" type="text" />
+          </>
+        )}
+      </Col>
+    </Row>
+  );
+};
+
+export default SetPoolFormFields;
