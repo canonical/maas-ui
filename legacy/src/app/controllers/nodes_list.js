89e3d836b149cdd036815bbbe98f6aeacdb71ab0
Caleb Ellis <caleb.ellis@canonical.com>
2020-04-02 14:16:42 +1000

    Fix legacy deploy form styling. (#929)

--- legacy/src/app/controllers/nodes_list.js
+++ legacy/src/app/controllers/nodes_list.js
@@ -44,6 +44,8 @@ function NodesListController(
   $rootScope.page = "machines";
 
   // Set initial values.
+  $scope.BASENAME = process.env.BASENAME;
+  $scope.REACT_BASENAME = process.env.REACT_BASENAME;
   $scope.machines = MachinesManager.getItems();
   $scope.zones = ZonesManager.getItems();
   $scope.pools = ResourcePoolsManager.getItems();
