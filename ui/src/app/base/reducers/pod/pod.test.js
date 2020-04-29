51e2b1266a1e385f68c52b19ddb5a9a47d7b464d
Caleb Ellis <caleb.ellis@canonical.com>
2020-04-29 16:49:53 +1000

    Add RSD form to Add hardware dropdown. (#1055)
    Co-authored-by: huwshimi <huwshimi@users.noreply.github.com>

---
+++ ui/src/app/base/reducers/pod/pod.test.js
@@ -0,0 +1,168 @@
+import pod from "./pod";
+
+describe("pod reducer", () => {
+  it("should return the initial state", () => {
+    expect(pod(undefined, {})).toEqual({
+      errors: {},
+      items: [],
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: false,
+    });
+  });
+
+  it("should correctly reduce FETCH_POD_START", () => {
+    expect(
+      pod(undefined, {
+        type: "FETCH_POD_START",
+      })
+    ).toEqual({
+      errors: {},
+      items: [],
+      loaded: false,
+      loading: true,
+      saved: false,
+      saving: false,
+    });
+  });
+
+  it("should correctly reduce FETCH_POD_SUCCESS", () => {
+    expect(
+      pod(
+        {
+          errors: {},
+          items: [],
+          loaded: false,
+          loading: true,
+          saved: false,
+          saving: false,
+        },
+        {
+          type: "FETCH_POD_SUCCESS",
+          payload: [
+            { id: 1, name: "pod1" },
+            { id: 2, name: "pod2" },
+          ],
+        }
+      )
+    ).toEqual({
+      errors: {},
+      loading: false,
+      loaded: true,
+      saved: false,
+      saving: false,
+
+      items: [
+        { id: 1, name: "pod1" },
+        { id: 2, name: "pod2" },
+      ],
+    });
+  });
+
+  it("should correctly reduce FETCH_POD_ERROR", () => {
+    expect(
+      pod(
+        {
+          errors: {},
+          items: [],
+          loaded: false,
+          loading: false,
+          saved: false,
+          saving: false,
+        },
+        {
+          error: "Could not fetch pods",
+          type: "FETCH_POD_ERROR",
+        }
+      )
+    ).toEqual({
+      errors: "Could not fetch pods",
+      items: [],
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: false,
+    });
+  });
+
+  it("should correctly reduce CREATE_POD_START", () => {
+    expect(
+      pod(
+        {
+          errors: {},
+          items: [],
+          loaded: false,
+          loading: false,
+          saved: true,
+          saving: false,
+        },
+        {
+          type: "CREATE_POD_START",
+        }
+      )
+    ).toEqual({
+      errors: {},
+      items: [],
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: true,
+    });
+  });
+
+  it("should correctly reduce CREATE_POD_ERROR", () => {
+    expect(
+      pod(
+        {
+          errors: {},
+          items: [],
+          loaded: false,
+          loading: false,
+          saved: false,
+          saving: true,
+        },
+        {
+          error: { name: "Pod name already exists" },
+          type: "CREATE_POD_ERROR",
+        }
+      )
+    ).toEqual({
+      errors: { name: "Pod name already exists" },
+      items: [],
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: false,
+    });
+  });
+
+  it("should correctly reduce CREATE_POD_NOTIFY", () => {
+    expect(
+      pod(
+        {
+          errors: {},
+          items: [{ id: 1, name: "pod1" }],
+          loaded: false,
+          loading: false,
+          saved: false,
+          saving: false,
+        },
+        {
+          payload: { id: 2, name: "pod2" },
+          type: "CREATE_POD_NOTIFY",
+        }
+      )
+    ).toEqual({
+      errors: {},
+      items: [
+        { id: 1, name: "pod1" },
+        { id: 2, name: "pod2" },
+      ],
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: false,
+    });
+  });
+});
