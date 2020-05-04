53c721acc0ae7bae5c1e219d8d8a4933ece2e43a
Huw Wilkins <huwshimi@gmail.com>
2020-05-04 11:13:06 +1000

    Add an unused var.

--- ui/src/app/machines/views/MachineList/MachineList.js
+++ ui/src/app/machines/views/MachineList/MachineList.js
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useStorageState } from "react-storage-hooks";
 import PropTypes from "prop-types";
 import React from "react";
-import classNames from "classnames";
+// import classNames from "classnames";
 
 import { formatErrors } from "app/utils";
 import { machine as machineActions } from "app/base/actions";
@@ -14,6 +14,7 @@ import MachineListTable from "./MachineListTable";
 
 const MachineList = ({ searchFilter, setSearchFilter }) => {
   useWindowTitle("Machines");
+  let unusedVar;
   const dispatch = useDispatch();
   const errors = useSelector(machineSelectors.errors);
   const errorMessage = formatErrors(errors);
