001b29d177961461330b20010170c1c980fdfa00
Kit Randel <kit.randel@canonical.com>
2020-05-15 10:57:59 +1200

    Add root error boundary.

--- ui/src/single-spa-entry.js
+++ ui/src/single-spa-entry.js
@@ -8,7 +8,25 @@ const reactLifecycles = singleSpaReact({
   React,
   ReactDOM,
   rootComponent,
+  errorBoundary(err, info, props) {
+    return (
+      <div>
+        <p>Unable to load MAAS.</p>
+        <p>
+          Please refresh your browser, and if the issue persists submit an issue
+          at:{" "}
+          <span>
+            https://github.com/canonical-web-and-design/maas-ui/issues
+          </span>{" "}
+          with the following details:
+        </p>
+        <p>{err}</p>
+        <p>{info}</p>
+      </div>
+    );
+  },
 });
+
 export const { bootstrap } = reactLifecycles;
 export const { mount } = reactLifecycles;
 export const { unmount } = reactLifecycles;
