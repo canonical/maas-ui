7c01d74540410e7524527c98be3c59116a18661c
Kit Randel <kit.randel@canonical.com>
2020-05-18 11:54:14 +1200

    Remove unused functions.

--- root/src/root-application.js
+++ root/src/root-application.js
@@ -1,24 +1,4 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable func-names */
 import { registerApplication, start } from "single-spa";
-
-function showWhenAnyOf(routes) {
-  return function (location) {
-    return routes.some((route) => location.pathname === route);
-  };
-}
-
-function showWhenPrefix(routes) {
-  return function (location) {
-    return routes.some((route) => location.pathname.startsWith(route));
-  };
-}
-
-function showExcept(routes) {
-  return function (location) {
-    return routes.every((route) => location.pathname !== route);
-  };
-}
 
 registerApplication({
   name: "legacy",
