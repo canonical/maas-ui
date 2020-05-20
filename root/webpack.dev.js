d090b0418158dd5dd1828e7a1c321ceb083f4675
Kit Randel <kit.randel@canonical.com>
2020-05-20 20:00:36 +1200

    Make root app relative.

---
+++ root/webpack.dev.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const common = require("./webpack.common.js");
+
+const publicPath = "/";
+
+module.exports = merge(common, {
+  mode: "development",
+  output: {
+    publicPath
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.ejs"),
+      inject: false,
+      templateParameters: {
+        publicPath
+      },
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    disableHostCheck: true,
+    historyApiFallback: true,
+    open: true,
+    public: "0.0.0.0:8400",
+  },
+});
