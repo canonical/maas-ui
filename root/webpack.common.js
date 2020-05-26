d5508a5aa32e2136ffeb5b3f37ab003d78504c9c
Huw Wilkins <huw.wilkins@canonical.com>
2020-05-26 14:15:04 +1000

    Display a favicon.

--- root/webpack.common.js
+++ root/webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const DotenvFlow = require("dotenv-flow-webpack");
 
 module.exports = {
@@ -27,6 +28,9 @@ module.exports = {
     new CleanWebpackPlugin({
       cleanAfterEveryBuildPatterns: ["dist"],
     }),
+    new CopyWebpackPlugin([
+      { from: path.resolve(__dirname, "src/*.png"), to: "[name].[ext]" },
+    ]),
     new DotenvFlow(),
   ],
   externals: [],
