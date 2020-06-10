17f1ab5831debd67cf5d3cee42f7231cd02d8bc9
Caleb Ellis <caleb.ellis@canonical.com>
2020-06-10 19:51:45 +1000

    Fix form card column sizes. (#1232)

--- ui/src/app/base/constants.js
+++ ui/src/app/base/constants.js
@@ -1,10 +1,20 @@
 /**
  * Message types defined by MAAS websocket API
  */
-const MESSAGE_TYPES = {
+export const MESSAGE_TYPES = {
   REQUEST: 0,
   RESPONSE: 1,
   NOTIFY: 2,
+};
+
+/**
+ * Common column sizes used in <Col> components
+ */
+export const COL_SIZES = {
+  CARD_TITLE: 2,
+  DELETE_ROW_BUTTONS: 4,
+  SIDEBAR: 3,
+  TOTAL: 12,
 };
 
 export default MESSAGE_TYPES;
