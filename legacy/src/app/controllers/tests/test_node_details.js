fc5cb83863234deeed692900b6b3d0681d780c9c
Caleb Ellis <caleb.ellis@canonical.com>
2020-06-11 10:38:23 +1000

    Add tooltip for interface DHCP relay info + improve table styling. (#1207)
    Co-authored-by: Kit Randel <kit.randel@canonical.com>

--- legacy/src/app/controllers/tests/test_node_details.js
+++ legacy/src/app/controllers/tests/test_node_details.js
@@ -2863,6 +2863,24 @@ describe("NodeDetailsController", function () {
       $scope.vlans = [];
       expect($scope.getDHCPStatus(iface)).toEqual("No DHCP");
     });
+
+    it("returns correct text if DHCP is relayed", () => {
+      makeController();
+      $scope.vlans = [{ id: 1, relay_vlan: 2 }]
+      const iface = { vlan_id: 1 };
+      expect($scope.getDHCPStatus(iface)).toEqual("Relayed");
+    });
+
+    it("returns correct text if DHCP is relayed and full name required", () => {
+      makeController();
+      const vlans = [{ id: 2, vid: "vlan-1", relay_vlan: 3}, { fabric: 1, id: 3, vid: "vlan-2"}];
+      const fabrics = [{ id: 1, name: "fabric-1" }];
+      const iface = { vlan_id: 2 };
+      $scope.vlans = vlans;
+      FabricsManager._items = fabrics;
+      VLANsManager._items = vlans;
+      expect($scope.getDHCPStatus(iface, true)).toEqual("Relayed via fabric-1.vlan-2");
+    });
   });
 
   describe("getFabricName", () => {
