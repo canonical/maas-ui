cec75a15a589067f89c3df063e73cdcd4c12bce2
Kit Randel <kit.randel@canonical.com>
2020-06-22 14:40:05 +1200

    Add betterer test to count progress towards migrating to ts. (#1269)

--- ui/.betterer.ts
+++ ui/.betterer.ts
@@ -1,9 +1,24 @@
 import { typescriptBetterer } from "@betterer/typescript";
-import { regexpBetterer } from '@betterer/regexp';
+import { regexpBetterer } from "@betterer/regexp";
+import { smaller } from "@betterer/constraints";
+
+const { execSync } = require("child_process");
+
+const jsFileCount = parseInt(execSync(
+  'find . -type f -name "*.js" -not -path "./node_modules/*" | wc -l',
+  { encoding: "utf8" }
+)
+  .replace("\n", "")
+  .trim());
 
 export default {
   "stricter compilation": typescriptBetterer("./tsconfig.json", {
     strict: true,
   }),
   "no TSFixMe types": regexpBetterer("**/*.ts", /(\s*TSFixMe)/i),
+  "migrate js files to ts": {
+    test: () => jsFileCount,
+    constraint: smaller,
+    goal: 0,
+  },
 };
