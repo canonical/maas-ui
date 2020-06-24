0249cbca3d77a77699e29957fca88c45c8bbf6a1
Kit Randel <kit.randel@canonical.com>
2020-06-24 19:01:16 +1200

    Clear cookie after test and move add machine test to separate spec.

--- proxy/cypress/integration/machines/list.spec.js
+++ proxy/cypress/integration/machines/list.spec.js
@@ -1,31 +1,17 @@
-import { customAlphabet } from 'nanoid'
-
-import { generateMac, login } from "../utils";
-
-const nanoid = customAlphabet('1234567890abcdefghi', 10);
 context("Machine listing", () => {
   beforeEach(() => {
-    Cypress.config('defaultCommandTimeout', 10000);
     login();
-    cy.setCookie('skipintro', 'true');
+    cy.setCookie("skipintro", "true");
     cy.visit(
       `${Cypress.env("BASENAME")}${Cypress.env("REACT_BASENAME")}/machines`
     );
   });
 
-  it("renders a heading", () => {
-    cy.get("li.p-heading--four").contains("Machines");
+  afterEach(() => {
+    cy.clearCookie("skipintro");
   });
 
-  it("can add a machine", () => {
-    const hostname = `cypress-${nanoid()}`;
-    cy.visit(
-      `${Cypress.env("BASENAME")}${Cypress.env("REACT_BASENAME")}/machines/add`
-    );
-    cy.get("input[name='hostname']").type(hostname);
-    cy.get("input[name='pxe_mac']").type(generateMac());
-    cy.get("select[name='power_type']").select("Manual").blur();
-    cy.get("button[type='submit']").click();
-    cy.get('p.p-notification__response').contains(`${hostname} added successfully.`);
+  it("renders a heading", () => {
+    cy.get("li.p-heading--four").contains("Machines");
   });
 });
