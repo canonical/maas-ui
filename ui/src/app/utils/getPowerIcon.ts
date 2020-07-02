0b0a4f8cc2be5a0992d8c9e59e4ec64760d0f8c0
Kit Randel <kit.randel@canonical.com>
2020-07-03 11:42:51 +1200

    Remove property check (type guard does this).

--- ui/src/app/utils/getPowerIcon.ts
+++ ui/src/app/utils/getPowerIcon.ts
@@ -10,7 +10,7 @@ export const getPowerIcon = (host?: Host, loading?: boolean): string => {
   if (loading && !host) {
     return "p-icon--spinner u-animation--spin";
   }
-  if (host && isMachine(host) && host.power_state) {
+  if (host && isMachine(host)) {
     return `p-icon--power-${host.power_state}`;
   }
   return "p-icon--power-unknown";
