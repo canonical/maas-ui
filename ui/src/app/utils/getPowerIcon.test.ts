00699278345eb1740b0708f761ed01e58ad0a7e7
Kit Randel <kit.randel@canonical.com>
2020-07-03 14:18:36 +1200

    Formatting

--- ui/src/app/utils/getPowerIcon.test.ts
+++ ui/src/app/utils/getPowerIcon.test.ts
@@ -11,21 +11,25 @@ const controller = controllerFactory();
 describe("getPowerIcon", () => {
   it("returns an 'on' icon for a machine with an on power state", () => {
     machine.power_state = "on";
+
     expect(getPowerIcon(machine)).toEqual("p-icon--power-on");
   });
 
   it("returns an 'off' icon for a machine with an off power state", () => {
     machine.power_state = "off";
+
     expect(getPowerIcon(machine)).toEqual("p-icon--power-off");
   });
 
   it("returns an 'error' icon for a machine with an error power state", () => {
     machine.power_state = "error";
+
     expect(getPowerIcon(machine)).toEqual("p-icon--power-error");
   });
 
   it("returns an 'unknown' icon for a machine with an unknown power state", () => {
     machine.power_state = "unknown";
+
     expect(getPowerIcon(machine)).toEqual("p-icon--power-unknown");
     expect(getPowerIcon()).toEqual("p-icon--power-unknown");
   });
