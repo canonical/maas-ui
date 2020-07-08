9ccd877fe54f5f79c9ae8cdeb9d6b531883c954d
Kit Randel <kit.randel@canonical.com>
2020-07-09 11:21:12 +1200

    Add PodState factory.

--- ui/src/testing/factories/state.ts
+++ ui/src/testing/factories/state.ts
@@ -3,27 +3,38 @@ import { array, define } from "cooky-cutter";
 import { message } from "./message";
 import { notification } from "./notification";
 import { user } from "./user";
+import { pod } from "./nodes";
 import type { AuthState, UserState } from "app/store/user/types";
 import type { MessageState } from "app/store/message/types";
 import type { NotificationState } from "app/store/notification/types";
+import type { PodState } from "app/store/pod/types";
 
-export const authState = define<AuthState>({
-  errors: null,
+const defaultState = {
+  errors: () => ({}),
   loaded: true,
   loading: false,
   saved: true,
   saving: false,
+};
+
+export const authState = define<AuthState>({
+  ...defaultState,
   user,
 });
 
 export const userState = define<UserState>({
+  ...defaultState,
   auth: authState,
-  errors: null,
   items: array(user),
-  loaded: true,
-  loading: false,
-  saved: true,
-  saving: false,
+});
+
+export const podState = define<PodState>({
+  ...defaultState,
+  items: array(pod),
+  selected: () => [],
+  statuses: (id: number) => ({
+    [id]: { deleting: false, refreshing: false },
+  }),
 });
 
 export const notificationState = define<NotificationState>({
