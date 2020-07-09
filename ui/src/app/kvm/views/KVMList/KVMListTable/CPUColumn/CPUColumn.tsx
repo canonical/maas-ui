b7c4844a2e460a4c03ddd0ef42cc5345f5fb000e
Caleb Ellis <caleb.ellis@canonical.com>
2020-07-09 14:34:45 +1000

    Build CPUPopover component. (#1349)

--- ui/src/app/kvm/views/KVMList/KVMListTable/CPUColumn/CPUColumn.tsx
+++ ui/src/app/kvm/views/KVMList/KVMListTable/CPUColumn/CPUColumn.tsx
@@ -3,30 +3,40 @@ import { useSelector } from "react-redux";
 
 import { pod as podSelectors } from "app/base/selectors";
 import type { RootState } from "app/store/root/types";
+import CPUPopover from "./CPUPopover";
 import Meter from "app/base/components/Meter";
 
 type Props = { id: number };
 
-const CPUColumn = ({ id }: Props): JSX.Element => {
+const CPUColumn = ({ id }: Props): JSX.Element | null => {
   const pod = useSelector((state: RootState) =>
     podSelectors.getById(state, id)
   );
 
-  return (
-    <Meter
-      className="u-no-margin--bottom"
-      data={[
-        {
-          key: `${pod.name}-cpu-meter`,
-          label: `${pod.total.cores}`,
-          value: pod.used.cores,
-        },
-      ]}
-      labelsClassName="u-align--right"
-      max={pod.total.cores * pod.cpu_over_commit_ratio}
-      small
-    />
-  );
+  if (pod) {
+    return (
+      <CPUPopover
+        assigned={pod.used.cores}
+        physical={pod.total.cores}
+        overcommit={pod.cpu_over_commit_ratio}
+      >
+        <Meter
+          className="u-no-margin--bottom"
+          data={[
+            {
+              key: `${pod.name}-cpu-meter`,
+              label: `${pod.total.cores}`,
+              value: pod.used.cores,
+            },
+          ]}
+          labelsClassName="u-align--right"
+          max={pod.total.cores * pod.cpu_over_commit_ratio}
+          small
+        />
+      </CPUPopover>
+    );
+  }
+  return null;
 };
 
 export default CPUColumn;
