a4d797502e0d64522785715b1df18acfffe3deb8
Huw Wilkins <huw.wilkins@canonical.com>
2020-07-10 13:55:27 +1000

    Use Host type.

--- ui/src/app/store/dhcpsnippet/types.ts
+++ ui/src/app/store/dhcpsnippet/types.ts
@@ -1,6 +1,5 @@
-import type { Controller } from "app/store/controller/types";
 import type { Device } from "app/store/device/types";
-import type { Machine } from "app/store/machine/types";
+import type { Host } from "app/store/types/host";
 import type { Model } from "app/store/types/model";
 import type { TSFixMe } from "app/base/types";
 
@@ -15,7 +14,7 @@ export type DHCPSnippet = Model & {
   enabled: boolean;
   history: DHCPSnippetHistory[];
   name: string;
-  node: Controller | Machine | Device | null;
+  node: Host | Device | null;
   subnet: TSFixMe | null; // Replace with Subnet["id"] when the Subnet type has been defined.
   updated: string;
   value: string;
