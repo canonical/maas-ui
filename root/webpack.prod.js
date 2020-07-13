75009821ec3e19b1f4979f2d97d310b05e5b85e0
Caleb Ellis <caleb.ellis@canonical.com>
2020-07-13 16:03:49 +1000

    Fix webpack-merge import.

--- root/webpack.prod.js
+++ root/webpack.prod.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 
 const common = require("./webpack.common.js");
 
