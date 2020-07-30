3dd3abe1ac6dd13448585bfd37a0556ad91b716f
Huw Wilkins <huw.wilkins@canonical.com>
2020-07-31 09:35:22 +1000

    Add tests for hiding/showing the feedback link.

--- shared/src/components/Footer/Footer.test.js
+++ shared/src/components/Footer/Footer.test.js
@@ -17,4 +17,58 @@ describe("Footer", () => {
     const wrapper = shallow(<Footer maasName="koala-maas" version="2.7.0" />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it("displays the feedback link when analytics enabled", () => {
+    const wrapper = shallow(
+      <Footer
+        maasName="koala-maas"
+        version="2.7.0"
+        debug={false}
+        enableAnalytics={true}
+      />
+    );
+    expect(
+      wrapper
+        .findWhere(
+          (node) => node.type() === "a" && node.text() === "Give feedback"
+        )
+        .exists()
+    ).toBe(true);
+  });
+
+  it("hides the feedback link when analytics disabled", () => {
+    const wrapper = shallow(
+      <Footer
+        maasName="koala-maas"
+        version="2.7.0"
+        debug={false}
+        enableAnalytics={false}
+      />
+    );
+    expect(
+      wrapper
+        .findWhere(
+          (node) => node.type() === "a" && node.text() === "Give feedback"
+        )
+        .exists()
+    ).toBe(false);
+  });
+
+  it("hides the feedback link in debug mode", () => {
+    const wrapper = shallow(
+      <Footer
+        maasName="koala-maas"
+        version="2.7.0"
+        debug={true}
+        enableAnalytics={true}
+      />
+    );
+    expect(
+      wrapper
+        .findWhere(
+          (node) => node.type() === "a" && node.text() === "Give feedback"
+        )
+        .exists()
+    ).toBe(false);
+  });
 });
