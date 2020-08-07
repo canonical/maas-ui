257cf08759546e758b44bf3f66fb1863db2a6208
Huw Wilkins <huw.wilkins@canonical.com>
2020-08-07 10:38:05 +1000

    Update cypress env vars. Build shared before serving.

--- proxy/cypress/integration/utils.ts
+++ proxy/cypress/integration/utils.ts
@@ -1,11 +1,13 @@
 import { customAlphabet } from "nanoid";
 
+import { BASENAME, REACT_BASENAME } from "@maas-ui/maas-ui-shared";
+
 const nanoid = customAlphabet("1234567890abcdefghi", 10);
 
 export const login = () => {
   cy.request({
     method: "POST",
-    url: `${Cypress.env("BASENAME")}/accounts/login/`,
+    url: `${BASENAME}/accounts/login/`,
     form: true,
     body: {
       username: "admin",
@@ -22,8 +24,7 @@ export const generateMac = () =>
 export const generateEmail = () => `${nanoid()}@example.com`;
 
 export const makeUIURL = (path: string) => {
-  return [
-    `${Cypress.env("BASENAME")}${Cypress.env("REACT_BASENAME")}`,
-    path,
-  ].join(path.startsWith("/") ? "" : "/");
+  return [`${BASENAME}${REACT_BASENAME}`, path].join(
+    path.startsWith("/") ? "" : "/"
+  );
 };
