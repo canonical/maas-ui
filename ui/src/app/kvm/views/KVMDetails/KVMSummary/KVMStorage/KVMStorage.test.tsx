6637eff3f0f86806c7c039405eb6fa1ae0df41a2
Caleb Ellis <caleb.ellis@canonical.com>
2020-08-19 20:43:56 +1000

    feat(ui): Build redesigned KVM storage cards.

--- ui/src/app/kvm/views/KVMDetails/KVMSummary/KVMStorage/KVMStorage.test.tsx
+++ ui/src/app/kvm/views/KVMDetails/KVMSummary/KVMStorage/KVMStorage.test.tsx
@@ -15,21 +15,35 @@ import KVMStorage from "./KVMStorage";
 const mockStore = configureStore();
 
 describe("KVMStorage", () => {
-  it("correctly chunks KVM storage pools into rows of 3", () => {
+  it("sorts pools by id, with default first", () => {
+    const [defaultPool, pool1, pool2] = [
+      podStoragePoolFactory({ id: "a" }),
+      podStoragePoolFactory({ id: "b" }),
+      podStoragePoolFactory({ id: "c" }),
+    ];
     const pod = podFactory({
-      storage_pools: Array.from(Array(4)).map(() => podStoragePoolFactory()),
+      default_storage_pool: defaultPool.id,
+      id: 1,
+      storage_pools: [pool2, defaultPool, pool1],
     });
     const state = rootStateFactory({ pod: podStateFactory({ items: [pod] }) });
     const store = mockStore(state);
     const wrapper = mount(
       <Provider store={store}>
-        <MemoryRouter initialEntries={[{ pathname: "/kvm/2", key: "testKey" }]}>
+        <MemoryRouter initialEntries={[{ pathname: "/kvm/1", key: "testKey" }]}>
           <KVMStorage id={pod.id} />
         </MemoryRouter>
       </Provider>
     );
-    expect(wrapper.find("Row")).toHaveLength(2);
-    expect(wrapper.find("Row").at(0).props().children).toHaveLength(3);
-    expect(wrapper.find("Row").at(1).props().children).toHaveLength(1);
+
+    expect(wrapper.find("[data-test='pool-name']").at(0).text()).toBe(
+      defaultPool.name
+    );
+    expect(wrapper.find("[data-test='pool-name']").at(1).text()).toBe(
+      pool1.name
+    );
+    expect(wrapper.find("[data-test='pool-name']").at(2).text()).toBe(
+      pool2.name
+    );
   });
 });
