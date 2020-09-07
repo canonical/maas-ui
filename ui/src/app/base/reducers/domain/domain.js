8fe70b0b6c640e274196660cb89ab21e3124a418
Kit Randel <kit.randel@canonical.com>
2020-09-07 15:27:29 +1200

    refactor(ui): add domain slice (#1602)
    Fixes canonical-web-and-design/maas-ui#141

--- ui/src/app/base/reducers/domain/domain.js
+++
@@ -1,7 +0,0 @@
-import { createStandardReducer } from "app/utils/redux";
-
-import { domain as domainActions } from "app/base/actions";
-
-const domain = createStandardReducer(domainActions);
-
-export default domain;
---
+++ ui/src/app/store/domain/reducer.test.ts
@@ -0,0 +1,187 @@
+import {
+  domain as domainFactory,
+  domainState as domainStateFactory,
+} from "testing/factories";
+import reducers, { actions } from "./slice";
+
+describe("domain reducer", () => {
+  it("should return the initial state", () => {
+    expect(reducers(undefined, { type: "" })).toEqual({
+      errors: null,
+      items: [],
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: false,
+    });
+  });
+
+  it("reduces fetch", () => {
+    expect(reducers(undefined, actions.fetch())).toEqual({
+      errors: null,
+      items: [],
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: false,
+    });
+  });
+
+  it("reduces fetchStart", () => {
+    expect(reducers(undefined, actions.fetchStart())).toEqual({
+      errors: null,
+      items: [],
+      loaded: false,
+      loading: true,
+      saved: false,
+      saving: false,
+    });
+  });
+
+  it("reduces fetchSuccess", () => {
+    const domains = [domainFactory()];
+    const domainState = domainStateFactory({
+      items: [],
+      loading: true,
+    });
+    expect(reducers(domainState, actions.fetchSuccess(domains))).toEqual({
+      errors: {},
+      loading: false,
+      loaded: true,
+      saved: false,
+      saving: false,
+      items: domains,
+    });
+  });
+
+  it("reduces fetchError", () => {
+    const domainState = domainStateFactory();
+
+    expect(
+      reducers(domainState, actions.fetchError("Could not fetch domains"))
+    ).toEqual({
+      errors: "Could not fetch domains",
+      items: [],
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: false,
+    });
+  });
+
+  it("reduces createStart", () => {
+    const domainState = domainStateFactory({ saved: true });
+
+    expect(reducers(domainState, actions.createStart())).toEqual({
+      errors: {},
+      items: [],
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: true,
+    });
+  });
+
+  it("reduces createError", () => {
+    const domainState = domainStateFactory();
+
+    expect(
+      reducers(
+        domainState,
+        actions.createError({ name: "Domain name already exists" })
+      )
+    ).toEqual({
+      errors: { name: "Domain name already exists" },
+      items: [],
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: false,
+    });
+  });
+
+  it("updates domains on createNotify", () => {
+    const domains = [domainFactory({ id: 1 })];
+    const newDomain = domainFactory({ id: 2 });
+    const domainState = domainStateFactory({
+      items: domains,
+    });
+
+    expect(reducers(domainState, actions.createNotify(newDomain))).toEqual({
+      errors: {},
+      items: [...domains, newDomain],
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: false,
+    });
+  });
+
+  it("reduces deleteStart", () => {
+    const domains = [domainFactory({ id: 1 })];
+    const domainState = domainStateFactory({
+      items: domains,
+    });
+
+    expect(
+      reducers(domainState, actions.deleteStart({ item: domains[0] }))
+    ).toEqual({
+      errors: {},
+      items: domains,
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: true,
+    });
+  });
+
+  it("reduces deleteSuccess", () => {
+    const domains = [domainFactory({ id: 1 })];
+    const domainState = domainStateFactory({
+      items: domains,
+    });
+    expect(
+      reducers(domainState, actions.deleteSuccess({ item: domains[0] }))
+    ).toEqual({
+      errors: null,
+      items: domains,
+      loaded: false,
+      loading: false,
+      saved: true,
+      saving: false,
+    });
+  });
+
+  it("reduces deleteError", () => {
+    const domains = [domainFactory({ id: 1 })];
+    const domainState = domainStateFactory({
+      items: domains,
+    });
+    expect(
+      reducers(domainState, actions.deleteError("Domain cannot be deleted"))
+    ).toEqual({
+      errors: "Domain cannot be deleted",
+      items: domains,
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: false,
+    });
+  });
+
+  it("reduces deleteNotify", () => {
+    const domains = [domainFactory({ id: 1 }), domainFactory({ id: 2 })];
+    const domainState = domainStateFactory({
+      items: domains,
+    });
+
+    expect(reducers(domainState, actions.deleteNotify(1))).toEqual({
+      errors: {},
+      items: [domains[1]],
+      loaded: false,
+      loading: false,
+      saved: false,
+      saving: false,
+    });
+  });
+});
