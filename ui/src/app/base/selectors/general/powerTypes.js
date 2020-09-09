567d06bde2d56af3f63b0ec2af0f9749c2a2fa7b
Caleb Ellis <caleb.ellis@canonical.com>
2020-09-09 16:46:56 +1000

    2.8: correctly filter list of power types for use in Add Chassis form

--- ui/src/app/base/selectors/general/powerTypes.js
+++ ui/src/app/base/selectors/general/powerTypes.js
@@ -6,6 +6,19 @@ import { createSelector } from "@reduxjs/toolkit";
 
 import { generateGeneralSelector } from "./utils";
 
+// List of power types that can be used with the add_chassis http method.
+// MAAS 2.9 handles this via the api but for 2.8 it has to be hardcoded.
+const ADD_CHASSIS_POWER_TYPES = [
+  "mscm",
+  "msftocs",
+  "powerkvm",
+  "recs_box",
+  "seamicro15k",
+  "ucsm",
+  "virsh",
+  "vmware",
+];
+
 const powerTypes = generateGeneralSelector("powerTypes");
 
 /**
@@ -13,8 +26,8 @@ const powerTypes = generateGeneralSelector("powerTypes");
  * @param {Object} state - The redux state.
  * @returns {Array} Chassis power types.
  */
-powerTypes.chassis = createSelector([powerTypes.get], (powerTypes) =>
-  powerTypes.filter((type) => type.chassis)
+powerTypes.canProbe = createSelector([powerTypes.get], (powerTypes) =>
+  powerTypes.filter((type) => ADD_CHASSIS_POWER_TYPES.includes(type.name))
 );
 
 export default powerTypes;
