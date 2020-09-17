45491b7a4e485401d61aba385ae8fe86073bc212
Caleb Ellis <caleb.ellis@canonical.com>
2020-09-18 09:08:07 +1000

    feat(ui): add hugepage + interface data to KVM aggregate view if KVM is NUMA-aware (#1624)
    If a KVM has numa_pinning data, it will be summed and added to the KVM aggregate view. If not, only
    the data on the base KVM object will be used.
    
    Co-authored-by: huwshimi <huwshimi@users.noreply.github.com>

--- ui/src/app/kvm/views/KVMDetails/KVMSummary/KVMAggregateResources/KVMAggregateResources.test.tsx
+++ ui/src/app/kvm/views/KVMDetails/KVMSummary/KVMAggregateResources/KVMAggregateResources.test.tsx
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import {
   pod as podFactory,
   podHint as podHintFactory,
+  podNumaNode as podNumaNodeFactory,
   podState as podStateFactory,
   rootState as rootStateFactory,
 } from "testing/factories";
@@ -64,5 +65,116 @@ describe("KVMAggregateResources", () => {
       "1MiB"
     );
     expect(wrapper.find("[data-test='ram-general-free']").text()).toBe("3MiB");
+  });
+
+  it(`correctly displays RAM information for NUMA-aware pods`, () => {
+    const oneMiB = Math.pow(1024, 2);
+    const pod = podFactory({
+      memory_over_commit_ratio: 2,
+      id: 1,
+      numa_pinning: [
+        podNumaNodeFactory({
+          memory: {
+            general: { allocated: 1, free: 2 },
+            hugepages: [
+              {
+                allocated: oneMiB,
+                free: oneMiB,
+                page_size: 1024,
+              },
+              {
+                allocated: oneMiB * 2,
+                free: oneMiB,
+                page_size: 1024,
+              },
+            ],
+          },
+        }),
+        podNumaNodeFactory({
+          memory: {
+            general: { allocated: 2, free: 3 },
+            hugepages: [
+              {
+                allocated: oneMiB * 4,
+                free: oneMiB * 3,
+                page_size: 1024,
+              },
+            ],
+          },
+        }),
+      ],
+      total: podHintFactory({ memory: 2 }),
+      used: podHintFactory({ memory: 1 }),
+    });
+    const state = rootStateFactory({ pod: podStateFactory({ items: [pod] }) });
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[{ pathname: "/kvm/1", key: "testKey" }]}>
+          <KVMAggregateResources id={pod.id} />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(wrapper.find("[data-test='hugepage-size']").text()).toBe(
+      "(Size: 1KiB)"
+    );
+    // Hugepage allocated = [1 + 2] + 4 = 7MiB
+    expect(wrapper.find("[data-test='hugepage-allocated']").text()).toBe(
+      "7MiB"
+    );
+    // Hugepage free = [1 + 1] + 3 = 5MiB
+    expect(wrapper.find("[data-test='hugepage-free']").text()).toBe("5MiB");
+    // Total memory is sum of general and all hugepages
+    expect(wrapper.find(".doughnut-chart__label").text()).toBe("16MiB");
+  });
+
+  it(`correctly displays interface information for NUMA-aware pods`, () => {
+    const pod = podFactory({
+      memory_over_commit_ratio: 2,
+      id: 1,
+      numa_pinning: [
+        podNumaNodeFactory({
+          interfaces: [
+            { id: 1, name: "eth0" },
+            {
+              id: 2,
+              name: "eth3",
+              virtual_functions: { allocated: 10, free: 5 },
+            },
+          ],
+        }),
+        podNumaNodeFactory({
+          interfaces: [
+            {
+              id: 3,
+              name: "eth2",
+              virtual_functions: { allocated: 15, free: 10 },
+            },
+          ],
+        }),
+      ],
+      total: podHintFactory({ memory: 2 }),
+      used: podHintFactory({ memory: 1 }),
+    });
+    const state = rootStateFactory({ pod: podStateFactory({ items: [pod] }) });
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[{ pathname: "/kvm/1", key: "testKey" }]}>
+          <KVMAggregateResources id={pod.id} />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(
+      wrapper.find("[data-test='vfs-meter'] [data-test='total']").text()
+    ).toBe("40");
+    expect(
+      wrapper.find("[data-test='vfs-meter'] [data-test='allocated']").text()
+    ).toBe("25");
+    expect(
+      wrapper.find("[data-test='vfs-meter'] [data-test='free']").text()
+    ).toBe("15");
   });
 });
