d2721078184a8126181a64328c261b846b39786d
Caleb Ellis <caleb.ellis@canonical.com>
2020-09-21 17:07:42 +1000

    feat(ui): Build AddRSDForm component for RSD list

---
+++ ui/src/app/rsd/views/RSDList/AddRSDForm/AddRSDFormFields/AddRSDFormFields.tsx
@@ -0,0 +1,68 @@
+import { Col, Row, Select } from "@canonical/react-components";
+import React from "react";
+import { useSelector } from "react-redux";
+
+import FormikField from "app/base/components/FormikField";
+import resourcePoolSelectors from "app/store/resourcepool/selectors";
+import zoneSelectors from "app/store/zone/selectors";
+
+export const AddRSDFormFields = (): JSX.Element => {
+  const resourcePools = useSelector(resourcePoolSelectors.all);
+  const zones = useSelector(zoneSelectors.all);
+
+  return (
+    <Row>
+      <Col size="5">
+        <FormikField
+          label="Name"
+          name="name"
+          placeholder="Name (optional)"
+          type="text"
+        />
+        <FormikField
+          component={Select}
+          label="Zone"
+          name="zone"
+          options={[
+            { label: "Select your zone", value: "", disabled: true },
+            ...zones.map((zone) => ({
+              key: `zone-${zone.id}`,
+              label: zone.name,
+              value: zone.name,
+            })),
+          ]}
+        />
+        <FormikField
+          component={Select}
+          label="Resource pool"
+          name="pool"
+          options={[
+            { label: "Select your resource pool", value: "", disabled: true },
+            ...resourcePools.map((pool) => ({
+              key: `pool-${pool.id}`,
+              label: pool.name,
+              value: pool.name,
+            })),
+          ]}
+        />
+      </Col>
+      <Col size="5">
+        <FormikField
+          label="Address"
+          name="power_address"
+          type="text"
+          required
+        />
+        <FormikField label="User" name="power_user" type="text" required />
+        <FormikField
+          label="Password"
+          name="power_pass"
+          type="password"
+          required
+        />
+      </Col>
+    </Row>
+  );
+};
+
+export default AddRSDFormFields;
