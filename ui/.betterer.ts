9ff524669fd76f75e35e436b76949e01de7e23ab
Kit Randel <kit.randel@canonical.com>
2020-10-01 10:15:19 +1300

    fix(ci): betterer js counts only src dir
    Betterer's "migrate js files to ts" test was counting all files, including dist etc. which lead to inconsistent counts. We really only care about the files in src.

--- ui/.betterer.ts
+++ ui/.betterer.ts
@@ -4,12 +4,13 @@ import { smaller } from "@betterer/constraints";
 
 const { execSync } = require("child_process");
 
-const jsFileCount = parseInt(execSync(
-  'find . -type f -name "*.js" -not -path "./node_modules/*" | wc -l',
-  { encoding: "utf8" }
-)
-  .replace("\n", "")
-  .trim());
+const jsFileCount = parseInt(
+  execSync('find . -type f -name "*.js" -path "./src/*" | wc -l', {
+    encoding: "utf8",
+  })
+    .replace("\n", "")
+    .trim()
+);
 
 export default {
   "stricter compilation": typescriptBetterer("./tsconfig.json", {
