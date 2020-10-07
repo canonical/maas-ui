4ff3d08d9cb4bafe6c80df3594a6270a6ca8bb90
Huw Wilkins <huw.wilkins@canonical.com>
2020-10-07 16:00:12 +1100

    test(cypress): improve the cypress machine add test
    Use a more reliable method for getting the success message when adding a machine in the cypress
    test.
    
    Fixes: canonical-web-and-design/maas-ui#1342

--- integration/cypress/integration/machines/add.spec.ts
+++ integration/cypress/integration/machines/add.spec.ts
@@ -20,10 +20,8 @@ context("Machine add", () => {
     const hostname = `cypress-${nanoid()}`;
     cy.get("input[name='hostname']").type(hostname);
     cy.get("input[name='pxe_mac']").type(generateMac());
-    cy.get("select[name='power_type']").select("Manual").blur();
+    cy.get("select[name='power_type']").select("manual").blur();
     cy.get("button[type='submit']").click();
-    cy.get("p.p-notification__response").contains(
-      `${hostname} added successfully.`
-    );
-  }).skip();
+    cy.get(`[data-test='message']:contains(${hostname} added successfully.)`);
+  });
 });
