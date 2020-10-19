37f6e32a99075914c404e5f7dabdd136eac3392c
Caleb Ellis <caleb.ellis@canonical.com>
2020-10-19 14:59:26 +1000

    test(legacy,ui): add simple integration smoke tests for all pages in the navigation
    Added integration tests that check that the heading and navigation highlight is correct for Devices,
    Controllers, KVM, Images, DNS, AZs, Subnets and user preferences.

--- integration/cypress/integration/settings/base.spec.ts
+++ integration/cypress/integration/settings/base.spec.ts
@@ -2,11 +2,11 @@ import { generateNewURL } from "@maas-ui/maas-ui-shared";
 
 import { login } from "../utils";
 
-context("Machine listing", () => {
+context("Settings", () => {
   beforeEach(() => {
     login();
     cy.setCookie("skipintro", "true");
-    cy.visit(generateNewURL("/machines"));
+    cy.visit(generateNewURL("/settings"));
   });
 
   afterEach(() => {
@@ -14,14 +14,14 @@ context("Machine listing", () => {
   });
 
   it("renders the correct heading", () => {
-    cy.get("[data-test='section-header-title']").contains("Machines");
+    cy.get("h1.p-heading--four").contains("Settings");
   });
 
   it("highlights the correct navigation link", () => {
     cy.get(".p-navigation__link.is-selected a").should(
       "have.attr",
       "href",
-      generateNewURL("/machines")
+      generateNewURL("/settings")
     );
   });
 });
