e7cb3765d32920f745e08d416118f2d90fd3f1a2
Huw Wilkins <huw.wilkins@canonical.com>
2020-11-09 16:49:48 +1100

    fix(ui): display machine notifications on the summary only
    Move the machine notifications to only display on the summary page and update the component for use
    with other tabs.
    
    Fixes: canonical-web-and-design/maas-ui#1851.

---
+++ ui/src/app/machines/views/MachineDetails/MachineSummary/SummaryNotifications/SummaryNotifications.test.tsx
@@ -0,0 +1,189 @@
+import { MemoryRouter } from "react-router-dom";
+import { mount } from "enzyme";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import React from "react";
+
+import {
+  architecturesState as architecturesStateFactory,
+  generalState as generalStateFactory,
+  machine as machineFactory,
+  machineEvent as machineEventFactory,
+  machineState as machineStateFactory,
+  powerType as powerTypeFactory,
+  powerTypesState as powerTypesStateFactory,
+  rootState as rootStateFactory,
+} from "testing/factories";
+import SummaryNotifications from "./SummaryNotifications";
+import type { RootState } from "app/store/root/types";
+
+const mockStore = configureStore();
+
+describe("SummaryNotifications", () => {
+  let state: RootState;
+  beforeEach(() => {
+    state = rootStateFactory({
+      general: generalStateFactory({
+        architectures: architecturesStateFactory({
+          data: ["amd64"],
+        }),
+        powerTypes: powerTypesStateFactory({
+          data: [powerTypeFactory()],
+        }),
+      }),
+      machine: machineStateFactory({
+        items: [
+          machineFactory({
+            architecture: "amd64",
+            events: [machineEventFactory()],
+            system_id: "abc123",
+          }),
+        ],
+      }),
+    });
+  });
+
+  it("handles no notifications", () => {
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: "/machine/abc123", key: "testKey" }]}
+        >
+          <SummaryNotifications id="abc123" />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(wrapper.find("Notification").exists()).toBe(false);
+  });
+
+  it("can display a power error", () => {
+    state.machine.items = [
+      machineFactory({
+        architecture: "amd64",
+        events: [
+          machineEventFactory({
+            description: "machine timed out",
+          }),
+        ],
+        power_state: "error",
+        system_id: "abc123",
+      }),
+    ];
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: "/machine/abc123", key: "testKey" }]}
+        >
+          <SummaryNotifications id="abc123" />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(
+      wrapper
+        .findWhere(
+          (n) =>
+            n.name() === "Notification" &&
+            n.text().includes("Script - machine timed out")
+        )
+        .exists()
+    ).toBe(true);
+  });
+
+  it("can display a rack connection error", () => {
+    state.general.powerTypes.data = [];
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: "/machine/abc123", key: "testKey" }]}
+        >
+          <SummaryNotifications id="abc123" />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(
+      wrapper
+        .findWhere(
+          (n) =>
+            n.name() === "Notification" &&
+            n.text().includes("no rack controller is currently connected")
+        )
+        .exists()
+    ).toBe(true);
+  });
+
+  it("can display an architecture error", () => {
+    state.machine.items[0].architecture = "";
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: "/machine/abc123", key: "testKey" }]}
+        >
+          <SummaryNotifications id="abc123" />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(
+      wrapper
+        .findWhere(
+          (n) =>
+            n.name() === "Notification" &&
+            n
+              .text()
+              .includes("This machine currently has an invalid architecture")
+        )
+        .exists()
+    ).toBe(true);
+  });
+
+  it("can display a boot images error", () => {
+    state.general.architectures = architecturesStateFactory({
+      data: [],
+    });
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: "/machine/abc123", key: "testKey" }]}
+        >
+          <SummaryNotifications id="abc123" />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(
+      wrapper
+        .findWhere(
+          (n) =>
+            n.name() === "Notification" &&
+            n.text().includes("No boot images have been imported")
+        )
+        .exists()
+    ).toBe(true);
+  });
+
+  it("can display a hardware error", () => {
+    state.machine.items[0].cpu_count = 0;
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: "/machine/abc123", key: "testKey" }]}
+        >
+          <SummaryNotifications id="abc123" />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(
+      wrapper
+        .findWhere(
+          (n) =>
+            n.name() === "Notification" &&
+            n.text().includes("Commission this machine to get CPU")
+        )
+        .exists()
+    ).toBe(true);
+  });
+});
---
+++ ui/src/app/machines/views/MachineDetails/MachineSummary/SummaryNotifications/SummaryNotifications.tsx
@@ -0,0 +1,123 @@
+import { Link } from "react-router-dom";
+import { Spinner } from "@canonical/react-components";
+import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from "react";
+
+import { general as generalActions } from "app/base/actions";
+import {
+  useCanEdit,
+  useHasInvalidArchitecture,
+  useIsRackControllerConnected,
+} from "app/store/machine/utils";
+import generalSelectors from "app/store/general/selectors";
+import LegacyLink from "app/base/components/LegacyLink";
+import MachineNotifications from "app/machines/views/MachineDetails/MachineNotifications";
+import machineSelectors from "app/store/machine/selectors";
+import type { RootState } from "app/store/root/types";
+import type { Event, Machine } from "app/store/machine/types";
+
+type Props = {
+  id: Machine["system_id"];
+};
+
+const formatEventText = (event: Event) => {
+  if (!event) {
+    return "";
+  }
+  const text = [];
+  if (event.type?.description) {
+    text.push(event.type.description);
+  }
+  if (event.description) {
+    text.push(event.description);
+  }
+  return text.join(" - ");
+};
+
+const SummaryNotifications = ({ id }: Props): JSX.Element => {
+  const dispatch = useDispatch();
+  const machine = useSelector((state: RootState) =>
+    machineSelectors.getById(state, id)
+  );
+  const architectures = useSelector(generalSelectors.architectures.get);
+  const hasUsableArchitectures = architectures.length > 0;
+  const canEdit = useCanEdit(machine, true);
+  const isRackControllerConnected = useIsRackControllerConnected();
+  const hasInvalidArchitecture = useHasInvalidArchitecture(machine);
+
+  useEffect(() => {
+    dispatch(generalActions.fetchArchitectures());
+  }, [dispatch]);
+
+  // Confirm that the full machine details have been fetched. This also allows
+  // TypeScript know we're using the right union type (otherwise it will
+  // complain that events don't exist on the base machine type).
+  if (!machine || !("events" in machine)) {
+    return <Spinner />;
+  }
+
+  return (
+    <MachineNotifications
+      notifications={[
+        {
+          active: machine.power_state === "error" && machine.events?.length > 0,
+          content: (
+            <>
+              {formatEventText(machine.events[0])}.{" "}
+              <Link
+                to={`/machine/${machine.system_id}/logs`}
+                className="p-notification__action"
+              >
+                See logs
+              </Link>
+            </>
+          ),
+          status: "Error:",
+          type: "negative",
+        },
+        {
+          active: canEdit && !isRackControllerConnected,
+          content:
+            "Editing is currently disabled because no rack controller is currently connected to the region.",
+          status: "Error:",
+          type: "negative",
+        },
+        {
+          active:
+            canEdit &&
+            hasInvalidArchitecture &&
+            isRackControllerConnected &&
+            hasUsableArchitectures,
+          content:
+            "This machine currently has an invalid architecture. Update the architecture of this machine to make it deployable.",
+          status: "Error:",
+          type: "negative",
+        },
+        {
+          active:
+            canEdit &&
+            hasInvalidArchitecture &&
+            isRackControllerConnected &&
+            !hasUsableArchitectures,
+          content: (
+            <>
+              No boot images have been imported for a valid architecture to be
+              selected. Visit the{" "}
+              <LegacyLink route="/images">images page</LegacyLink> to start the
+              import process.
+            </>
+          ),
+          status: "Error:",
+          type: "negative",
+        },
+        {
+          active: machine.cpu_count === 0,
+          content:
+            "Commission this machine to get CPU, Memory and Storage information.",
+        },
+      ]}
+    />
+  );
+};
+
+export default SummaryNotifications;
