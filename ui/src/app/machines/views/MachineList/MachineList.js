5496824173b86a8d2508604ab5a9a5d3df3dab62
Kit Randel <kit.randel@canonical.com>
2020-11-27 16:11:08 +1300

    Fix import

--- ui/src/app/machines/views/MachineList/MachineList.js
+++ ui/src/app/machines/views/MachineList/MachineList.js
@@ -2,7 +2,7 @@ import { Notification } from "@canonical/react-components";
 import { useDispatch, useSelector } from "react-redux";
 import { useStorageState } from "react-storage-hooks";
 import PropTypes from "prop-types";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 import { formatErrors } from "app/utils";
 import { actions as machineActions } from "app/store/machine";
