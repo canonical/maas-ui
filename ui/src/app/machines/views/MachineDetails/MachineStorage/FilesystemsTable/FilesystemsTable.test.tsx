8cf4ef986f7787e2c3bd0a26257dd94027cbd36f
Caleb Ellis <caleb.ellis@canonical.com>
2020-12-02 21:47:35 +1000

    feat(ui): add ability to unmount filesystems

--- ui/src/app/machines/views/MachineDetails/MachineStorage/FilesystemsTable/FilesystemsTable.test.tsx
+++ ui/src/app/machines/views/MachineDetails/MachineStorage/FilesystemsTable/FilesystemsTable.test.tsx
@@ -157,15 +157,14 @@ describe("FilesystemsTable", () => {
     expect(wrapper.find("AddSpecialFilesystem").exists()).toBe(true);
   });
 
-  it("can show an action to remove a filesystem", () => {
+  it("can remove a disk's filesystem", () => {
     const filesystem = fsFactory({ mount_point: "/disk-fs/path" });
+    const disk = diskFactory({ filesystem, partitions: [] });
     const state = rootStateFactory({
       machine: machineStateFactory({
         items: [
           machineDetailsFactory({
-            disks: [
-              diskFactory({ filesystem, name: "disk-fs", partitions: [] }),
-            ],
+            disks: [disk],
             system_id: "abc123",
           }),
         ],
@@ -182,13 +181,259 @@ describe("FilesystemsTable", () => {
     );
 
     wrapper.find("TableMenu button").at(0).simulate("click");
+    wrapper
+      .findWhere(
+        (button) =>
+          button.name() === "button" && button.text().includes("Remove")
+      )
+      .simulate("click");
+    wrapper.find("ActionButton").simulate("click");
 
-    expect(wrapper.find("ContextualMenuDropdown Button").at(0).text()).toBe(
-      "Remove filesystem..."
+    expect(wrapper.find("ActionConfirm").prop("message")).toBe(
+      "Are you sure you want to remove this filesystem?"
+    );
+    expect(
+      store
+        .getActions()
+        .find((action) => action.type === "machine/deleteFilesystem")
+    ).toStrictEqual({
+      meta: {
+        method: "delete_filesystem",
+        model: "machine",
+      },
+      payload: {
+        params: {
+          blockdevice_id: disk.id,
+          filesystem_id: filesystem.id,
+          system_id: "abc123",
+        },
+      },
+      type: "machine/deleteFilesystem",
+    });
+  });
+
+  it("can remove a partition's filesystem", () => {
+    const filesystem = fsFactory({ mount_point: "/disk-fs/path" });
+    const partition = partitionFactory({ filesystem });
+    const disk = diskFactory({ filesystem: null, partitions: [partition] });
+    const state = rootStateFactory({
+      machine: machineStateFactory({
+        items: [
+          machineDetailsFactory({
+            disks: [disk],
+            system_id: "abc123",
+          }),
+        ],
+        statuses: machineStatusesFactory({
+          abc123: machineStatusFactory(),
+        }),
+      }),
+    });
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <FilesystemsTable canEditStorage systemId="abc123" />
+      </Provider>
     );
 
-    wrapper.find("ContextualMenuDropdown button").at(0).simulate("click");
+    wrapper.find("TableMenu button").at(0).simulate("click");
+    wrapper
+      .findWhere(
+        (button) =>
+          button.name() === "button" && button.text().includes("Remove")
+      )
+      .simulate("click");
+    wrapper.find("ActionButton").simulate("click");
 
-    expect(wrapper.find("ActionConfirm").exists()).toBe(true);
+    expect(wrapper.find("ActionConfirm").prop("message")).toBe(
+      "Are you sure you want to remove this filesystem?"
+    );
+    expect(
+      store
+        .getActions()
+        .find((action) => action.type === "machine/deletePartition")
+    ).toStrictEqual({
+      meta: {
+        method: "delete_partition",
+        model: "machine",
+      },
+      payload: {
+        params: {
+          partition_id: partition.id,
+          system_id: "abc123",
+        },
+      },
+      type: "machine/deletePartition",
+    });
+  });
+
+  it("can remove a special filesystem", () => {
+    const filesystem = fsFactory({
+      fstype: "tmpfs",
+      mount_point: "/disk-fs/path",
+    });
+    const state = rootStateFactory({
+      machine: machineStateFactory({
+        items: [
+          machineDetailsFactory({
+            disks: [],
+            special_filesystems: [filesystem],
+            system_id: "abc123",
+          }),
+        ],
+        statuses: machineStatusesFactory({
+          abc123: machineStatusFactory(),
+        }),
+      }),
+    });
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <FilesystemsTable canEditStorage systemId="abc123" />
+      </Provider>
+    );
+
+    wrapper.find("TableMenu button").at(0).simulate("click");
+    wrapper
+      .findWhere(
+        (button) =>
+          button.name() === "button" && button.text().includes("Remove")
+      )
+      .simulate("click");
+    wrapper.find("ActionButton").simulate("click");
+
+    expect(wrapper.find("ActionConfirm").prop("message")).toBe(
+      "Are you sure you want to remove this special filesystem?"
+    );
+    expect(
+      store
+        .getActions()
+        .find((action) => action.type === "machine/unmountSpecial")
+    ).toStrictEqual({
+      meta: {
+        method: "unmount_special",
+        model: "machine",
+      },
+      payload: {
+        params: {
+          mount_point: filesystem.mount_point,
+          system_id: "abc123",
+        },
+      },
+      type: "machine/unmountSpecial",
+    });
+  });
+
+  it("can unmount a disk's filesystem", () => {
+    const filesystem = fsFactory({ mount_point: "/disk-fs/path" });
+    const disk = diskFactory({ filesystem, partitions: [] });
+    const state = rootStateFactory({
+      machine: machineStateFactory({
+        items: [
+          machineDetailsFactory({
+            disks: [disk],
+            system_id: "abc123",
+          }),
+        ],
+        statuses: machineStatusesFactory({
+          abc123: machineStatusFactory(),
+        }),
+      }),
+    });
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <FilesystemsTable canEditStorage systemId="abc123" />
+      </Provider>
+    );
+
+    wrapper.find("TableMenu button").at(0).simulate("click");
+    wrapper
+      .findWhere(
+        (button) =>
+          button.name() === "button" && button.text().includes("Unmount")
+      )
+      .simulate("click");
+    wrapper.find("ActionButton").simulate("click");
+
+    expect(wrapper.find("ActionConfirm").prop("message")).toBe(
+      "Are you sure you want to unmount this filesystem?"
+    );
+    expect(
+      store
+        .getActions()
+        .find((action) => action.type === "machine/updateFilesystem")
+    ).toStrictEqual({
+      meta: {
+        method: "update_filesystem",
+        model: "machine",
+      },
+      payload: {
+        params: {
+          block_id: disk.id,
+          mount_options: "",
+          mount_point: "",
+          system_id: "abc123",
+        },
+      },
+      type: "machine/updateFilesystem",
+    });
+  });
+
+  it("can unmount a partition's filesystem", () => {
+    const filesystem = fsFactory({ mount_point: "/disk-fs/path" });
+    const partition = partitionFactory({ filesystem });
+    const disk = diskFactory({ filesystem: null, partitions: [partition] });
+    const state = rootStateFactory({
+      machine: machineStateFactory({
+        items: [
+          machineDetailsFactory({
+            disks: [disk],
+            system_id: "abc123",
+          }),
+        ],
+        statuses: machineStatusesFactory({
+          abc123: machineStatusFactory(),
+        }),
+      }),
+    });
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <FilesystemsTable canEditStorage systemId="abc123" />
+      </Provider>
+    );
+
+    wrapper.find("TableMenu button").at(0).simulate("click");
+    wrapper
+      .findWhere(
+        (button) =>
+          button.name() === "button" && button.text().includes("Unmount")
+      )
+      .simulate("click");
+    wrapper.find("ActionButton").simulate("click");
+
+    expect(wrapper.find("ActionConfirm").prop("message")).toBe(
+      "Are you sure you want to unmount this filesystem?"
+    );
+    expect(
+      store
+        .getActions()
+        .find((action) => action.type === "machine/updateFilesystem")
+    ).toStrictEqual({
+      meta: {
+        method: "update_filesystem",
+        model: "machine",
+      },
+      payload: {
+        params: {
+          mount_options: "",
+          mount_point: "",
+          partition_id: partition.id,
+          system_id: "abc123",
+        },
+      },
+      type: "machine/updateFilesystem",
+    });
   });
 });
