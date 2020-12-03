7da7adb370974f6b8a371e31f2afe2b1aa127369
Kit Randel <kit.randel@canonical.com>
2020-12-04 12:47:08 +1300

    refactor(ui): add testing result selectors to noderesult selectors (#1971)

--- ui/src/app/machines/views/MachineDetails/MachineTests/MachineTests.tsx
+++ ui/src/app/machines/views/MachineDetails/MachineTests/MachineTests.tsx
@@ -6,13 +6,13 @@ import { useParams } from "react-router";
 
 import MachineTestsTable from "./MachineTestsTable";
 
-import { HardwareType, ResultType } from "app/base/enum";
+import { HardwareType } from "app/base/enum";
 import { useWindowTitle } from "app/base/hooks";
 import type { RouteParams } from "app/base/types";
 import machineSelectors from "app/store/machine/selectors";
 import { actions as nodeResultActions } from "app/store/noderesult";
 import nodeResultSelectors from "app/store/noderesult/selectors";
-import type { NodeResult, NodeResults } from "app/store/noderesult/types";
+import type { NodeResult } from "app/store/noderesult/types";
 import type { RootState } from "app/store/root/types";
 
 /**
@@ -27,38 +27,6 @@ const groupByKey = <I,>(items: I[], key: keyof I): { [x: string]: I[] } =>
     return obj;
   }, Object.create(null));
 
-const getTestingResults = (nodeResults: NodeResults): NodeResult[] =>
-  nodeResults.results.filter(
-    (result) => result.result_type === ResultType.Testing
-  );
-
-const getHardwareResults = (nodeResults: NodeResults) =>
-  groupByKey(
-    getTestingResults(nodeResults).filter(
-      (result) =>
-        result.hardware_type === HardwareType.CPU ||
-        result.hardware_type === HardwareType.Memory ||
-        result.hardware_type === HardwareType.Network
-    ),
-    "hardware_type"
-  );
-
-const getStorageResults = (nodeResults: NodeResults) =>
-  groupByKey(
-    getTestingResults(nodeResults).filter(
-      (result) => result.hardware_type === HardwareType.Storage
-    ),
-    "physical_blockdevice"
-  );
-
-const getOtherResults = (nodeResults: NodeResults) =>
-  groupByKey(
-    getTestingResults(nodeResults).filter(
-      (result) => result.hardware_type === HardwareType.Node
-    ),
-    "hardware_type"
-  );
-
 const MachineTests = (): JSX.Element => {
   const dispatch = useDispatch();
   const params = useParams<RouteParams>();
@@ -69,8 +37,16 @@ const MachineTests = (): JSX.Element => {
   );
   useWindowTitle(`${machine?.fqdn || "Machine"} tests`);
 
-  const nodeResults = useSelector((state: RootState) =>
-    nodeResultSelectors.get(state, id)
+  const hardwareResults = useSelector((state: RootState) =>
+    nodeResultSelectors.getHardwareTestingResults(state, id)
+  );
+
+  const storageResults = useSelector((state: RootState) =>
+    nodeResultSelectors.getStorageTestingResults(state, id)
+  );
+
+  const otherResults = useSelector((state: RootState) =>
+    nodeResultSelectors.getOtherTestingResults(state, id)
   );
 
   const loading = useSelector((state: RootState) =>
@@ -78,34 +54,38 @@ const MachineTests = (): JSX.Element => {
   );
 
   useEffect(() => {
-    if (!nodeResults && !loading) {
+    if ((!hardwareResults || storageResults || otherResults) && !loading) {
       dispatch(nodeResultActions.get(id));
     }
-  }, [dispatch, nodeResults, loading, id]);
+  }, [dispatch, hardwareResults, storageResults, otherResults, loading, id]);
 
-  if (nodeResults) {
-    const allResults = getHardwareResults(nodeResults);
-    const storageResults = getStorageResults(nodeResults);
-    const otherResults = getOtherResults(nodeResults);
-
+  if (
+    hardwareResults.length > 0 ||
+    storageResults.length > 0 ||
+    otherResults.length > 0
+  ) {
     return (
       <div>
-        {Object.entries(allResults).map(
-          ([hardware_type, nodeResults]: [string, NodeResult[]]) => {
-            return (
-              <div key={hardware_type}>
-                <h4 data-test="hardware-heading">
-                  {HardwareType[parseInt(hardware_type, 0)]}
-                </h4>
-                <MachineTestsTable nodeResults={nodeResults} />
-              </div>
-            );
-          }
-        )}
-        {Object.keys(storageResults).length !== 0 ? (
+        {hardwareResults.length > 0
+          ? Object.entries(groupByKey(hardwareResults, "hardware_type")).map(
+              ([hardware_type, nodeResults]: [string, NodeResult[]]) => {
+                return (
+                  <div key={hardware_type}>
+                    <h4 data-test="hardware-heading">
+                      {HardwareType[parseInt(hardware_type, 0)]}
+                    </h4>
+                    <MachineTestsTable nodeResults={nodeResults} />
+                  </div>
+                );
+              }
+            )
+          : null}
+        {storageResults.length > 0 ? (
           <>
             <h4 data-test="hardware-heading">Storage</h4>
-            {Object.entries(storageResults).map(
+            {Object.entries(
+              groupByKey(storageResults, "physical_blockdevice")
+            ).map(
               ([physical_blockdevice, nodeResults]: [string, NodeResult[]]) => {
                 return (
                   <div key={physical_blockdevice}>
@@ -117,10 +97,10 @@ const MachineTests = (): JSX.Element => {
             )}
           </>
         ) : null}
-        {Object.keys(otherResults).length !== 0 ? (
+        {otherResults.length > 0 ? (
           <>
             <h4 data-test="hardware-heading">Other Results</h4>
-            {Object.entries(otherResults).map(
+            {Object.entries(groupByKey(otherResults, "hardware_type")).map(
               ([hardware_type, nodeResults]: [string, NodeResult[]]) => {
                 return (
                   <div key={hardware_type}>
