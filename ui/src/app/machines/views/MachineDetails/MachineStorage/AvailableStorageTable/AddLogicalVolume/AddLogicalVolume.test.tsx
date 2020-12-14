08afd5585270917cbaa6c31ddea2cbc9aa2377fd
Caleb Ellis <caleb.ellis@canonical.com>
2020-12-15 09:29:43 +1000

    fix(ui): correctly validate size when creating a new partition or logical volume

--- ui/src/app/machines/views/MachineDetails/MachineStorage/AvailableStorageTable/AddLogicalVolume/AddLogicalVolume.test.tsx
+++ ui/src/app/machines/views/MachineDetails/MachineStorage/AvailableStorageTable/AddLogicalVolume/AddLogicalVolume.test.tsx
@@ -1,4 +1,5 @@
 import { mount } from "enzyme";
+import { act } from "react-dom/test-utils";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 
@@ -70,6 +71,103 @@ describe("AddLogicalVolume", () => {
 
     // Two logical volumes already exist so the next one should be lv2
     expect(wrapper.find("Input[name='name']").prop("value")).toBe("lv2");
+  });
+
+  it("can validate if the size meets the minimum requirement", async () => {
+    const disk = diskFactory({
+      available_size: 1000000000, // 1GB
+      id: 1,
+      type: DiskTypes.VOLUME_GROUP,
+    });
+    const state = rootStateFactory({
+      machine: machineStateFactory({
+        items: [machineDetailsFactory({ disks: [disk], system_id: "abc123" })],
+        statuses: machineStatusesFactory({
+          abc123: machineStatusFactory(),
+        }),
+      }),
+    });
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <AddLogicalVolume
+          closeExpanded={jest.fn()}
+          disk={disk}
+          systemId="abc123"
+        />
+      </Provider>
+    );
+
+    // Set logical volume size to 0.1MB
+    await act(async () => {
+      wrapper
+        .find("input[name='size']")
+        .props()
+        .onChange({
+          target: { name: "size", value: "0.1" },
+        } as React.ChangeEvent<HTMLInputElement>);
+    });
+    wrapper.update();
+    await act(async () => {
+      wrapper
+        .find("select[name='unit']")
+        .props()
+        .onChange({
+          target: { name: "unit", value: "MB" },
+        } as React.ChangeEvent<HTMLSelectElement>);
+    });
+    wrapper.update();
+
+    expect(
+      wrapper
+        .find(".p-form-validation__message")
+        .text()
+        .includes("is required to add a logical volume")
+    ).toBe(true);
+  });
+
+  it("can validate if the size is less than available disk space", async () => {
+    const disk = diskFactory({
+      available_size: 1000000000, // 1GB
+      id: 1,
+      type: DiskTypes.VOLUME_GROUP,
+    });
+    const state = rootStateFactory({
+      machine: machineStateFactory({
+        items: [machineDetailsFactory({ disks: [disk], system_id: "abc123" })],
+        statuses: machineStatusesFactory({
+          abc123: machineStatusFactory(),
+        }),
+      }),
+    });
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <AddLogicalVolume
+          closeExpanded={jest.fn()}
+          disk={disk}
+          systemId="abc123"
+        />
+      </Provider>
+    );
+
+    // Set logical volume size to 2GB
+    await act(async () => {
+      wrapper
+        .find("input[name='size']")
+        .props()
+        .onChange({
+          target: { name: "size", value: "2" },
+        } as React.ChangeEvent<HTMLInputElement>);
+    });
+    wrapper.update();
+
+    expect(
+      wrapper
+        .find(".p-form-validation__message")
+        .text()
+        .includes("available in this volume group")
+    ).toBe(true);
   });
 
   it("correctly dispatches an action to create a logical volume", () => {
