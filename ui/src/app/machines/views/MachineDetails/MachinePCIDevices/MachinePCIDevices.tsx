ca1f7b0c110d56bb595a9dc62e96af819ad9e206
Caleb Ellis <caleb.ellis@canonical.com>
2021-01-08 16:11:26 +1000

    feat(ui): add Placeholder component and use in PCI/USB tables

--- ui/src/app/machines/views/MachineDetails/MachinePCIDevices/MachinePCIDevices.tsx
+++ ui/src/app/machines/views/MachineDetails/MachinePCIDevices/MachinePCIDevices.tsx
@@ -1,18 +1,12 @@
 import { useEffect } from "react";
 
-import {
-  Button,
-  Col,
-  Icon,
-  Row,
-  Spinner,
-  Strip,
-} from "@canonical/react-components";
+import { Button, Col, Icon, Row, Strip } from "@canonical/react-components";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
 
 import type { SetSelectedAction } from "../MachineSummary";
 
+import Placeholder from "app/base/components/Placeholder";
 import { useWindowTitle } from "app/base/hooks";
 import type { RouteParams } from "app/base/types";
 import machineSelectors from "app/store/machine/selectors";
@@ -56,11 +50,39 @@ const MachinePCIDevices = ({ setSelectedAction }: Props): JSX.Element => {
             <th className="u-align--right">PCI address</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {nodeDevicesLoading ? (
+            <>
+              {Array.from(Array(5)).map((_, i) => (
+                <tr key={`pci-placeholder-${i}`}>
+                  <td>
+                    <Placeholder>Group name</Placeholder>
+                  </td>
+                  <td>
+                    <Placeholder>Example vendor description</Placeholder>
+                  </td>
+                  <td>
+                    <Placeholder>Example product description</Placeholder>
+                  </td>
+                  <td>
+                    <Placeholder>0000</Placeholder>
+                  </td>
+                  <td>
+                    <Placeholder>Driver name</Placeholder>
+                  </td>
+                  <td className="u-align--right">
+                    <Placeholder>0000</Placeholder>
+                  </td>
+                  <td className="u-align--right">
+                    <Placeholder>0000:00:00.0</Placeholder>
+                  </td>
+                </tr>
+              ))}
+            </>
+          ) : null}
+        </tbody>
       </table>
-      {nodeDevicesLoading ? (
-        <Spinner text="Loading..." />
-      ) : (
+      {!nodeDevicesLoading && (
         <Strip data-test="information-unavailable" shallow>
           <Row>
             <Col className="u-flex" emptyLarge={4} size={6}>
