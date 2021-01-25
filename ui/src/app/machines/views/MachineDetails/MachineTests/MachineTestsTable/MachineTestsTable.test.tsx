770b8dfbd57d56d296fcc025130d7921771c00d7
Kit Randel <kit.randel@canonical.com>
2021-01-25 16:29:03 +1300

    feat(ui): add metrics to script results view

--- ui/src/app/machines/views/MachineDetails/MachineTests/MachineTestsTable/MachineTestsTable.test.tsx
+++ ui/src/app/machines/views/MachineDetails/MachineTests/MachineTestsTable/MachineTestsTable.test.tsx
@@ -15,6 +15,7 @@ import {
   partialScriptResult as partialScriptResultFactory,
   rootState as rootStateFactory,
   scriptResult as scriptResultFactory,
+  scriptResultResult as scriptResultResultFactory,
   scriptResultState as scriptResultStateFactory,
 } from "testing/factories";
 
@@ -182,6 +183,9 @@ describe("MachineTestsTable", () => {
       </Provider>
     );
     wrapper.find("Button.p-contextual-menu__toggle").simulate("click");
+    wrapper
+      .find("Button[data-test='action-menu-show-previous']")
+      .simulate("click");
 
     expect(
       wrapper.find("tr[data-test='script-result-history']").exists()
@@ -207,8 +211,70 @@ describe("MachineTestsTable", () => {
       </Provider>
     );
 
-    expect(wrapper.find("Button.p-contextual-menu__toggle").exists()).toEqual(
-      false
+    expect(
+      wrapper.find("Button[data-test='action-menu-show-previous']").exists()
+    ).toEqual(false);
+  });
+
+  it("displays metrics when clicking the show metrics action button", () => {
+    const metrics = scriptResultResultFactory({
+      title: "test-title",
+      value: "test-value",
+    });
+    const scriptResults = [scriptResultFactory({ id: 1, results: [metrics] })];
+
+    state.nodescriptresult.items = { abc123: [1] };
+    state.scriptresult.items = scriptResults;
+
+    const store = mockStore(state);
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: "/machine/abc123", key: "testKey" }]}
+        >
+          <MachineTestsTable machineId="abc123" scriptResults={scriptResults} />
+        </MemoryRouter>
+      </Provider>
     );
+    wrapper.find("Button.p-contextual-menu__toggle").simulate("click");
+    wrapper
+      .find("Button[data-test='action-menu-show-metrics']")
+      .simulate("click");
+
+    expect(
+      wrapper.find("tr[data-test='script-result-metrics']").exists()
+    ).toEqual(true);
+    expect(
+      wrapper.find("tr[data-test='script-result-metrics'] td").at(0).text()
+    ).toEqual("test-title");
+    expect(
+      wrapper.find("tr[data-test='script-result-metrics'] td").at(1).text()
+    ).toEqual("test-value");
+  });
+
+  it("does not display an action item to view metrics for script results without metrics", () => {
+    const scriptResults = [scriptResultFactory({ id: 1 })];
+    const scriptResultState = scriptResultStateFactory({
+      items: scriptResults,
+    });
+    state.nodescriptresult.items = { abc123: [1] };
+    state.scriptresult = scriptResultState;
+
+    const store = mockStore(state);
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: "/machine/abc123", key: "testKey" }]}
+        >
+          <MachineTestsTable machineId="abc123" scriptResults={scriptResults} />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(
+      wrapper.find("Button[data-test='action-menu-show-metrics']").exists()
+    ).toEqual(false);
   });
 });
