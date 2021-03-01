25f670b43d9f52a36baba978d8060d98555ac44a
Huw Wilkins <huw.wilkins@canonical.com>
2021-03-01 15:01:09 +1100

    feat(ui): filter link modes for the interface type
    Update the link mode select to limit the options to those valid for the interface type
    
    Fixes: #2288.

--- ui/src/app/base/components/LinkModeSelect/LinkModeSelect.test.tsx
+++ ui/src/app/base/components/LinkModeSelect/LinkModeSelect.test.tsx
@@ -5,21 +5,92 @@ import configureStore from "redux-mock-store";
 
 import LinkModeSelect from "./LinkModeSelect";
 
+import { NetworkInterfaceTypes } from "app/store/machine/types";
 import { rootState as rootStateFactory } from "testing/factories";
 
 const mockStore = configureStore();
 
 describe("LinkModeSelect", () => {
-  it("displays the link mode options", () => {
+  it("only displays LINK_UP if a subnet is not provided", () => {
     const store = mockStore(rootStateFactory());
     const wrapper = mount(
       <Provider store={store}>
         <Formik initialValues={{ mode: "" }} onSubmit={jest.fn()}>
-          <LinkModeSelect name="mode" />
+          <LinkModeSelect
+            defaultOption={null}
+            interfaceType={NetworkInterfaceTypes.PHYSICAL}
+            name="mode"
+            subnet={null}
+          />
         </Formik>
       </Provider>
     );
-    expect(wrapper.find("FormikField")).toMatchSnapshot();
+    expect(wrapper.find("FormikField").prop("options")).toStrictEqual([
+      {
+        label: "Unconfigured",
+        value: "link_up",
+      },
+    ]);
+  });
+
+  it("can display all options", () => {
+    const store = mockStore(rootStateFactory());
+    const wrapper = mount(
+      <Provider store={store}>
+        <Formik initialValues={{ mode: "" }} onSubmit={jest.fn()}>
+          <LinkModeSelect
+            defaultOption={null}
+            interfaceType={NetworkInterfaceTypes.PHYSICAL}
+            name="mode"
+            subnet={1}
+          />
+        </Formik>
+      </Provider>
+    );
+    expect(wrapper.find("FormikField").prop("options")).toStrictEqual([
+      {
+        label: "Auto assign",
+        value: "auto",
+      },
+      {
+        label: "Static assign",
+        value: "static",
+      },
+      {
+        label: "Unconfigured",
+        value: "link_up",
+      },
+      {
+        label: "DHCP",
+        value: "dhcp",
+      },
+    ]);
+  });
+
+  it("only displays auto or static for an alias", () => {
+    const store = mockStore(rootStateFactory());
+    const wrapper = mount(
+      <Provider store={store}>
+        <Formik initialValues={{ mode: "" }} onSubmit={jest.fn()}>
+          <LinkModeSelect
+            defaultOption={null}
+            interfaceType={NetworkInterfaceTypes.ALIAS}
+            name="mode"
+            subnet={1}
+          />
+        </Formik>
+      </Provider>
+    );
+    expect(wrapper.find("FormikField").prop("options")).toStrictEqual([
+      {
+        label: "Auto assign",
+        value: "auto",
+      },
+      {
+        label: "Static assign",
+        value: "static",
+      },
+    ]);
   });
 
   it("can display a default option", () => {
@@ -31,7 +102,11 @@ describe("LinkModeSelect", () => {
     const wrapper = mount(
       <Provider store={store}>
         <Formik initialValues={{ mode: "" }} onSubmit={jest.fn()}>
-          <LinkModeSelect name="mode" defaultOption={defaultOption} />
+          <LinkModeSelect
+            interfaceType={NetworkInterfaceTypes.PHYSICAL}
+            name="mode"
+            defaultOption={defaultOption}
+          />
         </Formik>
       </Provider>
     );
@@ -45,11 +120,15 @@ describe("LinkModeSelect", () => {
     const wrapper = mount(
       <Provider store={store}>
         <Formik initialValues={{ mode: "" }} onSubmit={jest.fn()}>
-          <LinkModeSelect name="mode" defaultOption={null} />
+          <LinkModeSelect
+            interfaceType={NetworkInterfaceTypes.PHYSICAL}
+            name="mode"
+            defaultOption={null}
+          />
         </Formik>
       </Provider>
     );
-    expect(wrapper.find("FormikField").prop("options").length).toBe(4);
+    expect(wrapper.find("FormikField").prop("options").length).toBe(1);
     expect(wrapper.find("FormikField").prop("options")[0]).not.toStrictEqual({
       label: "Select IP mode",
       value: "",
