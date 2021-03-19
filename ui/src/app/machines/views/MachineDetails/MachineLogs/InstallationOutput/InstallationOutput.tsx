070bd4dba11185e457d0a8c7f2ae91c7bf9dc710
Huw Wilkins <huw.wilkins@canonical.com>
2021-03-19 17:29:02 +1100

    feat(ui): display installation status
    Display the installation status in the installation log
    
    Fixes: #canonical-web-and-design/maas-squad#2375.

--- ui/src/app/machines/views/MachineDetails/MachineLogs/InstallationOutput/InstallationOutput.tsx
+++ ui/src/app/machines/views/MachineDetails/MachineLogs/InstallationOutput/InstallationOutput.tsx
@@ -6,11 +6,52 @@ import { useDispatch, useSelector } from "react-redux";
 
 import machineSelectors from "app/store/machine/selectors";
 import type { Machine } from "app/store/machine/types";
+import { PowerState } from "app/store/machine/types";
 import type { RootState } from "app/store/root/types";
 import { actions as scriptResultActions } from "app/store/scriptresult";
 import scriptResultSelectors from "app/store/scriptresult/selectors";
+import { ScriptResultStatus } from "app/store/scriptresult/types";
+import type {
+  ScriptResult,
+  ScriptResultData,
+} from "app/store/scriptresult/types";
 
 type Props = { systemId: Machine["system_id"] };
+
+const generateOutput = (
+  machine: Machine,
+  log: ScriptResultData | null,
+  result: ScriptResult | null
+) => {
+  if (!result) {
+    return "No installation result found.";
+  }
+  switch (result.status) {
+    case ScriptResultStatus.PENDING:
+      if (machine.power_state === PowerState.OFF) {
+        return "System is off.";
+      }
+      return "System is booting...";
+    case ScriptResultStatus.RUNNING:
+      return "Installation has begun!";
+    case ScriptResultStatus.PASSED:
+      if (!log?.combined) {
+        return "Installation has succeeded but no output was given.";
+      }
+      return log?.combined;
+    case ScriptResultStatus.FAILED:
+      if (!log?.combined) {
+        return "Installation has failed and no output was given.";
+      }
+      return log?.combined;
+    case ScriptResultStatus.TIMEDOUT:
+      return "Installation failed after 40 minutes.";
+    case ScriptResultStatus.ABORTED:
+      return "Installation was aborted.";
+    default:
+      return `Unknown log status ${result.status}`;
+  }
+};
 
 const InstallationOutput = ({ systemId }: Props): JSX.Element => {
   const dispatch = useDispatch();
@@ -30,6 +71,7 @@ const InstallationOutput = ({ systemId }: Props): JSX.Element => {
     scriptResultSelectors.getInstallationByMachineId(state, systemId)
   );
 
+  const [installationResult] = installationResults || [];
   const [log] = installationLogs || [];
 
   useEffect(() => {
@@ -41,16 +83,22 @@ const InstallationOutput = ({ systemId }: Props): JSX.Element => {
   }, [dispatch, scriptResults, loading, systemId]);
 
   useEffect(() => {
-    if (!log && installationResults) {
+    if (
+      !log &&
+      installationResults &&
+      [ScriptResultStatus.PASSED, ScriptResultStatus.FAILED].includes(
+        installationResult?.status
+      )
+    ) {
       // We expect there to only be one result, but loop through the results to
       // be sure.
       installationResults.forEach((result) => {
         dispatch(scriptResultActions.getLogs(result.id, "combined"));
       });
     }
-  }, [dispatch, log, installationResults]);
+  }, [dispatch, installationResult, log, installationResults, scriptResults]);
 
-  if (!machine || !log) {
+  if (!machine) {
     return <Spinner text="Loading..." />;
   }
 
@@ -59,7 +107,7 @@ const InstallationOutput = ({ systemId }: Props): JSX.Element => {
       blocks={[
         {
           appearance: CodeSnippetBlockAppearance.NUMBERED,
-          code: log.combined,
+          code: generateOutput(machine, log, installationResult),
         },
       ]}
     />
