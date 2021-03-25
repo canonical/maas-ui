9c704e613fd1286c006779de1972b125a6c33dc9
Huw Wilkins <huw.wilkins@canonical.com>
2021-03-25 11:36:01 +1100

    Use the machine FQDN and current date in the log download filename.

--- ui/src/app/machines/views/MachineDetails/MachineLogs/DownloadMenu/DownloadMenu.tsx
+++ ui/src/app/machines/views/MachineDetails/MachineLogs/DownloadMenu/DownloadMenu.tsx
@@ -1,12 +1,22 @@
 import { ContextualMenu } from "@canonical/react-components";
+import { format } from "date-fns";
 import fileDownload from "js-file-download";
+import { useSelector } from "react-redux";
 
 import { useGetInstallationOutput } from "../hooks";
 
+import machineSelectors from "app/store/machine/selectors";
 import type { Machine } from "app/store/machine/types";
+import type { RootState } from "app/store/root/types";
 type Props = { systemId: Machine["system_id"] };
-export const DownloadMenu = ({ systemId }: Props): JSX.Element => {
+export const DownloadMenu = ({ systemId }: Props): JSX.Element | null => {
+  const machine = useSelector((state: RootState) =>
+    machineSelectors.getById(state, systemId)
+  );
   const installationOutput = useGetInstallationOutput(systemId);
+  if (!machine) {
+    return null;
+  }
   return (
     <div>
       <ContextualMenu
@@ -28,9 +38,10 @@ export const DownloadMenu = ({ systemId }: Props): JSX.Element => {
                   children: "Installation output",
                   onClick: () => {
                     if (installationOutput.log) {
+                      const today = format(new Date(), "yyyy-MM-dd");
                       fileDownload(
                         installationOutput.log,
-                        "installation-output.txt"
+                        `${machine.fqdn}-installation-output-${today}.txt`
                       );
                     }
                   },
