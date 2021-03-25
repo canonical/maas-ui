1d0752def1d974c05b51f0799dd25b82eafc5d71
Huw Wilkins <huw.wilkins@canonical.com>
2021-03-25 11:39:59 +1100

    Add aria labels and a test for in between items.

--- ui/src/app/base/components/ArrowPagination/ArrowPagination.test.tsx
+++ ui/src/app/base/components/ArrowPagination/ArrowPagination.test.tsx
@@ -16,6 +16,19 @@ describe("ArrowPagination", () => {
     expect(wrapper.find("Button").last().prop("disabled")).toBe(true);
   });
 
+  it("activates both buttons when between the start and end", () => {
+    const wrapper = mount(
+      <ArrowPagination
+        currentPage={2}
+        itemCount={75}
+        pageSize={25}
+        setCurrentPage={() => null}
+      />
+    );
+    expect(wrapper.find("Button").first().prop("disabled")).toBe(false);
+    expect(wrapper.find("Button").last().prop("disabled")).toBe(false);
+  });
+
   it("disables the back button when on the first page", () => {
     const wrapper = mount(
       <ArrowPagination
--- ui/src/app/base/components/ArrowPagination/ArrowPagination.tsx
+++ ui/src/app/base/components/ArrowPagination/ArrowPagination.tsx
@@ -1,3 +1,5 @@
+import type { HTMLProps } from "react";
+
 import { Button, Icon } from "@canonical/react-components";
 
 type Props = {
@@ -5,22 +7,24 @@ type Props = {
   itemCount: number;
   pageSize: number;
   setCurrentPage: (page: number) => void;
-};
+} & HTMLProps<HTMLElement>;
 
 const ArrowPagination = ({
   currentPage,
   itemCount,
   pageSize,
   setCurrentPage,
+  ...props
 }: Props): JSX.Element => {
   const onFirstPage = currentPage === 1;
   const onLastPage =
     itemCount === 0 || currentPage === Math.ceil(itemCount / pageSize);
 
   return (
-    <>
+    <nav aria-label="pagination" {...props}>
       <Button
         appearance="base"
+        aria-label={onFirstPage ? "" : `Page ${currentPage - 1}`}
         className="u-no-margin--right u-no-margin--bottom"
         disabled={onFirstPage}
         onClick={() => {
@@ -32,6 +36,7 @@ const ArrowPagination = ({
       </Button>
       <Button
         appearance="base"
+        aria-label={onLastPage ? "" : `Page ${currentPage + 1}`}
         className="u-no-margin--bottom"
         disabled={onLastPage}
         onClick={() => {
@@ -41,7 +46,7 @@ const ArrowPagination = ({
       >
         <Icon className="u-rotate-left" name="chevron-down" />
       </Button>
-    </>
+    </nav>
   );
 };
 
