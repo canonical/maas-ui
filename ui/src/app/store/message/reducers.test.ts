59592d26bc472c84f1423a4bdb38cbfdfc3094e6
Huw Wilkins <huw.wilkins@canonical.com>
2021-03-30 09:56:44 +1100

    Define the reducer types. Use factories for generating messages.

--- ui/src/app/store/message/reducers.test.ts
+++ ui/src/app/store/message/reducers.test.ts
@@ -1,56 +1,64 @@
 import reducers from "./slice";
 
+import {
+  message as messageFactory,
+  messageState as messageStateFactory,
+} from "testing/factories";
+
 describe("reducers", () => {
   it("should return the initial state", () => {
-    expect(reducers(undefined, { type: "" })).toStrictEqual({
-      items: [],
-    });
+    expect(reducers(undefined, { type: "" })).toStrictEqual(
+      messageStateFactory({
+        items: [],
+      })
+    );
   });
 
   it("should correctly reduce message/add", () => {
+    const message = messageFactory({
+      message: "User added",
+    });
     expect(
       reducers(undefined, {
         type: "message/add",
-        payload: {
-          message: "User added",
-        },
+        payload: message,
       })
-    ).toStrictEqual({
-      items: [
-        {
-          message: "User added",
-        },
-      ],
-    });
+    ).toStrictEqual(
+      messageStateFactory({
+        items: [message],
+      })
+    );
   });
 
   it("should correctly reduce message/remove", () => {
     expect(
       reducers(
-        {
+        messageStateFactory({
           items: [
-            {
+            messageFactory({
               id: 99,
               message: "User added",
-            },
-            {
+            }),
+            messageFactory({
               id: 100,
               message: "User updated",
-            },
+            }),
           ],
-        },
+        }),
         {
           type: "message/remove",
           payload: 99,
         }
       )
-    ).toStrictEqual({
-      items: [
-        {
-          id: 100,
-          message: "User updated",
-        },
-      ],
-    });
+    ).toStrictEqual(
+      messageStateFactory({
+        items: [
+          messageFactory({
+            id: 100,
+            message: "User updated",
+          }),
+        ],
+      })
+    );
   });
 });
