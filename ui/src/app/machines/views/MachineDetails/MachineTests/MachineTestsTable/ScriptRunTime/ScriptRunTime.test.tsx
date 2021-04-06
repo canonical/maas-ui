435a6ee98d0f7848ab7f22da3045a60df44d1347
Huw Wilkins <huw.wilkins@canonical.com>
2021-04-06 16:21:04 +1000

    Update time format to match API responses.

--- ui/src/app/machines/views/MachineDetails/MachineTests/MachineTestsTable/ScriptRunTime/ScriptRunTime.test.tsx
+++ ui/src/app/machines/views/MachineDetails/MachineTests/MachineTestsTable/ScriptRunTime/ScriptRunTime.test.tsx
@@ -23,28 +23,28 @@ describe("ScriptRunTime", () => {
       starttime: 1617254218,
     });
     const wrapper = mount(<ScriptRunTime scriptResult={scriptResult} />);
-    expect(wrapper.text()).toBe("00:05:00");
+    expect(wrapper.text()).toBe("0:05:00");
   });
 
   it("displays the elapsed time when running and runtime is known", () => {
     const scriptResult = scriptResultFactory({
-      estimated_runtime: "00:10:00",
+      estimated_runtime: "0:10:00",
       status: ScriptResultStatus.RUNNING,
       starttime: 1617254218,
     });
     const wrapper = mount(<ScriptRunTime scriptResult={scriptResult} />);
-    expect(wrapper.text()).toBe("00:05:00 of ~00:10:00");
+    expect(wrapper.text()).toBe("0:05:00 of ~0:10:00");
   });
 
   it("displays the elapsed time when the the start time is not known", () => {
     const scriptResult = scriptResultFactory({
-      estimated_runtime: "00:10:00",
+      estimated_runtime: "0:10:00",
       status: ScriptResultStatus.RUNNING,
       // Use undefined here so that the factory does not set the start time.
       starttime: undefined,
     });
     const wrapper = mount(<ScriptRunTime scriptResult={scriptResult} />);
-    expect(wrapper.text()).toBe("00:00:00 of ~00:10:00");
+    expect(wrapper.text()).toBe("0:00:00 of ~0:10:00");
   });
 
   it("displays the elapsed and estimated times when installing and runtime is not known", () => {
@@ -54,17 +54,17 @@ describe("ScriptRunTime", () => {
       starttime: 1617254218,
     });
     const wrapper = mount(<ScriptRunTime scriptResult={scriptResult} />);
-    expect(wrapper.text()).toBe("00:05:00");
+    expect(wrapper.text()).toBe("0:05:00");
   });
 
   it("displays the elapsed and estimated times when installing and runtime is known", () => {
     const scriptResult = scriptResultFactory({
-      estimated_runtime: "00:10:00",
+      estimated_runtime: "0:10:00",
       status: ScriptResultStatus.INSTALLING,
       starttime: 1617254218,
     });
     const wrapper = mount(<ScriptRunTime scriptResult={scriptResult} />);
-    expect(wrapper.text()).toBe("00:05:00 of ~00:10:00");
+    expect(wrapper.text()).toBe("0:05:00 of ~0:10:00");
   });
 
   it("updates the elapsed time every second", () => {
@@ -74,12 +74,12 @@ describe("ScriptRunTime", () => {
       starttime: 1617254218,
     });
     const wrapper = mount(<ScriptRunTime scriptResult={scriptResult} />);
-    expect(wrapper.text()).toBe("00:05:00");
+    expect(wrapper.text()).toBe("0:05:00");
     act(() => {
       jest.advanceTimersByTime(1000);
     });
     wrapper.update();
-    expect(wrapper.text()).toBe("00:05:01");
+    expect(wrapper.text()).toBe("0:05:01");
   });
 
   it("only shows the time if less than a day has elapsed", () => {
@@ -89,7 +89,7 @@ describe("ScriptRunTime", () => {
       starttime: 1617254218,
     });
     const wrapper = mount(<ScriptRunTime scriptResult={scriptResult} />);
-    expect(wrapper.text()).toBe("00:05:00");
+    expect(wrapper.text()).toBe("0:05:00");
   });
 
   it("shows the day and time if one day has elapsed", () => {
@@ -99,7 +99,7 @@ describe("ScriptRunTime", () => {
       starttime: 1617167818,
     });
     const wrapper = mount(<ScriptRunTime scriptResult={scriptResult} />);
-    expect(wrapper.text()).toBe("1 day, 00:05:00");
+    expect(wrapper.text()).toBe("1 day, 0:05:00");
   });
 
   it("shows the days and time if more than one day has elapsed", () => {
@@ -109,24 +109,24 @@ describe("ScriptRunTime", () => {
       starttime: 1617081418,
     });
     const wrapper = mount(<ScriptRunTime scriptResult={scriptResult} />);
-    expect(wrapper.text()).toBe("2 days, 00:05:00");
+    expect(wrapper.text()).toBe("2 days, 0:05:00");
   });
 
   it("displays the estimated time when pending and runtime is known", () => {
     const scriptResult = scriptResultFactory({
-      estimated_runtime: "00:10:00",
+      estimated_runtime: "0:10:00",
       status: ScriptResultStatus.PENDING,
     });
     const wrapper = mount(<ScriptRunTime scriptResult={scriptResult} />);
-    expect(wrapper.text()).toBe("~00:10:00");
+    expect(wrapper.text()).toBe("~0:10:00");
   });
 
   it("displays the runtime for other statuses", () => {
     const scriptResult = scriptResultFactory({
-      runtime: "00:15:00",
+      runtime: "0:15:00",
       status: ScriptResultStatus.FAILED_APPLYING_NETCONF,
     });
     const wrapper = mount(<ScriptRunTime scriptResult={scriptResult} />);
-    expect(wrapper.text()).toBe("00:15:00");
+    expect(wrapper.text()).toBe("0:15:00");
   });
 });
