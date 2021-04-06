510be13b57ff580dbf2c7ea5572df13e421d47a5
Caleb Ellis <caleb.ellis@canonical.com>
2021-04-06 10:04:30 +1000

    feat(ui): add ability to select VMs in LXD VM table (#2459)
    Co-authored-by: huwshimi <huwshimi@users.noreply.github.com>

--- ui/src/app/kvm/views/KVMDetails/LxdProject/ProjectVMs/VMsActionBar/VMsActionBar.tsx
+++ ui/src/app/kvm/views/KVMDetails/LxdProject/ProjectVMs/VMsActionBar/VMsActionBar.tsx
@@ -1,21 +1,42 @@
-import { Button, Icon, SearchBox, Spinner } from "@canonical/react-components";
+import {
+  Button,
+  Icon,
+  SearchBox,
+  Spinner,
+  Tooltip,
+} from "@canonical/react-components";
+import { useSelector } from "react-redux";
 
 import ArrowPagination from "app/base/components/ArrowPagination";
 import type { SetSelectedAction } from "app/kvm/views/KVMDetails";
 import { KVMAction } from "app/kvm/views/KVMDetails";
-import type { Machine } from "app/store/machine/types";
+import machineSelectors from "app/store/machine/selectors";
+import podSelectors from "app/store/pod/selectors";
+import type { Pod } from "app/store/pod/types";
+import type { RootState } from "app/store/root/types";
 
 type Props = {
-  loading?: boolean;
+  id: Pod["id"];
   setSelectedAction: SetSelectedAction;
-  vms: Machine[];
 };
 
-const VMsActionBar = ({
-  loading = false,
-  setSelectedAction,
-  vms,
-}: Props): JSX.Element => {
+const VMsActionBar = ({ id, setSelectedAction }: Props): JSX.Element | null => {
+  const loading = useSelector(machineSelectors.loading);
+  const selectedIDs = useSelector(machineSelectors.selectedIDs);
+  const pod = useSelector((state: RootState) =>
+    podSelectors.getById(state, Number(id))
+  );
+  const vms = useSelector((state: RootState) =>
+    podSelectors.getVMs(state, pod)
+  );
+
+  if (!pod) {
+    return null;
+  }
+  const selectedPodVms = selectedIDs.filter((id) =>
+    vms.some((vm) => vm.system_id === id)
+  );
+  const vmActionsDisabled = selectedPodVms.length === 0;
   return (
     <div className="vms-action-bar">
       <div className="vms-action-bar__actions">
@@ -28,21 +49,48 @@ const VMsActionBar = ({
           <Icon name="plus" />
           <span>Compose VM</span>
         </Button>
-        <Button appearance="base" hasIcon small>
-          <Icon name="menu" />
-        </Button>
-        <Button
-          appearance="base"
-          data-test="refresh-kvm"
-          hasIcon
-          onClick={() => setSelectedAction(KVMAction.REFRESH)}
-          small
+        <Tooltip
+          message={
+            vmActionsDisabled ? "Select VMs below to perform an action." : null
+          }
         >
-          <Icon name="restart" />
-        </Button>
-        <Button appearance="base" hasIcon small>
-          <Icon name="delete" />
-        </Button>
+          <Button
+            appearance="base"
+            data-test="vm-actions"
+            disabled={vmActionsDisabled}
+            hasIcon
+            small
+          >
+            <Icon name="menu" />
+          </Button>
+        </Tooltip>
+        <span className="u-nudge-right">
+          <Button
+            appearance="base"
+            data-test="refresh-kvm"
+            hasIcon
+            onClick={() => setSelectedAction(KVMAction.REFRESH)}
+            small
+          >
+            <Icon name="restart" />
+          </Button>
+        </span>
+        <Tooltip
+          className="u-nudge-right"
+          message={
+            vmActionsDisabled ? "Select VMs below to perform an action." : null
+          }
+        >
+          <Button
+            appearance="base"
+            data-test="delete-vm"
+            disabled={vmActionsDisabled}
+            hasIcon
+            small
+          >
+            <Icon name="delete" />
+          </Button>
+        </Tooltip>
       </div>
       <div className="vms-action-bar__search">
         <SearchBox className="u-no-margin--bottom" onChange={() => null} />
--- ui/src/app/kvm/views/KVMDetails/LxdProject/ProjectVMs/VMsTable/NameColumn/NameColumn.tsx
+++ ui/src/app/kvm/views/KVMDetails/LxdProject/ProjectVMs/VMsTable/NameColumn/NameColumn.tsx
@@ -1,20 +1,29 @@
 import { Spinner } from "@canonical/react-components";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 import DoubleRow from "app/base/components/DoubleRow";
 import RowCheckbox from "app/base/components/RowCheckbox";
+import { actions as machineActions } from "app/store/machine";
 import machineSelectors from "app/store/machine/selectors";
 import type { Machine } from "app/store/machine/types";
 import type { RootState } from "app/store/root/types";
+import { generateCheckboxHandlers } from "app/utils";
 
 type Props = {
   systemId: Machine["system_id"];
 };
 
 const NameColumn = ({ systemId }: Props): JSX.Element => {
+  const dispatch = useDispatch();
   const machine = useSelector((state: RootState) =>
     machineSelectors.getById(state, systemId)
+  );
+  const selectedIDs = useSelector(machineSelectors.selectedIDs);
+  const { handleRowCheckbox } = generateCheckboxHandlers<Machine["system_id"]>(
+    (machineIDs) => {
+      dispatch(machineActions.setSelected(machineIDs));
+    }
   );
 
   if (!machine) {
@@ -25,7 +34,10 @@ const NameColumn = ({ systemId }: Props): JSX.Element => {
       data-test="name-col"
       primary={
         <RowCheckbox
-          handleRowCheckbox={() => null}
+          checked={selectedIDs.includes(machine.system_id)}
+          handleRowCheckbox={() =>
+            handleRowCheckbox(machine.system_id, selectedIDs)
+          }
           item={systemId}
           items={[]}
           inputLabel={
