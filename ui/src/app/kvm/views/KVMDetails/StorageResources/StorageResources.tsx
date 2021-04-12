50c9fb423234005f22e020617ff00200df2d82fc
Caleb Ellis <caleb.ellis@canonical.com>
2021-04-12 17:30:51 +1000

    feat(ui): add dynamic storage pool cards to LXD project summary

--- ui/src/app/kvm/views/KVMDetails/StorageResources/StorageResources.tsx
+++ ui/src/app/kvm/views/KVMDetails/StorageResources/StorageResources.tsx
@@ -1,12 +1,12 @@
 import { useSelector } from "react-redux";
 
-import PodMeter from "app/kvm/components/PodMeter";
+import StorageCards from "./StorageCards";
+import StorageMeters from "./StorageMeters";
+
 import podSelectors from "app/store/pod/selectors";
 import type { Pod } from "app/store/pod/types";
 import type { RootState } from "app/store/root/types";
 import { formatBytes } from "app/utils";
-
-export const TRUNCATION_POINT = 3;
 
 type Props = { id: Pod["id"] };
 
@@ -15,8 +15,9 @@ const StorageResources = ({ id }: Props): JSX.Element | null => {
     podSelectors.getById(state, id)
   );
   const sortedPools = useSelector((state: RootState) =>
-    podSelectors.getSortedPools(state, id)
+    podSelectors.getSortedPools(state, Number(id))
   );
+  const showCards = sortedPools.length >= 3;
 
   if (!pod) {
     return null;
@@ -27,10 +28,13 @@ const StorageResources = ({ id }: Props): JSX.Element | null => {
     pod.total.local_storage - pod.used.local_storage,
     "B"
   );
+
   return (
     <div className="storage-resources">
-      <div className="storage-resources__stats">
-        <h4 className="p-heading--small u-sv1">Storage</h4>
+      <h4 className="storage-resources__header p-heading--small u-sv1">
+        Storage
+      </h4>
+      <div className="storage-resources__content">
         <div className="storage-resources__usage">
           <div className="u-nudge-left">
             <div className="p-heading--small u-text--muted">Total</div>
@@ -47,30 +51,13 @@ const StorageResources = ({ id }: Props): JSX.Element | null => {
             </div>
           </div>
         </div>
-      </div>
-      <div className="storage-resources__pools-container">
-        {sortedPools.map((pool) => {
-          const total = formatBytes(pool.total, "B");
-          const allocated = formatBytes(pool.used, "B", {
-            convertTo: total.unit,
-          });
-          const free = formatBytes(pool.total - pool.used, "B", {
-            convertTo: total.unit,
-          });
-
-          return (
-            <div className="storage-resources__pool" key={pool.id}>
-              <div className="storage-resources__pool-name">{pool.name}</div>
-              <PodMeter
-                allocated={allocated.value}
-                className="storage-resources__pool-meter"
-                free={free.value}
-                inverted
-                unit={total.unit}
-              />
-            </div>
-          );
-        })}
+        <div className="storage-resources__pools">
+          {showCards ? (
+            <StorageCards pools={sortedPools} />
+          ) : (
+            <StorageMeters pools={sortedPools} />
+          )}
+        </div>
       </div>
     </div>
   );
