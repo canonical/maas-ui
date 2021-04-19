26755150a5538632d14657374e5a8a999423d374
Huw Wilkins <huw.wilkins@canonical.com>
2021-04-20 09:34:45 +1000

    fix(all): update controller versions
    Use the new format for controller versions.
    
    Fixes: #2547.

--- ui/src/app/store/controller/types.ts
+++ ui/src/app/store/controller/types.ts
@@ -2,6 +2,25 @@ import type { TSFixMe } from "app/base/types";
 import type { NodeActions, BaseNode, NodeType } from "app/store/types/node";
 import type { GenericState } from "app/store/types/state";
 
+export enum ControllerInstallType {
+  UNKNOWN = "",
+  SNAP = "snap",
+  DEB = "deb",
+}
+
+export type ControllerVersionInfo = {
+  origin?: string;
+  revision?: string;
+  version: string;
+};
+
+export type ControllerVersions = {
+  current: ControllerVersionInfo;
+  install_type?: ControllerInstallType;
+  snap_cohort?: string;
+  update?: ControllerVersionInfo;
+};
+
 export type ControllerActions =
   | NodeActions.DELETE
   | NodeActions.IMPORT_IMAGES
@@ -19,9 +38,7 @@ export type Controller = BaseNode & {
     | NodeType.REGION_CONTROLLER
     | NodeType.REGION_AND_RACK_CONTROLLER;
   service_ids: number[];
-  version_long: string;
-  version_short: string;
-  version: string;
+  versions: ControllerVersions | null;
 };
 
 export type ControllerState = GenericState<Controller, TSFixMe>;
