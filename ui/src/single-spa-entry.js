f0f59380ef04605453f8cb127f49027f77aff301
Huw Wilkins <huw.wilkins@canonical.com>
2021-05-10 14:35:50 +1000

    fix(ui): fix loading urls with querystrings
    Fix updating the url when the React app loads so that it doesn't remove the querystring.
    
    Fixes: #2614.

--- ui/src/single-spa-entry.js
+++ ui/src/single-spa-entry.js
@@ -4,6 +4,7 @@ import singleSpaReact from "single-spa-react";
 
 import packageJson from "../package.json";
 import rootComponent from "./index";
+import { BASENAME, REACT_BASENAME } from "@maas-ui/maas-ui-shared";
 
 const reactLifecycles = singleSpaReact({
   React,
@@ -26,10 +27,18 @@ const reactLifecycles = singleSpaReact({
 
 export const { bootstrap } = reactLifecycles;
 export const mount = (props) => {
+  // Get the full path, querystring and hash. The regex gets the first
+  // forward slash that is not a double forward slash and everything after it.
+  const matches = window.location.href.match(/(?<!\/)\/(?!\/).+/);
+  // The app should never reach this entrypoint witout the basename and react
+  // path set, but to prevent possible future problems this sets the path if
+  // there isn't one already.
+  let currentURL =
+    matches && matches.length ? matches[0] : `/${BASENAME}/${REACT_BASENAME}`;
   // When the app is mounted there needs to be a history change so that
-  // react-router updates with the new url. This is requried when navigating
+  // react-router updates with the new url. This is re-queried when navigating
   // between the new/legacy clients.
-  window.history.replaceState(null, null, window.location.pathname);
+  window.history.replaceState(null, null, currentURL);
   return reactLifecycles.mount(props);
 };
 export const { unmount } = reactLifecycles;
