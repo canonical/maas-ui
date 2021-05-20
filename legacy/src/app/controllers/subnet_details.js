da733296afad44fb0030bcae9589cc95c4ff51b9
Caleb Ellis <caleb.ellis@canonical.com>
2021-05-20 10:42:37 +1000

    feat(legacy): allow disabling boot architectures in subnet details (#2677)
    Co-authored-by: huwshimi <huwshimi@users.noreply.github.com>

--- legacy/src/app/controllers/subnet_details.js
+++ legacy/src/app/controllers/subnet_details.js
@@ -66,10 +66,18 @@ export function SubnetDetailsController(
   $scope.editStaticRoute = null;
   $scope.deleteStaticRoute = null;
   $scope.snippets = DHCPSnippetsManager.getItems();
+  $scope.knownBootArchitectures = GeneralManager.getData(
+    "known_boot_architectures"
+  );
+  $scope.newDisabledArches = [];
 
   $scope.MAP_SUBNET_ACTION = {
     name: "map_subnet",
     title: "Map subnet",
+  };
+  $scope.EDIT_BOOT_ARCHITECTURES_ACTION = {
+    name: "edit_boot_architectures",
+    title: "Edit boot architectures",
   };
   $scope.DELETE_ACTION = {
     name: "delete",
@@ -242,12 +250,32 @@ export function SubnetDetailsController(
           $scope.actionError = ManagerHelperService.parseValidationError(error);
         }
       );
+    } else if ($scope.actionOption.name === "edit_boot_architectures") {
+      const newSubnet = {
+        id: $scope.subnet.id,
+        disabled_boot_architectures: $scope.newDisabledArches.join(","),
+      };
+      SubnetsManager.update(newSubnet).then(
+        () => {
+          $scope.actionOption = null;
+          $scope.actionError = null;
+        },
+        (error) => {
+          $scope.actionError = ManagerHelperService.parseValidationError(error);
+        }
+      );
     }
   };
 
   // Called when a action is selected.
   $scope.actionChanged = function () {
     $scope.actionError = null;
+    if (
+      $scope.actionOption &&
+      $scope.actionOption.name === "edit_boot_architectures"
+    ) {
+      $scope.newDisabledArches = $scope.subnet.disabled_boot_architectures;
+    }
   };
 
   // Called when the "Cancel" button is pressed.
@@ -260,7 +288,11 @@ export function SubnetDetailsController(
   // is allowed to perform.
   $scope.updateActions = function () {
     if (UsersManager.isSuperUser()) {
-      $scope.actionOptions = [$scope.MAP_SUBNET_ACTION, $scope.DELETE_ACTION];
+      $scope.actionOptions = [
+        $scope.MAP_SUBNET_ACTION,
+        $scope.EDIT_BOOT_ARCHITECTURES_ACTION,
+        $scope.DELETE_ACTION,
+      ];
     } else {
       $scope.actionOptions = [];
     }
@@ -373,6 +405,20 @@ export function SubnetDetailsController(
     return IPAddresses.length ? true : false;
   };
 
+  $scope.bootArchitectureEnabled = (archName) => {
+    return !$scope.newDisabledArches.includes(archName);
+  };
+
+  $scope.toggleBootArchitecture = (archName) => {
+    if ($scope.bootArchitectureEnabled(archName)) {
+      $scope.newDisabledArches = [...$scope.newDisabledArches, archName];
+    } else {
+      $scope.newDisabledArches = $scope.newDisabledArches.filter(
+        (arch) => arch !== archName
+      );
+    }
+  };
+
   // Called when the subnet has been loaded.
   function subnetLoaded(subnet) {
     $scope.subnet = subnet;
@@ -439,6 +485,9 @@ export function SubnetDetailsController(
     StaticRoutesManager,
     DHCPSnippetsManager,
   ]).then(function () {
+    if (!GeneralManager.isDataLoaded("known_boot_architectures")) {
+      GeneralManager.loadItems(["known_boot_architectures"]);
+    }
     $scope.updateActions();
     $scope.active_discovery_data = ConfigsManager.getItemFromList(
       "active_discovery_interval"
