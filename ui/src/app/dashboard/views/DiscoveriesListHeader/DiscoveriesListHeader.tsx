81ac0be197f27f9dd9a76251d932e09cb24a2689
Steve Rydz <steverydz@gmail.com>
2021-06-07 11:48:06 +0100

    Add basic dashboard header

---
+++ ui/src/app/dashboard/views/DiscoveriesListHeader/DiscoveriesListHeader.tsx
@@ -0,0 +1,7 @@
+import SectionHeader from "app/base/components/SectionHeader";
+
+const DiscoveriesListHeader = (): JSX.Element => {
+  return <SectionHeader title="Network discovery" />;
+};
+
+export default DiscoveriesListHeader;
