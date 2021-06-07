4c14407cda94cfca14760d1d9742a363e1995796
Beth Collins <beth.collins@canonical.com>
2021-06-07 15:13:43 +0100

    Add zones list header (#2737)

---
+++ ui/src/app/zones/views/ZonesListHeader/ZonesListHeader.tsx
@@ -0,0 +1,9 @@
+import ZonesListTitle from "../ZonesListTitle";
+
+import SectionHeader from "app/base/components/SectionHeader";
+
+const ZonesListHeader = (): JSX.Element => {
+  return <SectionHeader title={<ZonesListTitle />}></SectionHeader>;
+};
+
+export default ZonesListHeader;
