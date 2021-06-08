a739f16b5ba87d7e26d5e002341683b56cd1c102
Bartek Szopka <bartek.szopka@canonical.com>
2021-06-08 17:28:38 +0200

    Add sorting to domains table

--- ui/src/app/domains/views/DomainsList/DomainsTable/DomainsTable.tsx
+++ ui/src/app/domains/views/DomainsList/DomainsTable/DomainsTable.tsx
@@ -7,19 +7,41 @@ const DomainsTable = (): JSX.Element => {
   const domains = useSelector(domainSelectors.all);
 
   const headers = [
-    { content: "Domain" },
-    { content: "Authoriatative" },
-    { content: "Hosts", className: "u-align--right" },
-    { content: "Total records", className: "u-align--right" },
-    { content: "Actions", className: "u-align--right" },
+    {
+      content: "Domain",
+      sortKey: "name",
+      "data-test": "domain-name-header",
+    },
+    {
+      content: "Authoriatative",
+      sortKey: "authoritative",
+    },
+    {
+      content: "Hosts",
+      sortKey: "hosts",
+      className: "u-align--right",
+    },
+    {
+      content: "Total records",
+      sortKey: "records",
+      className: "u-align--right",
+    },
+    {
+      content: "Actions",
+      className: "u-align--right",
+    },
   ];
+
   const rows = domains.map((domain) => {
     return {
-      key: domain.id,
+      // making sure we don't pass id directly as a key because of
+      // https://github.com/canonical-web-and-design/react-components/issues/476
+      key: `domain-row-${domain.id}`,
       className: "p-table__row",
       columns: [
         {
           content: domain.name,
+          "data-test": "domain-name",
         },
         {
           content: domain.authoritative ? "Yes" : "No",
@@ -37,15 +59,24 @@ const DomainsTable = (): JSX.Element => {
           className: "u-align--right",
         },
       ],
+      sortData: {
+        name: domain.name,
+        authoritative: domain.authoritative,
+        hosts: domain.hosts,
+        records: domain.resource_count,
+      },
     };
   });
 
   return (
     <MainTable
-      headers={headers}
-      paginate={50}
-      rows={rows}
       data-test="domains-table"
+      headers={headers}
+      rows={rows}
+      paginate={50}
+      sortable
+      defaultSort="name"
+      defaultSortDirection="ascending"
     />
   );
 };
