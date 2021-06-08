0da3553e886a1b913c07947a7998855145f07032
Steve Rydz <steverydz@gmail.com>
2021-06-08 14:58:06 +0100

    Add dashboard configuration explanation text

--- ui/src/app/dashboard/views/DashboardConfigurationForm/DashboardConfigurationForm.tsx
+++ ui/src/app/dashboard/views/DashboardConfigurationForm/DashboardConfigurationForm.tsx
@@ -1,3 +1,5 @@
+import { Col, Row } from "@canonical/react-components";
+
 import DiscoveriesListHeader from "../DiscoveriesListHeader";
 
 import Section from "app/base/components/Section";
@@ -11,7 +13,29 @@ const DashboardConfigurationForm = (): JSX.Element => {
       header={<DiscoveriesListHeader />}
       headerClassName="u-no-padding--bottom"
     >
-      <h1>Hello</h1>
+      <Row>
+        <Col size={6}>Form</Col>
+        <Col size={6}>
+          <p>
+            <small>
+              When discovery is enabled, MAAS will use passive techniques (such
+              as listening to ARP requests and mDNS advertisements) to observe
+              networks attached to rack controllers. Active discovery (subnet
+              mapping) can be enabled below on the configured subnets. Each rack
+              will scan the subnets that allow it. This helps ensure discovery
+              information is accurate and complete.
+            </small>
+          </p>
+          <p>
+            <a
+              className="p-link--external p-text--small"
+              href="https://maas.io/docs/network-discovery"
+            >
+              About network discovery
+            </a>
+          </p>
+        </Col>
+      </Row>
     </Section>
   );
 };
