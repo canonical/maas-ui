5ff26c75fc1b1e625d0a8ace650537fc1a72b002
Steve Rydz <steverydz@gmail.com>
2021-06-08 14:38:08 +0100

    Add dashboard tabbed navigation

--- ui/src/app/dashboard/views/DiscoveriesListHeader/DiscoveriesListHeader.tsx
+++ ui/src/app/dashboard/views/DiscoveriesListHeader/DiscoveriesListHeader.tsx
@@ -1,7 +1,30 @@
+import { Link, useLocation } from "react-router-dom";
+
 import SectionHeader from "app/base/components/SectionHeader";
+import dashboardURLs from "app/dashboard/urls";
 
 const DiscoveriesListHeader = (): JSX.Element => {
-  return <SectionHeader title="Network discovery" />;
+  const location = useLocation();
+
+  return (
+    <SectionHeader
+      title="Network discovery"
+      tabLinks={[
+        {
+          active: location.pathname === dashboardURLs.index,
+          component: Link,
+          label: "Discoveries",
+          to: dashboardURLs.index,
+        },
+        {
+          active: location.pathname === dashboardURLs.configuration,
+          component: Link,
+          label: "Configuration",
+          to: dashboardURLs.configuration,
+        },
+      ]}
+    />
+  );
 };
 
 export default DiscoveriesListHeader;
