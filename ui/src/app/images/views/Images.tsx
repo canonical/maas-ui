dbedc821c85f568d4b2b566260d4d0072cc450e3
Huw Wilkins <huw.wilkins@canonical.com>
2021-06-08 12:11:02 +1000

    feat(ui): add Images view and route components
    Add the components for the Images view and routes.
    
    Fixes: canonical-web-and-design/app-squad#77.

---
+++ ui/src/app/images/views/Images.tsx
@@ -0,0 +1,20 @@
+import { Route, Switch } from "react-router-dom";
+
+import NotFound from "app/base/views/NotFound";
+import imagesURLs from "app/images/urls";
+import ImageList from "app/images/views/ImageList";
+
+const Images = (): JSX.Element => {
+  return (
+    <Switch>
+      <Route exact path={[imagesURLs.index]}>
+        <ImageList />
+      </Route>
+      <Route path="*">
+        <NotFound />
+      </Route>
+    </Switch>
+  );
+};
+
+export default Images;
