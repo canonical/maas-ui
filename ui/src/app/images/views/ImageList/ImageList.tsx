6b23127cc09a1ec7df736649160ea64e83bf4d2d
Huw Wilkins <huw.wilkins@canonical.com>
2021-06-09 16:03:06 +1000

    feat(ui): add the images header
    Add the basic images header component and polling selector.
    
    Fixes: canonical-web-and-design/app-squad#78.

--- ui/src/app/images/views/ImageList/ImageList.tsx
+++ ui/src/app/images/views/ImageList/ImageList.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 
 import { useDispatch } from "react-redux";
 
+import ImageListHeader from "./ImageListHeader";
+
 import Section from "app/base/components/Section";
 import { useWindowTitle } from "app/base/hooks";
 import { actions as bootResourceActions } from "app/store/bootresource";
@@ -15,7 +17,10 @@ const ImagesList = (): JSX.Element => {
   }, [dispatch]);
 
   return (
-    <Section header="Images" headerClassName="u-no-padding--bottom">
+    <Section
+      header={<ImageListHeader />}
+      headerClassName="u-no-padding--bottom"
+    >
       Images
     </Section>
   );
