2ea2b24a1c5953972d912a5d6ec87f09e2da1740
Beth Collins <bethcollinsliv92@gmail.com>
2021-06-10 14:46:48 +0100

    Add header to zone details with dummy button

--- ui/src/app/zones/views/ZoneDetails/ZoneDetails.tsx
+++ ui/src/app/zones/views/ZoneDetails/ZoneDetails.tsx
@@ -1,7 +1,19 @@
+import { useParams } from "react-router";
+
+import ZoneDetailsHeader from "./ZoneDetailsHeader";
+
 import Section from "app/base/components/Section";
+import type { RouteParams } from "app/base/types";
 
 const ZoneDetails = (): JSX.Element => {
-  return <Section header="Zone details" />;
+  const { id } = useParams<RouteParams>();
+
+  return (
+    <Section
+      header={<ZoneDetailsHeader id={parseInt(id)} />}
+      headerClassName="u-no-padding--bottom"
+    />
+  );
 };
 
 export default ZoneDetails;
