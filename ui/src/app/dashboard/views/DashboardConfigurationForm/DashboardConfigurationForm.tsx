10077ddb0a19e5f8b9a417ae67ddf943317a6bc4
petesfrench <peterfrench94@gmail.com>
2021-06-10 15:03:00 +0200

    Removes unused code

--- ui/src/app/dashboard/views/DashboardConfigurationForm/DashboardConfigurationForm.tsx
+++ ui/src/app/dashboard/views/DashboardConfigurationForm/DashboardConfigurationForm.tsx
@@ -1,26 +1,11 @@
-import { useEffect } from "react";
-
-import { useSelector, useDispatch } from "react-redux";
-
 import DiscoveriesListHeader from "../DiscoveriesListHeader";
 
 import Section from "app/base/components/Section";
 import { useWindowTitle } from "app/base/hooks";
 import NetworkDiscoveryForm from "app/settings/views/Network/NetworkDiscoveryForm";
-import { actions } from "app/store/discovery";
-import discoverySelectors from "app/store/discovery/selectors";
 
 const DashboardConfigurationForm = (): JSX.Element => {
   useWindowTitle("Dashboard");
-
-  const discoveries = useSelector(discoverySelectors.all);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(actions.fetch());
-  }, [dispatch]);
-
-  console.log("discoveries", discoveries);
 
   return (
     <Section
