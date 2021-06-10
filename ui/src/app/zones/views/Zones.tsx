60af79d875c0c148a89d887f96ce6d8701295a4c
Beth Collins <beth.collins@canonical.com>
2021-06-10 10:21:48 +0100

    add zone detail route and basic component (#2761)

--- ui/src/app/zones/views/Zones.tsx
+++ ui/src/app/zones/views/Zones.tsx
@@ -2,6 +2,7 @@ import { Route, Switch } from "react-router-dom";
 
 import NotFound from "app/base/views/NotFound";
 import zonesURLs from "app/zones/urls";
+import ZoneDetails from "app/zones/views/ZoneDetails";
 import ZonesList from "app/zones/views/ZonesList";
 
 const Zones = (): JSX.Element => {
@@ -9,6 +10,9 @@ const Zones = (): JSX.Element => {
     <Switch>
       <Route exact path={[zonesURLs.index]}>
         <ZonesList />
+      </Route>
+      <Route exact path={zonesURLs.details(null, true)}>
+        <ZoneDetails />
       </Route>
       <Route path="*">
         <NotFound />
