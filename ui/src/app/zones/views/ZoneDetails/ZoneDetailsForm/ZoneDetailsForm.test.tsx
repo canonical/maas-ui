de39d0a4dc8cfd719b64a5330e2c3438b0d6c875
Scott Mason Nash <sowasred2012@gmail.com>
2021-06-11 10:31:05 +0100

    add zone details form tests
    Co-authored-by: Beth Collins <bethcollinsliv92@gmail.com>

---
+++ ui/src/app/zones/views/ZoneDetails/ZoneDetailsForm/ZoneDetailsForm.test.tsx
@@ -0,0 +1,81 @@
+import { mount } from "enzyme";
+import { act } from "react-dom/test-utils";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
+import ZoneDetailsForm from "./ZoneDetailsForm";
+
+import type { RootState } from "app/store/root/types";
+import {
+  zone as zoneFactory,
+  zoneState as zoneStateFactory,
+  rootState as rootStateFactory,
+} from "testing/factories";
+
+const mockStore = configureStore();
+
+describe("ZoneDetailsForm", () => {
+  const testZone = {
+    desc: "test description",
+    id: 1,
+    name: "zone-name",
+  };
+  let initialState: RootState;
+
+  beforeEach(() => {
+    initialState = rootStateFactory({
+      zone: zoneStateFactory({
+        errors: {},
+        loading: false,
+        loaded: true,
+        items: [zoneFactory(testZone)],
+      }),
+    });
+  });
+
+  it("runs closeForm function when the cancel button is clicked", () => {
+    const closeForm = jest.fn();
+    const store = mockStore(initialState);
+    const wrapper = mount(
+      <Provider store={store}>
+        <ZoneDetailsForm id={testZone.id} closeForm={closeForm} />
+      </Provider>
+    );
+
+    wrapper.find("button[data-test='cancel-action']").simulate("click");
+    expect(closeForm).toHaveBeenCalled();
+  });
+
+  it("calls actions.update on save click", () => {
+    const store = mockStore(initialState);
+    const wrapper = mount(
+      <Provider store={store}>
+        <ZoneDetailsForm id={testZone.id} closeForm={jest.fn()} />
+      </Provider>
+    );
+    act(() =>
+      wrapper.find("Formik").props().onSubmit({
+        id: testZone.id,
+        description: testZone.desc,
+        name: testZone.name,
+      })
+    );
+
+    expect(
+      store.getActions().find((action) => action.type === "zone/update")
+    ).toStrictEqual({
+      type: "zone/update",
+      meta: {
+        method: "update",
+        model: "zone",
+      },
+      payload: {
+        params: {
+          id: testZone.id,
+          description: testZone.desc,
+          name: testZone.name,
+        },
+      },
+    });
+  });
+});
