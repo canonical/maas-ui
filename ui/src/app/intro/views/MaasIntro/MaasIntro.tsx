0fd0523579ea08e6fcb408f9216a23653a197a0a
Caleb Ellis <caleb.ellis@canonical.com>
2021-06-14 17:53:43 +1000

    feat(ui): set up routes for intro pages

---
+++ ui/src/app/intro/views/MaasIntro/MaasIntro.tsx
@@ -0,0 +1,11 @@
+import Section from "app/base/components/Section";
+
+const MaasIntro = (): JSX.Element => {
+  return (
+    <Section header="Welcome to MAAS">
+      <div>MAAS Intro</div>
+    </Section>
+  );
+};
+
+export default MaasIntro;
