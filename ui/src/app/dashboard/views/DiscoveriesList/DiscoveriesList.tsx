da6f42038f5a6f6433696df517fe9a648c16918e
Scott Mason Nash <sowasred2012@gmail.com>
2021-06-15 13:42:14 +0100

    use MainTable in DiscoveriesList

--- ui/src/app/dashboard/views/DiscoveriesList/DiscoveriesList.tsx
+++ ui/src/app/dashboard/views/DiscoveriesList/DiscoveriesList.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 
+import { MainTable, Tooltip, Icon } from "@canonical/react-components";
 import { useSelector, useDispatch } from "react-redux";
 
 import DiscoveriesListHeader from "../DiscoveriesListHeader";
@@ -19,56 +20,113 @@ const DiscoveriesList = (): JSX.Element => {
     dispatch(actions.fetch());
   }, [dispatch]);
 
+  const headers = [
+    {
+      content: "Name",
+      sortKey: "hostname",
+      className: "p-table--network-discoveries__name",
+    },
+    {
+      content: "Mac address",
+      sortKey: "mac_address",
+      className: "p-table--network-discoveries__mac",
+    },
+    {
+      content: "IP",
+      sortKey: "ip",
+      className: "p-table--network-discoveries__ip",
+    },
+    {
+      content: "Rack",
+      sortKey: "rack",
+      className: "p-table--network-discoveries__rack",
+    },
+    {
+      content: "Last seen",
+      sortKey: "last_seen",
+      className: "p-table--network-discoveries__last-seen",
+    },
+    {
+      content: "Action",
+      className: "p-table--network-discoveries__chevron u-align--right",
+    },
+  ];
+  const rows = discoveries.map((discovery) => {
+    return {
+      key: discovery.id,
+      className: "p-table__row",
+      columns: [
+        {
+          content: (
+            <>
+              {discovery.hostname || "Unknown"}
+              {discovery.is_external_dhcp ? (
+                <Tooltip
+                  message="This device is providing DHCP"
+                  className="u-nudge-right"
+                  position="top-center"
+                >
+                  <Icon name="information" />
+                </Tooltip>
+              ) : null}
+            </>
+          ),
+          className: "p-table--network-discoveries__name",
+        },
+        {
+          content: (
+            <>
+              <div className="u-truncate">
+                {discovery.mac_address || "Unknown"}
+              </div>
+              <div className="u-truncate">
+                <small>{discovery.mac_organization}</small>
+              </div>
+            </>
+          ),
+          className: "p-table--network-discoveries__mac",
+        },
+        {
+          content: discovery.ip,
+          className: "p-table--network-discoveries__ip",
+        },
+        {
+          content: discovery.observer_hostname,
+          className: "p-table--network-discoveries__rack",
+        },
+        {
+          content: discovery.last_seen,
+          className: "p-table--network-discoveries__last-seen",
+        },
+        {
+          content: "",
+          className: "p-table--network-discoveries__chevron",
+        },
+      ],
+      sortData: {
+        hostname: discovery.hostname,
+        mac_address: discovery.mac_address,
+        ip: discovery.ip,
+        rack: discovery.observer_hostname,
+        last_seen: discovery.last_seen,
+      },
+    };
+  });
+
   return (
     <Section
       header={<DiscoveriesListHeader />}
       headerClassName="u-no-padding--bottom"
     >
-      <table>
-        <thead>
-          <tr>
-            <th style={{ width: "20%" }}>Name</th>
-            <th style={{ width: "15%" }}>Mac address</th>
-            <th style={{ width: "15%" }}>IP</th>
-            <th style={{ width: "15%" }}>Rack</th>
-            <th>Last seen</th>
-            <th className="u-align--right" style={{ width: "75px" }}>
-              Action
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {discoveries
-            ? discoveries.map((discovery) => (
-                <tr key={`discovery-${discovery.id}`}>
-                  <td style={{ overflow: "visible" }}>
-                    {discovery.hostname || "Unknown"}
-                    {discovery.is_external_dhcp ? (
-                      <div className="p-tooltip--top-center">
-                        <i className="p-icon--information ng-hide"></i>
-                        <span className="p-tooltip__message" role="tooltip">
-                          This device is providing DHCP
-                        </span>
-                      </div>
-                    ) : null}
-                  </td>
-                  <td>
-                    <div className="u-truncate">
-                      {discovery.mac_address || "Unknown"}
-                    </div>
-                    <div className="u-truncate">
-                      <small>{discovery.mac_organization}</small>
-                    </div>
-                  </td>
-                  <td className="u-truncate">{discovery.ip}</td>
-                  <td className="u-truncate">{discovery.observer_hostname}</td>
-                  <td className="u-truncate">{discovery.last_seen}</td>
-                  <td className="u-truncate">&nbsp;</td>
-                </tr>
-              ))
-            : null}
-        </tbody>
-      </table>
+      <MainTable
+        className="p-table--network-discoveries"
+        data-test="discoveries-table"
+        defaultSort="last_seen"
+        defaultSortDirection="ascending"
+        headers={headers}
+        rows={rows}
+        sortable
+      />
     </Section>
   );
 };
