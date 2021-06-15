56f9cef9f43f499b2c41c3d0ddee354c28fa0c24
Bartek Szopka <bartek.szopka@canonical.com>
2021-06-15 15:34:49 +0200

    Add links to resources in domain details table

--- ui/src/app/domains/views/DomainDetails/ResourceRecords/ResourceRecords.tsx
+++ ui/src/app/domains/views/DomainDetails/ResourceRecords/ResourceRecords.tsx
@@ -1,9 +1,14 @@
 import { Col, MainTable, Row, Strip } from "@canonical/react-components";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
+import LegacyLink from "app/base/components/LegacyLink";
+import baseURLs from "app/base/urls";
+import machineURLs from "app/machines/urls";
 import domainsSelectors from "app/store/domain/selectors";
 import type { Domain } from "app/store/domain/types";
 import type { RootState } from "app/store/root/types";
+import { NodeType } from "app/store/types/node";
 
 type Props = {
   id: Domain["id"];
@@ -33,20 +38,52 @@ const ResourceRecords = ({ id }: Props): JSX.Element | null => {
     },
     {
       content: "Actions",
+      className: "u-align--right",
     },
   ];
 
   const rows = domain.rrsets.map((resource) => {
+    const nodeType: NodeType = resource.node_type;
+    let nameCell: JSX.Element;
+
+    switch (nodeType) {
+      case NodeType.MACHINE:
+        nameCell = (
+          <Link to={machineURLs.machine.index({ id: resource.system_id })}>
+            {resource.name}
+          </Link>
+        );
+        break;
+      case NodeType.DEVICE:
+        nameCell = (
+          <LegacyLink route={baseURLs.device({ id: resource.system_id })}>
+            {resource.name}
+          </LegacyLink>
+        );
+        break;
+      case NodeType.RACK_CONTROLLER:
+      case NodeType.REGION_CONTROLLER:
+      case NodeType.REGION_AND_RACK_CONTROLLER:
+        nameCell = (
+          <LegacyLink route={baseURLs.controller({ id: resource.system_id })}>
+            {resource.name}
+          </LegacyLink>
+        );
+        break;
+      default:
+        nameCell = <>{resource.name}</>;
+    }
+
     return {
       columns: [
         {
-          content: resource.name,
+          content: nameCell,
         },
         {
           content: resource.rrtype,
         },
         {
-          content: resource.ttl,
+          content: resource.ttl || "(default)",
         },
         {
           content: resource.rrdata,
