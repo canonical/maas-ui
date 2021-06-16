605e30f6df0885abcd6ee68af2f52ae5bf0ac623
Bartek Szopka <bartek.szopka@canonical.com>
2021-06-16 09:46:00 +0200

    Improve types for domain resource records

--- ui/src/app/store/domain/types/base.ts
+++ ui/src/app/store/domain/types/base.ts
@@ -1,17 +1,30 @@
 import type { TSFixMe } from "app/base/types";
 import type { Model } from "app/store/types/model";
+import type { BaseNode, NodeType } from "app/store/types/node";
 import type { GenericState } from "app/store/types/state";
+import type { User } from "app/store/user/types";
+
+export enum RecordType {
+  A = "A",
+  AAAA = "AAAA",
+  CNAME = "CNAME",
+  MX = "MX",
+  NS = "NS",
+  SRV = "SRV",
+  SSHPF = "SSHFP",
+  TXT = "TXT",
+}
 
 export type DomainResource = {
-  name: string;
-  system_id?: string;
-  node_type?: number; // other type?
-  user_id?: number;
-  dnsresource_id?: number;
-  ttl?: number;
-  rrtype?: string;
-  rrdata?: string;
-  dnsdata_id?: number;
+  name: string | null;
+  system_id: BaseNode["system_id"] | null;
+  node_type: NodeType | null;
+  user_id: User["id"] | null;
+  dnsresource_id: number | null;
+  ttl: number | null;
+  rrtype: RecordType;
+  rrdata: string | null;
+  dnsdata_id: number | null;
 };
 
 export type Domain = Model & {
