d6e67c000b5d92e4cbee2b758aa09665e07ba53e
Caleb Ellis <caleb.ellis@canonical.com>
2021-06-17 19:16:22 +1000

    test(cypress): fix zones Cypress tests but for real this time

--- integration/cypress/integration/zones/list.spec.ts
+++ integration/cypress/integration/zones/list.spec.ts
@@ -1,12 +1,12 @@
-import { generateLegacyURL } from "@maas-ui/maas-ui-shared";
+import { generateNewURL } from "@maas-ui/maas-ui-shared";
 
 import { login } from "../utils";
 
-context("AZs", () => {
+context("Zones", () => {
   beforeEach(() => {
     login();
     cy.setCookie("skipintro", "true");
-    cy.visit(generateLegacyURL("/zones"));
+    cy.visit(generateNewURL("/zones"));
   });
 
   afterEach(() => {
@@ -14,14 +14,14 @@ context("AZs", () => {
   });
 
   it("renders the correct heading", () => {
-    cy.get(".page-header__title").contains("Availability zones");
+    cy.get("[data-test='section-header-title']").contains("Availability zones");
   });
 
   it("highlights the correct navigation link", () => {
     cy.get(".p-navigation__link.is-selected a").should(
       "have.attr",
       "href",
-      generateLegacyURL("/zones")
+      generateNewURL("/zones")
     );
   });
 });
