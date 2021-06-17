3767bd05f0a4d0c753b02c5be878cab94930e1e3
petesfrench <peterfrench94@gmail.com>
2021-06-17 09:17:23 +0200

    Added links to subnets

--- ui/src/app/dashboard/views/DashboardConfigurationSubnetForm/DashboardConfigurationSubnetForm.tsx
+++ ui/src/app/dashboard/views/DashboardConfigurationSubnetForm/DashboardConfigurationSubnetForm.tsx
@@ -5,13 +5,15 @@ import { useDispatch, useSelector } from "react-redux";
 
 import FormikField from "app/base/components/FormikField";
 import FormikForm from "app/base/components/FormikForm";
-import { actions } from "app/store/discovery";
+import { actions as actionsFabric } from "app/store/fabric";
+import fabricSelectors from "app/store/fabric/selectors";
 import { actions as actionsSubnets } from "app/store/subnet";
 import subnetSelectors from "app/store/subnet/selectors";
 
 const DashboardConfigurationSubnetForm = (): JSX.Element => {
   const dispatch = useDispatch();
   const subnets = useSelector(subnetSelectors.all);
+  const fabrics = useSelector(fabricSelectors.all);
 
   const loaded = useSelector(subnetSelectors.loaded);
   const loading = useSelector(subnetSelectors.loading);
@@ -22,8 +24,8 @@ const DashboardConfigurationSubnetForm = (): JSX.Element => {
   subnets.forEach((subnet) => (values[subnet.id] = subnet.active_discovery));
 
   useEffect(() => {
-    dispatch(actions.fetch());
     dispatch(actionsSubnets.fetch());
+    dispatch(actionsFabric.fetch());
   }, [dispatch]);
 
   if (!loaded) return <Spinner />;
@@ -54,14 +56,25 @@ const DashboardConfigurationSubnetForm = (): JSX.Element => {
           saving={saving}
           saved={saved}
         >
-          {subnets.map((subnet) => (
-            <FormikField
-              type="checkbox"
-              label={`${subnet.cidr} on ${subnet.cidr}`}
-              name={String(subnet.id)}
-              key={subnet.cidr}
-            />
-          ))}
+          {subnets.map((subnet) => {
+            const targetFabric = fabrics.find((fabric) => {
+              return fabric.vlan_ids.includes(subnet.vlan);
+            });
+            return (
+              <FormikField
+                type="checkbox"
+                label={[
+                  <a href={`/MAAS/l/subnet/${subnet.id}`}>{subnet.cidr}</a>,
+                  ` on `,
+                  <a href={`/MAAS/l/fabric/${targetFabric.id}`}>
+                    {targetFabric.name}
+                  </a>,
+                ]}
+                name={String(subnet.id)}
+                key={subnet.cidr}
+              />
+            );
+          })}
         </FormikForm>
       )}
     </>
