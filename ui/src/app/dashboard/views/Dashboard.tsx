dfee909bcaf75d4e3e73329519d5a9462552d93f
Huw Wilkins <huw.wilkins@canonical.com>
2021-06-22 13:17:15 +1000

    feat(ui): disabled discovery warning
    Show a notification when discovery is disabled.
    
    Fixes: canonical-web-and-design/app-squad#128.

--- ui/src/app/dashboard/views/Dashboard.tsx
+++ ui/src/app/dashboard/views/Dashboard.tsx
@@ -1,3 +1,5 @@
+import { Notification } from "@canonical/react-components";
+import { useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 
 import DashboardConfigurationForm from "./DashboardConfigurationForm";
@@ -7,13 +9,20 @@ import DiscoveriesList from "./DiscoveriesList";
 import Section from "app/base/components/Section";
 import NotFound from "app/base/views/NotFound";
 import dashboardURLs from "app/dashboard/urls";
+import configSelectors from "app/store/config/selectors";
 
 const Dashboard = (): JSX.Element => {
+  const networkDiscovery = useSelector(configSelectors.networkDiscovery);
   return (
     <Section
       header={<DashboardHeader />}
       headerClassName="u-no-padding--bottom"
     >
+      {networkDiscovery === "disabled" && (
+        <Notification data-test="disabled-notification" type="caution">
+          List of devices will not update as discovery is turned off.
+        </Notification>
+      )}
       <Switch>
         <Route exact path={[dashboardURLs.index]}>
           <DiscoveriesList />
