66478f89551b4fb9ad12f374ecb01e5c891aa3ac
Huw Wilkins <huw.wilkins@canonical.com>
2021-06-22 15:06:36 +1000

    feat(ui): show discovery count
    Add the discovery count to the dashboard tab
    
    Fixes: canonical-web-and-design/app-squad#131.

---
+++ ui/src/app/dashboard/views/DashboardHeader/DashboardHeader.test.tsx
@@ -0,0 +1,53 @@
+import { mount } from "enzyme";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import configureStore from "redux-mock-store";
+
+import DashboardHeader from "./DashboardHeader";
+
+import dashboardURLs from "app/dashboard/urls";
+import type { RootState } from "app/store/root/types";
+import {
+  discovery as discoveryFactory,
+  discoveryState as discoveryStateFactory,
+  rootState as rootStateFactory,
+} from "testing/factories";
+
+const mockStore = configureStore();
+
+describe("DashboardHeader", () => {
+  let state: RootState;
+
+  beforeEach(() => {
+    state = rootStateFactory({
+      discovery: discoveryStateFactory({
+        loaded: true,
+        items: [
+          discoveryFactory({
+            hostname: "my-discovery-test",
+          }),
+          discoveryFactory({
+            hostname: "another-test",
+          }),
+        ],
+      }),
+    });
+  });
+
+  it("displays the discovery count in the header", () => {
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter
+          initialEntries={[{ pathname: "/dashboard", key: "testKey" }]}
+        >
+          <DashboardHeader />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(wrapper.find(`[to='${dashboardURLs.index}']`).text()).toBe(
+      "2 discoveries"
+    );
+  });
+});
