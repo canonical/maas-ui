ad23c94ad458b01a745dbf2bc7b6abd621148545
Caleb Ellis <caleb.ellis@canonical.com>
2021-07-01 15:31:37 +1000

    feat(ui): build generated images table (#2849)

---
+++ ui/src/app/images/views/ImageList/GeneratedImages/GeneratedImages.test.tsx
@@ -0,0 +1,88 @@
+import { mount } from "enzyme";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
+import GeneratedImages from "./GeneratedImages";
+
+import { BootResourceType } from "app/store/bootresource/types";
+import {
+  bootResource as bootResourceFactory,
+  bootResourceState as bootResourceStateFactory,
+  rootState as rootStateFactory,
+} from "testing/factories";
+
+const mockStore = configureStore();
+
+describe("GeneratedImages", () => {
+  it("does not render if there are no generated resources", () => {
+    const state = rootStateFactory({
+      bootresource: bootResourceStateFactory({
+        resources: [
+          bootResourceFactory({ rtype: BootResourceType.SYNCED }),
+          bootResourceFactory({ rtype: BootResourceType.UPLOADED }),
+        ],
+      }),
+    });
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <GeneratedImages />
+      </Provider>
+    );
+    expect(wrapper.find("ImagesTable").exists()).toBe(false);
+  });
+
+  it("correctly sets images values based on generated resources", () => {
+    const resources = [
+      bootResourceFactory({
+        arch: "amd64",
+        name: "esxi/7.0",
+        rtype: BootResourceType.UPLOADED,
+        title: "VMWare ESXi 7.0",
+      }),
+      bootResourceFactory({
+        arch: "arm64",
+        name: "windows/win2012hvr2",
+        rtype: BootResourceType.GENERATED,
+        title: "Windows 2012",
+      }),
+      bootResourceFactory({
+        arch: "i386",
+        name: "centos/centos70",
+        rtype: BootResourceType.GENERATED,
+        title: "CentOS 7",
+      }),
+      bootResourceFactory({
+        arch: "ppc64el",
+        name: "ubuntu/focal",
+        rtype: BootResourceType.SYNCED,
+        title: "20.04 LTS",
+      }),
+    ];
+    const state = rootStateFactory({
+      bootresource: bootResourceStateFactory({
+        resources,
+      }),
+    });
+    const store = mockStore(state);
+    const wrapper = mount(
+      <Provider store={store}>
+        <GeneratedImages />
+      </Provider>
+    );
+    expect(wrapper.find("ImagesTable").prop("images")).toStrictEqual([
+      {
+        arch: "arm64",
+        os: "windows",
+        release: "win2012hvr2",
+        title: "Windows 2012",
+      },
+      {
+        arch: "i386",
+        os: "centos",
+        release: "centos70",
+        title: "CentOS 7",
+      },
+    ]);
+  });
+});
