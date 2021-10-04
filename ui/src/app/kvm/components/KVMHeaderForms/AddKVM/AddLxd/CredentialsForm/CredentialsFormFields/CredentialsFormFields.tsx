ae1442e20bb540472fdb3e9ae1b06afae47b64a5
Caleb Ellis <caleb.ellis@canonical.com>
2021-10-05 09:26:32 +1000

    feat(ui): use datalist for LXD address select (#3084)

--- ui/src/app/kvm/components/KVMHeaderForms/AddKVM/AddLxd/CredentialsForm/CredentialsFormFields/CredentialsFormFields.tsx
+++ ui/src/app/kvm/components/KVMHeaderForms/AddKVM/AddLxd/CredentialsForm/CredentialsFormFields/CredentialsFormFields.tsx
@@ -1,5 +1,6 @@
 import { Col, Row } from "@canonical/react-components";
 import { useFormikContext } from "formik";
+import { useSelector } from "react-redux";
 
 import type { CredentialsFormValues } from "../../types";
 
@@ -10,6 +11,7 @@ import ZoneSelect from "app/base/components/ZoneSelect";
 import type { SetKvmType } from "app/kvm/components/KVMHeaderForms/AddKVM";
 import KvmTypeSelect from "app/kvm/components/KVMHeaderForms/AddKVM/KvmTypeSelect";
 import { PodType } from "app/store/pod/constants";
+import podSelectors from "app/store/pod/selectors";
 
 type Props = {
   setKvmType: SetKvmType;
@@ -22,6 +24,9 @@ export const CredentialsFormFields = ({
   setShouldGenerateCert,
   shouldGenerateCert,
 }: Props): JSX.Element => {
+  const lxdAddresses = useSelector(podSelectors.groupByLxdServer).map(
+    (group) => group.address
+  );
   const { setFieldValue } = useFormikContext<CredentialsFormValues>();
 
   return (
@@ -34,11 +39,20 @@ export const CredentialsFormFields = ({
         <ZoneSelect name="zone" required valueKey="id" />
         <ResourcePoolSelect name="pool" required valueKey="id" />
         <FormikField
+          autoComplete="off"
           label="LXD address"
+          list="lxd-addresses"
           name="power_address"
           required
           type="text"
         />
+        <datalist id="lxd-addresses">
+          {lxdAddresses.map((address) => (
+            <option key={address} value={address}>
+              {address}
+            </option>
+          ))}
+        </datalist>
         <CertificateFields
           onShouldGenerateCert={(shouldGenerateCert) => {
             setShouldGenerateCert(shouldGenerateCert);
