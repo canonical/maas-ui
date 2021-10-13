c0679f6463a1f625dd346a8c72668aa4e3f76845
Caleb Ellis <caleb.ellis@canonical.com>
2021-10-13 15:38:19 +1000

    feat(ui): display cluster data in header

--- ui/src/app/kvm/views/LXDClusterDetails/LXDClusterDetails.tsx
+++ ui/src/app/kvm/views/LXDClusterDetails/LXDClusterDetails.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+import { useDispatch, useSelector } from "react-redux";
 import { Redirect, Route, Switch, useParams } from "react-router-dom";
 
 import LXDClusterDetailsHeader from "./LXDClusterDetailsHeader";
@@ -12,13 +13,34 @@ import type { ClusterRouteParams } from "./types";
 import Section from "app/base/components/Section";
 import type { KVMHeaderContent } from "app/kvm/types";
 import kvmURLs from "app/kvm/urls";
+import { actions as podActions } from "app/store/pod";
+import type { RootState } from "app/store/root/types";
+import { actions as vmClusterActions } from "app/store/vmcluster";
+import vmClusterSelectors from "app/store/vmcluster/selectors";
 
 const LXDClusterDetails = (): JSX.Element => {
+  const dispatch = useDispatch();
   const params = useParams<ClusterRouteParams>();
   const clusterId = Number(params.clusterId);
+  const cluster = useSelector((state: RootState) =>
+    vmClusterSelectors.getById(state, clusterId)
+  );
+  const clustersLoaded = useSelector(vmClusterSelectors.loaded);
   const [headerContent, setHeaderContent] = useState<KVMHeaderContent | null>(
     null
   );
+
+  useEffect(() => {
+    dispatch(podActions.fetch());
+    // TODO: Update with "get" when api is fixed.
+    // https://bugs.launchpad.net/maas/+bug/1946914
+    dispatch(vmClusterActions.fetch());
+  }, [dispatch]);
+
+  // If cluster has been deleted, redirect to KVM list.
+  if (clustersLoaded && !cluster) {
+    return <Redirect to={kvmURLs.kvm} />;
+  }
 
   return (
     <Section
