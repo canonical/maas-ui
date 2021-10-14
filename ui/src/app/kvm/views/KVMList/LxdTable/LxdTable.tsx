04828eb9ea83ae815991b2f625f44ad7d9ababcc
huwshimi <huwshimi@users.noreply.github.com>
2021-10-15 09:30:49 +1100

    feat(ui): display clusters in the KVM list (#3154)
    * feat(ui): display clusters in the KVM list
    
    Fetch and display clusters in the KVM list.
    
    Fixes: canonical-web-and-design/app-squad#288. Fixes: canonical-web-and-design/app-squad#287.

--- ui/src/app/kvm/views/KVMList/LxdTable/LxdTable.tsx
+++ ui/src/app/kvm/views/KVMList/LxdTable/LxdTable.tsx
@@ -3,10 +3,13 @@ import { useSelector } from "react-redux";
 
 import LxdKVMHostTable from "./LxdKVMHostTable";
 import type { LxdKVMHostTableRow } from "./LxdKVMHostTable/LxdKVMHostTable";
+import { LxdKVMHostType } from "./LxdKVMHostTable/LxdKVMHostTable";
 
 import kvmURLs from "app/kvm/urls";
 import podSelectors from "app/store/pod/selectors";
 import type { Pod } from "app/store/pod/types";
+import vmclusterSelectors from "app/store/vmcluster/selectors";
+import type { VMCluster } from "app/store/vmcluster/types";
 
 export const generateSingleHostRows = (pods: Pod[]): LxdKVMHostTableRow[] =>
   pods.map((pod) => {
@@ -14,6 +17,7 @@ export const generateSingleHostRows = (pods: Pod[]): LxdKVMHostTableRow[] =>
       cpuCores: pod.resources.cores,
       cpuOverCommit: pod.cpu_over_commit_ratio,
       defaultPoolID: pod.default_storage_pool,
+      hostType: LxdKVMHostType.Single,
       key: pod.id,
       memory: pod.resources.memory,
       memoryOverCommit: pod.memory_over_commit_ratio,
@@ -30,15 +34,51 @@ export const generateSingleHostRows = (pods: Pod[]): LxdKVMHostTableRow[] =>
     };
   });
 
+export const generateClusterRows = (
+  vmclusters: VMCluster[]
+): LxdKVMHostTableRow[] =>
+  vmclusters.map((vmcluster) => ({
+    cpuCores: vmcluster.total_resources.cpu,
+    hostType: LxdKVMHostType.Cluster,
+    hostsCount: vmcluster.hosts.length,
+    key: vmcluster.id,
+    memory: vmcluster.total_resources.memory,
+    name: vmcluster.name,
+    podId: vmcluster.id,
+    // TODO: Add the pool data when it is available:
+    // https://github.com/canonical-web-and-design/app-squad/issues/402
+    pool: null,
+    project: vmcluster.project,
+    storage: vmcluster.total_resources.storage,
+    url: kvmURLs.lxd.cluster.index({ clusterId: vmcluster.id }),
+    // TODO: Add the version data when it is available:
+    // https://github.com/canonical-web-and-design/app-squad/issues/402
+    version: "",
+    vms: vmcluster.virtual_machines.length,
+    // TODO: Add the zone data when it is available:
+    // https://github.com/canonical-web-and-design/app-squad/issues/402
+    zone: null,
+  }));
+
 const LxdTable = (): JSX.Element | null => {
-  const pods = useSelector(podSelectors.lxd);
-  const loading = useSelector(podSelectors.loading);
-  const loaded = useSelector(podSelectors.loaded);
-  const rows = generateSingleHostRows(pods);
-  if (loading) {
+  const singleHosts = useSelector(podSelectors.lxdSingleHosts);
+  const singleHostsLoading = useSelector(podSelectors.loading);
+  const singleHostsLoaded = useSelector(podSelectors.loaded);
+  const vmclusters = useSelector(vmclusterSelectors.all);
+  const vmclustersLoading = useSelector(vmclusterSelectors.loading);
+  const vmclustersLoaded = useSelector(vmclusterSelectors.loaded);
+  const rows = generateSingleHostRows(singleHosts).concat(
+    generateClusterRows(vmclusters)
+  );
+  if (singleHostsLoading || vmclustersLoading) {
     return <Spinner data-test="loading-table" text="Loading..." />;
   }
-  if (loaded && !pods.length) {
+  if (
+    singleHostsLoaded &&
+    !singleHosts.length &&
+    vmclustersLoaded &&
+    !vmclusters.length
+  ) {
     // TODO: display an empty state.
     // https://github.com/canonical-web-and-design/app-squad/issues/340.
     return null;
