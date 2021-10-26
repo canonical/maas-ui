85f3863d14b9b41952ef30b5cc82850ded7dc558
Caleb Ellis <caleb.ellis@canonical.com>
2021-10-26 13:18:54 +1000

    fetch necessary data in add virsh form

--- ui/src/app/kvm/components/KVMHeaderForms/AddVirsh/AddVirsh.tsx
+++ ui/src/app/kvm/components/KVMHeaderForms/AddVirsh/AddVirsh.tsx
@@ -1,6 +1,6 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-import { Spinner } from "@canonical/react-components";
+import { Spinner, Strip } from "@canonical/react-components";
 import { useDispatch, useSelector } from "react-redux";
 import type { SchemaOf } from "yup";
 import * as Yup from "yup";
@@ -10,6 +10,7 @@ import AddVirshFields from "./AddVirshFields";
 import FormikForm from "app/base/components/FormikForm";
 import { useAddMessage } from "app/base/hooks";
 import type { ClearHeaderContent } from "app/base/types";
+import { actions as generalActions } from "app/store/general";
 import { powerTypes as powerTypesSelectors } from "app/store/general/selectors";
 import { PowerFieldScope } from "app/store/general/types";
 import {
@@ -22,7 +23,9 @@ import { actions as podActions } from "app/store/pod";
 import { PodType } from "app/store/pod/constants";
 import podSelectors from "app/store/pod/selectors";
 import type { Pod } from "app/store/pod/types";
+import { actions as resourcePoolActions } from "app/store/resourcepool";
 import resourcePoolSelectors from "app/store/resourcepool/selectors";
+import { actions as zoneActions } from "app/store/zone";
 import zoneSelectors from "app/store/zone/selectors";
 
 type Props = {
@@ -43,11 +46,21 @@ export const AddVirsh = ({ clearHeaderContent }: Props): JSX.Element => {
   const podSaving = useSelector(podSelectors.saving);
   const podErrors = useSelector(podSelectors.errors);
   const powerTypes = useSelector(powerTypesSelectors.get);
+  const powerTypesLoaded = useSelector(powerTypesSelectors.loaded);
   const resourcePools = useSelector(resourcePoolSelectors.all);
+  const resourcePoolsLoaded = useSelector(resourcePoolSelectors.loaded);
   const zones = useSelector(zoneSelectors.all);
+  const zonesLoaded = useSelector(zoneSelectors.loaded);
   const [savingPod, setSavingPod] = useState<string | null>(null);
   const cleanup = useCallback(() => podActions.cleanup(), []);
   const initialPowerParameters = useInitialPowerParameters();
+  const loaded = powerTypesLoaded && resourcePoolsLoaded && zonesLoaded;
+
+  useEffect(() => {
+    dispatch(generalActions.fetchPowerTypes());
+    dispatch(resourcePoolActions.fetch());
+    dispatch(zoneActions.fetch());
+  }, [dispatch]);
 
   useAddMessage(
     podSaved,
@@ -60,13 +73,18 @@ export const AddVirsh = ({ clearHeaderContent }: Props): JSX.Element => {
     (powerType) => powerType.name === PodType.VIRSH
   );
 
-  if (!virshPowerType) {
-    return <Spinner className="u-no-margin u-no-padding" text="Loading" />;
+  if (!loaded) {
+    return (
+      <Strip shallow>
+        <Spinner className="u-no-margin u-no-padding" text="Loading" />
+      </Strip>
+    );
   }
 
-  const powerParametersSchema = generatePowerParametersSchema(virshPowerType, [
-    PowerFieldScope.BMC,
-  ]);
+  const powerParametersSchema = generatePowerParametersSchema(
+    virshPowerType || null,
+    [PowerFieldScope.BMC]
+  );
   const AddVirshSchema: SchemaOf<AddVirshValues> = Yup.object()
     .shape({
       name: Yup.string(),
@@ -95,24 +113,33 @@ export const AddVirsh = ({ clearHeaderContent }: Props): JSX.Element => {
         label: "Save KVM",
       }}
       onSubmit={(values) => {
-        const params = {
-          name: values.name,
-          pool: Number(values.pool),
-          type: values.type,
-          zone: Number(values.zone),
-          ...formatPowerParameters(virshPowerType, values.power_parameters, [
-            PowerFieldScope.BMC,
-          ]),
-        };
-        dispatch(podActions.create(params));
-        setSavingPod(values.name || "virsh VM host");
+        if (virshPowerType) {
+          const params = {
+            name: values.name,
+            pool: Number(values.pool),
+            type: values.type,
+            zone: Number(values.zone),
+            ...formatPowerParameters(virshPowerType, values.power_parameters, [
+              PowerFieldScope.BMC,
+            ]),
+          };
+          dispatch(podActions.create(params));
+          setSavingPod(values.name || "virsh VM host");
+        }
       }}
       saving={podSaving}
       saved={podSaved}
+      submitDisabled={!virshPowerType}
       submitLabel="Save KVM"
       validationSchema={AddVirshSchema}
     >
-      <AddVirshFields />
+      {virshPowerType ? (
+        <AddVirshFields />
+      ) : (
+        <Strip data-test="virsh-unsupported" shallow>
+          Virsh is not supported on this MAAS.
+        </Strip>
+      )}
     </FormikForm>
   );
 };
