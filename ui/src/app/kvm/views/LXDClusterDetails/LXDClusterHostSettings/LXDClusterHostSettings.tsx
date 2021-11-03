cdf30413e4ac6ceb80d79ca0e04024f444a00bdb
huwshimi <huwshimi@users.noreply.github.com>
2021-11-03 16:00:54 +1100

    fix(ui): set the window title for cluster settings (#3221)
    Set the window title when viewing the cluster settings or a cluster host's settings.
    
    Fixes: canonical-web-and-design/app-squad#489.

--- ui/src/app/kvm/views/LXDClusterDetails/LXDClusterHostSettings/LXDClusterHostSettings.tsx
+++ ui/src/app/kvm/views/LXDClusterDetails/LXDClusterHostSettings/LXDClusterHostSettings.tsx
@@ -2,6 +2,7 @@ import { Spinner, Strip } from "@canonical/react-components";
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 
+import { useWindowTitle } from "app/base/hooks";
 import KVMConfigurationCard from "app/kvm/components/KVMConfigurationCard";
 import LXDHostToolbar from "app/kvm/components/LXDHostToolbar";
 import SettingsBackLink from "app/kvm/components/SettingsBackLink";
@@ -10,6 +11,7 @@ import podSelectors from "app/store/pod/selectors";
 import type { Pod } from "app/store/pod/types";
 import { isPodDetails } from "app/store/pod/utils";
 import type { RootState } from "app/store/root/types";
+import vmClusterSelectors from "app/store/vmcluster/selectors";
 import type { VMCluster } from "app/store/vmcluster/types";
 
 type Props = {
@@ -18,12 +20,18 @@ type Props = {
 };
 
 const LXDClusterHostSettings = ({ clusterId, hostId }: Props): JSX.Element => {
+  const cluster = useSelector((state: RootState) =>
+    vmClusterSelectors.getById(state, clusterId)
+  );
   const pod = useSelector((state: RootState) =>
     podSelectors.getById(state, hostId)
   );
   const loading = useSelector(podSelectors.loading);
   useActivePod(hostId);
   const redirectURL = useKVMDetailsRedirect(hostId);
+  useWindowTitle(
+    `${pod?.name || "Host"} in ${cluster?.name || "cluster"} settings`
+  );
 
   if (redirectURL) {
     return <Redirect to={redirectURL} />;
