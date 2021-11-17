d0bf11f57666e5f7ca7ef59a07203e54e10ec226
Peter Makowski <peter@makowski.uk>
2021-11-17 10:23:55 +0100

    add common eslint config

--- integration/.eslintrc.js
+++ integration/.eslintrc.js
@@ -2,11 +2,11 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   extends: [
     "plugin:cypress/recommended",
+    "../eslint/common",
     "plugin:prettier/recommended", // Ensure this is last in the list.
   ],
   plugins: [
     "cypress",
-    "no-only-tests",
     "@typescript-eslint",
     "prettier",
     "eslint-plugin-import",
@@ -19,7 +19,6 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    "no-only-tests/no-only-tests": "error",
     "cypress/no-force": "warn",
     "prettier/prettier": "error",
   },
