779d234d7e302e037c56115ab8c2030a41a731e2
Huw Wilkins <huw.wilkins@canonical.com>
2021-11-25 13:44:37 +1100

    feat(ui): add device actions to the slice
    Add support for the device websocket API methods to the device slice.
    
    Fixes: canonical-web-and-design/app-tribe#520.

--- ui/src/app/store/device/slice.ts
+++ ui/src/app/store/device/slice.ts
@@ -9,16 +9,33 @@ import type {
   UpdateParams,
   Device,
 } from "./types";
+import type {
+  CreatePhysicalParams,
+  DeleteInterfaceParams,
+  LinkSubnetParams,
+  SetZoneParams,
+  UnlinkSubnetParams,
+} from "./types/actions";
 
+import type { UpdateInterfaceParams } from "app/store/types/node";
+import { NodeActions } from "app/store/types/node";
 import {
   generateCommonReducers,
   generateStatusHandlers,
   genericInitialState,
   updateErrors,
 } from "app/store/utils/slice";
+import { kebabToCamelCase, preparePayloadParams } from "app/utils";
 
 export const DEFAULT_STATUSES = {
   creatingInterface: false,
+  creatingPhysical: false,
+  deleting: false,
+  deletingInterface: false,
+  linkingSubnet: false,
+  unlinkingSubnet: false,
+  updatingInterface: false,
+  settingZone: false,
 };
 
 const setErrors = (
@@ -38,6 +55,34 @@ const statusHandlers = generateStatusHandlers<
     {
       status: "createInterface",
       statusKey: "creatingInterface",
+    },
+    {
+      status: "createPhysical",
+      statusKey: "creatingPhysical",
+    },
+    {
+      status: NodeActions.DELETE,
+      statusKey: "deleting",
+    },
+    {
+      status: "deleteInterface",
+      statusKey: "deletingInterface",
+    },
+    {
+      status: "linkSubnet",
+      statusKey: "linkingSubnet",
+    },
+    {
+      status: "unlinkSubnet",
+      statusKey: "unlinkingSubnet",
+    },
+    {
+      status: kebabToCamelCase(NodeActions.SET_ZONE),
+      statusKey: "settingZone",
+    },
+    {
+      status: "updateInterface",
+      statusKey: "updatingInterface",
     },
   ],
   setErrors
@@ -66,7 +111,7 @@ const deviceSlice = createSlice({
           method: "create_interface",
         },
         payload: {
-          params,
+          params: preparePayloadParams(params),
         },
       }),
       reducer: () => {
@@ -76,6 +121,24 @@ const deviceSlice = createSlice({
     createInterfaceError: statusHandlers.createInterface.error,
     createInterfaceStart: statusHandlers.createInterface.start,
     createInterfaceSuccess: statusHandlers.createInterface.success,
+    // On the backend this endpoint is an alias for createInterface.
+    createPhysical: {
+      prepare: (params: CreatePhysicalParams) => ({
+        meta: {
+          model: DeviceMeta.MODEL,
+          method: "create_physical",
+        },
+        payload: {
+          params: preparePayloadParams(params),
+        },
+      }),
+      reducer: () => {
+        // No state changes need to be handled for this action.
+      },
+    },
+    createPhysicalError: statusHandlers.createPhysical.error,
+    createPhysicalStart: statusHandlers.createPhysical.start,
+    createPhysicalSuccess: statusHandlers.createPhysical.success,
     fetchSuccess: (state: DeviceState, action: PayloadAction<Device[]>) => {
       action.payload.forEach((newItem: Device) => {
         // Add items that don't already exist in the store. Existing items
@@ -94,6 +157,192 @@ const deviceSlice = createSlice({
       state.loading = false;
       state.loaded = true;
     },
+    delete: {
+      prepare: (system_id: Device[DeviceMeta.PK]) => ({
+        meta: {
+          model: DeviceMeta.MODEL,
+          method: "action",
+        },
+        payload: {
+          params: {
+            action: NodeActions.DELETE,
+            extra: {},
+            system_id,
+          },
+        },
+      }),
+      reducer: () => {
+        // No state changes need to be handled for this action.
+      },
+    },
+    deleteError: statusHandlers.delete.error,
+    deleteStart: statusHandlers.delete.start,
+    deleteSuccess: statusHandlers.delete.success,
+    deleteInterface: {
+      prepare: (params: DeleteInterfaceParams) => ({
+        meta: {
+          model: DeviceMeta.MODEL,
+          method: "delete_interface",
+        },
+        payload: {
+          params: preparePayloadParams(params),
+        },
+      }),
+      reducer: () => {
+        // No state changes need to be handled for this action.
+      },
+    },
+    deleteInterfaceError: statusHandlers.deleteInterface.error,
+    deleteInterfaceStart: statusHandlers.deleteInterface.start,
+    deleteInterfaceSuccess: statusHandlers.deleteInterface.success,
+    get: {
+      prepare: (id: Device[DeviceMeta.PK]) => ({
+        meta: {
+          model: DeviceMeta.MODEL,
+          method: "get",
+        },
+        payload: {
+          params: { [DeviceMeta.PK]: id },
+        },
+      }),
+      reducer: () => {
+        // No state changes need to be handled for this action.
+      },
+    },
+    getError: (
+      state: DeviceState,
+      action: PayloadAction<DeviceState["errors"]>
+    ) => {
+      state.errors = action.payload;
+      state = setErrors(state, action, "get");
+      state.loading = false;
+      state.saving = false;
+    },
+    getStart: (state: DeviceState) => {
+      state.loading = true;
+    },
+    getSuccess: (state: DeviceState, action: PayloadAction<Device>) => {
+      const device = action.payload;
+      // If the item already exists, update it, otherwise
+      // add it to the store.
+      const i = state.items.findIndex(
+        (draftItem: Device) =>
+          draftItem[DeviceMeta.PK] === device[DeviceMeta.PK]
+      );
+      if (i !== -1) {
+        state.items[i] = device;
+      } else {
+        state.items.push(device);
+        // Set up the statuses for this device.
+        state.statuses[device[DeviceMeta.PK]] = DEFAULT_STATUSES;
+      }
+      state.loading = false;
+    },
+    linkSubnet: {
+      prepare: (params: LinkSubnetParams) => ({
+        meta: {
+          model: DeviceMeta.MODEL,
+          method: "link_subnet",
+        },
+        payload: {
+          params: preparePayloadParams(params),
+        },
+      }),
+      reducer: () => {
+        // No state changes need to be handled for this action.
+      },
+    },
+    linkSubnetError: statusHandlers.linkSubnet.error,
+    linkSubnetStart: statusHandlers.linkSubnet.start,
+    linkSubnetSuccess: statusHandlers.linkSubnet.success,
+    setActive: {
+      prepare: (system_id: Device[DeviceMeta.PK] | null) => ({
+        meta: {
+          model: DeviceMeta.MODEL,
+          method: "set_active",
+        },
+        payload: {
+          // Server unsets active item if primary key (system_id) is not sent.
+          params: system_id ? { system_id } : null,
+        },
+      }),
+      reducer: () => {
+        // No state changes need to be handled for this action.
+      },
+    },
+    setActiveError: (
+      state: DeviceState,
+      action: PayloadAction<DeviceState["errors"]>
+    ) => {
+      state.active = null;
+      state.errors = action.payload;
+      state = setErrors(state, action, "setActive");
+    },
+    setActiveSuccess: (
+      state: DeviceState,
+      action: PayloadAction<Device | null>
+    ) => {
+      state.active = action.payload?.system_id || null;
+    },
+    setZone: {
+      prepare: (params: SetZoneParams) => ({
+        meta: {
+          model: DeviceMeta.MODEL,
+          method: "action",
+        },
+        payload: {
+          params: {
+            action: NodeActions.SET_ZONE,
+            extra: { zone_id: params.zone_id },
+            [DeviceMeta.PK]: params[DeviceMeta.PK],
+          },
+        },
+      }),
+      reducer: () => {
+        // No state changes need to be handled for this action.
+      },
+    },
+    setZoneError: statusHandlers.setZone.error,
+    setZoneStart: statusHandlers.setZone.start,
+    setZoneSuccess: statusHandlers.setZone.success,
+    unlinkSubnet: {
+      prepare: (params: UnlinkSubnetParams) => ({
+        meta: {
+          model: DeviceMeta.MODEL,
+          method: "unlink_subnet",
+        },
+        payload: {
+          params: preparePayloadParams(params),
+        },
+      }),
+      reducer: () => {
+        // No state changes need to be handled for this action.
+      },
+    },
+    unlinkSubnetError: statusHandlers.unlinkSubnet.error,
+    unlinkSubnetStart: statusHandlers.unlinkSubnet.start,
+    unlinkSubnetSuccess: statusHandlers.unlinkSubnet.success,
+    updateInterface: {
+      prepare: (
+        // This update endpoint is used for updating all interface types so
+        // must allow all possible parameters.
+        params: UpdateInterfaceParams
+      ) => ({
+        meta: {
+          model: DeviceMeta.MODEL,
+          method: "update_interface",
+        },
+        payload: {
+          params: preparePayloadParams(params),
+        },
+      }),
+      reducer: () => {
+        // No state changes need to be handled for this action.
+      },
+    },
+    updateInterfaceError: statusHandlers.updateInterface.error,
+    updateInterfaceStart: statusHandlers.updateInterface.start,
+    updateInterfaceSuccess: statusHandlers.updateInterface.success,
   },
 });
 
--- ui/src/app/store/device/types/actions.ts
+++ ui/src/app/store/device/types/actions.ts
@@ -5,8 +5,8 @@ import type { Controller, ControllerMeta } from "app/store/controller/types";
 import type { Domain } from "app/store/domain/types";
 import type { Machine, MachineMeta } from "app/store/machine/types";
 import type { Subnet, SubnetMeta } from "app/store/subnet/types";
-import type { NetworkInterface } from "app/store/types/node";
-import type { Zone } from "app/store/zone/types";
+import type { NetworkInterface, NetworkLink } from "app/store/types/node";
+import type { Zone, ZoneMeta } from "app/store/zone/types";
 
 export type CreateParams = {
   description?: DeviceDetails["description"];
@@ -44,6 +44,34 @@ export type CreateInterfaceParams = {
   subnet?: Subnet[SubnetMeta.PK];
   tags?: NetworkInterface["tags"];
   vlan?: NetworkInterface["vlan_id"];
+};
+
+// This endpoint is an alias for create_interface.
+export type CreatePhysicalParams = CreateInterfaceParams;
+
+export type DeleteInterfaceParams = {
+  interface_id: NetworkInterface["id"];
+  [MachineMeta.PK]: Machine[MachineMeta.PK];
+};
+
+export type LinkSubnetParams = {
+  [MachineMeta.PK]: Machine[MachineMeta.PK];
+  interface_id: NetworkInterface["id"];
+  ip_address?: NetworkLink["ip_address"];
+  ip_assignment?: DeviceIpAssignment;
+  link_id?: NetworkLink["id"];
+  subnet?: Subnet[SubnetMeta.PK];
+};
+
+export type SetZoneParams = {
+  [MachineMeta.PK]: Machine[MachineMeta.PK];
+  zone_id: Zone[ZoneMeta.PK];
+};
+
+export type UnlinkSubnetParams = {
+  [MachineMeta.PK]: Machine[MachineMeta.PK];
+  interface_id: NetworkInterface["id"];
+  link_id: NetworkLink["id"];
 };
 
 export type UpdateParams = Partial<CreateParams> & {
