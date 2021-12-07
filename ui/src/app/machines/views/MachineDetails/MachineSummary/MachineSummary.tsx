a3ebab711ec3dc20b259c46f3a72b82a7879eed9
Caleb Ellis <caleb.ellis@canonical.com>
2021-12-07 11:42:30 +1000

    feat(ui): build device summary page (#3378)

--- ui/src/app/machines/views/MachineDetails/MachineSummary/MachineSummary.tsx
+++ ui/src/app/machines/views/MachineDetails/MachineSummary/MachineSummary.tsx
@@ -3,18 +3,23 @@ import { useEffect } from "react";
 import { Spinner } from "@canonical/react-components";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 
-import NetworkCard from "./NetworkCard";
 import NumaCard from "./NumaCard";
 import OverviewCard from "./OverviewCard";
 import SystemCard from "./SystemCard";
+import TestResults from "./TestResults";
 import WorkloadCard from "./WorkloadCard";
 
+import NodeSummaryNetworkCard from "app/base/components/NodeSummaryNetworkCard";
+import { HardwareType } from "app/base/enum";
 import { useWindowTitle } from "app/base/hooks";
 import type { RouteParams } from "app/base/types";
 import type { MachineSetHeaderContent } from "app/machines/types";
+import machineURLs from "app/machines/urls";
 import { actions as machineActions } from "app/store/machine";
 import machineSelectors from "app/store/machine/selectors";
+import { isMachineDetails } from "app/store/machine/utils";
 import type { RootState } from "app/store/root/types";
 import { NodeStatusCode } from "app/store/types/node";
 
@@ -39,6 +44,7 @@ const MachineSummary = ({ setHeaderContent }: Props): JSX.Element => {
     return <Spinner text="Loading" />;
   }
 
+  const networkURL = machineURLs.machine.network({ id });
   const showWorkloadCard =
     "workload_annotations" in machine &&
     [NodeStatusCode.ALLOCATED, NodeStatusCode.DEPLOYED].includes(
@@ -50,7 +56,24 @@ const MachineSummary = ({ setHeaderContent }: Props): JSX.Element => {
       <OverviewCard id={id} setHeaderContent={setHeaderContent} />
       <SystemCard id={id} />
       <NumaCard id={id} />
-      <NetworkCard id={id} setHeaderContent={setHeaderContent} />
+      <div className="machine-summary__network-card">
+        <NodeSummaryNetworkCard
+          interfaces={isMachineDetails(machine) ? machine.interfaces : null}
+          networkURL={networkURL}
+        >
+          <p>
+            Information about tagged traffic can be seen in the{" "}
+            <Link to={networkURL}>Network tab</Link>.
+          </p>
+          {isMachineDetails(machine) && (
+            <TestResults
+              machine={machine}
+              hardwareType={HardwareType.Network}
+              setHeaderContent={setHeaderContent}
+            />
+          )}
+        </NodeSummaryNetworkCard>
+      </div>
       {showWorkloadCard ? <WorkloadCard id={id} /> : null}
     </div>
   );
