63c428636255081e97ef52fd528fa0c830cb72b5
Peter Makowski <peter@makowski.uk>
2021-12-09 09:22:32 +0100

    refactor HardwareMenu with testing library - add @testing-library/user-event - add onClick test - remove snapshot test

--- shared/src/components/Header/HardwareMenu/HardwareMenu.test.tsx
+++ shared/src/components/Header/HardwareMenu/HardwareMenu.test.tsx
@@ -1,17 +1,24 @@
-import { shallow } from "enzyme";
+import { render, screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import React from "react";
 
 import { HardwareMenu } from "./HardwareMenu";
+
+const generateLink = (link, linkClass) => (
+  <a href={link.url} className={linkClass}>
+    {link.label}
+  </a>
+);
 
 describe("HardwareMenu", () => {
   afterEach(() => {
     jest.resetModules();
   });
 
-  it("renders", () => {
-    const wrapper = shallow(
+  it("renders a list of links", () => {
+    render(
       <HardwareMenu
-        generateLink={jest.fn()}
+        generateLink={generateLink}
         links={[
           {
             inHardwareMenu: true,
@@ -23,6 +30,30 @@ describe("HardwareMenu", () => {
         toggleHardwareMenu={jest.fn()}
       />
     );
-    expect(wrapper).toMatchSnapshot();
+
+    expect(
+      within(screen.getByRole("list")).getByRole("link", { name: "Machines" })
+    ).toBeInTheDocument();
+  });
+
+  it("calls toggleHardwareMenu on click", () => {
+    const handleToggleHardwareMenu = jest.fn();
+    render(
+      <HardwareMenu
+        generateLink={generateLink}
+        links={[
+          {
+            inHardwareMenu: true,
+            isLegacy: false,
+            label: "Machines",
+            url: "/machines",
+          },
+        ]}
+        toggleHardwareMenu={handleToggleHardwareMenu}
+      />
+    );
+
+    userEvent.click(screen.getByRole("link", { name: "Machines" }));
+    expect(handleToggleHardwareMenu).toHaveBeenCalledTimes(1);
   });
 });
