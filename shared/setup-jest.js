a5389b412847b8c72ebff80c0997492fff60776d
Peter Makowski <peter@makowski.uk>
2022-01-05 09:33:51 +0100

    remove enzyme from maas-ui-shared

--- shared/setup-jest.js
+++ shared/setup-jest.js
@@ -1,6 +1,2 @@
 import "@testing-library/react";
 import "@testing-library/jest-dom";
-import Enzyme from "enzyme";
-import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-
-Enzyme.configure({ adapter: new Adapter() });
