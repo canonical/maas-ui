b3be9fbb4e1bbc741a260d4635280ba14abe6e6f
huwshimi <huwshimi@users.noreply.github.com>
2022-01-31 17:54:41 +1100

    feat(ui): display the VLAN title and delete button (#3563)
    * feat(ui): display the VLAN title and delete button
    
    Add the VLAN title and delete button.
    
    Fixes: canonical-web-and-design/app-tribe#715.
    
    * Remove comment

--- ui/src/app/subnets/views/VLANDetails/VLANDetailsHeader/VLANDetailsHeader.test.tsx
+++ ui/src/app/subnets/views/VLANDetails/VLANDetailsHeader/VLANDetailsHeader.test.tsx
@@ -1,33 +1,180 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router-dom";
+import configureStore from "redux-mock-store";
 
 import VLANDetailsHeader from "./VLANDetailsHeader";
 
+import type { RootState } from "app/store/root/types";
+import type { VLAN } from "app/store/vlan/types";
+import { VlanVid } from "app/store/vlan/types";
 import {
+  authState as authStateFactory,
+  fabric as fabricFactory,
+  fabricState as fabricStateFactory,
+  rootState as rootStateFactory,
+  user as userFactory,
+  userState as userStateFactory,
   vlan as vlanFactory,
   vlanDetails as vlanDetailsFactory,
+  vlanState as vlanStateFactory,
 } from "testing/factories";
 
-it("shows the vlan name as the section title", () => {
-  const vlan = vlanFactory({ id: 1, name: "vlan-1" });
-  render(<VLANDetailsHeader vlan={vlan} />);
+const mockStore = configureStore();
 
-  expect(screen.getByTestId("section-header-title")).toHaveTextContent(
-    "vlan-1"
-  );
-});
+describe("VLANDetailsHeader", () => {
+  let state: RootState;
+  let vlan: VLAN;
 
-it("shows a spinner subtitle if the vlan is loading details", () => {
-  const vlan = vlanFactory({ id: 1, name: "vlan-1" });
-  render(<VLANDetailsHeader vlan={vlan} />);
+  beforeEach(() => {
+    vlan = vlanDetailsFactory({ fabric: 2 });
+    state = rootStateFactory({
+      fabric: fabricStateFactory({
+        items: [fabricFactory({ id: 2, name: "fabric1" })],
+      }),
+      vlan: vlanStateFactory({
+        items: [vlan],
+      }),
+    });
+  });
 
-  expect(
-    screen.queryByTestId("section-header-subtitle-spinner")
-  ).toBeInTheDocument();
-});
+  it("shows the title when the vlan has a name", () => {
+    vlan = vlanDetailsFactory({ name: "vlan-1", fabric: 2 });
+    state.vlan.items = [vlan];
+    const store = mockStore(state);
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[{ pathname: "/vlan/1234" }]}>
+          <VLANDetailsHeader id={vlan.id} />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(screen.getByTestId("section-header-title")).toHaveTextContent(
+      "vlan-1 in fabric1"
+    );
+  });
 
-it("does not show a spinner subtitle if the vlan is detailed", () => {
-  const vlan = vlanDetailsFactory({ id: 1, name: "vlan-1" });
-  render(<VLANDetailsHeader vlan={vlan} />);
+  it("shows the title when it is the default for the fabric", () => {
+    vlan = vlanDetailsFactory({
+      fabric: 2,
+      name: undefined,
+      vid: VlanVid.UNTAGGED,
+    });
+    state.vlan.items = [vlan];
+    state.fabric.items = [
+      fabricFactory({ id: 2, name: "fabric1", default_vlan_id: vlan.id }),
+    ];
+    const store = mockStore(state);
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[{ pathname: "/vlan/1234" }]}>
+          <VLANDetailsHeader id={vlan.id} />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(screen.getByTestId("section-header-title")).toHaveTextContent(
+      "Default VLAN in fabric1"
+    );
+  });
 
-  expect(screen.queryByTestId("section-header-subtitle-spinner")).toBeNull();
+  it("shows the title when it is not the default for the fabric", () => {
+    vlan = vlanDetailsFactory({ fabric: 2, name: undefined, vid: 3 });
+    state.vlan.items = [vlan];
+    state.fabric.items = [
+      fabricFactory({ id: 2, name: "fabric1", default_vlan_id: 99 }),
+    ];
+    const store = mockStore(state);
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[{ pathname: "/vlan/1234" }]}>
+          <VLANDetailsHeader id={vlan.id} />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(screen.getByTestId("section-header-title")).toHaveTextContent(
+      "VLAN 3 in fabric1"
+    );
+  });
+
+  it("shows a spinner subtitle if the vlan is loading details", () => {
+    vlan = vlanFactory({ name: "vlan-1" });
+    state.vlan.items = [vlan];
+    const store = mockStore(state);
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[{ pathname: "/vlan/1234" }]}>
+          <VLANDetailsHeader id={vlan.id} />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(
+      screen.queryByTestId("section-header-subtitle-spinner")
+    ).toBeInTheDocument();
+  });
+
+  it("does not show a spinner subtitle if the vlan is detailed", () => {
+    const store = mockStore(state);
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[{ pathname: "/vlan/1234" }]}>
+          <VLANDetailsHeader id={vlan.id} />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(screen.queryByTestId("section-header-subtitle-spinner")).toBeNull();
+  });
+
+  it("shows the delete button when the user is an admin and the vlan is not the default", () => {
+    state.user = userStateFactory({
+      auth: authStateFactory({
+        user: userFactory({ is_superuser: true }),
+      }),
+    });
+    const store = mockStore(state);
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[{ pathname: "/vlan/1234" }]}>
+          <VLANDetailsHeader id={vlan.id} />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(screen.queryByTestId("delete-vlan")).toBeInTheDocument();
+  });
+
+  it("does not show the delete button if the user is not an admin", () => {
+    state.user = userStateFactory({
+      auth: authStateFactory({
+        user: userFactory({ is_superuser: false }),
+      }),
+    });
+    const store = mockStore(state);
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[{ pathname: "/vlan/1234" }]}>
+          <VLANDetailsHeader id={vlan.id} />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(screen.queryByTestId("delete-vlan")).toBeNull();
+  });
+
+  it("does not show the delete button if the vlan is the default", () => {
+    state.fabric.items = [
+      fabricFactory({ id: 2, name: "fabric1", default_vlan_id: vlan.id }),
+    ];
+    state.user = userStateFactory({
+      auth: authStateFactory({
+        user: userFactory({ is_superuser: true }),
+      }),
+    });
+    const store = mockStore(state);
+    render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={[{ pathname: "/vlan/1234" }]}>
+          <VLANDetailsHeader id={vlan.id} />
+        </MemoryRouter>
+      </Provider>
+    );
+    expect(screen.queryByTestId("delete-vlan")).toBeNull();
+  });
 });
