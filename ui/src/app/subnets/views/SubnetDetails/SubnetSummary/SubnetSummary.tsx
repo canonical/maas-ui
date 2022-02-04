4abf20039fc57a6176ac202bf121d713296f836f
Huw Wilkins <huw.wilkins@canonical.com>
2022-02-04 14:19:13 +1100

    Update subnet details to display definition labels above the content.

--- ui/src/app/subnets/views/SubnetDetails/SubnetSummary/SubnetSummary.tsx
+++ ui/src/app/subnets/views/SubnetDetails/SubnetSummary/SubnetSummary.tsx
@@ -3,30 +3,32 @@ import { useEffect } from "react";
 import { Row, Col, Tooltip, Icon } from "@canonical/react-components";
 import { useSelector, useDispatch } from "react-redux";
 
+import Definition from "app/base/components/Definition";
+import FabricLink from "app/base/components/FabricLink";
+import SpaceLink from "app/base/components/SpaceLink";
 import TitledSection from "app/base/components/TitledSection";
+import VLANLink from "app/base/components/VLANLink";
 import { actions as fabricActions } from "app/store/fabric";
-import fabricSelectors from "app/store/fabric/selectors";
-import { getFabricById } from "app/store/fabric/utils";
+import type { RootState } from "app/store/root/types";
 import { actions as spaceActions } from "app/store/space";
-import spaceSelectors from "app/store/space/selectors";
-import { getSpaceById } from "app/store/space/utils";
-import type { Subnet } from "app/store/subnet/types";
+import subnetSelectors from "app/store/subnet/selectors";
+import type { Subnet, SubnetMeta } from "app/store/subnet/types";
 import { actions as vlanActions } from "app/store/vlan";
 import vlanSelectors from "app/store/vlan/selectors";
-import { getVlanById } from "app/store/vlan/utils";
+import { isId } from "app/utils";
 
 type Props = {
-  subnet: Subnet;
+  id: Subnet[SubnetMeta.PK] | null;
 };
 
-const SubnetSummary = ({ subnet }: Props): JSX.Element => {
+const SubnetSummary = ({ id }: Props): JSX.Element | null => {
   const dispatch = useDispatch();
-  const spaces = useSelector(spaceSelectors.all);
-  const vlans = useSelector(vlanSelectors.all);
-  const fabrics = useSelector(fabricSelectors.all);
-  const space = getSpaceById(spaces, subnet.space) || null;
-  const vlan = getVlanById(vlans, subnet.vlan) || null;
-  const fabric = vlan ? getFabricById(fabrics, vlan.fabric) : null;
+  const subnet = useSelector((state: RootState) =>
+    subnetSelectors.getById(state, id)
+  );
+  const vlan = useSelector((state: RootState) =>
+    vlanSelectors.getById(state, subnet?.vlan)
+  );
 
   useEffect(() => {
     dispatch(spaceActions.fetch());
@@ -34,196 +36,125 @@ const SubnetSummary = ({ subnet }: Props): JSX.Element => {
     dispatch(fabricActions.fetch());
   }, [dispatch]);
 
+  if (!subnet) {
+    return null;
+  }
+
   return (
     <TitledSection title="Subnet summary">
       <Row>
         <Col size={6}>
-          <Row>
-            <Col size={2}>
-              <p className="u-text--muted" id="subnet-name">
-                Name
-              </p>
-            </Col>
-            <Col size={4}>
-              <p aria-labelledby="subnet-name">{subnet.name}</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={2}>
-              <p className="u-text--muted" id="subnet-cidr">
-                CIDR
-              </p>
-            </Col>
-            <Col size={4}>
-              <p aria-labelledby="subnet-cidr">{subnet.cidr}</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={2}>
-              <p className="u-text--muted" id="subnet-gateway-ip">
-                Gateway IP
-              </p>
-            </Col>
-            <Col size={4}>
-              <p aria-labelledby="subnet-gateway-ip">{subnet.gateway_ip}</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={2}>
-              <p className="u-text--muted" id="subnet-dns">
-                DNS
-              </p>
-            </Col>
-            <Col size={4}>
-              <p aria-labelledby="subnet-dns">{subnet.dns_servers}</p>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={2}>
-              <p className="u-text--muted" id="subnet-description">
-                Description
-              </p>
-            </Col>
-            <Col size={4}>
-              <p aria-labelledby="subnet-description">{subnet.description}</p>
-            </Col>
-          </Row>
+          <Definition label="Name" description={subnet.name} />
+          <Definition label="CIDR" description={subnet.cidr} />
+          <Definition label="Gateway IP">{subnet.gateway_ip}</Definition>
+          <Definition label="DNS" description={subnet.dns_servers} />
+          <Definition label="Description" description={subnet.description} />
+          <Definition
+            label={
+              <>
+                Managed allocation{" "}
+                {subnet.managed ? null : (
+                  <Tooltip
+                    message="MAAS allocates IP addresses 
+                  from this subnet, exluding the 
+                  reserved and dynamic ranges."
+                    position="btm-right"
+                  >
+                    <Icon name="information" />
+                  </Tooltip>
+                )}
+              </>
+            }
+          >
+            {subnet.managed ? "Enabled" : "Disabled"}
+          </Definition>
         </Col>
         <Col size={6}>
-          <Row>
-            <Col size={2}>
-              <p className="u-text--muted">
-                <span id="subnet-managed-allocation">Managed allocation</span>{" "}
-                <Tooltip
-                  message="MAAS allocates IP addresses 
-from this subnet, exluding the 
-reserved and dynamic ranges."
-                  position="btm-right"
-                >
-                  <Icon name="information" />
-                </Tooltip>
-              </p>
-            </Col>
-            <Col size={4}>
-              <p aria-labelledby="subnet-managed-allocation">
-                {subnet.managed ? "Enabled" : "Disabled"}
-              </p>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={2}>
-              <p className="u-text--muted" id="subnet-active-discovery">
-                <span id="subnet-active-discovery">Active discovery</span>{" "}
-                <Tooltip
-                  message="When enabled, MAAS will scan 
+          <Definition
+            label={
+              <>
+                Active discovery{" "}
+                {subnet.managed ? (
+                  <Tooltip
+                    message="When enabled, MAAS will scan 
 this subnet to discover hosts 
 that have not been discovered 
 passively."
-                  position="btm-right"
-                >
-                  <Icon name="information" />
-                </Tooltip>
-              </p>
-            </Col>
-            <Col size={4}>
-              <p aria-labelledby="subnet-active-discovery">
-                {subnet.active_discovery ? "Enabled" : "Disabled"}
-              </p>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={2}>
-              <p className="u-text--muted" id="subnet-proxy-access">
-                <span id="subnet-proxy-access">Proxy access</span>{" "}
+                    position="btm-right"
+                  >
+                    <Icon name="information" />
+                  </Tooltip>
+                ) : null}
+              </>
+            }
+          >
+            {subnet.active_discovery ? "Enabled" : "Disabled"}
+          </Definition>
+          <Definition
+            label={
+              <>
+                Proxy access{" "}
                 <Tooltip
-                  message="MAAS will allow clients from 
+                  data-testid="proxy-access-tooltip"
+                  message={`MAAS will${
+                    subnet.allow_proxy ? "" : " not"
+                  } allow clients from 
 this subnet to access the 
-MAAS proxy."
+MAAS proxy.`}
                   position="btm-right"
                 >
                   <Icon name="information" />
                 </Tooltip>
-              </p>
-            </Col>
-            <Col size={4}>
-              <p aria-labelledby="subnet-proxy-access">
-                {subnet.allow_proxy ? "Allowed" : "Disallowed"}
-              </p>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={2}>
-              <p className="u-text--muted" id="subnet-allow-dns-resolution">
-                <span id="subnet-allow-dns-resolution">
-                  Allow DNS resolution
-                </span>{" "}
+              </>
+            }
+          >
+            {subnet.allow_proxy ? "Allowed" : "Disallowed"}
+          </Definition>
+          <Definition
+            label={
+              <>
+                Allow DNS resolution{" "}
                 <Tooltip
-                  message="MAAS will allow clients from 
+                  data-testid="allow-dns-tooltip"
+                  message={`MAAS will${
+                    subnet.allow_dns ? "" : " not"
+                  } allow clients from 
 this subnet to use MAAS 
-for DNS resolution."
+for DNS resolution.`}
                   position="btm-right"
                 >
                   <Icon name="information" />
                 </Tooltip>
-              </p>
-            </Col>
-            <Col size={4}>
-              <p aria-labelledby="subnet-allow-dns-resolution">
-                {subnet.allow_dns ? "Allowed" : "Disallowed"}
-              </p>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={2}>
-              <p className="u-text--muted" id="subnet-fabric">
-                Fabric
-              </p>
-            </Col>
-            <Col size={4}>
-              <p aria-labelledby="subnet-fabric">
-                {fabric ? fabric.name : "No fabric"}
-              </p>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={2}>
-              <p className="u-text--muted" id="subnet-vlan">
-                VLAN
-              </p>
-            </Col>
-            <Col size={4}>
-              <p aria-labelledby="subnet-vlan">
-                {vlan && vlan.name ? vlan.name : "untagged"}
-              </p>
-            </Col>
-          </Row>
-          <Row>
-            <Col size={2}>
-              <p className="u-text--muted" id="subnet-space">
-                Space
-              </p>
-            </Col>
-            <Col size={4}>
-              <p>
-                {space && space.name ? (
-                  <span aria-labelledby="subnet-space">{space.name}</span>
-                ) : (
-                  <>
-                    <span aria-labelledby="subnet-space">No space</span>{" "}
-                    <Tooltip
-                      message="This subnet does not belong to 
+              </>
+            }
+          >
+            {subnet.allow_dns ? "Allowed" : "Disallowed"}
+          </Definition>
+          <Definition label="Fabric">
+            <FabricLink id={vlan?.fabric} />
+          </Definition>
+          <Definition label="VLAN">
+            <VLANLink id={subnet.vlan} />
+          </Definition>
+          <Definition label="Space">
+            <>
+              <SpaceLink id={subnet.space} />
+              {isId(subnet.space) ? null : (
+                <>
+                  {" "}
+                  <Tooltip
+                    message="This subnet does not belong to 
 a space. MAAS integrations require 
 a space in order to determine the 
 purpose of a network."
-                      position="btm-right"
-                    >
-                      <Icon name="warning" />
-                    </Tooltip>
-                  </>
-                )}
-              </p>
-            </Col>
-          </Row>
+                    position="btm-right"
+                  >
+                    <Icon name="warning" />
+                  </Tooltip>
+                </>
+              )}
+            </>
+          </Definition>
         </Col>
       </Row>
     </TitledSection>
