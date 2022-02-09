4e9423e1f4d56f693e138da0f9e9eebebd5d5b3a
Caleb Ellis <caleb.ellis@canonical.com>
2022-02-09 18:51:03 +1000

    feat(ui): build "Edit boot architectures" form in subnet details (#3607)

---
+++ ui/src/app/subnets/views/SubnetDetails/SubnetDetailsHeader/SubnetActionForms/components/EditBootArchitectures/BootArchitecturesTable/BootArchitecturesTable.tsx
@@ -0,0 +1,130 @@
+import { MainTable } from "@canonical/react-components";
+import classNames from "classnames";
+import { useFormikContext } from "formik";
+import { useSelector } from "react-redux";
+
+import type { FormValues } from "../EditBootArchitectures";
+
+import RowCheckbox from "app/base/components/RowCheckbox";
+import { knownBootArchitectures as knownBootArchitecturesSelectors } from "app/store/general/selectors";
+import type { KnownBootArchitecture } from "app/store/general/types";
+
+export enum Headers {
+  BootloaderArch = "Bootloader architecture",
+  BootMethod = "BIOS boot method",
+  Name = "Name",
+  Octet = "Architecture octet",
+  Protocol = "Protocol",
+}
+
+export const BootArchitecturesTable = (): JSX.Element => {
+  const {
+    setFieldValue,
+    values: { disabled_boot_architectures },
+  } = useFormikContext<FormValues>();
+  const knownBootArchitectures = useSelector(
+    knownBootArchitecturesSelectors.get
+  );
+
+  const isChecked = (bootArchName: KnownBootArchitecture["name"]) =>
+    !disabled_boot_architectures.includes(bootArchName);
+
+  const handleArchChange = (bootArchName: KnownBootArchitecture["name"]) => {
+    setFieldValue(
+      "disabled_boot_architectures",
+      isChecked(bootArchName)
+        ? [...disabled_boot_architectures, bootArchName]
+        : disabled_boot_architectures.filter((item) => item !== bootArchName)
+    );
+  };
+
+  return (
+    <MainTable
+      className="boot-architectures-table"
+      defaultSort="name"
+      defaultSortDirection="ascending"
+      headers={[
+        {
+          className: "name-col",
+          content: Headers.Name,
+          sortKey: "name",
+        },
+        {
+          className: "bios-boot-col",
+          content: Headers.BootMethod,
+          sortKey: "bios_boot_method",
+        },
+        {
+          className: "bootloader-col",
+          content: Headers.BootloaderArch,
+          sortKey: "bootloader_arches",
+        },
+        {
+          className: "protocol-col",
+          content: Headers.Protocol,
+          sortKey: "protocol",
+        },
+        {
+          className: "octet-col",
+          content: Headers.Octet,
+          sortKey: "arch_octet",
+        },
+      ]}
+      responsive
+      rows={knownBootArchitectures.map((bootArch) => {
+        const {
+          arch_octet,
+          bios_boot_method,
+          bootloader_arches,
+          name,
+          protocol,
+        } = bootArch;
+        return {
+          className: classNames("boot-architectures-table__row", {
+            "is-selected": isChecked(name),
+          }),
+          columns: [
+            {
+              "aria-label": Headers.Name,
+              content: (
+                <RowCheckbox
+                  checkSelected={isChecked}
+                  handleRowCheckbox={handleArchChange}
+                  item={name}
+                  items={disabled_boot_architectures}
+                  inputLabel={name}
+                />
+              ),
+            },
+            {
+              "aria-label": Headers.BootMethod,
+              content: bios_boot_method,
+            },
+            {
+              "aria-label": Headers.BootloaderArch,
+              content: bootloader_arches || "—",
+            },
+            {
+              "aria-label": Headers.Protocol,
+              content: protocol,
+            },
+            {
+              "aria-label": Headers.Octet,
+              content: arch_octet || "—",
+            },
+          ],
+          sortData: {
+            arch_octet,
+            bios_boot_method,
+            bootloader_arches,
+            name,
+            protocol,
+          },
+        };
+      })}
+      sortable
+    />
+  );
+};
+
+export default BootArchitecturesTable;
--- ui/src/app/subnets/views/SubnetDetails/SubnetDetailsHeader/SubnetActionForms/components/EditBootArchitectures/EditBootArchitectures.tsx
+++ ui/src/app/subnets/views/SubnetDetails/SubnetDetailsHeader/SubnetActionForms/components/EditBootArchitectures/EditBootArchitectures.tsx
@@ -1,15 +1,29 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
-import { Button, Spinner, Strip } from "@canonical/react-components";
+import { Spinner, Strip } from "@canonical/react-components";
 import { useDispatch, useSelector } from "react-redux";
+import * as Yup from "yup";
 
+import BootArchitecturesTable from "./BootArchitecturesTable";
+
+import FormikForm from "app/base/components/FormikForm";
 import { actions as generalActions } from "app/store/general";
 import { knownBootArchitectures as knownBootArchitecturesSelectors } from "app/store/general/selectors";
 import type { RootState } from "app/store/root/types";
+import { actions as subnetActions } from "app/store/subnet";
 import subnetSelectors from "app/store/subnet/selectors";
+import type { Subnet } from "app/store/subnet/types";
 import type { SubnetActionProps } from "app/subnets/views/SubnetDetails/types";
 
-export const MapSubnet = ({
+export type FormValues = {
+  disabled_boot_architectures: Subnet["disabled_boot_architectures"];
+};
+
+const Schema = Yup.object().shape({
+  disabled_boot_architectures: Yup.array().of(Yup.string()),
+});
+
+export const EditBootArchitectures = ({
   id,
   setActiveForm,
 }: Omit<SubnetActionProps, "activeForm">): JSX.Element | null => {
@@ -20,6 +34,10 @@ export const MapSubnet = ({
   const subnet = useSelector((state: RootState) =>
     subnetSelectors.getById(state, id)
   );
+  const errors = useSelector(subnetSelectors.errors);
+  const saved = useSelector(subnetSelectors.saved);
+  const saving = useSelector(subnetSelectors.saving);
+  const cleanup = useCallback(() => subnetActions.cleanup(), []);
 
   useEffect(() => {
     dispatch(generalActions.fetchKnownBootArchitectures());
@@ -34,7 +52,39 @@ export const MapSubnet = ({
   }
 
   const closeForm = () => setActiveForm(null);
-  return <Button onClick={closeForm}>Close</Button>;
+  return (
+    <FormikForm<FormValues>
+      buttonsBordered={false}
+      cleanup={cleanup}
+      errors={errors}
+      initialValues={{
+        disabled_boot_architectures: subnet.disabled_boot_architectures,
+      }}
+      onCancel={closeForm}
+      onSaveAnalytics={{
+        action: "Edit boot architectures",
+        category: "Subnet details",
+        label: "Edit boot architectures",
+      }}
+      onSubmit={(values) => {
+        dispatch(cleanup());
+        dispatch(
+          subnetActions.update({
+            disabled_boot_architectures:
+              values.disabled_boot_architectures.join(", "),
+            id: subnet.id,
+          })
+        );
+      }}
+      onSuccess={closeForm}
+      saved={saved}
+      saving={saving}
+      submitLabel="Save"
+      validationSchema={Schema}
+    >
+      <BootArchitecturesTable />
+    </FormikForm>
+  );
 };
 
-export default MapSubnet;
+export default EditBootArchitectures;
