4199c99aa477464e822f56b19682f1d3b66e1afa
Peter Makowski <peter@makowski.uk>
2022-02-10 09:45:06 +0100

    refactor: subnets build edit static route form

---
+++ ui/src/app/subnets/views/SubnetDetails/StaticRoutes/EditStaticRouteForm/EditStaticRouteForm.tsx
@@ -0,0 +1,149 @@
+import { useEffect } from "react";
+
+import { Col, Row, Select, Spinner } from "@canonical/react-components";
+import { useDispatch, useSelector } from "react-redux";
+import * as Yup from "yup";
+
+import { Labels } from "../StaticRoutes";
+
+import FormikField from "app/base/components/FormikField";
+import FormikForm from "app/base/components/FormikForm";
+import type { RootState } from "app/store/root/types";
+import { actions as staticRouteActions } from "app/store/staticroute";
+import staticRouteSelectors from "app/store/staticroute/selectors";
+import type { StaticRoute, StaticRouteMeta } from "app/store/staticroute/types";
+import { actions as subnetActions } from "app/store/subnet";
+import subnetSelectors from "app/store/subnet/selectors";
+import type { Subnet } from "app/store/subnet/types";
+import { toFormikNumber } from "app/utils";
+
+export type AddStaticRouteValues = Pick<
+  StaticRoute,
+  "source" | "destination" | "metric" | "gateway_ip"
+>;
+
+export enum EditStaticRouteFormLabels {
+  EditStaticRoute = "Edit static route",
+  Save = "Save",
+  Cancel = "Cancel",
+}
+
+const addStaticRouteSchema = Yup.object().shape({
+  gateway_ip: Yup.string().required("Gateway IP is required"),
+  destination: Yup.string().required("Destination is required"),
+  metric: Yup.number().required("Metric is required"),
+});
+
+const getDestinationsForSource = (subnets: Subnet[], source: Subnet | null) => {
+  const filtered: Subnet[] = [];
+  if (source) {
+    subnets.forEach((subnet) => {
+      if (subnet.id !== source.id && subnet.version === source.version) {
+        filtered.push(subnet);
+      }
+    });
+  }
+
+  return filtered;
+};
+
+export type Props = {
+  staticRouteId: StaticRoute[StaticRouteMeta.PK];
+  handleDismiss: () => void;
+};
+const EditStaticRouteForm = ({
+  staticRouteId,
+  handleDismiss,
+}: Props): JSX.Element | null => {
+  const staticRouteErrors = useSelector(staticRouteSelectors.errors);
+  const saving = useSelector(staticRouteSelectors.saving);
+  const saved = useSelector(staticRouteSelectors.saved);
+  const dispatch = useDispatch();
+  const staticRoutesLoading = useSelector(staticRouteSelectors.loading);
+  const subnetsLoading = useSelector(subnetSelectors.loading);
+  const staticRoutesLoaded = useSelector(staticRouteSelectors.loaded);
+  const subnetsLoaded = useSelector(subnetSelectors.loaded);
+  const loading = staticRoutesLoading || subnetsLoading;
+  const subnets = useSelector(subnetSelectors.all);
+  const staticRoute = useSelector((state: RootState) =>
+    staticRouteSelectors.getById(state, staticRouteId)
+  );
+  const source = useSelector((state: RootState) =>
+    subnetSelectors.getById(state, staticRoute?.source)
+  );
+  const destinationSubnets = getDestinationsForSource(subnets, source);
+
+  useEffect(() => {
+    if (!staticRoutesLoaded) dispatch(staticRouteActions.fetch());
+    if (!subnetsLoaded) dispatch(subnetActions.fetch());
+  }, [dispatch, staticRoutesLoaded, subnetsLoaded]);
+
+  if (!staticRoute || loading) {
+    return <Spinner text="Loading..." />;
+  }
+
+  return (
+    <FormikForm<AddStaticRouteValues>
+      aria-label={EditStaticRouteFormLabels.EditStaticRoute}
+      cleanup={staticRouteActions.cleanup}
+      errors={staticRouteErrors}
+      initialValues={{
+        source: staticRoute.source,
+        gateway_ip: staticRoute.gateway_ip,
+        destination: staticRoute.destination,
+        metric: staticRoute.metric,
+      }}
+      onSaveAnalytics={{
+        action: EditStaticRouteFormLabels.Save,
+        category: "Subnet",
+        label: EditStaticRouteFormLabels.EditStaticRoute,
+      }}
+      onSubmit={({ gateway_ip, destination, metric }) => {
+        dispatch(staticRouteActions.cleanup());
+        dispatch(
+          staticRouteActions.update({
+            id: staticRouteId,
+            source: staticRoute.source,
+            gateway_ip,
+            destination: toFormikNumber(destination) as number,
+            metric: toFormikNumber(metric),
+          })
+        );
+      }}
+      onSuccess={() => {
+        handleDismiss();
+      }}
+      resetOnSave
+      saving={saving}
+      saved={saved}
+      submitLabel={EditStaticRouteFormLabels.Save}
+      onCancel={handleDismiss}
+      validationSchema={addStaticRouteSchema}
+    >
+      <Row>
+        <Col size={4}>
+          <FormikField label={Labels.GatewayIp} name="gateway_ip" type="text" />
+        </Col>
+        <Col size={4}>
+          <FormikField
+            component={Select}
+            label={Labels.Destination}
+            name="destination"
+            options={[
+              { label: "Select destination", value: "", disabled: true },
+              ...destinationSubnets.map((subnet) => ({
+                label: subnet.cidr,
+                value: subnet.id,
+              })),
+            ]}
+          />
+        </Col>
+        <Col size={4}>
+          <FormikField label={Labels.Metric} name="metric" type="text" />
+        </Col>
+      </Row>
+    </FormikForm>
+  );
+};
+
+export default EditStaticRouteForm;
--- ui/src/app/subnets/views/SubnetDetails/StaticRoutes/StaticRoutes.test.tsx
+++ ui/src/app/subnets/views/SubnetDetails/StaticRoutes/StaticRoutes.test.tsx
@@ -5,6 +5,7 @@ import { MemoryRouter } from "react-router-dom";
 import configureStore from "redux-mock-store";
 
 import { AddStaticRouteFormLabels } from "./AddStaticRouteForm/AddStaticRouteForm";
+import { EditStaticRouteFormLabels } from "./EditStaticRouteForm/EditStaticRouteForm";
 import StaticRoutes, { Labels } from "./StaticRoutes";
 
 import {
@@ -127,6 +128,69 @@ it("can open and close the add static route form", async () => {
     expect(
       screen.queryByRole("form", {
         name: AddStaticRouteFormLabels.AddStaticRoute,
+      })
+    ).not.toBeInTheDocument()
+  );
+});
+
+it("can open and close the edit static route form", async () => {
+  const subnet = subnetFactory({ id: 1 });
+  const state = rootStateFactory({
+    user: userStateFactory({
+      auth: authStateFactory({
+        user: userFactory(),
+      }),
+      items: [userFactory(), userFactory(), userFactory()],
+    }),
+    staticroute: staticRouteStateFactory({
+      items: [],
+    }),
+    subnet: subnetStateFactory({
+      items: [subnet, subnetFactory({ id: 2 })],
+    }),
+  });
+
+  const store = mockStore(state);
+  render(
+    <Provider store={store}>
+      <MemoryRouter initialEntries={[{ pathname: "/" }]}>
+        <StaticRoutes subnetId={subnet.id} />
+      </MemoryRouter>
+    </Provider>
+  );
+  await waitFor(() =>
+    expect(
+      screen.getByRole("button", {
+        name: "Edit",
+      })
+    ).toBeInTheDocument()
+  );
+  userEvent.click(
+    screen.getByRole("button", {
+      name: "Edit",
+    })
+  );
+
+  await waitFor(() =>
+    expect(
+      screen.getByRole("form", {
+        name: EditStaticRouteFormLabels.EditStaticRoute,
+      })
+    ).toBeInTheDocument()
+  );
+
+  userEvent.click(
+    within(
+      screen.getByRole("form", {
+        name: EditStaticRouteFormLabels.EditStaticRoute,
+      })
+    ).getByRole("button", { name: "Cancel" })
+  );
+
+  await waitFor(() =>
+    expect(
+      screen.queryByRole("form", {
+        name: EditStaticRouteFormLabels.EditStaticRoute,
       })
     ).not.toBeInTheDocument()
   );
