a9497e1da1b37bc218d3e635dd2c2732fdac0bac
Peter Makowski <peter@makowski.uk>
2022-02-22 09:32:34 +0100

    fix(ui): oversized canonical banner

--- shared/src/components/Footer/Footer.tsx
+++ shared/src/components/Footer/Footer.tsx
@@ -74,7 +74,7 @@ export const Footer = ({
             Canonical are registered trademarks of Canonical Ltd.
           </p>
         </div>
-        <div className="col-2">
+        <div className="col-small-1 col-medium-1 col-2">
           <svg
             className="p-footer__logo u-float--right"
             xmlns="http://www.w3.org/2000/svg"
