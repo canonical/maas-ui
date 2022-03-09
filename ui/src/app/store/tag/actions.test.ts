a19d5ddb6ac9440ec640dc7eec52cba6c4e0733d
huwshimi <huwshimi@users.noreply.github.com>
2022-03-10 03:26:04 +1100

    feat(ui): add tag create, update and delete actions (#3667)
    Add actions and reducers to the tag slice to support create, update and delete.
    
    Fixes: canonical-web-and-design/app-tribe#712.

--- ui/src/app/store/tag/actions.test.ts
+++ ui/src/app/store/tag/actions.test.ts
@@ -11,4 +11,58 @@ describe("tag actions", () => {
       payload: null,
     });
   });
+
+  it("can create an action for creating a tag", () => {
+    const params = {
+      comment: "It's a tag",
+      definition: "tag def",
+      kernel_opts: "opts",
+      name: "tag1",
+    };
+    expect(actions.create(params)).toEqual({
+      type: "tag/create",
+      meta: {
+        model: "tag",
+        method: "create",
+      },
+      payload: {
+        params,
+      },
+    });
+  });
+
+  it("can create an action for deleting a tag", () => {
+    expect(actions.delete(1)).toEqual({
+      type: "tag/delete",
+      meta: {
+        model: "tag",
+        method: "delete",
+      },
+      payload: {
+        params: {
+          id: 1,
+        },
+      },
+    });
+  });
+
+  it("can create an action for updating a tag", () => {
+    const params = {
+      comment: "It's a tag",
+      definition: "tag def",
+      id: 1,
+      kernel_opts: "opts",
+      name: "tag1",
+    };
+    expect(actions.update(params)).toEqual({
+      type: "tag/update",
+      meta: {
+        model: "tag",
+        method: "update",
+      },
+      payload: {
+        params,
+      },
+    });
+  });
 });
--- ui/src/app/store/tag/slice.ts
+++ ui/src/app/store/tag/slice.ts
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 import { TagMeta } from "./types";
-import type { TagState } from "./types";
+import type { TagState, CreateParams, UpdateParams } from "./types";
 
 import {
   generateCommonReducers,
@@ -11,10 +11,12 @@ import {
 const tagSlice = createSlice({
   name: TagMeta.MODEL,
   initialState: genericInitialState as TagState,
-  reducers: generateCommonReducers<TagState, TagMeta.PK, void, void>(
-    TagMeta.MODEL,
-    TagMeta.PK
-  ),
+  reducers: generateCommonReducers<
+    TagState,
+    TagMeta.PK,
+    CreateParams,
+    UpdateParams
+  >(TagMeta.MODEL, TagMeta.PK),
 });
 
 export const { actions } = tagSlice;
