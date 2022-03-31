78ab2334eadc92c537c1653c8dc84dbef9388499
Caleb Ellis <caleb.ellis@canonical.com>
2022-03-31 10:09:55 +1000

    feat(ui): update machine config form to latest design (#3735)
    Removes tags from Machine configuration form and move fields to center of page

--- ui/src/app/machines/views/MachineDetails/MachineConfiguration/MachineForm/MachineForm.test.tsx
+++ ui/src/app/machines/views/MachineDetails/MachineConfiguration/MachineForm/MachineForm.test.tsx
@@ -1,249 +1,124 @@
-import { mount } from "enzyme";
-import { act } from "react-dom/test-utils";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 
 import MachineForm from "./MachineForm";
 
+import { actions as machineActions } from "app/store/machine";
+import type { RootState } from "app/store/root/types";
 import {
+  architecturesState as architecturesStateFactory,
+  generalState as generalStateFactory,
   machineDetails as machineDetailsFactory,
   machineState as machineStateFactory,
   machineStatus as machineStatusFactory,
   machineStatuses as machineStatusesFactory,
   rootState as rootStateFactory,
 } from "testing/factories";
-import { submitFormikForm } from "testing/utils";
 
 const mockStore = configureStore();
 
 describe("MachineForm", () => {
+  let state: RootState;
+
+  beforeEach(() => {
+    state = rootStateFactory({
+      general: generalStateFactory({
+        architectures: architecturesStateFactory({
+          data: ["amd64"],
+        }),
+      }),
+      machine: machineStateFactory({
+        items: [
+          machineDetailsFactory({
+            permissions: ["edit"],
+            system_id: "abc123",
+          }),
+        ],
+        statuses: machineStatusesFactory({
+          abc123: machineStatusFactory(),
+        }),
+      }),
+    });
+  });
+
   it("is not editable if machine does not have edit permission", () => {
-    const machine = machineDetailsFactory({
-      permissions: [],
-      system_id: "abc123",
-    });
-    const state = rootStateFactory({
-      machine: machineStateFactory({
-        items: [machine],
-        statuses: machineStatusesFactory({
-          abc123: machineStatusFactory(),
-        }),
-      }),
-    });
+    state.machine.items[0].permissions = [];
     const store = mockStore(state);
-    const wrapper = mount(
+    render(
       <Provider store={store}>
         <MachineForm systemId="abc123" />
       </Provider>
     );
 
     expect(
-      wrapper.find("button[data-testid='edit-machine-config']").exists()
-    ).toBe(false);
+      screen.queryByRole("button", { name: "Edit" })
+    ).not.toBeInTheDocument();
   });
 
-  it("is disabled until the edit button is pressed", () => {
-    const machine = machineDetailsFactory({
-      permissions: ["edit"],
-      system_id: "abc123",
-    });
-    const state = rootStateFactory({
-      machine: machineStateFactory({
-        items: [machine],
-        statuses: machineStatusesFactory({
-          abc123: machineStatusFactory(),
-        }),
-      }),
-    });
+  it("is editable if machine has edit permission", () => {
+    state.machine.items[0].permissions = ["edit"];
     const store = mockStore(state);
-    const wrapper = mount(
-      <Provider store={store}>
-        <MachineForm systemId="abc123" />
-      </Provider>
-    );
-    expect(wrapper.find("FormikForm").prop("editable")).toBe(false);
-
-    wrapper.find("button[data-testid='edit-machine-config']").simulate("click");
-    expect(wrapper.find("FormikForm").prop("editable")).toBe(true);
-  });
-
-  it("only shows errors if the form is in editing state", () => {
-    const machine = machineDetailsFactory({
-      permissions: ["edit"],
-      system_id: "abc123",
-    });
-    const state = rootStateFactory({
-      machine: machineStateFactory({
-        errors: "Everything is ruined",
-        items: [machine],
-        statuses: machineStatusesFactory({
-          abc123: machineStatusFactory(),
-        }),
-      }),
-    });
-    const store = mockStore(state);
-    const wrapper = mount(
-      <Provider store={store}>
-        <MachineForm systemId="abc123" />
-      </Provider>
-    );
-    const notificationExists = () =>
-      wrapper
-        .findWhere(
-          (n) =>
-            n.name() === "Notification" &&
-            n.text().includes("Everything is ruined")
-        )
-        .exists();
-    expect(notificationExists()).toBe(false);
-
-    // Click "Edit" button
-    wrapper.find("button[data-testid='edit-machine-config']").simulate("click");
-    expect(notificationExists()).toBe(true);
-  });
-
-  it("resets the form fields when editing is cancelled", async () => {
-    const machine = machineDetailsFactory({
-      description: "I'm a description!",
-      permissions: ["edit"],
-      system_id: "abc123",
-    });
-    const state = rootStateFactory({
-      machine: machineStateFactory({
-        items: [machine],
-        statuses: machineStatusesFactory({
-          abc123: machineStatusFactory(),
-        }),
-      }),
-    });
-    const store = mockStore(state);
-    const wrapper = mount(
+    render(
       <Provider store={store}>
         <MachineForm systemId="abc123" />
       </Provider>
     );
 
-    // Check that the description is initialised correctly.
-    expect(wrapper.find("input[name='description']").prop("value")).toBe(
-      "I'm a description!"
-    );
-
-    // Get into editing state and change the description.
-    await act(async () => {
-      wrapper
-        .find("button[data-testid='edit-machine-config']")
-        .simulate("click");
-      wrapper.find("input[name='description']").simulate("change", {
-        target: { name: "description", value: "I'm not a description" },
-      });
-    });
-    wrapper.update();
-
-    // Check that description has changed.
-    expect(wrapper.find("input[name='description']").prop("value")).toBe(
-      "I'm not a description"
-    );
-
-    // Click the "Cancel" button
-    await act(async () => {
-      wrapper.find("button[data-testid='cancel-action']").simulate("click");
-    });
-    wrapper.update();
-
-    // Check that the description is reverted to its initial value.
-    expect(wrapper.find("input[name='description']").prop("value")).toBe(
-      "I'm a description!"
-    );
+    expect(screen.getAllByRole("button", { name: "Edit" }).length).not.toBe(0);
   });
 
-  it("dispatches cleanup action on cancel", () => {
-    const machine = machineDetailsFactory({
-      permissions: ["edit"],
-      system_id: "abc123",
-    });
-    const state = rootStateFactory({
-      machine: machineStateFactory({
-        items: [machine],
-        statuses: machineStatusesFactory({
-          abc123: machineStatusFactory(),
-        }),
-      }),
-    });
+  it("renders read-only text fields until edit button is pressed", () => {
     const store = mockStore(state);
-    const wrapper = mount(
+    render(
       <Provider store={store}>
         <MachineForm systemId="abc123" />
       </Provider>
     );
 
-    // Get into editing state and then cancel editing.
-    act(() => {
-      wrapper
-        .find("button[data-testid='edit-machine-config']")
-        .simulate("click");
-    });
-    wrapper.update();
-    act(() => {
-      wrapper.find("button[data-testid='cancel-action']").simulate("click");
-    });
-    wrapper.update();
+    expect(screen.queryByRole("textbox")).not.toBeInTheDocument();
 
-    // Check that the cleanup action was dispatched.
-    expect(
-      store.getActions().some((action) => action.type === "machine/cleanup")
-    );
+    userEvent.click(screen.getAllByRole("button", { name: "Edit" })[0]);
+
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
   });
 
-  it("correctly dispatches an action to update a machine", () => {
+  it("correctly dispatches an action to update a machine", async () => {
     const machine = machineDetailsFactory({
+      architecture: "amd64",
       permissions: ["edit"],
       system_id: "abc123",
     });
-    const state = rootStateFactory({
-      machine: machineStateFactory({
-        items: [machine],
-        statuses: machineStatusesFactory({
-          abc123: machineStatusFactory(),
-        }),
-      }),
-    });
+    state.machine.items = [machine];
     const store = mockStore(state);
-    const wrapper = mount(
+    render(
       <Provider store={store}>
         <MachineForm systemId="abc123" />
       </Provider>
     );
 
-    submitFormikForm(wrapper, {
-      architecture: "amd64/generic",
-      description: "It's on",
-      minHweKernel: "groovy (ga-20.10)",
-      pool: "swimming",
-      tags: [1, 2],
-      zone: "twilight",
-    });
+    userEvent.click(screen.getAllByRole("button", { name: "Edit" })[0]);
+    const noteField = screen.getByRole("textbox", { name: "Note" });
+    userEvent.clear(noteField);
+    userEvent.type(noteField, "New note");
+    userEvent.click(screen.getByRole("button", { name: "Save changes" }));
 
-    expect(
-      store.getActions().find((action) => action.type === "machine/update")
-    ).toStrictEqual({
-      meta: {
-        method: "update",
-        model: "machine",
-      },
-      payload: {
-        params: {
-          architecture: "amd64/generic",
-          description: "It's on",
-          extra_macs: machine.extra_macs,
-          min_hwe_kernel: "groovy (ga-20.10)",
-          pool: { name: "swimming" },
-          pxe_mac: machine.pxe_mac,
-          system_id: machine.system_id,
-          tags: [1, 2],
-          zone: { name: "twilight" },
-        },
-      },
-      type: "machine/update",
+    const expectedAction = machineActions.update({
+      architecture: machine.architecture,
+      description: "New note",
+      extra_macs: machine.extra_macs,
+      min_hwe_kernel: machine.min_hwe_kernel,
+      pool: { name: machine.pool.name },
+      pxe_mac: machine.pxe_mac,
+      system_id: machine.system_id,
+      zone: { name: machine.zone.name },
+    });
+    const actualActions = store.getActions();
+    await waitFor(() => {
+      expect(
+        actualActions.find((action) => action.type === expectedAction.type)
+      ).toStrictEqual(expectedAction);
     });
   });
 });
--- ui/src/app/machines/views/MachineDetails/MachineConfiguration/MachineForm/MachineFormFields/MachineFormFields.tsx
+++ ui/src/app/machines/views/MachineDetails/MachineConfiguration/MachineForm/MachineFormFields/MachineFormFields.tsx
@@ -1,59 +1,20 @@
-import { Col, Row } from "@canonical/react-components";
-import { useFormikContext } from "formik";
-import { useSelector } from "react-redux";
-
-import type { MachineFormValues } from "../MachineForm";
+import { Col, Row, Textarea } from "@canonical/react-components";
 
 import ArchitectureSelect from "app/base/components/ArchitectureSelect";
 import FormikField from "app/base/components/FormikField";
 import MinimumKernelSelect from "app/base/components/MinimumKernelSelect";
 import ResourcePoolSelect from "app/base/components/ResourcePoolSelect";
-import TagIdField from "app/base/components/TagIdField";
-import TagLinks from "app/base/components/TagLinks";
 import ZoneSelect from "app/base/components/ZoneSelect";
-import machineURLs from "app/machines/urls";
-import { FilterMachines } from "app/store/machine/utils";
-import type { RootState } from "app/store/root/types";
-import tagSelectors from "app/store/tag/selectors";
 
-type Props = { editing: boolean };
-
-const MachineFormFields = ({ editing }: Props): JSX.Element => {
-  const tags = useSelector(tagSelectors.all);
-  const { initialValues } = useFormikContext<MachineFormValues>();
-  const initialTags = useSelector((state: RootState) =>
-    tagSelectors.getByIDs(state, initialValues.tags)
-  );
-
+const MachineFormFields = (): JSX.Element => {
   return (
     <Row>
       <Col size={6}>
-        <ArchitectureSelect disabled={!editing} name="architecture" />
-        <MinimumKernelSelect disabled={!editing} name="minHweKernel" />
-        <ZoneSelect disabled={!editing} name="zone" />
-        <ResourcePoolSelect disabled={!editing} name="pool" />
-        <FormikField
-          disabled={!editing}
-          label="Note"
-          name="description"
-          type="text"
-        />
-        {editing ? (
-          <TagIdField tagList={tags} />
-        ) : (
-          <>
-            <p>Tags</p>
-            <TagLinks
-              getLinkURL={(tag) => {
-                const filter = FilterMachines.filtersToQueryString({
-                  tags: [`=${tag.name}`],
-                });
-                return `${machineURLs.machines.index}${filter}`;
-              }}
-              tags={initialTags}
-            />
-          </>
-        )}
+        <ArchitectureSelect name="architecture" />
+        <MinimumKernelSelect name="minHweKernel" />
+        <ZoneSelect name="zone" />
+        <ResourcePoolSelect name="pool" />
+        <FormikField component={Textarea} label="Note" name="description" />
       </Col>
     </Row>
   );
