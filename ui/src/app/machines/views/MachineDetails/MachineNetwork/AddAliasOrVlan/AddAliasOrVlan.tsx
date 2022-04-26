011cf6138666569e61991f880e723da3c5649310
Caleb Ellis <caleb.ellis@canonical.com>
2022-04-26 16:37:34 +1000

    fix(ui): select correct fabric/VLAN when adding an alias or VLAN to nic (#3796) (#3845)

--- ui/src/app/machines/views/MachineDetails/MachineNetwork/AddAliasOrVlan/AddAliasOrVlan.tsx
+++ ui/src/app/machines/views/MachineDetails/MachineNetwork/AddAliasOrVlan/AddAliasOrVlan.tsx
@@ -54,6 +54,9 @@ const AddAliasOrVlan = ({
   const unusedVLANs = useSelector((state: RootState) =>
     vlanSelectors.getUnusedForInterface(state, machine, nic)
   );
+  const nicVLAN = useSelector((state: RootState) =>
+    vlanSelectors.getById(state, nic?.vlan_id)
+  );
   const cleanup = useCallback(() => machineActions.cleanup(), []);
   const isAlias = interfaceType === NetworkInterfaceTypes.ALIAS;
   const { errors, saved, saving } = useMachineDetailsForm(
@@ -84,6 +87,8 @@ const AddAliasOrVlan = ({
         initialValues={{
           ...networkFieldsInitialValues,
           ...(isAlias ? {} : { tags: [] }),
+          fabric: nicVLAN?.fabric || "",
+          vlan: nic.vlan_id,
         }}
         onSaveAnalytics={{
           action: `Add ${interfaceType}`,
