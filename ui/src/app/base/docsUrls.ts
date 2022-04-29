de6e04b0cc334446a35d9e90d3264b4fb106c3bb
Peter Makowski <peter.makowski@canonical.com>
2022-04-29 08:14:08 +0200

    Apply suggestions from code review
    Co-authored-by: Caleb Ellis <calebellis2805@gmail.com>

--- ui/src/app/base/docsUrls.ts
+++ ui/src/app/base/docsUrls.ts
@@ -1,20 +1,20 @@
 const docsUrls = {
   customisingDeployedMachines:
     "https://maas.io/docs/how-to-customise-machines#heading--about-customising-deployed-machines",
-  dhcp: "https://maas.io/docs/dhcp",
-  sshKeys: "https://maas.io/docs/user-accounts#heading--ssh-keys",
-  rackController: "https://maas.io/docs/rack-controller",
-  networkDiscovery: "https://maas.io/docs/network-discovery",
-  configurationJourney: "https://maas.io/docs/configuration-journey",
+  dhcp: "https://maas.io/docs/how-to-manage-dhcp",
+  sshKeys: "https://maas.io/docs/how-to-manage-user-accounts#heading--ssh-keys",
+  rackController: "https://maas.io/docs/how-to-manage-racks",
+  networkDiscovery: "https://maas.io/docs/about-networking#heading--about-network-discovery",
+  configurationJourney: "https://maas.io/docs/how-to-install-maas#heading--configure-maas",
   cloudInit:
-    "https://maas.io/docs/custom-node-setup-preseed#heading--cloud-init",
-  kvmIntroduction: "https://maas.io/docs/kvm-introduction",
+    "https://maas.io/docs/how-to-customise-machines#heading--cloud-init",
+  kvmIntroduction: "https://maas.io/docs/about-vm-hosting",
   addNodesViaChassis:
-    "https://maas.io/docs/add-machines#heading--add-nodes-via-a-chassis",
-  addMachines: "https://maas.io/docs/add-machines",
-  images: "https://maas.io/docs/images",
-  ipmi: "https://maas.io/docs/snap/2.9/ui/power-management#heading--ipmi",
-  ipRanges: "https://maas.io/docs/ip-ranges",
+    "https://maas.io/docs/how-to-manage-machines#heading--how-to-add-machines-via-a-chassis",
+  addMachines: "https://maas.io/docs/how-to-manage-machines#heading--how-to-create-delete-and-configure-machines",
+  images: "https://maas.io/docs/about-images",
+  ipmi: "https://maas.io/docs/power-management-reference#heading--ipmi",
+  ipRanges: "https://maas.io/docs/how-to-manage-ip-ranges",
   tagsXpathExpressions:
     "https://maas.io/docs/how-to-work-with-tags#heading--xpath-expressions",
   tagsKernelOptions:
