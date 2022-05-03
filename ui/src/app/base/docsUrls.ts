409f0592a6faf53b38cf1f17deff308d22d4bc99
Caleb Ellis <caleb.ellis@canonical.com>
2022-05-03 15:05:07 +1000

    feat(ui): add docs links to native TLS pages

--- ui/src/app/base/docsUrls.ts
+++ ui/src/app/base/docsUrls.ts
@@ -1,29 +1,33 @@
 const docsUrls = {
+  aboutNativeTLS:
+    "https://maas.io/docs/how-to-enable-tls-encryption#heading--about-maas-native-tls",
+  addMachines:
+    "https://maas.io/docs/how-to-manage-machines#heading--how-to-create-delete-and-configure-machines",
+  addNodesViaChassis:
+    "https://maas.io/docs/how-to-manage-machines#heading--how-to-add-machines-via-a-chassis",
+  autoRenewTLSCert:
+    "https://maas.io/docs/how-to-enable-tls-encryption#heading--about-auto-renewal-for-certificates",
+  cloudInit:
+    "https://maas.io/docs/how-to-customise-machines#heading--cloud-init",
+  configurationJourney:
+    "https://maas.io/docs/how-to-install-maas#heading--configure-maas",
   customisingDeployedMachines:
     "https://maas.io/docs/about-customising-machines#heading--about-customising-deployed-machines",
   dhcp: "https://maas.io/docs/how-to-manage-dhcp",
-  sshKeys: "https://maas.io/docs/how-to-manage-user-accounts#heading--ssh-keys",
-  rackController: "https://maas.io/docs/how-to-manage-racks",
-  networkDiscovery:
-    "https://maas.io/docs/about-networking#heading--about-network-discovery",
-  configurationJourney:
-    "https://maas.io/docs/how-to-install-maas#heading--configure-maas",
-  cloudInit:
-    "https://maas.io/docs/how-to-customise-machines#heading--cloud-init",
-  kvmIntroduction: "https://maas.io/docs/about-vm-hosting",
-  addNodesViaChassis:
-    "https://maas.io/docs/how-to-manage-machines#heading--how-to-add-machines-via-a-chassis",
-  addMachines:
-    "https://maas.io/docs/how-to-manage-machines#heading--how-to-create-delete-and-configure-machines",
   images: "https://maas.io/docs/about-images",
   ipmi: "https://maas.io/docs/power-management-reference#heading--ipmi",
   ipRanges: "https://maas.io/docs/how-to-manage-ip-ranges",
-  tagsXpathExpressions:
-    "https://maas.io/docs/how-to-work-with-tags#heading--xpath-expressions",
-  tagsKernelOptions:
-    "https://maas.io/docs/how-to-work-with-tags#heading--kernel-options",
+  kvmIntroduction: "https://maas.io/docs/about-vm-hosting",
+  networkDiscovery:
+    "https://maas.io/docs/about-networking#heading--about-network-discovery",
+  rackController: "https://maas.io/docs/how-to-manage-racks",
+  sshKeys: "https://maas.io/docs/how-to-manage-user-accounts#heading--ssh-keys",
   tagsAutomatic:
     "https://maas.io/docs/how-to-work-with-tags#heading--automatic-tags",
-};
+  tagsKernelOptions:
+    "https://maas.io/docs/how-to-work-with-tags#heading--kernel-options",
+  tagsXpathExpressions:
+    "https://maas.io/docs/how-to-work-with-tags#heading--xpath-expressions",
+} as const;
 
 export default docsUrls;
