93019391f9be05fadcc98119733d421820b3d76a
Peter Makowski <peter@makowski.uk>
2022-05-04 10:34:56 +0200

    add e2e test for device tagging

--- integration/cypress/integration/with-users/devices/list.spec.ts
+++ integration/cypress/integration/with-users/devices/list.spec.ts
@@ -1,4 +1,5 @@
 import { generateNewURL } from "@maas-ui/maas-ui-shared";
+import { generateMac } from "../utils";
 
 context("Device listing", () => {
   beforeEach(() => {
@@ -16,5 +17,38 @@ context("Device listing", () => {
       "href",
       generateNewURL("/devices")
     );
+  });
+
+  it("can add a tag to the device", () => {
+    // can add a device
+    cy.findByRole("button", { name: /Add device/ }).click();
+    const mac = generateMac();
+    cy.findByLabelText(/Device name/).type("cypress-device");
+    cy.get("input[placeholder='00:00:00:00:00:00']").type(mac);
+    cy.findByRole("button", { name: /Save device/ }).click();
+
+    // can view device details on click of device details link
+    cy.findByRole("link", { name: /cypress-device/ }).click();
+
+    cy.findByRole("link", {
+      name: /Configuration/,
+    }).click();
+    cy.findByRole("button", {
+      name: /Edit/,
+    }).click();
+    cy.get("input[placeholder='Create or remove tags']").type("device-tag");
+
+    cy.findByRole("button", {
+      name: /Create tag "device-tag"/,
+    }).click();
+
+    cy.findByRole("button", {
+      name: /Create and add to tag changes/,
+    }).click();
+
+    cy.findByRole("button", { name: /Save changes/ }).click();
+
+    cy.findByRole("link", { name: /Summary/ }).click();
+    cy.findByText(/device-tag/).should("exist");
   });
 });
