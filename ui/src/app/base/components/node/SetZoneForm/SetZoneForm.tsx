b457d0f37b89b332819644d6a002483a7b21eaf1
Caleb Ellis <caleb.ellis@canonical.com>
2022-05-05 16:08:10 +1000

    fix(ui): initialise zone in node set zone form

--- ui/src/app/base/components/node/SetZoneForm/SetZoneForm.tsx
+++ ui/src/app/base/components/node/SetZoneForm/SetZoneForm.tsx
@@ -5,7 +5,9 @@ import type { NodeActionFormProps } from "../types";
 
 import ActionForm from "app/base/components/ActionForm";
 import ZoneSelect from "app/base/components/ZoneSelect";
+import type { Node } from "app/store/types/node";
 import { NodeActions } from "app/store/types/node";
+import { nodeIsDevice, nodeIsMachine } from "app/store/utils";
 import type { Zone } from "app/store/zone/types";
 import { capitaliseFirst } from "app/utils";
 
@@ -15,6 +17,16 @@ type Props<E = null> = NodeActionFormProps<E> & {
 
 export type SetZoneFormValues = {
   zone: Zone["id"] | "";
+};
+
+const getInitialZoneValue = (nodes: Node[]): SetZoneFormValues["zone"] => {
+  if (
+    nodes.length === 1 &&
+    (nodeIsDevice(nodes[0]) || nodeIsMachine(nodes[0]))
+  ) {
+    return nodes[0].zone.id;
+  }
+  return "";
 };
 
 const SetZoneSchema = Yup.object().shape({
@@ -36,7 +48,9 @@ export const SetZoneForm = <E,>({
       actionName={NodeActions.SET_ZONE}
       cleanup={cleanup}
       errors={errors}
-      initialValues={{ zone: "" }}
+      initialValues={{
+        zone: getInitialZoneValue(nodes),
+      }}
       modelName={modelName}
       onCancel={clearHeaderContent}
       onSaveAnalytics={{
