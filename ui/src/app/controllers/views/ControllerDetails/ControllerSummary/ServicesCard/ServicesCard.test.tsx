c0f0fba218c3c15f075115237f3fee43e6b68518
Caleb Ellis <caleb.ellis@canonical.com>
2022-06-06 14:57:19 +1000

    feat(ui): build controller services card (#3993)

--- ui/src/app/controllers/views/ControllerDetails/ControllerSummary/ServicesCard/ServicesCard.test.tsx
+++ ui/src/app/controllers/views/ControllerDetails/ControllerSummary/ServicesCard/ServicesCard.test.tsx
@@ -5,10 +5,14 @@ import configureStore from "redux-mock-store";
 import ServicesCard from "./ServicesCard";
 
 import { actions as serviceActions } from "app/store/service";
+import { ServiceName, ServiceStatus } from "app/store/service/types";
+import { getServiceDisplayName } from "app/store/service/utils";
+import { NodeType } from "app/store/types/node";
 import {
   controllerDetails as controllerDetailsFactory,
   controllerState as controllerStateFactory,
   rootState as rootStateFactory,
+  service as serviceFactory,
   serviceState as serviceStateFactory,
 } from "testing/factories";
 
@@ -55,5 +59,202 @@ it("shows a spinner if services are loading", () => {
 
   expect(
     screen.getByRole("alert", { name: "Loading services" })
+  ).toBeInTheDocument();
+});
+
+it("renders the title correctly when at least one service is dead", () => {
+  const services = [
+    serviceFactory({ status: ServiceStatus.DEAD }),
+    serviceFactory({ status: ServiceStatus.DEGRADED }),
+    serviceFactory({ status: ServiceStatus.DEGRADED }),
+    serviceFactory({ status: ServiceStatus.RUNNING }),
+    serviceFactory({ status: ServiceStatus.RUNNING }),
+    serviceFactory({ status: ServiceStatus.RUNNING }),
+    serviceFactory({ status: ServiceStatus.OFF }),
+    serviceFactory({ status: ServiceStatus.UNKNOWN }),
+  ];
+  const controller = controllerDetailsFactory({
+    service_ids: services.map(({ id }) => id),
+  });
+  const state = rootStateFactory({
+    controller: controllerStateFactory({
+      items: [controller],
+    }),
+    service: serviceStateFactory({
+      items: services,
+    }),
+  });
+  const store = mockStore(state);
+
+  render(
+    <Provider store={store}>
+      <ServicesCard controller={controller} />
+    </Provider>
+  );
+
+  expect(screen.getByTestId("title")).toHaveTextContent(
+    "1 dead, 2 degraded, 3 running"
+  );
+  expect(screen.getByTestId("title-icon")).toHaveAccessibleName("error");
+});
+
+it("renders the title corectly when at least one service is degraded and none are dead", () => {
+  const services = [
+    serviceFactory({ status: ServiceStatus.DEGRADED }),
+    serviceFactory({ status: ServiceStatus.RUNNING }),
+    serviceFactory({ status: ServiceStatus.RUNNING }),
+    serviceFactory({ status: ServiceStatus.OFF }),
+    serviceFactory({ status: ServiceStatus.UNKNOWN }),
+  ];
+  const controller = controllerDetailsFactory({
+    service_ids: services.map(({ id }) => id),
+  });
+  const state = rootStateFactory({
+    controller: controllerStateFactory({
+      items: [controller],
+    }),
+    service: serviceStateFactory({
+      items: services,
+    }),
+  });
+  const store = mockStore(state);
+
+  render(
+    <Provider store={store}>
+      <ServicesCard controller={controller} />
+    </Provider>
+  );
+
+  expect(screen.getByTestId("title")).toHaveTextContent(
+    "1 degraded, 2 running"
+  );
+  expect(screen.getByTestId("title-icon")).toHaveAccessibleName("warning");
+});
+
+it("renders the title corectly when at least one service is running and none are dead or degraded", () => {
+  const services = [
+    serviceFactory({ status: ServiceStatus.RUNNING }),
+    serviceFactory({ status: ServiceStatus.OFF }),
+    serviceFactory({ status: ServiceStatus.UNKNOWN }),
+  ];
+  const controller = controllerDetailsFactory({
+    service_ids: services.map(({ id }) => id),
+  });
+  const state = rootStateFactory({
+    controller: controllerStateFactory({
+      items: [controller],
+    }),
+    service: serviceStateFactory({
+      items: services,
+    }),
+  });
+  const store = mockStore(state);
+
+  render(
+    <Provider store={store}>
+      <ServicesCard controller={controller} />
+    </Provider>
+  );
+
+  expect(screen.getByTestId("title")).toHaveTextContent("1 running");
+  expect(screen.getByTestId("title-icon")).toHaveAccessibleName("success");
+});
+
+it("only renders rack controller services for a rack controller", () => {
+  const services = [
+    serviceFactory({ name: ServiceName.RACKD }),
+    serviceFactory({ name: ServiceName.REGIOND }),
+  ];
+  const controller = controllerDetailsFactory({
+    node_type: NodeType.RACK_CONTROLLER,
+    service_ids: services.map(({ id }) => id),
+  });
+  const state = rootStateFactory({
+    controller: controllerStateFactory({
+      items: [controller],
+    }),
+    service: serviceStateFactory({
+      items: services,
+    }),
+  });
+  const store = mockStore(state);
+
+  render(
+    <Provider store={store}>
+      <ServicesCard controller={controller} />
+    </Provider>
+  );
+
+  expect(
+    screen.getByText(getServiceDisplayName(ServiceName.RACKD))
+  ).toBeInTheDocument();
+  expect(
+    screen.queryByText(getServiceDisplayName(ServiceName.REGIOND))
+  ).not.toBeInTheDocument();
+});
+
+it("only renders region controller services for a region controller", () => {
+  const services = [
+    serviceFactory({ name: ServiceName.RACKD }),
+    serviceFactory({ name: ServiceName.REGIOND }),
+  ];
+  const controller = controllerDetailsFactory({
+    node_type: NodeType.REGION_CONTROLLER,
+    service_ids: services.map(({ id }) => id),
+  });
+  const state = rootStateFactory({
+    controller: controllerStateFactory({
+      items: [controller],
+    }),
+    service: serviceStateFactory({
+      items: services,
+    }),
+  });
+  const store = mockStore(state);
+
+  render(
+    <Provider store={store}>
+      <ServicesCard controller={controller} />
+    </Provider>
+  );
+
+  expect(
+    screen.getByText(getServiceDisplayName(ServiceName.REGIOND))
+  ).toBeInTheDocument();
+  expect(
+    screen.queryByText(getServiceDisplayName(ServiceName.RACKD))
+  ).not.toBeInTheDocument();
+});
+
+it("renders both region and rack controller services for a region+rack controller", () => {
+  const services = [
+    serviceFactory({ name: ServiceName.RACKD }),
+    serviceFactory({ name: ServiceName.REGIOND }),
+  ];
+  const controller = controllerDetailsFactory({
+    node_type: NodeType.REGION_AND_RACK_CONTROLLER,
+    service_ids: services.map(({ id }) => id),
+  });
+  const state = rootStateFactory({
+    controller: controllerStateFactory({
+      items: [controller],
+    }),
+    service: serviceStateFactory({
+      items: services,
+    }),
+  });
+  const store = mockStore(state);
+
+  render(
+    <Provider store={store}>
+      <ServicesCard controller={controller} />
+    </Provider>
+  );
+
+  expect(
+    screen.getByText(getServiceDisplayName(ServiceName.REGIOND))
+  ).toBeInTheDocument();
+  expect(
+    screen.getByText(getServiceDisplayName(ServiceName.RACKD))
   ).toBeInTheDocument();
 });
--- ui/src/testing/factories/service.ts
+++ ui/src/testing/factories/service.ts
@@ -2,12 +2,12 @@ import { extend } from "cooky-cutter";
 
 import { model } from "./model";
 
-import { ServiceStatus } from "app/store/service/types";
+import { ServiceName, ServiceStatus } from "app/store/service/types";
 import type { Service } from "app/store/service/types";
 import type { Model } from "app/store/types/model";
 
 export const service = extend<Model, Service>(model, {
-  name: (i: number) => `test service ${i}`,
+  name: ServiceName.PROXY_RACK,
   status: ServiceStatus.RUNNING,
   status_info: "test info",
 });
