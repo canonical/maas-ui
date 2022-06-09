6f6d9946c02f76a45ac00798e4f949f9cb73cd77
Huw Wilkins <huw.wilkins@canonical.com>
2022-06-10 09:41:51 +1000

    refactor(shared): remove the shared workspace
    Remove the shared workspace as it is no longer required.
    
    Fixes: canonical-web-and-design/app-tribe#1019.

--- shared/src/types.ts
+++
@@ -1,9 +0,0 @@
-export type TSFixMe = any; // eslint-disable-line @typescript-eslint/no-explicit-any
-
-type UsabillaConfig =
-  | string
-  | {
-      [x: string]: unknown;
-    };
-
-export type UsabillaLive = (type: string, config?: UsabillaConfig) => void;
