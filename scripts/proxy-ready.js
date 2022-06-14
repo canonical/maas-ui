ad3e1b1d264595bddc0b6ef162a058a44c875b76
huwshimi <huwshimi@users.noreply.github.com>
2022-06-14 16:15:59 +1000

    fix(proxy): fix watching proxy files (#4090)
    Use the correct path to watch the proxy files.
    
    Fixes: canonical-web-and-design/app-tribe#1033.

--- scripts/proxy-ready.js
+++ scripts/proxy-ready.js
@@ -1,11 +1,17 @@
-const address = require('address');
-const colors = require('colors');
+const address = require("address");
+require("colors");
 
 console.log("");
 console.log("*****************");
 console.log("");
-console.log("Wait for the React dev server to start and then visit:".red, `http://${address.ip()}:8400`.blue);
-console.log("The URL displayed by the React dev server message will be incorrect.".red);
+console.log(
+  "The MAAS web client is now available at:",
+  `http://${address.ip()}:8400`.blue
+);
+console.log(
+  "Note: the URL displayed by the React dev server message will be incorrect."
+    .red
+);
 console.log("");
 console.log("*****************");
 console.log("");
