b400dbd296935041e81518371ee3384ac04b4c41
Peter Makowski <peter@makowski.uk>
2022-06-21 15:11:14 +0200

    fix: rack controller instructions for correct version (#4096)

--- src/app/controllers/components/ControllerHeaderForms/AddController/AddController.tsx
+++ src/app/controllers/components/ControllerHeaderForms/AddController/AddController.tsx
@@ -1,9 +1,13 @@
+import type { ChangeEvent } from "react";
+import { useState } from "react";
+
 import { Button, CodeSnippet, Col, Row } from "@canonical/react-components";
 import { useSelector } from "react-redux";
 
 import docsUrls from "app/base/docsUrls";
 import type { ClearHeaderContent } from "app/base/types";
 import configSelectors from "app/store/config/selectors";
+import { version as versionSelectors } from "app/store/general/selectors";
 
 type Props = {
   clearHeaderContent: ClearHeaderContent;
@@ -12,50 +16,84 @@ type Props = {
 export const AddController = ({ clearHeaderContent }: Props): JSX.Element => {
   const maasUrl = useSelector(configSelectors.maasUrl);
   const rpcSharedSecret = useSelector(configSelectors.rpcSharedSecret);
+  const minorVersion = useSelector(versionSelectors.minor);
+  const [variant, setVariant] = useState("Snap");
+
+  const variantDropdown = {
+    "aria-label": "version",
+    value: variant,
+    onChange: (e: ChangeEvent<HTMLSelectElement>) => {
+      setVariant(e.target.value);
+    },
+    options: [
+      {
+        label: `v${minorVersion} Snap`,
+        value: "snap",
+      },
+      {
+        label: `v${minorVersion} Packages`,
+        value: "packages",
+      },
+    ],
+  };
+  const dropdowns = [variantDropdown];
 
   return (
     <>
       <p>
-        To add a new rack controller, SSH into the rack controller. Install the
-        maas-rack-controller package. Confirm that the MAAS version is the same
-        as the main rack controller.
+        To add a new rack controller, SSH into the rack controller and run the
+        commands below. Confirm that the MAAS version is the same as the main
+        rack controller.
       </p>
-      <CodeSnippet
-        blocks={[
-          {
-            code: "sudo apt install maas-rack-controller",
-          },
-        ]}
-      />
-      <p>Or if you use snap</p>
-      <CodeSnippet
-        blocks={[
-          {
-            code: "sudo snap install maas",
-          },
-        ]}
-      />
+      {variant === "packages" ? (
+        <CodeSnippet
+          blocks={[
+            {
+              dropdowns,
+              code: `sudo apt-add-repository ppa:maas/${minorVersion}`,
+            },
+            {
+              code: "sudo apt install maas-rack-controller",
+            },
+          ]}
+        />
+      ) : (
+        <CodeSnippet
+          blocks={[
+            {
+              dropdowns,
+              code: `sudo snap install maas --channel=${minorVersion}`,
+            },
+          ]}
+        />
+      )}
       <p>
         Register the rack controller with this MAAS. If the rack controller (and
         machines) don't have access to the URL, use a different IP address to
         allow connection.
       </p>
-      <CodeSnippet
-        blocks={[
-          {
-            code: `sudo maas-rack register --url ${maasUrl} --secret ${rpcSharedSecret}`,
-          },
-        ]}
-        data-testid="register-snippet"
-      />
-      <p>Or if you use snap</p>
-      <CodeSnippet
-        blocks={[
-          {
-            code: `sudo maas init rack --maas-url ${maasUrl} --secret ${rpcSharedSecret}`,
-          },
-        ]}
-      />
+      {variant === "packages" ? (
+        <CodeSnippet
+          blocks={[
+            {
+              dropdowns,
+              code: `sudo maas-rack register --url ${maasUrl} --secret ${rpcSharedSecret}`,
+            },
+          ]}
+          data-testid="register-snippet"
+        />
+      ) : (
+        <CodeSnippet
+          blocks={[
+            {
+              dropdowns,
+              code: `sudo maas init rack --maas-url ${maasUrl} --secret ${rpcSharedSecret}`,
+            },
+          ]}
+          data-testid="register-snippet"
+        />
+      )}
+
       <Row>
         <Col size={6}>
           <a
