26f5ebbb9523b2a35e60b5cc46f62ece9525ca6e
Huw Wilkins <huw.wilkins@canonical.com>
2022-06-22 16:40:47 +1000

    Fetch db dump.

--- sitespeed.io/scripts/machines.js
+++ sitespeed.io/scripts/machines.js
@@ -1,7 +1,7 @@
 const fetchMachines = async (context, commands) => {
   await commands.measure.start("Fetch machines with cold cache");
   await commands.navigate(`${context.options.hostname}/MAAS/r/machines`);
-  await commands.wait.byXpath("//*[text()='100 Machines']", 15000);
+  await commands.wait.byXpath("//*[text()='100 Machines']", 10000);
   return commands.measure.stop();
 };
 
@@ -9,17 +9,17 @@ const fetchMachinesWarm = async (context, commands) => {
   await commands.navigate(`${context.options.hostname}/MAAS/r/settings`);
   await commands.measure.start("Fetch machines with warm cache");
   await commands.click.byLinkText("Machines");
-  await commands.wait.byXpath("//*[text()='100 Machines']", 15000);
+  await commands.wait.byXpath("//*[text()='100 Machines']", 10000);
   return commands.measure.stop();
 };
 
 const fetchMachinesHot = async (context, commands) => {
   await commands.navigate(`${context.options.hostname}/MAAS/r/machines`);
-  await commands.wait.byXpath("//*[text()='100 Machines']", 15000);
+  await commands.wait.byXpath("//*[text()='100 Machines']", 10000);
   await commands.click.byLinkText("Settings");
   await commands.measure.start("Fetch machines with hot cache");
   await commands.click.byLinkText("Machines");
-  await commands.wait.byXpath("//*[text()='100 Machines']", 15000);
+  await commands.wait.byXpath("//*[text()='100 Machines']", 10000);
   return commands.measure.stop();
 };
 
