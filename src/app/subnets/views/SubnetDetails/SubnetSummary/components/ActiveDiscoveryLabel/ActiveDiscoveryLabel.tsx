e22f2c1686fb354b27dec8f9898bd1d11eca0e91
Peter Makowski <peter@makowski.uk>
2022-07-01 14:21:47 +0200

    fix: subnet icons misaligned

--- src/app/subnets/views/SubnetDetails/SubnetSummary/components/ActiveDiscoveryLabel/ActiveDiscoveryLabel.tsx
+++ src/app/subnets/views/SubnetDetails/SubnetSummary/components/ActiveDiscoveryLabel/ActiveDiscoveryLabel.tsx
@@ -13,6 +13,7 @@ const ActiveDiscoveryLabel = ({ managed }: Props): JSX.Element => (
         message={`When enabled, MAAS will scan this subnet to discover hosts
         that have not been discovered passively.`}
         position="btm-right"
+        positionElementClassName="u-display--inline"
       />
     ) : null}
   </>
