626afe0a91c509406fff37df2f7cfbca0bd4c6de
Huw Wilkins <huw.wilkins@canonical.com>
2022-07-06 13:51:27 +1000

    Remove single switch in domains list.

--- src/app/domains/views/DomainsList/DomainsList.tsx
+++ src/app/domains/views/DomainsList/DomainsList.tsx
@@ -1,14 +1,12 @@
 import { useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
-import { Route, Switch } from "react-router-dom";
 
 import DomainListHeader from "./DomainListHeader";
 import DomainsTable from "./DomainsTable";
 
 import Section from "app/base/components/Section";
 import { useWindowTitle } from "app/base/hooks";
-import urls from "app/base/urls";
 import { actions } from "app/store/domain";
 import domainsSelectors from "app/store/domain/selectors";
 
@@ -24,13 +22,7 @@ const DomainsList = (): JSX.Element => {
 
   return (
     <Section header={<DomainListHeader />}>
-      <Switch>
-        <Route
-          exact
-          path={urls.domains.index}
-          render={() => <>{domains.length > 0 && <DomainsTable />}</>}
-        />
-      </Switch>
+      {domains.length > 0 && <DomainsTable />}
     </Section>
   );
 };
