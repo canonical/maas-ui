3e1e1515e72b8bf16b2e2344425feb06b45bf655
huwshimi <huwshimi@users.noreply.github.com>
2022-07-07 01:09:04 +1000

    refactor(dashboard): update dashboard to RRv6 (#4187)
    Update dashboard components to React Router v6.
    
    Fixes: #4186.

--- src/app/dashboard/views/DashboardConfigurationForm/DashboardConfigurationForm.tsx
+++ src/app/dashboard/views/DashboardConfigurationForm/DashboardConfigurationForm.tsx
@@ -3,14 +3,18 @@ import DashboardConfigurationSubnetForm from "./DashboardConfigurationSubnetForm
 import { useWindowTitle } from "app/base/hooks";
 import NetworkDiscoveryForm from "app/settings/views/Network/NetworkDiscoveryForm";
 
+export enum Label {
+  Title = "Dashboard configuration",
+}
+
 const DashboardConfigurationForm = (): JSX.Element => {
-  useWindowTitle("Dashboard configuration");
+  useWindowTitle(Label.Title);
 
   return (
-    <>
+    <div aria-label={Label.Title}>
       <NetworkDiscoveryForm />
       <DashboardConfigurationSubnetForm />
-    </>
+    </div>
   );
 };
 
