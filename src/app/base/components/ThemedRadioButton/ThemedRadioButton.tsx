83349220e086b60806a82877f5ff97d26c0c789d
ndv99 <nick.devilliers@canonical.com>
2022-07-11 17:06:24 +0100

    Remove old comments in ThemedRadioButton (jsx and scss)

--- src/app/base/components/ThemedRadioButton/ThemedRadioButton.tsx
+++ src/app/base/components/ThemedRadioButton/ThemedRadioButton.tsx
@@ -1,28 +1,10 @@
 // nick notes
-// -build a component that works like a radio button
-// -Similar to a toggle but takes a colour
-// -"Switch"
 // -Set up a useEffect for live preview
-// -New values in redux for colours
 // -Remove from redux when "save" or "cancel"
-// -Pass in a component to formikfield (custom radio button)
 
 import type { ReactNode } from "react";
 
 import classNames from "classnames";
-
-// const colours = {
-//   default: "#262626",
-//   bark: "#585841",
-//   sage: "#4E5F51",
-//   olive: "#3D5F11",
-//   viridian: "#025A3D",
-//   prussian_green: "#225D5C",
-//   blue: "#0060BF",
-//   purple: "#7764D8",
-//   magenta: "#974097",
-//   red: "#A71B33",
-// };
 
 export type Props = {
   className?: string;
