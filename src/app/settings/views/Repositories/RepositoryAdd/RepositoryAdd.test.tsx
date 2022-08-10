d7c0e28a4c9b1541898e37f35dfc8ddca3b8b53b
Nick De Villiers <nick.devilliers@canonical.com>
2022-08-10 09:55:11 +0100

    Modify tests in RepositoryAdd.test to use renderWIthMockStore

--- src/app/settings/views/Repositories/RepositoryAdd/RepositoryAdd.test.tsx
+++ src/app/settings/views/Repositories/RepositoryAdd/RepositoryAdd.test.tsx
@@ -1,8 +1,6 @@
-import { screen, render } from "@testing-library/react";
-import { Provider } from "react-redux";
+import { screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { CompatRouter, Route, Routes } from "react-router-dom-v5-compat";
-import configureStore from "redux-mock-store";
 
 import RepositoryAdd from "./RepositoryAdd";
 
@@ -16,8 +14,7 @@ import {
   generalState as generalStateFactory,
   rootState as rootStateFactory,
 } from "testing/factories";
-
-const mockStore = configureStore();
+import { renderWithMockStore } from "testing/utils";
 
 describe("RepositoryAdd", () => {
   let state: RootState;
@@ -43,51 +40,47 @@ describe("RepositoryAdd", () => {
   });
 
   it("can display a repository add form with type ppa", () => {
-    const store = mockStore(state);
-    render(
-      <Provider store={store}>
-        <MemoryRouter
-          initialEntries={[
-            { pathname: "/settings/repositories/add/ppa", key: "testKey" },
-          ]}
-        >
-          <CompatRouter>
-            <Routes>
-              <Route
-                element={<RepositoryAdd />}
-                path="/settings/repositories/add/:type"
-              />
-            </Routes>
-          </CompatRouter>
-        </MemoryRouter>
-      </Provider>
+    renderWithMockStore(
+      <MemoryRouter
+        initialEntries={[
+          { pathname: "/settings/repositories/add/ppa", key: "testKey" },
+        ]}
+      >
+        <CompatRouter>
+          <Routes>
+            <Route
+              element={<RepositoryAdd />}
+              path="/settings/repositories/add/:type"
+            />
+          </Routes>
+        </CompatRouter>
+      </MemoryRouter>,
+      { state }
     );
 
     expect(screen.getByRole("form", { name: "Add PPA" })).toBeInTheDocument();
   });
 
   it("can display a repository add form with type repository", () => {
-    const store = mockStore(state);
-    render(
-      <Provider store={store}>
-        <MemoryRouter
-          initialEntries={[
-            {
-              pathname: "/settings/repositories/add/repository",
-              key: "testKey",
-            },
-          ]}
-        >
-          <CompatRouter>
-            <Routes>
-              <Route
-                element={<RepositoryAdd />}
-                path="/settings/repositories/add/:type"
-              />
-            </Routes>
-          </CompatRouter>
-        </MemoryRouter>
-      </Provider>
+    renderWithMockStore(
+      <MemoryRouter
+        initialEntries={[
+          {
+            pathname: "/settings/repositories/add/repository",
+            key: "testKey",
+          },
+        ]}
+      >
+        <CompatRouter>
+          <Routes>
+            <Route
+              element={<RepositoryAdd />}
+              path="/settings/repositories/add/:type"
+            />
+          </Routes>
+        </CompatRouter>
+      </MemoryRouter>,
+      { state }
     );
     expect(
       screen.getByRole("form", { name: "Add repository" })
