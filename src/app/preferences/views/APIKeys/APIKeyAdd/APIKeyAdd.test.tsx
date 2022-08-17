c93b2622f5b0a0bf45cc8391e7dc668e222e5eac
Nick De Villiers <nick.devilliers@canonical.com>
2022-08-17 09:50:25 +0100

    Use renderWithBrowserRouter in APIKeyAdd.test

--- src/app/preferences/views/APIKeys/APIKeyAdd/APIKeyAdd.test.tsx
+++ src/app/preferences/views/APIKeys/APIKeyAdd/APIKeyAdd.test.tsx
@@ -1,26 +1,20 @@
 import { screen } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
-import { CompatRouter } from "react-router-dom-v5-compat";
 
 import { Label as APIKeyFormLabels } from "../APIKeyForm/APIKeyForm";
 
 import { APIKeyAdd } from "./APIKeyAdd";
 
 import type { RootState } from "app/store/root/types";
-import { renderWithMockStore } from "testing/utils";
+import { renderWithBrowserRouter } from "testing/utils";
 
 describe("APIKeyAdd", () => {
   let state: RootState;
 
   it("can render", () => {
-    renderWithMockStore(
-      <MemoryRouter initialEntries={["/"]}>
-        <CompatRouter>
-          <APIKeyAdd />
-        </CompatRouter>
-      </MemoryRouter>,
-      { state }
-    );
+    renderWithBrowserRouter(<APIKeyAdd />, {
+      route: "/",
+      wrapperProps: { state },
+    });
     expect(
       screen.getByRole("form", { name: APIKeyFormLabels.AddFormLabel })
     ).toBeInTheDocument();
