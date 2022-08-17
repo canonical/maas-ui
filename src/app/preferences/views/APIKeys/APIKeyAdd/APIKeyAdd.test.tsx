4e41b4640e2a81831ebbfeaf93ca696883af0870
Nick De Villiers <nick.devilliers@canonical.com>
2022-08-17 09:48:26 +0100

    Remove rootStateFactory in APIKeyAdd.test

--- src/app/preferences/views/APIKeys/APIKeyAdd/APIKeyAdd.test.tsx
+++ src/app/preferences/views/APIKeys/APIKeyAdd/APIKeyAdd.test.tsx
@@ -7,15 +7,10 @@ import { Label as APIKeyFormLabels } from "../APIKeyForm/APIKeyForm";
 import { APIKeyAdd } from "./APIKeyAdd";
 
 import type { RootState } from "app/store/root/types";
-import { rootState as rootStateFactory } from "testing/factories";
 import { renderWithMockStore } from "testing/utils";
 
 describe("APIKeyAdd", () => {
   let state: RootState;
-
-  beforeEach(() => {
-    state = rootStateFactory();
-  });
 
   it("can render", () => {
     renderWithMockStore(
