872b9f3ae75c00676c6930905b3ab6b350bb977e
Huw Wilkins <huw.wilkins@canonical.com>
2022-08-29 10:02:40 +1000

    Fix lint

--- src/app/machines/views/MachineList/MachineListTable/MachineListPagination/MachineListPagination.test.tsx
+++ src/app/machines/views/MachineList/MachineListTable/MachineListPagination/MachineListPagination.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import MachineListPagination, { Label } from "./MachineListPagination";
 
