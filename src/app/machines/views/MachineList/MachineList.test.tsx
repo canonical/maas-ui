1fe68704dce85f3c2e9658596887aefd6286018f
Huw Wilkins <huw.wilkins@canonical.com>
2022-09-01 11:26:12 +1000

    Remove unused import.

--- src/app/machines/views/MachineList/MachineList.test.tsx
+++ src/app/machines/views/MachineList/MachineList.test.tsx
@@ -1,5 +1,5 @@
 import reduxToolkit from "@reduxjs/toolkit";
-import { screen, waitFor, within } from "@testing-library/react";
+import { screen, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { mount } from "enzyme";
 import { act } from "react-dom/test-utils";
