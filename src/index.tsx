50dd7a333222cd15b245a13a2a451fd10454f635
Peter Makowski <peter@makowski.uk>
2022-09-09 15:34:24 +0200

    expose maas-ui version info - log version info in the browser console

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { Provider } from "react-redux";
 import { Router } from "react-router-dom";
 import { CompatRouter } from "react-router-dom-v5-compat";
 
+import packageInfo from "../package.json";
+
 import App from "./app/App";
 import * as serviceWorker from "./serviceWorker";
 
@@ -31,6 +33,13 @@ const rootNode = document.getElementById("root");
 if (rootNode) {
   ReactDOM.render(<Root />, rootNode);
 }
+
+// log the maas-ui version to the console
+console.info(
+  `${packageInfo.name} ${packageInfo.version} ${
+    process.env.REACT_APP_GIT_SHA ?? ""
+  }`
+);
 
 export default Root;
 
