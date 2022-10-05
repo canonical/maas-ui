ed80b27d6fe52d8680b5ec62cf47492f7764cdf2
Peter Makowski <peter@makowski.uk>
2022-10-05 16:52:08 +0200

    fix dhcp spec

--- cypress/e2e/with-users/settings/dhcp.spec.ts
+++ cypress/e2e/with-users/settings/dhcp.spec.ts
@@ -7,7 +7,23 @@ context("Settings - DHCP Snippets", () => {
     cy.visit(generateMAASURL("/settings/dhcp/add"));
   });
 
-  it("can add a DHCP snippet to a machine", () => {
+  it("can add a Global DHCP snippet", () => {
+    const snippetName = generateName("dhcp-snippet");
+    cy.get("[data-testid='section-header-title']").contains("Settings");
+    cy.findByLabelText("Snippet name").type(snippetName);
+    cy.findByLabelText("Type").select("Global");
+    cy.findByLabelText("DHCP snippet").type("ddns-update-style none;");
+    cy.findByRole("button", { name: "Save snippet" }).click();
+    // expect to be redirected to the list page
+    cy.findByLabelText("Search DHCP snippets").type(snippetName);
+    cy.findByRole("grid").within(() => {
+      cy.findByText(snippetName).should("be.visible");
+      cy.findByRole("button", { name: /Delete/ }).click();
+      cy.get("[data-testid='action-confirm']").click();
+    });
+  });
+
+  it("can request adding of a DHCP snippet to a machine", () => {
     const snippetName = generateName("dhcp-snippet");
     cy.get("[data-testid='section-header-title']").contains("Settings");
     cy.findByLabelText("Snippet name").type(snippetName);
@@ -20,12 +36,8 @@ context("Settings - DHCP Snippets", () => {
     });
     cy.findByLabelText("DHCP snippet").type("ddns-update-style none;");
     cy.findByRole("button", { name: "Save snippet" }).click();
-    // expect to be redirected to the list page
-    cy.findByLabelText("Search DHCP snippets").type(snippetName);
-    cy.findByRole("grid").within(() => {
-      cy.findByText(snippetName).should("be.visible");
-      cy.findByRole("button", { name: /Delete/ }).click();
-      cy.get("[data-testid='action-confirm']").click();
-    });
+    cy.findByText(
+      "Unable to validate DHCP config, no available rack controller connected."
+    ).should("be.visible");
   });
 });
