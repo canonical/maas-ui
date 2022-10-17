f8a4ae82ce136be54661c0519a1031ba3b563a30
Nick De Villiers <nick.devilliers@canonical.com>
2022-10-17 11:42:36 +0100

    Fix vaultEnabled status check in settings and remove old code

--- src/app/settings/views/Configuration/Security/VaultSettings/VaultSettings.tsx
+++ src/app/settings/views/Configuration/Security/VaultSettings/VaultSettings.tsx
@@ -41,7 +41,7 @@ const VaultSettings = (): JSX.Element => {
   if (controllersLoading || vaultEnabledLoading)
     return <Spinner aria-label={Labels.Loading} text={Labels.Loading} />;
 
-  if (vaultEnabled) {
+  if (vaultEnabled?.vault_enabled) {
     return (
       <>
         <p>
@@ -132,68 +132,6 @@ const VaultSettings = (): JSX.Element => {
             </div>
           </>
         )}
-        {/* {configuredControllers.length >= 1 &&
-        unconfiguredControllers.length >= 1 ? (
-          <p>
-            <Icon name="warning" />
-            <span className="u-nudge-right--small">
-              Incomplete Vault integration, configure{" "}
-              {unconfiguredControllers.length} other{" "}
-              {unconfiguredControllers.length > 1
-                ? "controllers"
-                : "controller"}{" "}
-              with Vault to complete this operation.
-            </span>
-          </p>
-        ) : configuredControllers.length >= 1 &&
-          unconfiguredControllers.length === 0 ? (
-          <p>
-            <Icon name="warning" />
-            <span className="u-nudge-right--small">
-              Incomplete Vault integration, migrate secrets on one region
-              controller to complete setup.
-            </span>
-          </p>
-        ) : (
-          <p>
-            <Icon name="secured" />
-            <span className="u-nudge-right--small">Integrate with Vault</span>
-          </p>
-        )}
-        <div aria-label={Labels.SetupInstructions}>
-          <p>
-            1. Get the $wrapped_token and $role_id from Vault.{" "}
-            <a
-              href="https://learn.hashicorp.com/tutorials/vault/approle-best-practices?in=vault/auth-methods#approle-response-wrapping"
-              rel="noreferrer noopener"
-              target="_blank"
-            >
-              Find out more from Hashicorp Vault
-            </a>
-            .
-          </p>
-          <p>2. SSH into each region controller and configure Vault.</p>
-          <CodeSnippet
-            blocks={[
-              {
-                appearance: CodeSnippetBlockAppearance.LINUX_PROMPT,
-                code: "sudo maas config-vault configure $url $approle_id $wrapped_token $secrets_path --secrets-mount $secret_mount",
-              },
-            ]}
-          />
-          <p>
-            3. After Vault is configured on all region controllers, migrate
-            secrets on one of the region controllers.
-          </p>
-          <CodeSnippet
-            blocks={[
-              {
-                appearance: CodeSnippetBlockAppearance.LINUX_PROMPT,
-                code: "sudo maas config-vault migrate",
-              },
-            ]}
-          />
-        </div> */}
 
         <a href={docsUrls.vaultIntegration}>More about Vault integration</a>
       </>
