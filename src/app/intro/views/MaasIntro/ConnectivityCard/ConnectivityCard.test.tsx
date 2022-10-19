1dfd46d09c9b95e0950bc1bd1095786aea34e13f
Peter Makowski <peter@makowski.uk>
2022-10-19 08:14:41 +0200

    fix: encoded amp character - use unicode character - refactor ConnectivityCard.test.tsx

--- src/app/intro/views/MaasIntro/ConnectivityCard/ConnectivityCard.test.tsx
+++ src/app/intro/views/MaasIntro/ConnectivityCard/ConnectivityCard.test.tsx
@@ -8,42 +8,32 @@ import ConnectivityCard, {
   Labels as ConnectivityCardLabels,
 } from "./ConnectivityCard";
 
+const renderTestCase = () =>
+  render(
+    <Formik
+      initialValues={{
+        httpProxy: "http://www.website.com",
+        mainArchiveUrl: "http://www.mainarchive.com",
+        portsArchiveUrl: "http://www.portsarchive.com",
+        upstreamDns: "8.8.8.8",
+      }}
+      onSubmit={jest.fn()}
+      validationSchema={MaasIntroSchema}
+    >
+      <ConnectivityCard />
+    </Formik>
+  );
+
 describe("ConnectivityCard", () => {
   it("displays a tick when there are no errors", () => {
-    render(
-      <Formik
-        initialValues={{
-          httpProxy: "http://www.website.com",
-          mainArchiveUrl: "http://www.mainarchive.com",
-          portsArchiveUrl: "http://www.portsarchive.com",
-          upstreamDns: "8.8.8.8",
-        }}
-        onSubmit={jest.fn()}
-        validationSchema={MaasIntroSchema}
-      >
-        <ConnectivityCard />
-      </Formik>
-    );
+    renderTestCase();
     const icon = screen.getByLabelText("success");
     expect(icon).toBeInTheDocument();
     expect(icon).toHaveClass("p-icon--success");
   });
 
   it("displays an error icon when there are errors", async () => {
-    render(
-      <Formik
-        initialValues={{
-          httpProxy: "http://www.website.com",
-          mainArchiveUrl: "http://www.mainarchive.com",
-          portsArchiveUrl: "http://www.portsarchive.com",
-          upstreamDns: "8.8.8.8",
-        }}
-        onSubmit={jest.fn()}
-        validationSchema={MaasIntroSchema}
-      >
-        <ConnectivityCard />
-      </Formik>
-    );
+    renderTestCase();
     await userEvent.clear(
       screen.getByRole("textbox", {
         name: ConnectivityCardLabels.MainArchiveUrl,
@@ -52,5 +42,12 @@ describe("ConnectivityCard", () => {
     const icon = screen.getByLabelText("error");
     expect(icon).toBeInTheDocument();
     expect(icon).toHaveClass("p-icon--error");
+  });
+
+  it("displays HTTP proxy field", () => {
+    renderTestCase();
+    expect(
+      screen.getByLabelText("APT & HTTP/HTTPS proxy server")
+    ).toBeInTheDocument();
   });
 });
