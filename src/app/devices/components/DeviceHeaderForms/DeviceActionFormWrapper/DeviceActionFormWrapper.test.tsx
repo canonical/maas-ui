f735024f83973ceb61d1dfc7ccd145199d3d9749
Nick De Villiers <nick.devilliers@canonical.com>
2022-11-02 16:43:25 +0100

    test: Migrate DeviceActionFormWrapper.test to RTL

--- src/app/devices/components/DeviceHeaderForms/DeviceActionFormWrapper/DeviceActionFormWrapper.test.tsx
+++ src/app/devices/components/DeviceHeaderForms/DeviceActionFormWrapper/DeviceActionFormWrapper.test.tsx
@@ -1,43 +1,40 @@
-import { mount } from "enzyme";
-import { Provider } from "react-redux";
-import { MemoryRouter } from "react-router-dom";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import configureStore from "redux-mock-store";
 
 import DeviceActionFormWrapper from "./DeviceActionFormWrapper";
 
 import { actions as deviceActions } from "app/store/device";
+import type { RootState } from "app/store/root/types";
 import { NodeActions } from "app/store/types/node";
 import {
   device as deviceFactory,
   rootState as rootStateFactory,
 } from "testing/factories";
+import { renderWithBrowserRouter } from "testing/utils";
 
-const mockStore = configureStore();
+const mockStore = configureStore<RootState>();
 
 describe("DeviceActionFormWrapper", () => {
-  it("can set selected devices to those that can perform action", () => {
+  it("can set selected devices to those that can perform action", async () => {
     const state = rootStateFactory();
     const devices = [
       deviceFactory({ system_id: "abc123", actions: [NodeActions.DELETE] }),
       deviceFactory({ system_id: "def456", actions: [] }),
     ];
     const store = mockStore(state);
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter
-          initialEntries={[{ pathname: "/devices", key: "testKey" }]}
-        >
-          <DeviceActionFormWrapper
-            action={NodeActions.DELETE}
-            clearHeaderContent={jest.fn()}
-            devices={devices}
-            viewingDetails={false}
-          />
-        </MemoryRouter>
-      </Provider>
+    renderWithBrowserRouter(
+      <DeviceActionFormWrapper
+        action={NodeActions.DELETE}
+        clearHeaderContent={jest.fn()}
+        devices={devices}
+        viewingDetails={false}
+      />,
+      { route: "/devices", wrapperProps: { store } }
     );
 
-    wrapper.find('button[data-testid="on-update-selected"]').simulate("click");
+    // wrapper.find('button[data-testid="on-update-selected"]').simulate("click");
+    await userEvent.click(screen.getByTestId("on-update-selected"));
 
     const expectedAction = deviceActions.setSelected(["abc123"]);
     const actualActions = store.getActions();
