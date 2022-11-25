a5d0acd61170a508a9899934a3dae4bfc490379f
Nick De Villiers <nick.devilliers@canonical.com>
2022-11-25 12:54:44 +0000

    fix(vault): Hide additional notifications if a Vault error is displayed (#4586)
    * fix(vault): Hide additional notifications if a Vault error is displayed
    
    * Refactor: hideNotifications --> isNotificationListHidden

--- src/app/base/components/Section/Section.tsx
+++ src/app/base/components/Section/Section.tsx
@@ -10,12 +10,14 @@ export type Props = {
   children?: ReactNode;
   header?: ReactNode;
   sidebar?: ReactNode;
+  isNotificationListHidden?: boolean;
 } & HTMLProps<HTMLDivElement>;
 
 const Section = ({
   children,
   header,
   sidebar,
+  isNotificationListHidden = false,
   ...props
 }: Props): JSX.Element => {
   const { SIDEBAR, TOTAL } = COL_SIZES;
@@ -43,7 +45,7 @@ const Section = ({
           className="section__content"
           size={(sidebar ? TOTAL - SIDEBAR : TOTAL) as ColSize}
         >
-          <NotificationList />
+          {!isNotificationListHidden && <NotificationList />}
           {children}
         </Col>
       </Strip>
