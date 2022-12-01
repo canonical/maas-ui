b8d2c043735039291abd9a288214a64f3e481bd4
Peter Makowski <peter@makowski.uk>
2022-12-01 11:30:22 +0100

    add playwright (#4580)

---
+++ tests/machines.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from "@playwright/test";
+
+let machineListRequests: (string | Buffer)[] = [];
+let machineCountRequests: (string | Buffer)[] = [];
+
+test.beforeEach(async ({ page, context }) => {
+  machineListRequests = [];
+  machineCountRequests = [];
+  await context.addCookies([
+    { name: "skipsetupintro", value: "true", url: "http://0.0.0.0:5240/" },
+    { name: "skipintro", value: "true", url: "http://0.0.0.0:5240/" },
+  ]);
+  await page.goto("/MAAS/r/machines");
+  await page.getByLabel("Username").click();
+  await page.getByLabel("Username").fill("admin");
+  await page.getByLabel("Username").press("Tab");
+  await page.getByLabel("Password").fill("test");
+  await page.getByLabel("Password").press("Enter");
+});
+
+test("machines list loads", async ({ page }) => {
+  page.on("websocket", async (ws) => {
+    console.log(`WebSocket opened: ${ws.url()}>`);
+    await ws.on("framesent", async (data) => {
+      console.log(data.payload);
+      if (data.payload.includes("machine.list")) {
+        machineListRequests.push(data.payload);
+      }
+      if (data.payload.includes("machine.count")) {
+        machineCountRequests.push(data.payload);
+      }
+    });
+    ws.on("close", () => console.log("WebSocket closed"));
+  });
+  await expect(page).toHaveTitle(/Machines/);
+  await expect(page.getByTestId("section-header-title")).toHaveText("Machines");
+  await expect(page.getByTestId("subtitle-string")).toHaveText(
+    /machines available/
+  );
+  // expect a single machine.list and machine.count request
+  await expect(machineListRequests.length).toBe(1);
+  await expect(machineCountRequests.length).toBe(1);
+});
