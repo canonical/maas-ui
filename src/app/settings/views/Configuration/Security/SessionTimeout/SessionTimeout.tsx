785eae4bfa8a29da776a2b02734fbf0278c34945
Nick De Villiers <nick.devilliers@canonical.com>
2022-12-12 12:32:16 +0000

    Add math to calculate hours/days from seconds and logic to open/close form

--- src/app/settings/views/Configuration/Security/SessionTimeout/SessionTimeout.tsx
+++ src/app/settings/views/Configuration/Security/SessionTimeout/SessionTimeout.tsx
@@ -1,4 +1,6 @@
-import { Spinner, Select } from "@canonical/react-components";
+import { useState } from "react";
+
+import { Spinner, Select, Button, Icon } from "@canonical/react-components";
 import { useDispatch, useSelector } from "react-redux";
 
 import FormikField from "app/base/components/FormikField";
@@ -26,10 +28,11 @@ export enum Labels {
 const SessionTimeout = (): JSX.Element => {
   const dispatch = useDispatch();
   const configLoading = useSelector(configSelectors.loading);
-  // const sessionLength = useSelector(configSelectors.sessionLength);
-  const sessionLength = 14;
+  const sessionLength = useSelector(configSelectors.sessionLength) || 1209600; // fallback to default value of 14 days if none is set
   const saved = useSelector(configSelectors.saved);
   const saving = useSelector(configSelectors.saving);
+
+  const [isFormOpen, setFormOpen] = useState(false);
 
   const timeUnitOptions = [
     { label: Labels.Hours, value: "hours" },
@@ -37,56 +40,78 @@ const SessionTimeout = (): JSX.Element => {
   ];
 
   if (configLoading) {
-    return <Spinner aria-label={Labels.Loading} />;
+    return <Spinner aria-label={Labels.Loading} text={Labels.Loading} />;
   }
 
-  return (
-    <>
-      <FormikForm<SessionTimeoutFormValues>
-        aria-label={Labels.ConfigureSessionTimeout}
-        buttonsAlign="right"
-        buttonsBordered
-        cleanup={configActions.cleanup}
-        initialValues={{
-          session_length: sessionLength,
-          time_unit: "days",
-        }}
-        onCancel={(values, { resetForm }) => {
-          resetForm();
-          values.session_length = 14;
-          values.time_unit = "hours";
-        }}
-        onSubmit={(values) => {
-          dispatch(configActions.cleanup());
-          dispatch(
-            configActions.update({
-              session_length: Number(values.session_length),
-            })
-          );
-        }}
-        saved={saved}
-        saving={saving}
-      >
-        <span className="p-form__group">
-          <FormikField
-            className="p-form__session-length-input"
-            label={Labels.Expiration}
-            name="session_length"
-            required
-            type="number"
-            wrapperClassName="p-form__session-length-input"
-          />
-          <FormikField
-            component={Select}
-            label={Labels.TimeUnit}
-            name="time_unit"
-            options={timeUnitOptions}
-            wrapperClassName="p-form__time-unit-input"
-          />
+  if (!isFormOpen) {
+    return (
+      <>
+        <span className="u-text--muted">
+          Session timeout expiration
+          <Button
+            className="is-small p-button__show-form"
+            onClick={() => setFormOpen(true)}
+          >
+            <Icon name="edit" />
+            Edit
+          </Button>
         </span>
-      </FormikForm>
-    </>
-  );
+        <p>{sessionLength / 24 / 3600} days</p>
+      </>
+    );
+  } else
+    return (
+      <>
+        <FormikForm<SessionTimeoutFormValues>
+          aria-label={Labels.ConfigureSessionTimeout}
+          buttonsAlign="right"
+          buttonsBordered
+          cleanup={configActions.cleanup}
+          initialValues={{
+            session_length: sessionLength / 24 / 3600,
+            time_unit: "days",
+          }}
+          onCancel={(values, { resetForm }) => {
+            resetForm();
+            values.session_length = sessionLength / 24 / 3600;
+            values.time_unit = "days";
+            setFormOpen(false);
+          }}
+          onSubmit={(values) => {
+            dispatch(configActions.cleanup());
+            dispatch(
+              configActions.update({
+                session_length: Number(
+                  values.time_unit === "hours"
+                    ? values.session_length * 3600
+                    : values.session_length * 3600 * 24
+                ),
+              })
+            );
+          }}
+          saved={saved}
+          saving={saving}
+        >
+          <span className="p-form__group">
+            <FormikField
+              className="p-form__session-length-input"
+              label={Labels.Expiration}
+              name="session_length"
+              required
+              type="number"
+              wrapperClassName="p-form__session-length-input"
+            />
+            <FormikField
+              component={Select}
+              label={Labels.TimeUnit}
+              name="time_unit"
+              options={timeUnitOptions}
+              wrapperClassName="p-form__time-unit-input"
+            />
+          </span>
+        </FormikForm>
+      </>
+    );
 };
 
 export default SessionTimeout;
