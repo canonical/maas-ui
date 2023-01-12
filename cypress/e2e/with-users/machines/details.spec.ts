26e541aeec12897efaa6b8bb6e03567d16b6732a
Nick De Villiers <nick.devilliers@canonical.com>
2023-01-12 16:23:59 +0000

    Fix cypress test for Details page

--- cypress/e2e/with-users/machines/details.spec.ts
+++ cypress/e2e/with-users/machines/details.spec.ts
@@ -51,18 +51,9 @@ context("Machine details", () => {
     cy.findByRole("link", { name: new RegExp(name, "i") }).click();
 
     cy.waitForPageToLoad();
-    cy.get("[data-testid='section-header-buttons']").within(() =>
-      cy
-        .findByRole("button", {
-          name: /Take action/i,
-        })
-        .click()
-    );
 
     // abort commissioning
-    cy.findByLabelText("submenu").within(() => {
-      cy.findByRole("button", { name: /Abort/i }).click();
-    });
+    cy.findByRole("button", { name: /Abort/i }).click();
     cy.findByRole("button", { name: /Abort actions for machine/i }).click();
 
     cy.findByRole("link", { name: "Commissioning" }).click();
@@ -77,16 +68,7 @@ context("Machine details", () => {
     cy.findByRole("heading", { level: 2, name: /details/i }).should("exist");
 
     // delete the machine
-    cy.get("[data-testid='section-header-buttons']").within(() =>
-      cy
-        .findByRole("button", {
-          name: /Take action/i,
-        })
-        .click()
-    );
-    cy.findByLabelText("submenu").within(() => {
-      cy.findByRole("button", { name: /Delete/i }).click();
-    });
+    cy.findByRole("button", { name: /Delete/i }).click();
     cy.findByRole("button", { name: /Delete machine/i }).click();
     cy.waitForPageToLoad();
     // verify the user has been redirected to the machine list
