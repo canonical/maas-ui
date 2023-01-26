6c68234d3b4e333216ae7ac71effe9284e3ea1e9
Nick De Villiers <nick.devilliers@canonical.com>
2023-01-26 12:50:01 +0000

    Create nav link groups

--- src/app/base/components/GlobalSideNav/GlobalSideNav.tsx
+++ src/app/base/components/GlobalSideNav/GlobalSideNav.tsx
@@ -26,6 +26,112 @@ import controllerSelectors from "app/store/controller/selectors";
 import type { RootState } from "app/store/root/types";
 import { actions as statusActions } from "app/store/status";
 
+type NavItem = {
+  adminOnly?: boolean;
+  highlight?: string | string[];
+  label: string;
+  url: string;
+};
+
+type NavGroup = {
+  navLinks: NavItem[];
+  groupTitle?: string;
+  groupIcon?: string;
+};
+
+const navGroups: NavGroup[] = [
+  {
+    navLinks: [
+      {
+        label: "Images",
+        url: urls.images.index,
+      },
+      {
+        label: "Storage",
+        url: urls.settings.storage,
+      },
+      {
+        label: "Tags",
+        url: urls.tags.index,
+      },
+      {
+        highlight: [urls.zones.index, urls.zones.details(null)],
+        label: "AZs",
+        url: urls.zones.index,
+      },
+      {
+        label: "Pools",
+        url: urls.pools.index,
+      },
+    ],
+  },
+  {
+    groupTitle: "Hardware",
+    groupIcon: "",
+    navLinks: [
+      {
+        highlight: [urls.machines.index, urls.machines.machine.index(null)],
+        label: "Machines",
+        url: urls.machines.index,
+      },
+      {
+        highlight: [urls.devices.index, urls.devices.device.index(null)],
+        label: "Devices",
+        url: urls.devices.index,
+      },
+      {
+        adminOnly: true,
+        highlight: [
+          urls.controllers.index,
+          urls.controllers.controller.index(null),
+        ],
+        label: "Controllers",
+        url: urls.controllers.index,
+      },
+    ],
+  },
+  {
+    groupTitle: "KVM",
+    groupIcon: "",
+    navLinks: [
+      {
+        label: "LXD",
+        url: urls.kvm.lxd.index,
+      },
+      {
+        label: "Virsh",
+        url: urls.kvm.virsh.index,
+      },
+    ],
+  },
+  {
+    groupTitle: "Networking",
+    groupIcon: "",
+    navLinks: [
+      {
+        highlight: [
+          urls.subnets.index,
+          urls.subnets.subnet.index(null),
+          urls.subnets.space.index(null),
+          urls.subnets.fabric.index(null),
+          urls.subnets.vlan.index(null),
+        ],
+        label: "Subnets",
+        url: urls.subnets.index,
+      },
+      {
+        highlight: [urls.domains.index, urls.domains.details(null)],
+        label: "DNS",
+        url: urls.domains.index,
+      },
+      {
+        label: "Network discovery",
+        url: urls.dashboard.index,
+      },
+    ],
+  },
+];
+
 const GlobalSideNav = (): JSX.Element => {
   return (
     <>
