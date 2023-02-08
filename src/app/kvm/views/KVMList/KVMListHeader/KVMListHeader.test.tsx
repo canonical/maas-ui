b5437beac611f58714dfb461d40de07125b9ef5e
Nick De Villiers <nick.devilliers@canonical.com>
2023-02-08 15:37:40 +0000

    fix(MAASENG-1313): Remove duplicated tab navigation items (#4745)

--- src/app/kvm/views/KVMList/KVMListHeader/KVMListHeader.test.tsx
+++ src/app/kvm/views/KVMList/KVMListHeader/KVMListHeader.test.tsx
@@ -43,6 +43,7 @@ describe("KVMListHeader", () => {
             <KVMListHeader
               setSidePanelContent={jest.fn()}
               sidePanelContent={null}
+              title="some text"
             />
           </CompatRouter>
         </MemoryRouter>
@@ -61,6 +62,7 @@ describe("KVMListHeader", () => {
             <KVMListHeader
               setSidePanelContent={jest.fn()}
               sidePanelContent={null}
+              title="some text"
             />
           </CompatRouter>
         </MemoryRouter>
@@ -69,48 +71,6 @@ describe("KVMListHeader", () => {
     expect(wrapper.find('[data-testid="section-header-subtitle"]').text()).toBe(
       "2 KVM hosts available"
     );
-  });
-
-  it("sets the LXD tab as active when at the LXD URL", () => {
-    const store = mockStore(state);
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter
-          initialEntries={[{ pathname: urls.kvm.lxd.index, key: "testKey" }]}
-        >
-          <CompatRouter>
-            <KVMListHeader
-              setSidePanelContent={jest.fn()}
-              sidePanelContent={null}
-            />
-          </CompatRouter>
-        </MemoryRouter>
-      </Provider>
-    );
-    expect(wrapper.find("a[data-testid='lxd-tab']").prop("aria-selected")).toBe(
-      true
-    );
-  });
-
-  it("sets the Virsh tab as active when at the Virsh URL", () => {
-    const store = mockStore(state);
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter
-          initialEntries={[{ pathname: urls.kvm.virsh.index, key: "testKey" }]}
-        >
-          <CompatRouter>
-            <KVMListHeader
-              setSidePanelContent={jest.fn()}
-              sidePanelContent={null}
-            />
-          </CompatRouter>
-        </MemoryRouter>
-      </Provider>
-    );
-    expect(
-      wrapper.find("a[data-testid='virsh-tab']").prop("aria-selected")
-    ).toBe(true);
   });
 
   it("can open the add LXD form at the LXD URL", () => {
@@ -125,6 +85,7 @@ describe("KVMListHeader", () => {
             <KVMListHeader
               setSidePanelContent={setSidePanelContent}
               sidePanelContent={null}
+              title="LXD"
             />
           </CompatRouter>
         </MemoryRouter>
@@ -151,6 +112,7 @@ describe("KVMListHeader", () => {
             <KVMListHeader
               setSidePanelContent={setSidePanelContent}
               sidePanelContent={null}
+              title="Virsh"
             />
           </CompatRouter>
         </MemoryRouter>
