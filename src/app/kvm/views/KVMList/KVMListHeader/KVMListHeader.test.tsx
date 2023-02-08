cfe32cf6aad91b971f71f146d4a513929d1f2f82
Nick De Villiers <nick.devilliers@canonical.com>
2023-02-08 11:55:00 +0000

    Remove legacy tests

--- src/app/kvm/views/KVMList/KVMListHeader/KVMListHeader.test.tsx
+++ src/app/kvm/views/KVMList/KVMListHeader/KVMListHeader.test.tsx
@@ -71,48 +71,6 @@ describe("KVMListHeader", () => {
     );
   });
 
-  it("sets the LXD tab as active when at the LXD URL", () => {
-    const store = mockStore(state);
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter
-          initialEntries={[{ pathname: urls.kvm.lxd.index, key: "testKey" }]}
-        >
-          <CompatRouter>
-            <KVMListHeader
-              setSidePanelContent={jest.fn()}
-              sidePanelContent={null}
-            />
-          </CompatRouter>
-        </MemoryRouter>
-      </Provider>
-    );
-    expect(wrapper.find("a[data-testid='lxd-tab']").prop("aria-selected")).toBe(
-      true
-    );
-  });
-
-  it("sets the Virsh tab as active when at the Virsh URL", () => {
-    const store = mockStore(state);
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter
-          initialEntries={[{ pathname: urls.kvm.virsh.index, key: "testKey" }]}
-        >
-          <CompatRouter>
-            <KVMListHeader
-              setSidePanelContent={jest.fn()}
-              sidePanelContent={null}
-            />
-          </CompatRouter>
-        </MemoryRouter>
-      </Provider>
-    );
-    expect(
-      wrapper.find("a[data-testid='virsh-tab']").prop("aria-selected")
-    ).toBe(true);
-  });
-
   it("can open the add LXD form at the LXD URL", () => {
     const setSidePanelContent = jest.fn();
     const store = mockStore(state);
