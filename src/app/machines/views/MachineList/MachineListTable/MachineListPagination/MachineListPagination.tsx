75bce9f7047e803562299542f4fbb252cf105708
Nick De Villiers <nick.devilliers@canonical.com>
2023-02-22 12:07:49 +0000

    feat(machines): New pagination controls

--- src/app/machines/views/MachineList/MachineListTable/MachineListPagination/MachineListPagination.tsx
+++ src/app/machines/views/MachineList/MachineListTable/MachineListPagination/MachineListPagination.tsx
@@ -2,7 +2,7 @@ import type {
   PaginationProps,
   PropsWithSpread,
 } from "@canonical/react-components";
-import { Pagination } from "@canonical/react-components";
+import { Button, Icon, Input } from "@canonical/react-components";
 
 import { useFetchedCount } from "app/store/machine/utils";
 
@@ -27,14 +27,39 @@ const MachineListPagination = ({
   ...props
 }: Props): JSX.Element | null => {
   const count = useFetchedCount(machineCount, machinesLoading);
+  const totalPages = machineCount
+    ? Math.ceil(machineCount / props.itemsPerPage)
+    : 1;
 
   return count > 0 ? (
-    <Pagination
-      aria-label={Label.Pagination}
-      className="u-nudge-down"
-      totalItems={count}
-      {...props}
-    />
+    <span className="u-flex--align-baseline p-pagination--items">
+      <Button
+        className="p-pagination__link--previous"
+        disabled={props.currentPage === 1}
+        onClick={() => props.paginate(props.currentPage - 1)}
+      >
+        <Icon name="chevron-down" />
+      </Button>
+      <strong>Page </strong>{" "}
+      <Input
+        className="p-pagination__input"
+        defaultValue={props.currentPage}
+        onChange={(e) =>
+          e.target.valueAsNumber > 0 && e.target.valueAsNumber <= totalPages
+            ? props.paginate(e.target.valueAsNumber)
+            : {}
+        }
+        type="number"
+      />{" "}
+      <strong> of {totalPages}</strong>
+      <Button
+        className="p-pagination__link--next"
+        disabled={props.currentPage === totalPages}
+        onClick={() => props.paginate(props.currentPage + 1)}
+      >
+        <Icon name="chevron-down" />
+      </Button>
+    </span>
   ) : null;
 };
 
