9c6a16324c523770f45632f9387cc02f9760406e
Peter Makowski <peter@makowski.uk>
2023-02-23 11:10:29 +0000

    test: update navigation cypress tests (#4776)

--- cypress/e2e/with-users/domains/list.spec.ts
+++ cypress/e2e/with-users/domains/list.spec.ts
@@ -9,12 +9,4 @@ context("DNS", () => {
   it("renders the correct heading", () => {
     cy.get("[data-testid='section-header-title']").contains("DNS");
   });
-
-  it("highlights the correct navigation link", () => {
-    cy.findByRole("link", { current: "page" }).should(
-      "have.attr",
-      "href",
-      generateMAASURL("/domains")
-    );
-  });
 });
