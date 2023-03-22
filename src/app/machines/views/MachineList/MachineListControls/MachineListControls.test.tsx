7e4f344c67a19ffc3dd1de09f02df0adce021b54
Nick De Villiers <nick.devilliers@canonical.com>
2023-03-22 10:51:55 +0000

    help

--- src/app/machines/views/MachineList/MachineListControls/MachineListControls.test.tsx
+++ src/app/machines/views/MachineList/MachineListControls/MachineListControls.test.tsx
@@ -7,10 +7,16 @@ import configureStore from "redux-mock-store";
 import MachineListControls from "./MachineListControls";
 import MachinesFilterAccordion from "./MachinesFilterAccordion";
 
+import { actions as machineActions } from "app/store/machine";
 import type { RootState } from "app/store/root/types";
-import { rootState as rootStateFactory } from "testing/factories";
+import {
+  machine as machineFactory,
+  machineState as machineStateFactory,
+  rootState as rootStateFactory,
+} from "testing/factories";
+import { renderWithBrowserRouter, screen, userEvent } from "testing/utils";
 
-const mockStore = configureStore();
+const mockStore = configureStore<RootState>();
 
 jest.useFakeTimers("modern");
 
@@ -19,6 +25,18 @@ describe("MachineListControls", () => {
 
   beforeEach(() => {
     initialState = rootStateFactory();
+    initialState.machine = machineStateFactory({
+      loaded: true,
+      loading: false,
+      filtersLoaded: true,
+      filtersLoading: false,
+      items: [
+        machineFactory({
+          fqdn: "abc123",
+          system_id: "abc123",
+        }),
+      ],
+    });
   });
 
   afterEach(() => {
@@ -52,5 +70,115 @@ describe("MachineListControls", () => {
       wrapper.find(MachinesFilterAccordion).props().setSearchText("status:new");
     });
     expect(setFilter).toHaveBeenCalledWith("status:new");
+  });
+
+  it("shows search bar, filter accordion, and grouping select when no machines are selected", () => {
+    renderWithBrowserRouter(
+      <MachineListControls
+        filter=""
+        grouping={null}
+        hiddenColumns={[]}
+        setFilter={jest.fn()}
+        setGrouping={jest.fn()}
+        setHiddenColumns={jest.fn()}
+        setHiddenGroups={jest.fn()}
+        setSidePanelContent={jest.fn()}
+      />,
+      { route: "/machines", state: initialState }
+    );
+
+    expect(screen.getByRole("button", { name: "Filters" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("searchbox", { name: "Search" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("combobox", { name: "Group by" })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.queryByRole("button", { name: "Actions" })
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "Power cycle" })
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "Troubleshoot" })
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "Categorise" })
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "Lock" })
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "Delete" })
+    ).not.toBeInTheDocument();
+  });
+
+  it("hides search bar, filter accordion, and grouping select when machines are selected", () => {
+    initialState.machine.selectedMachines = { items: ["abc123"] };
+    renderWithBrowserRouter(
+      <MachineListControls
+        filter=""
+        grouping={null}
+        hiddenColumns={[]}
+        setFilter={jest.fn()}
+        setGrouping={jest.fn()}
+        setHiddenColumns={jest.fn()}
+        setHiddenGroups={jest.fn()}
+        setSidePanelContent={jest.fn()}
+      />,
+      { route: "/machines", state: initialState }
+    );
+
+    expect(screen.getByRole("button", { name: "Actions" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Power cycle" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Troubleshoot" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Categorise" })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Lock" })).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Delete" })).toBeInTheDocument();
+
+    expect(
+      screen.queryByRole("button", { name: "Filters" })
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("searchbox", { name: "Search" })
+    ).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("combobox", { name: "Group by" })
+    ).not.toBeInTheDocument();
+  });
+
+  it("dispatches an action to clear selected machines when the 'Clear seleciton' button is clicked", async () => {
+    initialState.machine.selectedMachines = { items: ["abc123"] };
+    const store = mockStore(initialState);
+    renderWithBrowserRouter(
+      <MachineListControls
+        filter=""
+        grouping={null}
+        hiddenColumns={[]}
+        setFilter={jest.fn()}
+        setGrouping={jest.fn()}
+        setHiddenColumns={jest.fn()}
+        setHiddenGroups={jest.fn()}
+        setSidePanelContent={jest.fn()}
+      />,
+      { route: "/machines", store }
+    );
+
+    const clearSelection = screen.getByRole("button", {
+      name: "Clear selection",
+    });
+
+    await userEvent.click(clearSelection);
+
+    // const actions = store.getActions();
+    // console.log(actions);
   });
 });
