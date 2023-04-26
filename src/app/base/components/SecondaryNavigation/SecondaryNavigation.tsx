3a501e5f3f0552f31c3945a0bc4c0526c88e7fb0
Nick De Villiers <nick.devilliers@canonical.com>
2023-04-26 11:44:38 +0100

    Add props for items and title and move items to new files

--- src/app/base/components/SecondaryNavigation/SecondaryNavigation.tsx
+++ src/app/base/components/SecondaryNavigation/SecondaryNavigation.tsx
@@ -3,8 +3,6 @@ import classNames from "classnames";
 import type { Location } from "react-router-dom-v5-compat";
 import { Link, matchPath, useLocation } from "react-router-dom-v5-compat";
 
-import settingsURLs from "app/settings/urls";
-
 export type NavItem = {
   label: string;
   path?: string;
@@ -90,103 +88,14 @@ const SideNavigationItem = ({ item }: ItemProps) => {
   );
 };
 
-export const secondaryNavItems: NavItem[] = [
-  {
-    label: "Configuration",
-    items: [
-      { path: settingsURLs.configuration.general, label: "General" },
-      {
-        path: settingsURLs.configuration.commissioning,
-        label: "Commissioning",
-      },
-      { path: settingsURLs.configuration.deploy, label: "Deploy" },
-      {
-        path: settingsURLs.configuration.kernelParameters,
-        label: "Kernel parameters",
-      },
-    ],
-  },
-  {
-    label: "Security",
-    items: [
-      {
-        path: settingsURLs.security.securityProtocols,
-        label: "Security protocols",
-      },
-      {
-        path: settingsURLs.security.secretStorage,
-        label: "Secret storage",
-      },
-      {
-        path: settingsURLs.security.sessionTimeout,
-        label: "Session timeout",
-      },
-      {
-        path: settingsURLs.security.ipmiSettings,
-        label: "IPMI settings",
-      },
-    ],
-  },
-  {
-    path: settingsURLs.users.index,
-    label: "Users",
-  },
-  {
-    label: "Images",
-    items: [
-      { path: settingsURLs.images.ubuntu, label: "Ubuntu" },
-      { path: settingsURLs.images.windows, label: "Windows" },
-      { path: settingsURLs.images.vmware, label: "VMware" },
-    ],
-  },
-  {
-    path: settingsURLs.licenseKeys.index,
-    label: "License keys",
-  },
-  {
-    path: settingsURLs.storage,
-    label: "Storage",
-  },
-  {
-    label: "Network",
-    items: [
-      { path: settingsURLs.network.proxy, label: "Proxy" },
-      { path: settingsURLs.network.dns, label: "DNS" },
-      { path: settingsURLs.network.ntp, label: "NTP" },
-      { path: settingsURLs.network.syslog, label: "Syslog" },
-      {
-        path: settingsURLs.network.networkDiscovery,
-        label: "Network discovery",
-      },
-    ],
-  },
-  {
-    label: "Scripts",
-    items: [
-      {
-        path: settingsURLs.scripts.commissioning.index,
-        label: "Commissioning scripts",
-      },
-      {
-        path: settingsURLs.scripts.testing.index,
-        label: "Testing scripts",
-      },
-    ],
-  },
-  {
-    path: settingsURLs.dhcp.index,
-    label: "DHCP snippets",
-  },
-  {
-    path: settingsURLs.repositories.index,
-    label: "Package repos",
-  },
-];
-
 export const SecondaryNavigation = ({
   isOpen,
+  items,
+  title,
 }: {
   isOpen?: boolean;
+  items: NavItem[];
+  title: string;
 }): JSX.Element => {
   return (
     <div
@@ -196,10 +105,10 @@ export const SecondaryNavigation = ({
     >
       <nav className="p-side-navigation__drawer u-padding-top--medium">
         <h2 className="p-side-navigation__title p-heading--4 p-panel__logo-name">
-          Settings
+          {title}
         </h2>
         <ul className="p-side-navigation__list">
-          {secondaryNavItems.map((item) => (
+          {items.map((item) => (
             <SideNavigationItem item={item} key={item.label} />
           ))}
         </ul>
