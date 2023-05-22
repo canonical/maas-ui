3ef0c56316593162c24bde46ce5643e59cb93792
Nick De Villiers <nick.devilliers@canonical.com>
2023-05-22 12:08:21 +0100

    test: Migrate DeviceListTable to RTL MAASENG-1721

--- src/app/devices/views/DeviceList/DeviceListTable/DeviceListTable.test.tsx
+++ src/app/devices/views/DeviceList/DeviceListTable/DeviceListTable.test.tsx
@@ -1,10 +1,3 @@
-import type { ReactWrapper } from "enzyme";
-import { mount } from "enzyme";
-import { Provider } from "react-redux";
-import { MemoryRouter } from "react-router-dom";
-import { CompatRouter } from "react-router-dom-v5-compat";
-import configureStore from "redux-mock-store";
-
 import DeviceListTable from "./DeviceListTable";
 
 import urls from "app/base/urls";
@@ -16,8 +9,7 @@ import {
   deviceState as deviceStateFactory,
   rootState as rootStateFactory,
 } from "testing/factories";
-
-const mockStore = configureStore();
+import { renderWithBrowserRouter, screen, userEvent } from "testing/utils";
 
 describe("DeviceListTable", () => {
   let device: Device;
@@ -43,109 +35,87 @@ describe("DeviceListTable", () => {
 
   it("links to a device's details page", () => {
     device.system_id = "def456";
-    const store = mockStore(state);
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter>
-          <CompatRouter>
-            <DeviceListTable
-              devices={[device]}
-              onSelectedChange={jest.fn()}
-              selectedIDs={[]}
-            />
-          </CompatRouter>
-        </MemoryRouter>
-      </Provider>
+    renderWithBrowserRouter(
+      <DeviceListTable
+        devices={[device]}
+        onSelectedChange={jest.fn()}
+        selectedIDs={[]}
+      />,
+      { state }
     );
 
-    expect(
-      wrapper.find("Link[data-testid='device-details-link']").at(0).prop("to")
-    ).toBe(urls.devices.device.index({ id: device.system_id }));
+    expect(screen.getAllByTestId("device-details-link")[0]).toHaveAttribute(
+      "href",
+      urls.devices.device.index({ id: device.system_id })
+    );
   });
 
   it("can show when a device has more than one mac address", () => {
     device.primary_mac = "11:11:11:11:11:11";
     device.extra_macs = ["22:22:22:22:22:22", "33:33:33:33:33:33"];
-    const store = mockStore(state);
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter>
-          <CompatRouter>
-            <DeviceListTable
-              devices={[device]}
-              onSelectedChange={jest.fn()}
-              selectedIDs={[]}
-            />
-          </CompatRouter>
-        </MemoryRouter>
-      </Provider>
+    renderWithBrowserRouter(
+      <DeviceListTable
+        devices={[device]}
+        onSelectedChange={jest.fn()}
+        selectedIDs={[]}
+      />,
+      { state }
     );
 
-    expect(wrapper.find("[data-testid='mac-display']").at(0).text()).toBe(
+    expect(screen.getAllByTestId("mac-display")[0]).toHaveTextContent(
       "11:11:11:11:11:11 (+2)"
     );
   });
 
   it("links to a device's zone's details page", () => {
     device.zone = { id: 101, name: "danger" };
-    const store = mockStore(state);
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter>
-          <CompatRouter>
-            <DeviceListTable
-              devices={[device]}
-              onSelectedChange={jest.fn()}
-              selectedIDs={[]}
-            />
-          </CompatRouter>
-        </MemoryRouter>
-      </Provider>
+    renderWithBrowserRouter(
+      <DeviceListTable
+        devices={[device]}
+        onSelectedChange={jest.fn()}
+        selectedIDs={[]}
+      />,
+      { state }
     );
 
-    expect(
-      wrapper.find("Link[data-testid='device-zone-link']").at(0).prop("to")
-    ).toBe(urls.zones.details({ id: device.zone.id }));
+    expect(screen.getAllByTestId("device-zone-link")[0]).toHaveAttribute(
+      "href",
+      urls.zones.details({ id: device.zone.id })
+    );
   });
 
   describe("device list sorting", () => {
-    const getRowTestId = (wrapper: ReactWrapper, index: number) =>
-      wrapper.find("tbody tr").at(index).prop("data-testid");
+    const getRowTestId = (index: number) =>
+      screen.getAllByRole("row")[index].getAttribute("data-testid");
 
-    it("can sort by FQDN", () => {
+    it("can sort by FQDN", async () => {
       const devices = [
         deviceFactory({ fqdn: "b", system_id: "b" }),
         deviceFactory({ fqdn: "c", system_id: "c" }),
         deviceFactory({ fqdn: "a", system_id: "a" }),
       ];
-      const store = mockStore(state);
-      const wrapper = mount(
-        <Provider store={store}>
-          <MemoryRouter>
-            <CompatRouter>
-              <DeviceListTable
-                devices={devices}
-                onSelectedChange={jest.fn()}
-                selectedIDs={[]}
-              />
-            </CompatRouter>
-          </MemoryRouter>
-        </Provider>
+      renderWithBrowserRouter(
+        <DeviceListTable
+          devices={devices}
+          onSelectedChange={jest.fn()}
+          selectedIDs={[]}
+        />,
+        { state }
       );
 
       // Table is sorted be descending FQDN by default
-      expect(getRowTestId(wrapper, 0)).toBe("device-a");
-      expect(getRowTestId(wrapper, 1)).toBe("device-b");
-      expect(getRowTestId(wrapper, 2)).toBe("device-c");
+      expect(getRowTestId(1)).toBe("device-a");
+      expect(getRowTestId(2)).toBe("device-b");
+      expect(getRowTestId(3)).toBe("device-c");
 
       // Change sort to ascending FQDN
-      wrapper.find("[data-testid='fqdn-header']").simulate("click");
-      expect(getRowTestId(wrapper, 0)).toBe("device-c");
-      expect(getRowTestId(wrapper, 1)).toBe("device-b");
-      expect(getRowTestId(wrapper, 2)).toBe("device-a");
+      await userEvent.click(screen.getByRole("button", { name: "FQDN" }));
+      expect(getRowTestId(1)).toBe("device-c");
+      expect(getRowTestId(2)).toBe("device-b");
+      expect(getRowTestId(3)).toBe("device-a");
     });
 
-    it("can sort by IP assignment", () => {
+    it("can sort by IP assignment", async () => {
       const devices = [
         deviceFactory({
           ip_assignment: DeviceIpAssignment.EXTERNAL,
@@ -160,208 +130,160 @@ describe("DeviceListTable", () => {
           system_id: "c",
         }),
       ];
-      const store = mockStore(state);
-      const wrapper = mount(
-        <Provider store={store}>
-          <MemoryRouter>
-            <CompatRouter>
-              <DeviceListTable
-                devices={devices}
-                onSelectedChange={jest.fn()}
-                selectedIDs={[]}
-              />
-            </CompatRouter>
-          </MemoryRouter>
-        </Provider>
+      renderWithBrowserRouter(
+        <DeviceListTable
+          devices={devices}
+          onSelectedChange={jest.fn()}
+          selectedIDs={[]}
+        />,
+        { state }
       );
 
       // Change sort to descending IP assignment
-      wrapper.find("[data-testid='ip-header']").simulate("click");
-      expect(getRowTestId(wrapper, 0)).toBe("device-a");
-      expect(getRowTestId(wrapper, 1)).toBe("device-b");
-      expect(getRowTestId(wrapper, 2)).toBe("device-c");
+      await userEvent.click(
+        screen.getByRole("button", { name: "IP assignment" })
+      );
+      expect(getRowTestId(1)).toBe("device-a");
+      expect(getRowTestId(2)).toBe("device-b");
+      expect(getRowTestId(3)).toBe("device-c");
 
       // Change sort to ascending IP assignment
-      wrapper.find("[data-testid='ip-header']").simulate("click");
-      expect(getRowTestId(wrapper, 0)).toBe("device-c");
-      expect(getRowTestId(wrapper, 1)).toBe("device-b");
-      expect(getRowTestId(wrapper, 2)).toBe("device-a");
+      await userEvent.click(
+        screen.getByRole("button", { name: "IP assignment" })
+      );
+      expect(getRowTestId(1)).toBe("device-c");
+      expect(getRowTestId(2)).toBe("device-b");
+      expect(getRowTestId(3)).toBe("device-a");
     });
 
-    it("can sort by zone", () => {
+    it("can sort by zone", async () => {
       const devices = [
         deviceFactory({ system_id: "c", zone: { id: 1, name: "twilight" } }),
         deviceFactory({ system_id: "a", zone: { id: 2, name: "danger" } }),
         deviceFactory({ system_id: "b", zone: { id: 3, name: "forbidden" } }),
       ];
-      const store = mockStore(state);
-      const wrapper = mount(
-        <Provider store={store}>
-          <MemoryRouter>
-            <CompatRouter>
-              <DeviceListTable
-                devices={devices}
-                onSelectedChange={jest.fn()}
-                selectedIDs={[]}
-              />
-            </CompatRouter>
-          </MemoryRouter>
-        </Provider>
+      renderWithBrowserRouter(
+        <DeviceListTable
+          devices={devices}
+          onSelectedChange={jest.fn()}
+          selectedIDs={[]}
+        />,
+        { state }
       );
 
       // Change sort to descending zone name
-      wrapper.find("[data-testid='zone-header']").simulate("click");
-      expect(getRowTestId(wrapper, 0)).toBe("device-a");
-      expect(getRowTestId(wrapper, 1)).toBe("device-b");
-      expect(getRowTestId(wrapper, 2)).toBe("device-c");
+      await userEvent.click(screen.getByRole("button", { name: "Zone" }));
+      expect(getRowTestId(1)).toBe("device-a");
+      expect(getRowTestId(2)).toBe("device-b");
+      expect(getRowTestId(3)).toBe("device-c");
 
       // Change sort to ascending zone name
-      wrapper.find("[data-testid='zone-header']").simulate("click");
-      expect(getRowTestId(wrapper, 0)).toBe("device-c");
-      expect(getRowTestId(wrapper, 1)).toBe("device-b");
-      expect(getRowTestId(wrapper, 2)).toBe("device-a");
+      await userEvent.click(screen.getByRole("button", { name: "Zone" }));
+      expect(getRowTestId(1)).toBe("device-c");
+      expect(getRowTestId(2)).toBe("device-b");
+      expect(getRowTestId(3)).toBe("device-a");
     });
 
-    it("can sort by owner", () => {
+    it("can sort by owner", async () => {
       const devices = [
         deviceFactory({ owner: "user", system_id: "c" }),
         deviceFactory({ owner: "admin", system_id: "a" }),
         deviceFactory({ owner: "bob", system_id: "b" }),
       ];
-      const store = mockStore(state);
-      const wrapper = mount(
-        <Provider store={store}>
-          <MemoryRouter>
-            <CompatRouter>
-              <DeviceListTable
-                devices={devices}
-                onSelectedChange={jest.fn()}
-                selectedIDs={[]}
-              />
-            </CompatRouter>
-          </MemoryRouter>
-        </Provider>
+      renderWithBrowserRouter(
+        <DeviceListTable
+          devices={devices}
+          onSelectedChange={jest.fn()}
+          selectedIDs={[]}
+        />,
+        { state }
       );
 
       // Change sort to descending owner
-      wrapper.find("[data-testid='owner-header']").simulate("click");
-      expect(getRowTestId(wrapper, 0)).toBe("device-a");
-      expect(getRowTestId(wrapper, 1)).toBe("device-b");
-      expect(getRowTestId(wrapper, 2)).toBe("device-c");
+      await userEvent.click(screen.getByRole("button", { name: "Owner" }));
+      expect(getRowTestId(1)).toBe("device-a");
+      expect(getRowTestId(2)).toBe("device-b");
+      expect(getRowTestId(3)).toBe("device-c");
 
       // Change sort to ascending owner
-      wrapper.find("[data-testid='owner-header']").simulate("click");
-      expect(getRowTestId(wrapper, 0)).toBe("device-c");
-      expect(getRowTestId(wrapper, 1)).toBe("device-b");
-      expect(getRowTestId(wrapper, 2)).toBe("device-a");
+      await userEvent.click(screen.getByRole("button", { name: "Owner" }));
+      expect(getRowTestId(1)).toBe("device-c");
+      expect(getRowTestId(2)).toBe("device-b");
+      expect(getRowTestId(3)).toBe("device-a");
     });
   });
 
   describe("device selection", () => {
-    it("handles selecting a single device", () => {
+    it("handles selecting a single device", async () => {
       const devices = [deviceFactory({ system_id: "abc123" })];
       const onSelectedChange = jest.fn();
-      const store = mockStore(state);
-      const wrapper = mount(
-        <Provider store={store}>
-          <MemoryRouter>
-            <CompatRouter>
-              <DeviceListTable
-                devices={devices}
-                onSelectedChange={onSelectedChange}
-                selectedIDs={[]}
-              />
-            </CompatRouter>
-          </MemoryRouter>
-        </Provider>
+      renderWithBrowserRouter(
+        <DeviceListTable
+          devices={devices}
+          onSelectedChange={onSelectedChange}
+          selectedIDs={[]}
+        />,
+        { state }
       );
 
-      wrapper
-        .find("[data-testid='device-checkbox'] input")
-        .at(0)
-        .simulate("change");
+      await userEvent.click(screen.getAllByTestId("device-checkbox")[0]);
 
       expect(onSelectedChange).toHaveBeenCalledWith(["abc123"]);
     });
 
-    it("handles unselecting a single device", () => {
+    it("handles unselecting a single device", async () => {
       const devices = [deviceFactory({ system_id: "abc123" })];
       const onSelectedChange = jest.fn();
-      const store = mockStore(state);
-      const wrapper = mount(
-        <Provider store={store}>
-          <MemoryRouter>
-            <CompatRouter>
-              <DeviceListTable
-                devices={devices}
-                onSelectedChange={onSelectedChange}
-                selectedIDs={["abc123"]}
-              />
-            </CompatRouter>
-          </MemoryRouter>
-        </Provider>
+      renderWithBrowserRouter(
+        <DeviceListTable
+          devices={devices}
+          onSelectedChange={onSelectedChange}
+          selectedIDs={["abc123"]}
+        />,
+        { state }
       );
 
-      wrapper
-        .find("[data-testid='device-checkbox'] input")
-        .at(0)
-        .simulate("change");
+      await userEvent.click(screen.getAllByTestId("device-checkbox")[0]);
 
       expect(onSelectedChange).toHaveBeenCalledWith([]);
     });
 
-    it("handles selecting all devices", () => {
+    it("handles selecting all devices", async () => {
       const devices = [
         deviceFactory({ system_id: "abc123" }),
         deviceFactory({ system_id: "def456" }),
       ];
       const onSelectedChange = jest.fn();
-      const store = mockStore(state);
-      const wrapper = mount(
-        <Provider store={store}>
-          <MemoryRouter>
-            <CompatRouter>
-              <DeviceListTable
-                devices={devices}
-                onSelectedChange={onSelectedChange}
-                selectedIDs={[]}
-              />
-            </CompatRouter>
-          </MemoryRouter>
-        </Provider>
+      renderWithBrowserRouter(
+        <DeviceListTable
+          devices={devices}
+          onSelectedChange={onSelectedChange}
+          selectedIDs={[]}
+        />,
+        { state }
       );
 
-      wrapper
-        .find("[data-testid='all-devices-checkbox'] input")
-        .simulate("change");
+      await userEvent.click(screen.getByTestId("all-devices-checkbox"));
 
       expect(onSelectedChange).toHaveBeenCalledWith(["abc123", "def456"]);
     });
 
-    it("handles unselecting all devices", () => {
+    it("handles unselecting all devices", async () => {
       const devices = [
         deviceFactory({ system_id: "abc123" }),
         deviceFactory({ system_id: "def456" }),
       ];
       const onSelectedChange = jest.fn();
-      const store = mockStore(state);
-      const wrapper = mount(
-        <Provider store={store}>
-          <MemoryRouter>
-            <CompatRouter>
-              <DeviceListTable
-                devices={devices}
-                onSelectedChange={onSelectedChange}
-                selectedIDs={["abc123", "def456"]}
-              />
-            </CompatRouter>
-          </MemoryRouter>
-        </Provider>
+      renderWithBrowserRouter(
+        <DeviceListTable
+          devices={devices}
+          onSelectedChange={onSelectedChange}
+          selectedIDs={["abc123", "def456"]}
+        />,
+        { state }
       );
 
-      wrapper
-        .find("[data-testid='all-devices-checkbox'] input")
-        .simulate("change");
+      await userEvent.click(screen.getByTestId("all-devices-checkbox"));
 
       expect(onSelectedChange).toHaveBeenCalledWith([]);
     });
