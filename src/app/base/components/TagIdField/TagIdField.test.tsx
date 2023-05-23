25a1a0d986435d3011e96527ec4197bf2162a082
Nick De Villiers <nick.devilliers@canonical.com>
2023-05-23 13:16:20 +0100

    test: Migrate 9 small base components to RTL MAASENG-1737

--- src/app/base/components/TagIdField/TagIdField.test.tsx
+++ src/app/base/components/TagIdField/TagIdField.test.tsx
@@ -1,12 +1,12 @@
-import { mount } from "enzyme";
 import { Formik } from "formik";
 
 import TagIdField from "./TagIdField";
 
 import type { Tag } from "app/store/tag/types";
 import { tag as tagFactory } from "testing/factories";
+import { screen, render, userEvent } from "testing/utils";
 
-describe("FormikField", () => {
+describe("TagIdField", () => {
   let tags: Tag[];
 
   beforeEach(() => {
@@ -17,53 +17,40 @@ describe("FormikField", () => {
   });
 
   it("maps the initial value to the tag format", () => {
-    const wrapper = mount(
+    render(
       <Formik initialValues={{ tags: [2] }} onSubmit={jest.fn()}>
         <TagIdField tagList={tags} />
       </Formik>
     );
-    expect(wrapper.find("FormikField").prop("initialSelected")).toStrictEqual([
-      {
-        id: tags[1].id,
-        name: tags[1].name,
-      },
-    ]);
-    expect(wrapper.find("FormikField").prop("tags")).toStrictEqual([
-      {
-        id: tags[0].id,
-        name: tags[0].name,
-      },
-      {
-        id: tags[1].id,
-        name: tags[1].name,
-      },
-    ]);
+
+    expect(screen.getByRole("button", { name: "tag2" })).toBeInTheDocument();
+    expect(
+      screen.queryByRole("button", { name: "tag1" })
+    ).not.toBeInTheDocument();
   });
 
   it("can override the field name", () => {
-    const wrapper = mount(
+    render(
       <Formik initialValues={{ tags: null }} onSubmit={jest.fn()}>
         <TagIdField name="wombatTags" tagList={tags} />
       </Formik>
     );
-    expect(wrapper.find("FormikField").prop("name")).toBe("wombatTags");
+
+    // The first element with this text is the div where the name is affected
+    expect(screen.getAllByLabelText("Tags")[0]).toHaveAttribute(
+      "name",
+      "wombatTags"
+    );
   });
 
-  it("can populate the list of tags", () => {
-    const wrapper = mount(
+  it("can populate the list of tags", async () => {
+    render(
       <Formik initialValues={{ tags: null }} onSubmit={jest.fn()}>
         <TagIdField tagList={tags} />
       </Formik>
     );
-    expect(wrapper.find("FormikField").prop("tags")).toStrictEqual([
-      {
-        id: tags[0].id,
-        name: tags[0].name,
-      },
-      {
-        id: tags[1].id,
-        name: tags[1].name,
-      },
-    ]);
+    await userEvent.click(screen.getByRole("textbox", { name: "Tags" }));
+    expect(screen.getByRole("option", { name: "tag1" })).toBeInTheDocument();
+    expect(screen.getByRole("option", { name: "tag2" })).toBeInTheDocument();
   });
 });
