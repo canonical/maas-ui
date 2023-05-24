e1de65c992d8f491d95067dd1f8bf8f0c7ed6f4c
Mauricio Faria de Oliveira <mfo@canonical.com>
2023-05-24 17:26:00 -0300

    Revert "fix(legacy): QA fixes for subnet boot architectures form (#2706)"
    This reverts commit d92e02757eef0a6e2084643cea2dec9ecd12efde.
    
    MAAS 3.0 support is in bug-fix only mode (no features/backports).
    
    This is a fix-up commit to a commit introducing a feature.
    (#2706 -> canonical/MAAS-squad#2420)

--- legacy/src/app/controllers/subnet_details.js
+++ legacy/src/app/controllers/subnet_details.js
@@ -70,7 +70,6 @@ export function SubnetDetailsController(
     "known_boot_architectures"
   );
   $scope.newDisabledArches = [];
-  $scope.bootArchSort = "name";
 
   $scope.MAP_SUBNET_ACTION = {
     name: "map_subnet",
@@ -419,32 +418,6 @@ export function SubnetDetailsController(
       );
     }
   };
-
-  const isCurrentSort = (key) => {
-    const sortKey = $scope.bootArchSort.replace("-", "");
-    return key === sortKey;
-  }
-
-  $scope.getBootArchSort = (key) => {
-    if (isCurrentSort(key)) {
-      return $scope.bootArchSort.startsWith("-")
-        ? "descending"
-        : "ascending";
-    }
-    return "none";
-  };
-
-  $scope.setBootArchSort = (key) => {
-    if (isCurrentSort(key)) {
-      if ($scope.bootArchSort.startsWith("-")) {
-        $scope.bootArchSort = key;
-      } else {
-        $scope.bootArchSort = `-${key}`;
-      }
-    } else {
-      $scope.bootArchSort = key;
-    }
-  }
 
   // Called when the subnet has been loaded.
   function subnetLoaded(subnet) {
