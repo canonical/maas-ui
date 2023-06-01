cdf2b5ec37032d504a8863c25499ad0a7d1f0615
Peter Makowski <me+git@petermakowski.io>
2023-06-01 17:28:04 +0200

    remove snapshot test

--- src/app/base/components/DoughnutChart/DoughnutChart.test.tsx
+++ src/app/base/components/DoughnutChart/DoughnutChart.test.tsx
@@ -3,31 +3,6 @@ import DoughnutChart, { TestIds } from "./DoughnutChart";
 import { render, screen } from "testing/utils";
 
 describe("DoughnutChart", () => {
-  it("renders", () => {
-    const { container } = render(
-      <DoughnutChart
-        label="200GB"
-        segmentHoverWidth={20}
-        segmentWidth={15}
-        segments={[
-          {
-            color: "#06C",
-            tooltip: "Allocated",
-            value: 20,
-          },
-          {
-            color: "#cce0f5",
-            tooltip: "Free",
-            value: 5,
-          },
-        ]}
-        size={96}
-      />
-    );
-
-    expect(container).toMatchSnapshot();
-  });
-
   it("can render with a label", () => {
     render(
       <DoughnutChart
