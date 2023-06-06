b6029a042697828d3229d4b1ef098823d4e6d560
Nick De Villiers <nick.devilliers@canonical.com>
2023-06-06 15:48:41 +0100

    Address review comments

--- src/app/zones/views/ZonesList/ZonesList.tsx
+++ src/app/zones/views/ZonesList/ZonesList.tsx
@@ -1,28 +1,25 @@
 import { useEffect } from "react";
 
-import type { ValueOf } from "@canonical/react-components";
 import { useDispatch, useSelector } from "react-redux";
 
 import ZonesListForm from "./ZonesListForm";
 import ZonesListHeader from "./ZonesListHeader";
 import ZonesListTable from "./ZonesListTable";
 
-import PageContent from "app/base/components/PageContent/PageContent";
+import PageContent from "app/base/components/PageContent";
 import { useWindowTitle } from "app/base/hooks";
 import type { SidePanelContextType } from "app/base/side-panel-context";
 import { useSidePanel } from "app/base/side-panel-context";
-import type { SidePanelContent } from "app/base/types";
 import { actions } from "app/store/zone";
 import zoneSelectors from "app/store/zone/selectors";
+import type { ZoneSidePanelContent } from "app/zones/constants";
 import { ZoneActionSidePanelViews } from "app/zones/constants";
 
 const ZonesList = (): JSX.Element => {
   const dispatch = useDispatch();
   const zonesCount = useSelector(zoneSelectors.count);
   const { sidePanelContent, setSidePanelContent } =
-    useSidePanel() as SidePanelContextType<
-      SidePanelContent<ValueOf<typeof ZoneActionSidePanelViews>>
-    >;
+    useSidePanel() as SidePanelContextType<ZoneSidePanelContent>;
 
   useWindowTitle("Zones");
 
