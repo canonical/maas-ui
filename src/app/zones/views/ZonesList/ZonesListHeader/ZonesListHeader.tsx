eda1b65e7861ab82d5bf4351792131b5b090c2bd
Nick De Villiers <nick.devilliers@canonical.com>
2023-06-07 10:10:50 +0100

    chore: Refactor ZonesList to use PageContent MAASENG-1831 (#4974)
    * Refactored ZonesList and ZonesListHeader to use PageContent

--- src/app/zones/views/ZonesList/ZonesListHeader/ZonesListHeader.tsx
+++ src/app/zones/views/ZonesList/ZonesListHeader/ZonesListHeader.tsx
@@ -3,18 +3,19 @@ import { useEffect } from "react";
 import { Button } from "@canonical/react-components";
 import { useDispatch, useSelector } from "react-redux";
 
-import ZonesListForm from "../ZonesListForm";
-
 import ZonesListTitle from "./ZonesListTitle";
 
 import SectionHeader from "app/base/components/SectionHeader";
-import { useSidePanel } from "app/base/side-panel-context";
+import type { SetSidePanelContent } from "app/base/side-panel-context";
 import { actions } from "app/store/zone";
 import zoneSelectors from "app/store/zone/selectors";
-import { ZoneActionHeaderViews } from "app/zones/constants";
+import { ZoneActionSidePanelViews } from "app/zones/constants";
 
-const ZonesListHeader = (): JSX.Element => {
-  const { sidePanelContent, setSidePanelContent } = useSidePanel();
+const ZonesListHeader = ({
+  setSidePanelContent,
+}: {
+  setSidePanelContent: SetSidePanelContent;
+}): JSX.Element => {
   const dispatch = useDispatch();
   const zonesCount = useSelector(zoneSelectors.count);
   const zonesLoaded = useSelector(zoneSelectors.loaded);
@@ -28,33 +29,16 @@ const ZonesListHeader = (): JSX.Element => {
       data-testid="add-zone"
       key="add-zone"
       onClick={() => {
-        setSidePanelContent({ view: ZoneActionHeaderViews.CREATE_ZONE });
+        setSidePanelContent({ view: ZoneActionSidePanelViews.CREATE_ZONE });
       }}
     >
       Add AZ
     </Button>,
   ];
-  let content = null;
-
-  if (
-    sidePanelContent &&
-    sidePanelContent.view === ZoneActionHeaderViews.CREATE_ZONE
-  ) {
-    content = (
-      <ZonesListForm
-        closeForm={() => {
-          setSidePanelContent(null);
-        }}
-        key="add-zone-form"
-      />
-    );
-  }
 
   return (
     <SectionHeader
       buttons={buttons}
-      sidePanelContent={content}
-      sidePanelTitle="Add AZ"
       subtitle={`${zonesCount} AZs available`}
       subtitleLoading={!zonesLoaded}
       title={<ZonesListTitle />}
