6971f683da3466ff0eefd26541f4baea291421fc
Jones Ogolo <jay.topher11@gmail.com>
2023-06-15 09:47:30 +0100

    fix linting issues

--- src/app/controllers/urls.ts
+++ src/app/controllers/urls.ts
@@ -1,8 +1,5 @@
-import type { Controller, ControllerMeta } from "app/store/controller/types";
-import type {
-  ScriptResult,
-  ScriptResultMeta,
-} from "app/store/scriptresult/types";
+import { Controller, ControllerMeta } from "app/store/controller/types";
+import { ScriptResult, ScriptResultMeta } from "app/store/scriptresult/types";
 import { argPath } from "app/utils";
 
 const withId = argPath<{ id: Controller[ControllerMeta.PK] }>;
