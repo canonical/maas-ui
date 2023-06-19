2187ada5493e67474de95f9297da0cdf06313b84
Peter Makowski <peter.makowski@canonical.com>
2023-06-19 09:28:11 +0200

    refactor: subnet details MAASENG-1826 (#5004)

--- src/app/Routes.tsx
+++ src/app/Routes.tsx
@@ -185,6 +185,22 @@ const Routes = (): JSX.Element => (
       }
       path={`${urls.kvm.index}/*`}
     />
+    <Route
+      element={
+        <ErrorBoundary>
+          <SubnetsList />
+        </ErrorBoundary>
+      }
+      path={`${urls.subnets.index}/*`}
+    />
+    <Route
+      element={
+        <ErrorBoundary>
+          <SubnetDetails />
+        </ErrorBoundary>
+      }
+      path={`${urls.subnets.subnet.index(null)}/*`}
+    />
     {/* TODO: Remove this wrapper route once all pages use the new page component wrapper */}
     {/* https://warthogs.atlassian.net/browse/MAASENG-1832 */}
     <Route element={<LegacyPageContentWrapper />}>
@@ -203,22 +219,6 @@ const Routes = (): JSX.Element => (
           </ErrorBoundary>
         }
         path={`${urls.pools.index}/*`}
-      />
-      <Route
-        element={
-          <ErrorBoundary>
-            <SubnetsList />
-          </ErrorBoundary>
-        }
-        path={`${urls.subnets.index}/*`}
-      />
-      <Route
-        element={
-          <ErrorBoundary>
-            <SubnetDetails />
-          </ErrorBoundary>
-        }
-        path={`${urls.subnets.subnet.index(null)}/*`}
       />
       <Route
         element={
