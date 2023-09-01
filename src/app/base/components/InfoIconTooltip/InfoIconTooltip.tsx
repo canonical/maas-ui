0f3a10ab4c2ad53e87af9783262f2e854abebfc3
Jones Ogolo <jay.topher11@gmail.com>
2023-09-01 11:43:12 +0100

    create infoIconTooltip

---
+++ src/app/base/components/InfoIconTooltip/InfoIconTooltip.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode } from "react";
+
+import { Icon, Tooltip } from "@canonical/react-components";
+
+const InfoIconTooltip = ({ message }: { message: ReactNode }) => {
+  return (
+    <Tooltip message={message} position="right">
+      <Icon name="information" />
+    </Tooltip>
+  );
+};
+
+export default InfoIconTooltip;
