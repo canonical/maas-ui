c5c2fa17312fcb35fe719281c9f59c850786ab4d
Jones Ogolo <jay.topher11@gmail.com>
2023-09-13 10:19:42 +0100

    remove jsDoc

--- src/app/utils/IsEphemerallyDeployed.ts
+++ src/app/utils/IsEphemerallyDeployed.ts
@@ -1,11 +1,6 @@
 import type { Machine } from "app/store/machine/types";
 import { NodeStatusCode } from "app/store/types/node";
 
-/**
- * Check if a machine is deployed ephemerally or not
- * @param machine - A machine object
- * @returns boolean value
- */
 export const isEphemerallyDeployed = (machine: Machine | null) => {
   return (
     machine &&
