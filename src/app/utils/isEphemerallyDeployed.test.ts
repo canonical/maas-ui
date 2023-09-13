cb0727293330423640d1a04f7f1eda0aee06424b
Jones Ogolo <jay.topher11@gmail.com>
2023-09-13 10:19:29 +0100

    reove test nesting

--- src/app/utils/isEphemerallyDeployed.test.ts
+++ src/app/utils/isEphemerallyDeployed.test.ts
@@ -1,28 +1,24 @@
 import { isEphemerallyDeployed } from "./IsEphemerallyDeployed";
 
-import type { Machine } from "app/store/machine/types";
 import { NodeStatusCode } from "app/store/types/node";
 import { machine as machineFactory } from "testing/factories";
 
-describe("isEphemerallyDeployed", () => {
-  let machine: Machine;
-
-  beforeEach(() => {
-    machine = machineFactory({
-      system_id: "abc123",
-      status_code: NodeStatusCode.DEPLOYED,
-    });
+it("returns true if a machine is deployed ephemerally", () => {
+  const machine = machineFactory({
+    system_id: "abc123",
+    status_code: NodeStatusCode.DEPLOYED,
+    ephemeral_deploy: true,
   });
 
-  it("returns true if a machine is deployed ephemerally", () => {
-    machine.ephemeral_deploy = true;
+  expect(isEphemerallyDeployed(machine)).toBe(true);
+});
 
-    expect(isEphemerallyDeployed(machine)).toBe(true);
+it("returns false if a machine is not deployed ephemerally", () => {
+  const machine = machineFactory({
+    system_id: "abc123",
+    status_code: NodeStatusCode.DEPLOYED,
+    ephemeral_deploy: false,
   });
 
-  it("returns false if a machine is not deployed ephemerally", () => {
-    machine.ephemeral_deploy = false;
-
-    expect(isEphemerallyDeployed(machine)).toBe(false);
-  });
+  expect(isEphemerallyDeployed(machine)).toBe(false);
 });
