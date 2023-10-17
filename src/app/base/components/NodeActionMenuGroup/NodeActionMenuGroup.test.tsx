dadab4884841db797e54c32c4723531bb00cc863
Nick De Villiers <nick.devilliers@canonical.com>
2023-10-17 09:47:56 +0100

    Fix failing tests

--- src/app/base/components/NodeActionMenuGroup/NodeActionMenuGroup.test.tsx
+++ src/app/base/components/NodeActionMenuGroup/NodeActionMenuGroup.test.tsx
@@ -64,8 +64,8 @@ describe("NodeActionMenuGroup", () => {
       screen.queryByRole("button", { name: Labels.Lock })
     ).not.toBeInTheDocument();
     expect(
-      screen.queryByRole("button", { name: Labels.Power })
-    ).not.toBeInTheDocument();
+      screen.getByRole("button", { name: Labels.Power })
+    ).toBeInTheDocument();
     expect(
       screen.queryByRole("button", { name: Labels.Troubleshoot })
     ).not.toBeInTheDocument();
