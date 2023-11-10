e965b912bb28c83dd135b9147c45b7e44f65ddc6
Nick De Villiers <nick.devilliers@canonical.com>
2023-11-10 12:28:52 +0200

    add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from "path";
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -12,7 +13,7 @@ export default defineConfig(({ mode }) => {
     define: {
       "process.env": env,
     },
-    plugins: [react()],
+    plugins: [react(), eslint()],
     server: { port: 8401, hmr: { port: 8402 } },
     resolve: {
       alias: { "@": path.resolve(__dirname, "src") },
