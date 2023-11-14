f8b4c36ba3f3828d1f07d6acc429f6f7e741126e
Nick De Villiers <nick.devilliers@canonical.com>
2023-11-14 11:07:04 +0000

    fix intro tests

--- src/app/intro/views/MaasIntroSuccess/MaasIntroSuccess.test.tsx
+++ src/app/intro/views/MaasIntroSuccess/MaasIntroSuccess.test.tsx
@@ -54,7 +54,7 @@ describe("MaasIntroSuccess", () => {
     });
     expect(
       screen.getByRole("link", { name: MaasIntroSuccessLabels.FinishSetup })
-    ).toHaveProperty("href", `http://example.com${urls.intro.user}`);
+    ).toHaveAttribute("href", urls.intro.user);
   });
 
   it("links to the dashboard if an admin that has completed the user intro", () => {
@@ -67,7 +67,7 @@ describe("MaasIntroSuccess", () => {
     });
     expect(
       screen.getByRole("link", { name: MaasIntroSuccessLabels.FinishSetup })
-    ).toHaveProperty("href", `http://example.com${urls.dashboard.index}`);
+    ).toHaveAttribute("href", urls.dashboard.index);
   });
 
   it("links to the machine list if a non-admin that has completed the user intro", () => {
@@ -80,7 +80,7 @@ describe("MaasIntroSuccess", () => {
     });
     expect(
       screen.getByRole("link", { name: MaasIntroSuccessLabels.FinishSetup })
-    ).toHaveProperty("href", `http://example.com${urls.machines.index}`);
+    ).toHaveAttribute("href", urls.machines.index);
   });
 
   it("dispatches an action to update completed intro config", async () => {
