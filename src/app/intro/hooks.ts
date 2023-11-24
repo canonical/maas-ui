9648d3b4bf6c2683631266451e7aac0f325c5b6d
Jones Ogolo <jay.topher11@gmail.com>
2023-11-24 09:55:59 +0100

    fix: (driveby) update hook to route to machine list

--- src/app/intro/hooks.ts
+++ src/app/intro/hooks.ts
@@ -1,13 +1,9 @@
-import { useSelector } from "react-redux";
-
 import urls from "app/base/urls";
-import authSelectors from "app/store/auth/selectors";
 
 /**
  * Get the URL to redirect to when the intro closes.
  * @returns The URL to redirect to.
  */
 export const useExitURL = (): string => {
-  const authUser = useSelector(authSelectors.get);
-  return authUser?.is_superuser ? urls.dashboard.index : urls.machines.index;
+  return urls.machines.index;
 };
