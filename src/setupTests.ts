aeb426fbf3a8909ceb63cbb263a85afa99db0972
Nick De Villiers <nick.devilliers@canonical.com>
2023-12-11 11:57:04 +0000

    chore: use MainToolbar in Controllers list

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,8 +3,22 @@ import "@testing-library/jest-dom";
 import { enableFetchMocks } from "jest-fetch-mock";
 
 enableFetchMocks();
+const originalObserver = window.ResizeObserver;
 
 beforeAll(() => {
   // disable act warnings
   global.IS_REACT_ACT_ENVIRONMENT = false;
+});
+
+// mock ResizeObserver for MainToolbar
+beforeEach(() => {
+  window.ResizeObserver = jest.fn(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = originalObserver;
 });
