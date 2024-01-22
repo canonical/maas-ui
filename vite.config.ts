e3444af222f24672469ed3fc743953d3503bd338
Peter Makowski <peter.makowski@canonical.com>
2024-01-22 15:43:55 +0100

    build: optimize js bundle (#5300)
    - use dynamic import for @canonical/macaroon-bakery
    - reduce JS bundle by 505.47 kB for installations without external auth

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
 import eslint from "vite-plugin-eslint";
 
+const manualChunks = [
+  "@canonical/react-components",
+  "@canonical/maas-react-components",
+  "@canonical/macaroon-bakery",
+  "@/app/store/machine/slice",
+];
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, "./");
@@ -20,6 +27,18 @@ export default defineConfig(({ mode }) => {
       rollupOptions: {
         output: {
           sanitizeFileName: false,
+          // Creates an object with sanitized camel-cased module names as keys and original module names as values.
+          // e.g.: { "canonicalReactComponents": ["@canonical/react-components"] }.
+          manualChunks: manualChunks.reduce((chunks, module) => {
+            const sanitizedModule = module
+              .replace(/(^|[-\/])(.)/g, (_, _separator, letter) =>
+                letter.toUpperCase()
+              )
+              .replace(/[^a-zA-Z0-9]/g, "")
+              .replace(/^./, (firstChar) => firstChar.toLowerCase());
+            chunks[sanitizedModule] = [module];
+            return chunks;
+          }, {}),
         },
       },
       sourcemap: true,
