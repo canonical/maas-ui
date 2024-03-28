1ff4f697899b51423135571323e5fe6af4bef826
Peter Makowski <me+git@petermakowski.io>
2024-03-28 15:11:03 +0100

    chore: downgrade cypress 13.6.4

--- tests/machines.spec.ts
+++ tests/machines.spec.ts
@@ -1,52 +1,36 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
+import { exec } from "child_process";
+import { promisify } from "util";
 
-let machineListRequests: (string | Buffer)[] = [];
-let machineCountRequests: (string | Buffer)[] = [];
+const execAsync = promisify(exec);
 
-test.beforeEach(async ({ page, context }) => {
-  machineListRequests = [];
-  machineCountRequests = [];
-  await context.addCookies([
-    { name: "skipsetupintro", value: "true", url: "http://0.0.0.0:5240/" },
-    { name: "skipintro", value: "true", url: "http://0.0.0.0:5240/" },
-  ]);
-  await page.goto("/MAAS/r/machines");
-  await page.getByLabel("Username").click();
-  await page.getByLabel("Username").fill("admin");
-  await page.getByLabel("Username").press("Tab");
-  await page.getByLabel("Password").fill("test");
-  await page.getByLabel("Password").press("Enter");
-});
+export async function runMAASCommand(
+  action: string,
+  params: string
+): Promise<void> {
+  const apiKey = process.env.MAAS_API_KEY;
+  const maasURL = process.env.MAAS_URL;
 
-test("machines list loads", async ({ page }) => {
-  page.on("websocket", async (ws) => {
-    console.log(`WebSocket opened: ${ws.url()}>`);
-    await ws.on("framesent", async (data) => {
-      console.log(data.payload);
-      if (data.payload.includes("machine.list")) {
-        machineListRequests.push(data.payload);
-      }
-      if (data.payload.includes("machine.count")) {
-        machineCountRequests.push(data.payload);
-      }
-    });
-    ws.on("close", () => console.log("WebSocket closed"));
-  });
-  await expect(page).toHaveTitle(/Machines/);
-  await expect(page.getByTestId("main-toolbar-heading")).toHaveText(
-    /[0-9]+ machine[s]? in [0-9]+ pool[s]?/i
-  );
-  await expect(page.getByRole("grid", { name: /Loading/i })).toBeHidden();
-  // expect a single machine.list and machine.count request
-  await expect(machineListRequests.length).toBe(1);
-  await expect(machineCountRequests.length).toBe(1);
-  // perform machine search
-  await page.getByLabel("Search").locator("visible=true").type("doesnotexist");
-  await expect(page.getByRole("grid", { name: /Loading/i })).toBeHidden();
-  await expect(
-    page.getByText(/No machines match the search criteria/)
-  ).toBeVisible();
-  // expect an additional single machine.list request
-  await expect(machineListRequests.length).toBe(2);
-  await expect(machineCountRequests.length).toBe(1);
+  if (!apiKey || !maasURL) {
+    throw new Error(
+      "MAAS_API_KEY or MAAS_URL environment variables are not set."
+    );
+  }
+
+  // Login command
+  const loginCommand = `maas login admin http://${maasURL}/MAAS/api/2.0/ ${apiKey}`;
+  const actionCommand = `maas admin ${action} ${params}`;
+  await execAsync(loginCommand);
+
+  const { stdout, stderr } = await execAsync(actionCommand);
+
+  if (stderr) {
+    throw new Error(`Error executing MAAS command: ${stderr}`);
+  }
+
+  console.log(stdout);
+}
+
+test("example test using runMAASCommand", async () => {
+  await runMAASCommand("zones create", "name=zone-1");
 });
