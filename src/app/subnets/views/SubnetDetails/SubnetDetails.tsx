70de6d30232c34a356c9bd6d8006b64119338715
Nick De Villiers <nick.devilliers@canonical.com>
2024-04-18 08:58:52 +0100

    static ips boilerplate

--- src/app/subnets/views/SubnetDetails/SubnetDetails.tsx
+++ src/app/subnets/views/SubnetDetails/SubnetDetails.tsx
@@ -5,6 +5,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 import StaticRoutes from "./StaticRoutes";
 import SubnetDetailsHeader from "./SubnetDetailsHeader";
+import SubnetStaticIPs from "./SubnetStaticIPs";
 import SubnetSummary from "./SubnetSummary";
 import SubnetUsedIPs from "./SubnetUsedIPs";
 import Utilisation from "./Utilisation";
@@ -120,7 +121,12 @@ const SubnetDetails = (): JSX.Element => {
           path={getRelativeRoute(urls.subnets.subnet.staticRoutes(null), base)}
         />
         <Route
-          element={<ReservedRanges subnetId={id} />}
+          element={
+            <>
+              <SubnetStaticIPs />
+              <ReservedRanges subnetId={id} />
+            </>
+          }
           path={getRelativeRoute(
             urls.subnets.subnet.reservedIpAddresses(null),
             base
