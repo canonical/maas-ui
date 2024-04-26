54c9874889dd094a378d35255ad3b2ea77a45fb3
Nick De Villiers <nick.devilliers@canonical.com>
2024-04-26 16:30:03 +0100

    feat: integrate LimitedIpInput with add form

--- src/app/subnets/views/SubnetDetails/StaticDHCPLease/ReserveDHCPLease/ReserveDHCPLease.tsx
+++ src/app/subnets/views/SubnetDetails/StaticDHCPLease/ReserveDHCPLease/ReserveDHCPLease.tsx
@@ -6,10 +6,15 @@ import type { SubnetActionProps } from "../../types";
 
 import FormikField from "@/app/base/components/FormikField";
 import FormikForm from "@/app/base/components/FormikForm";
+import LimitedIpInput from "@/app/base/components/LimitedIpInput";
 import MacAddressField from "@/app/base/components/MacAddressField";
 import type { RootState } from "@/app/store/root/types";
 import subnetSelectors from "@/app/store/subnet/selectors";
-import { isIpInSubnet } from "@/app/utils/subnetIpRange";
+import {
+  getImmutableAndEditableOctets,
+  getIpRangeFromCidr,
+  isIpInSubnet,
+} from "@/app/utils/subnetIpRange";
 
 type Props = Pick<SubnetActionProps, "subnetId" | "setSidePanelContent">;
 
@@ -29,19 +34,6 @@ const ReserveDHCPLease = ({ subnetId, setSidePanelContent }: Props) => {
     setSidePanelContent(null);
   };
 
-  const ReserveDHCPLeaseSchema = Yup.object().shape({
-    ip_address: Yup.string()
-      .required("IP address is required")
-      .test({
-        name: "is-valid-ip",
-        message: "Invalid IP address",
-        test: (ip_address) =>
-          isIpInSubnet(ip_address as string, subnet?.cidr as string),
-      }),
-    mac_address: Yup.string().required("MAC address is required"),
-    comment: Yup.string(),
-  });
-
   if (loading) {
     return <Spinner text="Loading..." />;
   }
@@ -50,13 +42,49 @@ const ReserveDHCPLease = ({ subnetId, setSidePanelContent }: Props) => {
     return <div>uh oh</div>;
   }
 
-  const subnetMask = subnet.cidr.split("/")[0].slice(0, -1);
+  const [startIp, endIp] = getIpRangeFromCidr(subnet.cidr);
+  const [immutableOctets, _] = getImmutableAndEditableOctets(startIp, endIp);
+
+  const ReserveDHCPLeaseSchema = Yup.object().shape({
+    ip_address: Yup.string()
+      .required("IP address is required")
+      .test({
+        name: "ip-is-valid",
+        message: "This is not a valid IP address",
+        test: (ip_address) => {
+          let valid = true;
+          const octets = `${ip_address}`.split(".");
+          octets.forEach((octet) => {
+            if (isNaN(parseInt(octet))) {
+              valid = false;
+            } else {
+              const octetInt = parseInt(octet);
+              if (octetInt < 0 || octetInt > 255) {
+                valid = false;
+              }
+            }
+          });
+          return valid;
+        },
+      })
+      .test({
+        name: "ip-is-in-subnet",
+        message: "The IP address is outside of the subnet's range.",
+        test: (ip_address) =>
+          isIpInSubnet(
+            `${immutableOctets}.${ip_address}`,
+            subnet?.cidr as string
+          ),
+      }),
+    mac_address: Yup.string().required("MAC address is required"),
+    comment: Yup.string(),
+  });
 
   return (
     <FormikForm<FormValues>
       aria-label="Reserve static DHCP lease"
       initialValues={{
-        ip_address: subnetMask,
+        ip_address: "",
         mac_address: "",
         comment: "",
       }}
@@ -65,7 +93,13 @@ const ReserveDHCPLease = ({ subnetId, setSidePanelContent }: Props) => {
       submitLabel="Reserve static DHCP lease"
       validationSchema={ReserveDHCPLeaseSchema}
     >
-      <FormikField label="IP address" name="ip_address" required type="text" />
+      <FormikField
+        cidr={subnet.cidr}
+        component={LimitedIpInput}
+        label="IP address"
+        name="ip_address"
+        required
+      />
       <MacAddressField label="MAC address" name="mac_address" required />
       <FormikField
         label="Comment"
