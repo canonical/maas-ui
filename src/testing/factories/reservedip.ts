5afa5d695a5103a0093312e17f115abbc5cbed72
Nick De Villiers <nick.devilliers@canonical.com>
2024-05-08 11:18:30 +0100

    add factory for node summary and integrate with reserved ip factory

--- src/testing/factories/reservedip.ts
+++ src/testing/factories/reservedip.ts
@@ -1,9 +1,19 @@
-import { extend, sequence } from "cooky-cutter";
+import { define, extend, sequence } from "cooky-cutter";
 
 import { timestampedModel } from "./model";
 
 import type { ReservedIp } from "@/app/store/reservedip/types";
+import type { ReservedIpNodeSummary } from "@/app/store/reservedip/types/base";
 import type { TimestampedModel } from "@/app/store/types/model";
+import { NodeType } from "@/app/store/types/node";
+
+export const reservedIpNodeSummary = define<ReservedIpNodeSummary>({
+  fqdn: "node.example.com",
+  hostname: "node",
+  node_type: NodeType.MACHINE,
+  system_id: "abc123",
+  via: "eth0",
+});
 
 export const reservedIp = extend<TimestampedModel, ReservedIp>(
   timestampedModel,
@@ -13,5 +23,6 @@ export const reservedIp = extend<TimestampedModel, ReservedIp>(
     ip: (i: number) => `192.168.1.${i}`,
     mac_address: (i: number) => `00:00:00:00:00:${i}`,
     subnet: 1,
+    node_summary: reservedIpNodeSummary,
   }
 );
