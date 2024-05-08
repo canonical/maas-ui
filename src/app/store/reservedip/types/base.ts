4cc5863f6bcfabf2b53b60f6bfce6bf37137118d
Nick De Villiers <nick.devilliers@canonical.com>
2024-05-08 10:58:24 +0100

    create ReservedIpNodeSummary type and add to ReservedIp type

--- src/app/store/reservedip/types/base.ts
+++ src/app/store/reservedip/types/base.ts
@@ -1,6 +1,11 @@
 import type { Subnet, SubnetMeta } from "../../subnet/types";
 import type { TimestampedModel } from "../../types/model";
-import type { SimpleNode } from "../../types/node";
+import type {
+  NetworkInterface,
+  Node,
+  NodeType,
+  SimpleNode,
+} from "../../types/node";
 import type { GenericState } from "../../types/state";
 
 import type { APIError } from "@/app/base/types";
@@ -22,6 +27,15 @@ export type ReservedIp = TimestampedModel & {
   mac_address?: string;
   comment?: string;
   subnet: Subnet[SubnetMeta.PK];
+  node_summary?: ReservedIpNodeSummary;
+};
+
+export type ReservedIpNodeSummary = {
+  fqdn: Node["fqdn"];
+  hostname: Node["hostname"];
+  node_type: NodeType;
+  system_id: Node["system_id"];
+  via?: NetworkInterface["name"];
 };
 
 export type ReservedIpState = GenericState<ReservedIp, APIError>;
