f8023b5f26120a056ec8f40192d2857b3f07c1ad
Nick De Villiers <nick.devilliers@canonical.com>
2024-05-09 12:22:30 +0100

    fix slice

--- src/app/store/reservedip/slice.ts
+++ src/app/store/reservedip/slice.ts
@@ -1,12 +1,6 @@
-import type { PayloadAction } from "@reduxjs/toolkit";
 import { createSlice } from "@reduxjs/toolkit";
 
-import type {
-  CreateParams,
-  ReservedIp,
-  ReservedIpState,
-  UpdateParams,
-} from "./types";
+import type { CreateParams, ReservedIpState, UpdateParams } from "./types";
 import { ReservedIpMeta } from "./types/enum";
 
 import {
@@ -23,48 +17,7 @@ const reservedIpSlice = createSlice({
       ReservedIpMeta.PK,
       CreateParams,
       UpdateParams
-    >(ReservedIpMeta.MODEL, ReservedIpMeta.PK),
-
-    get: {
-      prepare: (id: ReservedIp[ReservedIpMeta.PK]) => ({
-        meta: {
-          model: ReservedIpMeta.MODEL,
-          method: "get",
-        },
-        payload: {
-          params: { id },
-        },
-      }),
-      reducer: () => {
-        // No state changes need to be handled for this action.
-      },
-    },
-    getStart: (state: ReservedIpState) => {
-      state.loading = true;
-    },
-    getError: (
-      state: ReservedIpState,
-      action: PayloadAction<ReservedIpState["errors"]>
-    ) => {
-      state.errors = action.payload;
-      state.loading = false;
-      state.saving = false;
-    },
-    getSuccess: (state: ReservedIpState, action: PayloadAction<ReservedIp>) => {
-      const reservedIp = action.payload;
-      // If the item already exists, update it, otherwise
-      // add it to the store.
-      const i = state.items.findIndex(
-        (draftItem: ReservedIp) => draftItem.id === reservedIp.id
-      );
-      if (i !== -1) {
-        state.items[i] = reservedIp;
-      } else {
-        state.items.push(reservedIp);
-      }
-      state.loading = false;
-      state.saving = false;
-    },
+    >({ modelName: ReservedIpMeta.MODEL, primaryKey: ReservedIpMeta.PK }),
   },
 });
 
