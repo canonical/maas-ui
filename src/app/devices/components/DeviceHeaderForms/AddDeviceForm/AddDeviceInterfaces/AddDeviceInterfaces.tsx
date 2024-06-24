85b7507fb7f2909d9d26f12eb64eff0cfb09435c
Nick De Villiers <nick.devilliers@canonical.com>
2024-06-24 09:50:43 +0100

    abstract AddDeviceInterface form fields to separate component

--- src/app/devices/components/DeviceHeaderForms/AddDeviceForm/AddDeviceInterfaces/AddDeviceInterfaces.tsx
+++ src/app/devices/components/DeviceHeaderForms/AddDeviceForm/AddDeviceInterfaces/AddDeviceInterfaces.tsx
@@ -2,21 +2,94 @@ import type { ChangeEvent } from "react";
 import { useRef } from "react";
 
 import { Button, Card, Icon } from "@canonical/react-components";
-import { useFormikContext } from "formik";
+import { Field, useFormikContext } from "formik";
+import { useSelector } from "react-redux";
 
-import type { AddDeviceValues } from "../types";
+import type { AddDeviceInterface, AddDeviceValues } from "../types";
 
 import FormikField from "@/app/base/components/FormikField";
 import IpAssignmentSelect from "@/app/base/components/IpAssignmentSelect";
 import MacAddressField from "@/app/base/components/MacAddressField";
+import PrefixedIpInput from "@/app/base/components/PrefixedIpInput";
 import SubnetSelect from "@/app/base/components/SubnetSelect";
 import { DeviceIpAssignment } from "@/app/store/device/types";
+import type { RootState } from "@/app/store/root/types";
+import subnetSelectors from "@/app/store/subnet/selectors";
 import { getNextName } from "@/app/utils";
+
+const AddDeviceInterfaceFields = ({
+  deleteDisabled,
+  iface,
+  index,
+  removeInterface,
+}: {
+  deleteDisabled: boolean;
+  iface: AddDeviceInterface;
+  index: number;
+  removeInterface: (id: number) => void;
+}) => {
+  const { handleChange, setFieldValue, errors } =
+    useFormikContext<AddDeviceValues>();
+  const subnet = useSelector((state: RootState) =>
+    subnetSelectors.getById(state, parseInt(iface.subnet))
+  );
+
+  const showSubnetField = iface.ip_assignment === DeviceIpAssignment.STATIC;
+  const showIpAddressField =
+    iface.ip_assignment === DeviceIpAssignment.STATIC ||
+    iface.ip_assignment === DeviceIpAssignment.EXTERNAL;
+
+  return (
+    <Card data-testid="interface-card" key={iface.id}>
+      <FormikField
+        label="Name"
+        name={`interfaces[${index}].name`}
+        type="text"
+      />
+      <MacAddressField
+        label="MAC address"
+        name={`interfaces[${index}].mac`}
+        required
+      />
+      <IpAssignmentSelect
+        name={`interfaces[${index}].ip_assignment`}
+        onChange={(e: ChangeEvent<HTMLSelectElement>) => {
+          handleChange(e);
+          setFieldValue(`interfaces[${index}].subnet`, "");
+          setFieldValue(`interfaces[${index}].ip_address`, "");
+        }}
+        required
+      />
+      {showSubnetField ? (
+        <SubnetSelect
+          data-testid="subnet-field"
+          name={`interfaces[${index}].subnet`}
+        />
+      ) : null}
+      {showIpAddressField && subnet ? (
+        <Field
+          cidr={subnet.cidr}
+          component={PrefixedIpInput}
+          data-testid="ip-address-field"
+          errors={errors.interfaces?.[index]}
+          label="IP address"
+          name={`interfaces[${index}].ip_address`}
+        />
+      ) : null}
+      {!deleteDisabled ? (
+        <div className="u-align--right">
+          <Button onClick={() => removeInterface(iface.id)} type="button">
+            Delete
+          </Button>
+        </div>
+      ) : null}
+    </Card>
+  );
+};
 
 export const AddDeviceInterfaces = (): JSX.Element => {
   const currentId = useRef<number>(0);
   const {
-    handleChange,
     setFieldValue,
     values: { interfaces },
   } = useFormikContext<AddDeviceValues>();
@@ -49,59 +122,15 @@ export const AddDeviceInterfaces = (): JSX.Element => {
   return (
     <>
       <h4>Interfaces</h4>
-      {interfaces.map((iface, i) => {
-        const showSubnetField =
-          iface.ip_assignment === DeviceIpAssignment.STATIC;
-        const showIpAddressField =
-          iface.ip_assignment === DeviceIpAssignment.STATIC ||
-          iface.ip_assignment === DeviceIpAssignment.EXTERNAL;
-        const deleteDisabled = interfaces.length <= 1;
-
-        return (
-          <Card data-testid="interface-card" key={iface.id}>
-            <FormikField
-              label="Name"
-              name={`interfaces[${i}].name`}
-              type="text"
-            />
-            <MacAddressField
-              label="MAC address"
-              name={`interfaces[${i}].mac`}
-              required
-            />
-            <IpAssignmentSelect
-              name={`interfaces[${i}].ip_assignment`}
-              onChange={(e: ChangeEvent<HTMLSelectElement>) => {
-                handleChange(e);
-                setFieldValue(`interfaces[${i}].subnet`, "");
-                setFieldValue(`interfaces[${i}].ip_address`, "");
-              }}
-              required
-            />
-            {showSubnetField ? (
-              <SubnetSelect
-                data-testid="subnet-field"
-                name={`interfaces[${i}].subnet`}
-              />
-            ) : null}
-            {showIpAddressField ? (
-              <FormikField
-                data-testid="ip-address-field"
-                label="IP address"
-                name={`interfaces[${i}].ip_address`}
-                type="text"
-              />
-            ) : null}
-            {!deleteDisabled ? (
-              <div className="u-align--right">
-                <Button onClick={() => removeInterface(iface.id)} type="button">
-                  Delete
-                </Button>
-              </div>
-            ) : null}
-          </Card>
-        );
-      })}
+      {interfaces.map((iface, i) => (
+        <AddDeviceInterfaceFields
+          deleteDisabled={interfaces.length === 1}
+          iface={iface}
+          index={i}
+          key={iface.id}
+          removeInterface={removeInterface}
+        />
+      ))}
       <Button
         data-testid="add-interface"
         hasIcon
