ec6d2c9bf765106169258a755eeee2b82d6be414
Nick De Villiers <nick.devilliers@canonical.com>
2024-09-30 11:53:50 +0100

    feat(settings): Add kernel crash dump checkbox to settings MAASENG-3758

--- src/app/settings/views/Configuration/KernelParametersForm/KernelParametersForm.test.tsx
+++ src/app/settings/views/Configuration/KernelParametersForm/KernelParametersForm.test.tsx
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 import { MemoryRouter } from "react-router-dom";
 import configureStore from "redux-mock-store";
+import { test } from "vitest";
 
 import KernelParametersForm, {
   Labels as FormLabels,
@@ -9,7 +10,16 @@ import KernelParametersForm, {
 import { ConfigNames } from "@/app/store/config/types";
 import type { RootState } from "@/app/store/root/types";
 import * as factory from "@/testing/factories";
-import { screen, render } from "@/testing/utils";
+import {
+  screen,
+  render,
+  renderWithBrowserRouter,
+  userEvent,
+  waitFor,
+} from "@/testing/utils";
+
+const kernelCrashDumpEnabled =
+  process.env.VITE_APP_KERNEL_CRASH_DUMP_ENABLED === "true";
 
 const mockStore = configureStore();
 
@@ -46,4 +56,75 @@ describe("KernelParametersForm", () => {
       })
     ).toHaveValue("foo");
   });
+
+  it("dispatches an action to update the kernel_opts value", async () => {
+    const state = { ...initialState };
+    const store = mockStore(state);
+
+    renderWithBrowserRouter(<KernelParametersForm />, { store });
+
+    await userEvent.clear(
+      screen.getByRole("textbox", { name: FormLabels.GlobalBootParams })
+    );
+    await userEvent.type(
+      screen.getByRole("textbox", { name: FormLabels.GlobalBootParams }),
+      "bar"
+    );
+
+    await userEvent.click(screen.getByRole("button", { name: "Save" }));
+    expect(
+      store.getActions().find((action) => action.type === "config/update")
+    ).toStrictEqual({
+      meta: {
+        method: "bulk_update",
+        model: "config",
+      },
+      type: "config/update",
+      payload: {
+        params: {
+          items: {
+            kernel_opts: "bar",
+          },
+        },
+      },
+    });
+  });
+
+  test.runIf(kernelCrashDumpEnabled)(
+    "shows a tooltip for minimum OS requirements",
+    async () => {
+      renderWithBrowserRouter(<KernelParametersForm />, {
+        state: { ...initialState },
+      });
+
+      await userEvent.hover(
+        screen.getAllByRole("button", { name: "help-mid-dark" })[1]
+      );
+
+      await waitFor(() => {
+        expect(screen.getByRole("tooltip")).toHaveTextContent(
+          "Ubuntu 24.04 LTS or higher."
+        );
+      });
+    }
+  );
+
+  test.runIf(kernelCrashDumpEnabled)(
+    "shows a tooltip for minimum hardware requirements",
+    async () => {
+      renderWithBrowserRouter(<KernelParametersForm />, {
+        state: { ...initialState },
+      });
+
+      await userEvent.hover(
+        screen.getAllByRole("button", { name: "help-mid-dark" })[0]
+      );
+
+      await waitFor(() => {
+        expect(screen.getByRole("tooltip")).toHaveTextContent(
+          ">= 4 CPU threads, >= 6GB RAM, Reserve >5x RAM size as free disk space in /var."
+        );
+      });
+    }
+  );
 });
