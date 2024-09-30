6bd12d898e3fa008da10566d9be190100802a877
Nick De Villiers <nick.devilliers@canonical.com>
2024-09-30 16:44:15 +0100

    feat(settings): Add kernel crash dump checkbox to settings MAASENG-3758 (#5538)
    - Added kernel crash dump checkbox to settings
    - Added feature flag for kernel crash dump
    - Added modified `help` icon with lighter color to match designs
    - Added tests for tooltips in form field
    - Specified exact values for submission (rather than just submitting `values` object
    
    Resolves [MAASENG-3758](https://warthogs.atlassian.net/browse/MAASENG-3758)

--- src/app/settings/views/Configuration/KernelParametersForm/KernelParametersForm.tsx
+++ src/app/settings/views/Configuration/KernelParametersForm/KernelParametersForm.tsx
@@ -1,8 +1,10 @@
+import { ExternalLink } from "@canonical/maas-react-components";
 import { useDispatch, useSelector } from "react-redux";
 import * as Yup from "yup";
 
 import FormikField from "@/app/base/components/FormikField";
 import FormikForm from "@/app/base/components/FormikForm";
+import TooltipButton from "@/app/base/components/TooltipButton";
 import { configActions } from "@/app/store/config";
 import configSelectors from "@/app/store/config/selectors";
 
@@ -13,6 +15,7 @@ type KernelParametersValues = {
 export enum Labels {
   FormLabel = "Configuration - Kernel parameters",
   GlobalBootParams = "Global boot parameters always passed to the kernel",
+  KernelCrashDump = "Try to enable kernel crash dump by default",
 }
 
 const KernelParametersSchema = Yup.object()
@@ -45,18 +48,54 @@ const KernelParametersForm = (): JSX.Element => {
         label: "Kernel parameters form",
       }}
       onSubmit={(values, { resetForm }) => {
-        dispatch(updateConfig(values));
+        dispatch(updateConfig({ kernel_opts: values.kernel_opts }));
         resetForm({ values });
       }}
       saved={saved}
       saving={saving}
       validationSchema={KernelParametersSchema}
     >
+      <span className="p-heading--5">General</span>
       <FormikField
         label={Labels.GlobalBootParams}
         name="kernel_opts"
         type="text"
       />
+      {import.meta.env.VITE_APP_KERNEL_CRASH_DUMP_ENABLED === "true" && (
+        <>
+          <span className="p-heading--5">Kernel crash dump</span>
+          <FormikField
+            help={
+              <>
+                To enable kernel crash dump, the hardware{" "}
+                <TooltipButton
+                  iconName="help-mid-dark"
+                  message={
+                    <span className="u-align-text--center u-flex--center">
+                      {" "}
+                      &gt;= 4 CPU threads, <br /> &gt;= 6GB RAM, <br />
+                      Reserve &gt;5x RAM size as free disk space in /var.
+                    </span>
+                  }
+                />{" "}
+                and OS{" "}
+                <TooltipButton
+                  iconName="help-mid-dark"
+                  message="Ubuntu 24.04 LTS or higher."
+                />{" "}
+                must meet the minimum requirements and secure boot must be
+                disabled. Check crash dump status in machine details.{" "}
+                <ExternalLink to="https://ubuntu.com/server/docs/kernel-crash-dump">
+                  More about kernel crash dump
+                </ExternalLink>
+              </>
+            }
+            label={Labels.KernelCrashDump}
+            name="kernel_crash_dump"
+            type="checkbox"
+          />
+        </>
+      )}
     </FormikForm>
   );
 };
