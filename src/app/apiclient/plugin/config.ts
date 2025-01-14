e3e01248c571a833c5017d2e74c063cc4199c83e
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-01-14 10:25:50 +0300

    feat: added baseUrl

--- src/app/apiclient/plugin/config.ts
+++ src/app/apiclient/plugin/config.ts
@@ -4,13 +4,13 @@ import { handler } from "./plugin";
 import type { Config } from "./types";
 
 export const defaultConfig: Plugin.Config<Config> = {
-    _dependencies: ["@hey-api/typescript", "@hey-api/sdk"],
-    _handler: handler,
-    _handlerLegacy: () => {},
-    name: "custom-name-plugin",
+  _dependencies: ["@hey-api/typescript", "@hey-api/sdk"],
+  _handler: handler,
+  _handlerLegacy: () => {},
+  name: "custom-name-plugin",
 };
 
 export const defineConfig: Plugin.DefineConfig<Config> = (config) => ({
-    ...defaultConfig,
-    ...config,
+  ...defaultConfig,
+  ...config,
 });
