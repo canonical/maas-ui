5df6cd14f2b3be1d9dc97d3b42d38e3e259023c4
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-01-14 11:19:20 +0300

    feat: plugin removes parsed path from method name

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -1,11 +1,5 @@
 import { defineConfig as defineCustomPlugin } from "./src/app/apiclient/plugin";
 
-import { client } from "./src/app/apiclient/codegen";
-
-client.setConfig({
-  baseUrl: "http://maas-ui-demo.internal:5240/MAAS/a/v3/",
-});
-
 export default {
   client: "@hey-api/client-fetch",
   input: "http://maas-ui-demo.internal:5240/MAAS/a/openapi.json",
--- src/app/apiclient/plugin/plugin.ts
+++ src/app/apiclient/plugin/plugin.ts
@@ -17,7 +17,21 @@ export const handler: Plugin.Handler<Config> = ({ context }) => {
           method as keyof IR.PathItemObject
         ] as IR.OperationObject;
 
-        methodData.id = methodData.id.split("Maas")[0];
+        const pathName = path
+          .split("/")
+          .filter(Boolean)
+          .map((segment) => segment.replace(/[{}]/g, ""))
+          .map((segment) =>
+            segment
+              .split("_")
+              .map(
+                (part) =>
+                  part.charAt(0).toUpperCase() + part.slice(1).toLowerCase()
+              )
+              .join("")
+          )
+          .join("");
+        methodData.id = methodData.id.split(pathName)[0];
       });
     });
   });
