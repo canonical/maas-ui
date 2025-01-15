a04eefb36a16a0fc1f02e0bd57069b17fa7460ea
Nick De Villiers <nick.devilliers@canonical.com>
2025-01-15 11:01:30 +0000

    chore(base): Add comments and docstrings to initial react-query code MAAASENG-4252 (#5577)
    - Added comments and docstrings to helper functions surrounding react-query implementation
    
    Resolves [MAAASENG-4252](https://warthogs.atlassian.net/browse/MAAASENG-4252)

--- src/app/api/query/zones.ts
+++ src/app/api/query/zones.ts
@@ -8,15 +8,29 @@ const zoneKeys = {
   list: ["zones"] as const,
 };
 
+/**
+ * Fetches a list of zones.
+ *
+ * @returns The zones list as a UseQueryResult
+ */
 export const useZones = () => {
   return useWebsocketAwareQuery(zoneKeys.list, fetchZones);
 };
 
+/**
+ * Fetches the number of zones.
+ * @returns The number of zones as a UseQueryResult
+ */
 export const useZoneCount = () =>
   useWebsocketAwareQuery<Zone[], Zone[], number>(["zones"], fetchZones, {
     select: (data) => data?.length ?? 0,
   });
 
+/**
+ * Get a zone by its ID from the list result.
+ * @param id The zone's ID
+ * @returns A Zone, or null as a UseQueryResult
+ */
 export const useZoneById = (id?: ZonePK | null) =>
   useWebsocketAwareQuery(zoneKeys.list, fetchZones, {
     select: selectById<Zone>(id ?? null),
