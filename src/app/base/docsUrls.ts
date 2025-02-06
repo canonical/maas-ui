477dccb20790e65a2495cce24250767dfdeeedfb
Nick De Villiers <nick.devilliers@canonical.com>
2025-02-06 12:15:45 +0000

    use Playwright to check links instead of Cypress

--- src/app/base/docsUrls.ts
+++ src/app/base/docsUrls.ts
@@ -1,25 +1,26 @@
 const docsUrls = {
-  aboutNativeTLS: "https://maas.io/docs/how-to-implement-tls#enabling-tls-2",
+  aboutNativeTLS:
+    "https://maas.io/docs/how-to-implement-tls#p-9022-enabling-tls",
   addMachines: "https://maas.io/docs/how-to-provision-machines",
   addNodesViaChassis:
-    "https://maas.io/docs/how-to-manage-machines#add-machines-via-chassis-ui-2",
+    "https://maas.io/docs/how-to-provision-machines#p-9078-add-machines",
   autoRenewTLSCert:
-    "https://maas.io/docs/how-to-implement-tls#auto-renew-certs-8",
+    "https://maas.io/docs/how-to-implement-tls#p-9022-auto-renew-certs",
   cloudInit:
     "https://maas.io/docs/how-to-customise-machines#p-9014-cloud-init-pre-seeding",
   configurationJourney:
     "https://maas.io/docs/how-to-install-maas#p-9034-maas-ui-setup",
   customisingDeployedMachines: "https://maas.io/docs/how-to-customise-machines",
   dhcp: "https://maas.io/docs/about-dhcp-in-maas",
-  ipmi: "https://maas.io/docs/reference-power-drivers#ipmi-6",
+  ipmi: "https://maas.io/docs/reference-power-drivers#p-17434-ipmi",
   ipRanges:
     "https://maas.io/docs/how-to-customise-dhcp#p-9038-edit-delete-or-extend-ip-range",
   kvmIntroduction: "https://maas.io/docs/how-to-use-lxd-vms",
   networkDiscovery:
-    "https://maas.io/docs/how-to-customise-maas-networks#p-9070-network-discovery",
+    "https://maas.io/docs/how-to-customise-maas-networks#p-9070-manage-network-discovery",
   rackController:
-    "https://maas.io/docs/how-to-provision-machines#p-9078-configure-controllers",
-  sshKeys: "https://maas.io/docs/how-to-manage-user-access#add-ssh-keys-5",
+    "https://maas.io/docs/about-controllers#p-13954-rack-controllers",
+  sshKeys: "https://maas.io/docs/how-to-manage-user-access#p-9090-add-ssh-keys",
   tagsAutomatic:
     "https://maas.io/docs/about-device-labels#p-13973-automatic-tags",
   tagsKernelOptions:
