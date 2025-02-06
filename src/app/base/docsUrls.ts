fc4c5da41d0e7fc5fec3504831f03b3e44be085b
Nick De Villiers <nick.devilliers@canonical.com>
2025-02-06 09:00:41 +0000

    add link suggestions from Bill

--- src/app/base/docsUrls.ts
+++ src/app/base/docsUrls.ts
@@ -12,18 +12,18 @@ const docsUrls = {
   customisingDeployedMachines: "https://maas.io/docs/how-to-customise-machines",
   dhcp: "https://maas.io/docs/about-dhcp-in-maas",
   ipmi: "https://maas.io/docs/reference-power-drivers#ipmi-6",
-  ipRanges: "https://maas.io/docs/how-to-enable-dhcp#create-an-ip-range-ui-2", // broken link, no suitable replacement page that explains IP ranges
+  ipRanges: "https://maas.io/docs/how-to-customise-dhcp#p-9038-edit-delete-or-extend-ip-range",
   kvmIntroduction: "https://maas.io/docs/how-to-use-lxd-vms",
   networkDiscovery:
     "https://maas.io/docs/how-to-customise-maas-networks#p-9070-network-discovery",
   rackController:
     "https://maas.io/docs/how-to-provision-machines#p-9078-configure-controllers",
   sshKeys: "https://maas.io/docs/how-to-manage-user-access#add-ssh-keys-5",
-  tagsAutomatic: "https://maas.io/docs/how-to-manage-tags#automatic-tags-17", // moved to /how-to-use-group-machines, no functioning header link
+  tagsAutomatic: "https://maas.io/docs/about-device-labels#p-13973-automatic-tags", 
   tagsKernelOptions:
-    "https://maas.io/docs/how-to-manage-tags#update-tag-kernel-options-22", // moved to /how-to-use-group-machines, no functioning header link
+    "https://maas.io/docs/how-to-provision-machines#p-9078-set-boot-options", 
   tagsXpathExpressions:
-    "https://maas.io/docs/how-to-manage-tags#automatic-tags-17", // no suitable replacement page that explains Xpath expressions
+    "https://www.w3schools.com/xml/XPath_intro.asp",
   vaultIntegration: "https://maas.io/docs/how-to-integrate-vault",
   windowsImages:
     "https://maas.io/docs/how-to-build-maas-images#p-17423-building-windows-images-with-packer",
