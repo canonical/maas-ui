06f9033ebddd54e538f26fc7e083cbeeaa8e66d6
Nick De Villiers <nick.devilliers@canonical.com>
2025-02-06 09:14:17 +0000

    lint

--- src/app/base/docsUrls.ts
+++ src/app/base/docsUrls.ts
@@ -12,18 +12,19 @@ const docsUrls = {
   customisingDeployedMachines: "https://maas.io/docs/how-to-customise-machines",
   dhcp: "https://maas.io/docs/about-dhcp-in-maas",
   ipmi: "https://maas.io/docs/reference-power-drivers#ipmi-6",
-  ipRanges: "https://maas.io/docs/how-to-customise-dhcp#p-9038-edit-delete-or-extend-ip-range",
+  ipRanges:
+    "https://maas.io/docs/how-to-customise-dhcp#p-9038-edit-delete-or-extend-ip-range",
   kvmIntroduction: "https://maas.io/docs/how-to-use-lxd-vms",
   networkDiscovery:
     "https://maas.io/docs/how-to-customise-maas-networks#p-9070-network-discovery",
   rackController:
     "https://maas.io/docs/how-to-provision-machines#p-9078-configure-controllers",
   sshKeys: "https://maas.io/docs/how-to-manage-user-access#add-ssh-keys-5",
-  tagsAutomatic: "https://maas.io/docs/about-device-labels#p-13973-automatic-tags", 
+  tagsAutomatic:
+    "https://maas.io/docs/about-device-labels#p-13973-automatic-tags",
   tagsKernelOptions:
-    "https://maas.io/docs/how-to-provision-machines#p-9078-set-boot-options", 
-  tagsXpathExpressions:
-    "https://www.w3schools.com/xml/XPath_intro.asp",
+    "https://maas.io/docs/how-to-provision-machines#p-9078-set-boot-options",
+  tagsXpathExpressions: "https://www.w3schools.com/xml/XPath_intro.asp",
   vaultIntegration: "https://maas.io/docs/how-to-integrate-vault",
   windowsImages:
     "https://maas.io/docs/how-to-build-maas-images#p-17423-building-windows-images-with-packer",
