501f4155894417ccb49b677f6b751aa7803f8aac
Nick De Villiers <nick.devilliers@canonical.com>
2025-02-10 11:35:20 +0000

    fix: lint again

--- src/app/preferences/views/APIKeys/APIKeyAdd/APIKeyAdd.test.tsx
+++ src/app/preferences/views/APIKeys/APIKeyAdd/APIKeyAdd.test.tsx
@@ -3,10 +3,15 @@ import { Label as APIKeyFormLabels } from "../APIKeyForm/APIKeyForm";
 import { APIKeyAdd } from "./APIKeyAdd";
 
 import type { RootState } from "@/app/store/root/types";
+import * as factory from "@/testing/factories";
 import { screen, renderWithBrowserRouter } from "@/testing/utils";
 
 describe("APIKeyAdd", () => {
   let state: RootState;
+
+  beforeEach(() => {
+    state = factory.rootState();
+  });
 
   it("can render", () => {
     renderWithBrowserRouter(<APIKeyAdd />, {
