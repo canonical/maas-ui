a959ccfd4886dd5dbd5b025517be8c7be9cefd9a
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-02-20 17:13:06 +0300

    feat(images): added accordions and styling to select upstream images form

--- src/app/images/components/ImagesTable/DownloadImages/DownloadImagesSelect/DownloadImagesSelect.tsx
+++ src/app/images/components/ImagesTable/DownloadImages/DownloadImagesSelect/DownloadImagesSelect.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import { useState } from "react";
 
 import { MultiSelect, type MultiSelectItem } from "@canonical/react-components";
 import { Field } from "formik";
 
 import type { GroupedImages } from "@/app/images/components/ImagesTable/DownloadImages/DownloadImages";
+import "./_index.scss";
+
+const getValueKey = (distro: string, release: string) =>
+  `${distro}-${release}`.replace(".", "-");
 
 type DownloadImagesSelectProps = {
   values: Record<string, MultiSelectItem[]>;
@@ -11,49 +15,75 @@ type DownloadImagesSelectProps = {
   groupedImages: GroupedImages;
 };
 
-const getValueKey = (distro: string, release: string) =>
-  `${distro}-${release}`.replace(".", "-");
-
-const DownloadImagesSelect: React.FC<DownloadImagesSelectProps> = ({
+const DownloadImagesSelect = ({
   values,
   setFieldValue,
   groupedImages,
-}) => {
+}: DownloadImagesSelectProps) => {
+  const [expandedDistro, setExpandedDistro] = useState<Array<string>>([]);
+
   return (
     <>
-      {Object.keys(groupedImages).map((distro) => (
-        <span key={distro}>
-          <h2 className="p-heading--4">{distro} images</h2>
-          <table className="download-images-table">
-            <thead>
-              <tr>
-                <th>Release</th>
-                <th>Architecture</th>
-              </tr>
-            </thead>
-            <tbody>
-              {Object.keys(groupedImages[distro]).map((release) => (
-                <tr aria-label={release} key={release}>
-                  <td>{release}</td>
-                  <td>
-                    <Field
-                      as={MultiSelect}
-                      items={groupedImages[distro][release]}
-                      name={`${distro}-${release}`}
-                      onItemsUpdate={(items: MultiSelectItem) =>
-                        setFieldValue(getValueKey(distro, release), items)
-                      }
-                      placeholder="Select architectures"
-                      selectedItems={values[getValueKey(distro, release)]}
-                      variant="condensed"
-                    />
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </span>
-      ))}
+      {Object.keys(groupedImages).map((distro) => {
+        const isExpanded = expandedDistro.indexOf(distro) !== -1;
+
+        return (
+          <div className="p-accordion" key={distro}>
+            <button
+              aria-expanded={isExpanded}
+              className="p-accordion__tab"
+              onClick={() =>
+                setExpandedDistro((prevState) => {
+                  if (isExpanded) {
+                    return prevState.filter((value) => value !== distro);
+                  }
+                  return [...prevState, distro];
+                })
+              }
+              role="tab"
+              type="button"
+            >
+              <h2 className="p-heading--4">{distro}</h2>
+            </button>
+            {isExpanded && (
+              <section
+                aria-hidden={!isExpanded}
+                className="p-accordion__panel"
+                role="tabpanel"
+              >
+                <table className="download-images-table">
+                  <thead>
+                    <tr>
+                      <th>Release Title</th>
+                      <th>Architecture</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {Object.keys(groupedImages[distro]).map((release) => (
+                      <tr key={release}>
+                        <td>{release}</td>
+                        <td>
+                          <Field
+                            as={MultiSelect}
+                            items={groupedImages[distro][release]}
+                            name={getValueKey(distro, release)}
+                            onItemsUpdate={(items: MultiSelectItem) =>
+                              setFieldValue(getValueKey(distro, release), items)
+                            }
+                            placeholder="Select architectures"
+                            selectedItems={values[getValueKey(distro, release)]}
+                            variant="condensed"
+                          />
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </section>
+            )}
+          </div>
+        );
+      })}
     </>
   );
 };
