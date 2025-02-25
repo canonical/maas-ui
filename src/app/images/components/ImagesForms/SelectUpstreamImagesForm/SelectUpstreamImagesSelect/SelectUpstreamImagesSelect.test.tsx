1f4a8e36d709a97ea049161a316d58fd396f067b
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-02-25 18:18:37 +0300

    chore(deps): Update MAAS UI react-components v2 MAASENG-4478 (#5622)

--- src/app/images/components/ImagesForms/SelectUpstreamImagesForm/SelectUpstreamImagesSelect/SelectUpstreamImagesSelect.test.tsx
+++ src/app/images/components/ImagesForms/SelectUpstreamImagesForm/SelectUpstreamImagesSelect/SelectUpstreamImagesSelect.test.tsx
@@ -9,7 +9,7 @@ import SelectUpstreamImagesSelect from "@/app/images/components/ImagesForms/Sele
 import { ConfigNames } from "@/app/store/config/types";
 import type { RootState } from "@/app/store/root/types";
 import * as factory from "@/testing/factories";
-import { screen, userEvent, renderWithMockStore } from "@/testing/utils";
+import { screen, userEvent, renderWithProviders } from "@/testing/utils";
 
 describe("SelectUpstreamImagesSelect", () => {
   let state: RootState;
@@ -50,7 +50,7 @@ describe("SelectUpstreamImagesSelect", () => {
     );
     const imagesByOS = groupImagesByOS(downloadableImages);
     const groupedImages = groupArchesByRelease(imagesByOS);
-    renderWithMockStore(
+    renderWithProviders(
       <Formik initialValues={{ images: [] }} onSubmit={vi.fn()}>
         {({ values, setFieldValue }: { values: any; setFieldValue: any }) => (
           <SelectUpstreamImagesSelect
@@ -91,7 +91,7 @@ describe("SelectUpstreamImagesSelect", () => {
     );
     const imagesByOS = groupImagesByOS(downloadableImages);
     const groupedImages = groupArchesByRelease(imagesByOS);
-    renderWithMockStore(
+    renderWithProviders(
       <Formik initialValues={{ images: [] }} onSubmit={vi.fn()}>
         {({ values, setFieldValue }: { values: any; setFieldValue: any }) => (
           <SelectUpstreamImagesSelect
@@ -106,9 +106,11 @@ describe("SelectUpstreamImagesSelect", () => {
 
     await userEvent.click(screen.getByText("Ubuntu"));
 
-    const checkboxes = screen.getAllByRole("checkbox", {
-      hidden: true,
-    });
+    const combobox = screen.getByRole("combobox");
+
+    await userEvent.click(combobox);
+
+    const checkboxes = screen.getAllByRole("checkbox");
 
     const labels = checkboxes
       .map((checkbox) => checkbox.closest("label"))
