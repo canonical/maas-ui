d42479d051830e7c64ff1bf99c4bc90cf4dec936
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-03-05 15:42:02 +0300

    fix(images): switched to vanilla Accordion

--- src/app/images/components/ImagesForms/SelectUpstreamImagesForm/SelectUpstreamImagesSelect/SelectUpstreamImagesSelect.tsx
+++ src/app/images/components/ImagesForms/SelectUpstreamImagesForm/SelectUpstreamImagesSelect/SelectUpstreamImagesSelect.tsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import { useMemo } from "react";
 
-import { MultiSelect, type MultiSelectItem } from "@canonical/react-components";
+import {
+  Accordion,
+  MultiSelect,
+  type MultiSelectItem,
+} from "@canonical/react-components";
+import type { Section } from "@canonical/react-components/dist/components/Accordion/Accordion";
 import { Field } from "formik";
 
 import type { GroupedImages } from "@/app/images/components/ImagesForms/SelectUpstreamImagesForm/SelectUpstreamImagesForm";
+
 import "./_index.scss";
 
 const getValueKey = (distro: string, release: string) =>
@@ -20,72 +26,47 @@ const SelectUpstreamImagesSelect = ({
   setFieldValue,
   groupedImages,
 }: DownloadImagesSelectProps) => {
-  const [expandedDistro, setExpandedDistro] = useState<Array<string>>([]);
-
-  return (
-    <>
-      {Object.keys(groupedImages).map((distro) => {
-        const isExpanded = expandedDistro.indexOf(distro) !== -1;
-
-        return (
-          <div className="p-accordion" key={distro}>
-            <button
-              aria-expanded={isExpanded}
-              className="p-accordion__tab"
-              onClick={() =>
-                setExpandedDistro((prevState) => {
-                  if (isExpanded) {
-                    return prevState.filter((value) => value !== distro);
-                  }
-                  return [...prevState, distro];
-                })
-              }
-              role="tab"
-              type="button"
-            >
-              <h2 className="p-heading--4">{distro}</h2>
-            </button>
-            {isExpanded && (
-              <section
-                aria-hidden={!isExpanded}
-                className="p-accordion__panel"
-                role="tabpanel"
-              >
-                <table className="download-images-table">
-                  <thead>
-                    <tr>
-                      <th>Release Title</th>
-                      <th>Architecture</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {Object.keys(groupedImages[distro]).map((release) => (
-                      <tr key={release}>
-                        <td>{release}</td>
-                        <td>
-                          <Field
-                            as={MultiSelect}
-                            items={groupedImages[distro][release]}
-                            name={getValueKey(distro, release)}
-                            onItemsUpdate={(items: MultiSelectItem) =>
-                              setFieldValue(getValueKey(distro, release), items)
-                            }
-                            placeholder="Select architectures"
-                            selectedItems={values[getValueKey(distro, release)]}
-                            variant="condensed"
-                          />
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </section>
-            )}
-          </div>
-        );
-      })}
-    </>
+  const accordionSections = useMemo(
+    () =>
+      Object.keys(groupedImages).map((distro) => {
+        return {
+          key: distro,
+          content: (
+            <table className="download-images-table">
+              <thead>
+                <tr>
+                  <th>Release Title</th>
+                  <th>Architecture</th>
+                </tr>
+              </thead>
+              <tbody>
+                {Object.keys(groupedImages[distro]).map((release) => (
+                  <tr key={release}>
+                    <td>{release}</td>
+                    <td>
+                      <Field
+                        as={MultiSelect}
+                        items={groupedImages[distro][release]}
+                        name={getValueKey(distro, release)}
+                        onItemsUpdate={(items: MultiSelectItem) =>
+                          setFieldValue(getValueKey(distro, release), items)
+                        }
+                        placeholder="Select architectures"
+                        selectedItems={values[getValueKey(distro, release)]}
+                        variant="condensed"
+                      />
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ),
+          titleElement: "h4",
+          title: distro,
+        } as Section;
+      }),
+    [groupedImages, values, setFieldValue]
   );
+  return <Accordion sections={accordionSections} />;
 };
-
 export default SelectUpstreamImagesSelect;
