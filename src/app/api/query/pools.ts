28c1904eddee8b660da30665fbb2d4dc276306a2
Katarzyna Rumanowska <kasia.rumanowska@canonical.com>
2025-03-12 11:55:26 +0100

    fix(pools): fix styling and testing

--- src/app/api/query/pools.ts
+++ src/app/api/query/pools.ts
@@ -3,6 +3,7 @@ import type { UseQueryOptions } from "@tanstack/react-query";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { useWebsocketAwareQuery } from "./base";
+
 import type {
   CreateResourcePoolData,
   CreateResourcePoolError,
