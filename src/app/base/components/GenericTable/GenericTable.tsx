18d3a5cc044b614aa6a890cc1faadeca20e564f7
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-04-03 14:02:17 +0300

    feat(base): added pagination support to GenericTable.tsx

--- src/app/base/components/GenericTable/GenericTable.tsx
+++ src/app/base/components/GenericTable/GenericTable.tsx
@@ -1,12 +1,7 @@
-import {
-  type Dispatch,
-  type ReactNode,
-  type SetStateAction,
-  useMemo,
-  useState,
-} from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import type { Dispatch, ReactNode, SetStateAction, ChangeEvent } from "react";
 
-import { DynamicTable } from "@canonical/maas-react-components";
+import { DynamicTable, Pagination } from "@canonical/maas-react-components";
 import type {
   Column,
   Row,
@@ -29,6 +24,8 @@ import classNames from "classnames";
 
 import TableCheckbox from "@/app/base/components/GenericTable/TableCheckbox";
 import TableHeader from "@/app/base/components/GenericTable/TableHeader";
+import { DEFAULT_DEBOUNCE_INTERVAL } from "@/app/machines/views/MachineList/MachineListTable/MachineListPagination/MachineListPagination";
+import PageSizeSelect from "@/app/machines/views/MachineList/MachineListTable/PageSizeSelect";
 
 import "./_index.scss";
 
@@ -40,7 +37,11 @@ type GenericTableProps<T extends { id: string | number }> = {
   filterHeaders?: (header: Header<T, unknown>) => boolean;
   groupBy?: string[];
   noData?: ReactNode;
-  pin?: { value: string; isTop: boolean }[];
+  pagination?: { page: number; size: number; total: number };
+  setPagination?: Dispatch<
+    SetStateAction<{ page: number; size: number; total: number }>
+  >;
+  pinGroup?: { value: string; isTop: boolean }[];
   sortBy?: ColumnSort[];
   rowSelection?: RowSelectionState;
   setRowSelection?: Dispatch<SetStateAction<RowSelectionState>>;
@@ -55,15 +56,32 @@ const GenericTable = <T extends { id: string | number }>({
   filterHeaders = () => true,
   groupBy,
   noData,
-  pin,
+  pagination,
+  setPagination,
+  pinGroup,
   sortBy,
   rowSelection,
   setRowSelection,
   variant = "full-height",
 }: GenericTableProps<T>) => {
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
   const [grouping, setGrouping] = useState<GroupingState>(groupBy ?? []);
   const [expanded, setExpanded] = useState<ExpandedState>(true);
   const [sorting, setSorting] = useState<SortingState>(sortBy ?? []);
+
+  const [currentPage, setCurrentPage] = useState<number | undefined>(
+    pagination?.page
+  );
+  const [paginationError, setPaginationError] = useState<string>("");
+
+  useEffect(() => {
+    return () => {
+      if (intervalRef.current) {
+        clearTimeout(intervalRef.current);
+      }
+    };
+  }, []);
 
   if (canSelect) {
     columns = [
@@ -93,10 +111,18 @@ const GenericTable = <T extends { id: string | number }>({
     }
   }
 
+  let totalPages = 0;
+  if (pagination) {
+    totalPages =
+      (pagination.total - (pagination.total % pagination.size)) /
+        pagination.size +
+      (pagination.total % pagination.size > 0 ? 1 : 0);
+  }
+
   data = useMemo(() => {
     return [...data].sort((a, b) => {
-      if (pin && pin.length > 0 && grouping.length > 0) {
-        for (const { value, isTop } of pin) {
+      if (pinGroup && pinGroup.length > 0 && grouping.length > 0) {
+        for (const { value, isTop } of pinGroup) {
           const groupId = grouping[0];
           const aValue = a[groupId as keyof typeof a];
           const bValue = b[groupId as keyof typeof b];
@@ -133,7 +159,7 @@ const GenericTable = <T extends { id: string | number }>({
       }
       return 0;
     });
-  }, [data, sorting, grouping, pin]);
+  }, [data, sorting, grouping, pinGroup]);
 
   const table = useReactTable<T>({
     data,
@@ -163,53 +189,123 @@ const GenericTable = <T extends { id: string | number }>({
   });
 
   return (
-    <DynamicTable className="p-generic-table" variant={variant}>
-      <thead>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.filter(filterHeaders).map((header, index) => (
-              <>
-                <TableHeader header={header} key={header.id} />
-                {index === 2 ? <th className="select-alignment" /> : null}
-              </>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      {table.getRowModel().rows.length < 1 ? (
-        noData
-      ) : (
-        <DynamicTable.Body>
-          {table.getRowModel().rows.map((row) => {
-            const { getIsGrouped, id, getVisibleCells } = row;
-            const isIndividualRow = !getIsGrouped();
-            return (
-              <tr
-                className={classNames({
-                  "individual-row": isIndividualRow,
-                  "group-row": !isIndividualRow,
-                })}
-                key={id}
-              >
-                {getVisibleCells()
-                  .filter((cell) => filterCells(row, cell.column))
-                  .map((cell) => {
-                    const { column, id: cellId } = cell;
-                    return (
-                      <td
-                        className={classNames(`${cell.column.id}`)}
-                        key={cellId}
-                      >
-                        {flexRender(column.columnDef.cell, cell.getContext())}
-                      </td>
+    <>
+      {pagination && setPagination != undefined ? (
+        <span className="u-flex--end">
+          <Pagination
+            className="u-nudge-left--x-large"
+            currentPage={currentPage}
+            error={paginationError}
+            onInputBlur={() => {
+              setCurrentPage(pagination?.page);
+              setPaginationError("");
+            }}
+            onInputChange={function (e: ChangeEvent<HTMLInputElement>): void {
+              if (e.target.value) {
+                setCurrentPage(e.target.valueAsNumber);
+                if (intervalRef.current) {
+                  clearTimeout(intervalRef.current);
+                }
+                intervalRef.current = setTimeout(() => {
+                  if (
+                    e.target.valueAsNumber > totalPages ||
+                    e.target.valueAsNumber < 1
+                  ) {
+                    setPaginationError(
+                      `"${e.target.valueAsNumber}" is not a valid page number.`
                     );
+                  } else {
+                    setPaginationError("");
+                    setPagination((prevState) => {
+                      return { ...prevState, page: e.target.valueAsNumber };
+                    });
+                  }
+                }, DEFAULT_DEBOUNCE_INTERVAL);
+              } else {
+                setCurrentPage(undefined);
+                setPaginationError("Enter a page number.");
+              }
+            }}
+            onNextClick={function (): void {
+              setCurrentPage((prevState) => Number(prevState) + 1);
+              setPagination((prevState) => {
+                return { ...prevState, page: prevState.page + 1 };
+              });
+            }}
+            onPreviousClick={function (): void {
+              setCurrentPage((prevState) => Number(prevState) - 1);
+              setPagination((prevState) => {
+                return { ...prevState, page: prevState.page - 1 };
+              });
+            }}
+            totalPages={totalPages}
+          />
+          <PageSizeSelect
+            pageSize={pagination.size}
+            paginate={function (page: number): void {
+              setCurrentPage(page);
+              setPagination((prevState) => {
+                return { ...prevState, page };
+              });
+            }}
+            setPageSize={function (pageSize: number): void {
+              setPagination((prevState) => {
+                return { ...prevState, size: pageSize };
+              });
+            }}
+          />
+        </span>
+      ) : null}
+      <DynamicTable className="p-generic-table" variant={variant}>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers
+                .filter(filterHeaders)
+                .map((header, index) => (
+                  <>
+                    <TableHeader header={header} key={header.id} />
+                    {index === 2 ? <th className="select-alignment" /> : null}
+                  </>
+                ))}
+            </tr>
+          ))}
+        </thead>
+        {table.getRowModel().rows.length < 1 ? (
+          noData
+        ) : (
+          <DynamicTable.Body>
+            {table.getRowModel().rows.map((row) => {
+              const { getIsGrouped, id, getVisibleCells } = row;
+              const isIndividualRow = !getIsGrouped();
+              return (
+                <tr
+                  className={classNames({
+                    "individual-row": isIndividualRow,
+                    "group-row": !isIndividualRow,
                   })}
-              </tr>
-            );
-          })}
-        </DynamicTable.Body>
-      )}
-    </DynamicTable>
+                  key={id}
+                >
+                  {getVisibleCells()
+                    .filter((cell) => filterCells(row, cell.column))
+                    .map((cell) => {
+                      const { column, id: cellId } = cell;
+                      return (
+                        <td
+                          className={classNames(`${cell.column.id}`)}
+                          key={cellId}
+                        >
+                          {flexRender(column.columnDef.cell, cell.getContext())}
+                        </td>
+                      );
+                    })}
+                </tr>
+              );
+            })}
+          </DynamicTable.Body>
+        )}
+      </DynamicTable>
+    </>
   );
 };
 
--- src/app/pools/components/PoolsTable/PoolsTable.tsx
+++ src/app/pools/components/PoolsTable/PoolsTable.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+
 import usePoolsTableColumns from "./usePoolsTableColumns/usePoolsTableColumns";
 
-import { usePools } from "@/app/api/query/pools";
+import { usePoolCount, usePools } from "@/app/api/query/pools";
 import GenericTable from "@/app/base/components/GenericTable";
 
 const PoolsTable = () => {
-  const listPools = usePools();
+  const { data: poolCount } = usePoolCount();
+  const [pagination, setPagination] = useState({
+    page: 1,
+    size: 50,
+    total: poolCount ?? 0,
+  });
+
+  const listPools = usePools({ query: pagination });
   const resourcePools = listPools.data?.items || [];
+
   return (
     <GenericTable
       columns={usePoolsTableColumns()}
       data={resourcePools}
       noData="No pools found."
+      pagination={pagination}
+      setPagination={setPagination}
       variant="full-height"
     />
   );
