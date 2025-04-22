62bf11905508d4572b61fc94f8fa0906ce64b82f
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-04-22 12:10:33 +0300

    feat(base): Add sticky header to GenericTable MAASENG-4718 (#5683)

--- src/app/zones/views/ZonesList/ZonesList.test.tsx
+++ src/app/zones/views/ZonesList/ZonesList.test.tsx
@@ -2,31 +2,33 @@ import ZonesList from "./ZonesListTable/ZonesListTable";
 
 import { zoneResolvers } from "@/testing/resolvers/zones";
 import {
-  renderWithBrowserRouter,
+  renderWithProviders,
   screen,
   setupMockServer,
+  waitFor,
 } from "@/testing/utils";
 
-setupMockServer(zoneResolvers.listZones.handler());
+const mockServer = setupMockServer(zoneResolvers.listZones.handler());
 
 describe("ZonesList", () => {
   it("correctly fetches the necessary data", async () => {
-    renderWithBrowserRouter(<ZonesList />, {
-      route: "/zones",
-    });
+    renderWithProviders(<ZonesList />);
 
     expect(await screen.findByText("zone-1")).toBeInTheDocument();
   });
 
   it("shows a zones table if there are any zones", async () => {
-    renderWithBrowserRouter(<ZonesList />, { route: "/zones" });
+    renderWithProviders(<ZonesList />);
 
-    expect(screen.getByRole("table")).toBeInTheDocument();
+    expect(screen.getByRole("grid")).toBeInTheDocument();
   });
 
   it("shows a message if there are no zones", async () => {
-    renderWithBrowserRouter(<ZonesList />, { route: "/zones" });
+    mockServer.use(zoneResolvers.listZones.handler({ items: [], total: 0 }));
+    renderWithProviders(<ZonesList />);
 
-    expect(screen.getByText("No zones available.")).toBeInTheDocument();
+    await waitFor(() =>
+      expect(screen.getByText("No zones available.")).toBeInTheDocument()
+    );
   });
 });
