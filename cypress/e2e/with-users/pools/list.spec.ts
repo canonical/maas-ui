164f834176cae749f7b11d9f32804b070a94355a
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-05-15 09:00:11 +0300

    tests(pools): fixed cypress tests

--- cypress/e2e/with-users/pools/list.spec.ts
+++ cypress/e2e/with-users/pools/list.spec.ts
@@ -10,6 +10,6 @@ context("Pools list", () => {
     cy.findByRole("heading", {
       name: /[0-9]+ machine[s]? in [0-9]+ pool[s]?/i,
     }).should("exist");
-    cy.findByLabelText(/Pool list/i);
+    cy.findByLabelText(/Showing [0-9]+ out of [0-9]+ pool[s]?/i);
   });
 });
