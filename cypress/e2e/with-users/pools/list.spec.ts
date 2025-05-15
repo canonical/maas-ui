54d4b8507f4ebc8c49a1090c36e69e570894dd56
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-05-15 09:28:24 +0300

    tests(pools): removed div label check

--- cypress/e2e/with-users/pools/list.spec.ts
+++ cypress/e2e/with-users/pools/list.spec.ts
@@ -10,6 +10,5 @@ context("Pools list", () => {
     cy.findByRole("heading", {
       name: /[0-9]+ machine[s]? in [0-9]+ pool[s]?/i,
     }).should("exist");
-    cy.findByLabelText(/Showing [0-9]+ out of [0-9]+ pool[s]?/i);
   });
 });
