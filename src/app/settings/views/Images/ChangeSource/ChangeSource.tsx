759721990638569d074d1455926866720fb48831
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-05-20 16:14:48 +0300

    fix(images): Save source update MAASENG-4589 (#5709)

--- src/app/settings/views/Images/ChangeSource/ChangeSource.tsx
+++ src/app/settings/views/Images/ChangeSource/ChangeSource.tsx
@@ -1,5 +1,5 @@
 import type { ReactElement } from "react";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { ContentSection } from "@canonical/maas-react-components";
 import { Notification } from "@canonical/react-components";
@@ -9,10 +9,17 @@ import * as Yup from "yup";
 
 import FormikForm from "@/app/base/components/FormikForm";
 import type { APIError } from "@/app/base/types";
+import {
+  getDownloadableImages,
+  getSyncedImages,
+} from "@/app/images/components/ImagesForms/SelectUpstreamImagesForm/SelectUpstreamImagesForm";
 import ChangeSourceFields from "@/app/settings/views/Images/ChangeSource/ChangeSourceFields";
 import { bootResourceActions } from "@/app/store/bootresource";
 import bootResourceSelectors from "@/app/store/bootresource/selectors";
-import { BootResourceSourceType } from "@/app/store/bootresource/types";
+import {
+  BootResourceSourceType,
+  type BootResourceUbuntuSource,
+} from "@/app/store/bootresource/types";
 import { configActions } from "@/app/store/config";
 import configSelectors from "@/app/store/config/selectors";
 
@@ -40,7 +47,10 @@ export type ChangeSourceValues = {
 const ChangeSource = (): ReactElement => {
   const dispatch = useDispatch();
   const resources = useSelector(bootResourceSelectors.resources);
+  const sources = useSelector(bootResourceSelectors.ubuntu);
+  const otherImages = useSelector(bootResourceSelectors.otherImages);
   const autoImport = useSelector(configSelectors.bootImagesAutoImport);
+  const pollingSources = useSelector(bootResourceSelectors.polling);
   const errors = useSelector(bootResourceSelectors.fetchError);
   const saving = useSelector(bootResourceSelectors.fetching);
   const previousSaving = usePrevious(saving);
@@ -51,6 +61,89 @@ const ChangeSource = (): ReactElement => {
   const saved = !saving && previousSaving && !errors;
 
   const canChangeSource = resources.every((resource) => !resource.downloading);
+  const source: BootResourceUbuntuSource =
+    sources !== null &&
+    sources.sources[0].source_type === BootResourceSourceType.CUSTOM
+      ? sources.sources[0]
+      : {
+          keyring_data: "",
+          keyring_filename: "",
+          source_type: BootResourceSourceType.MAAS_IO,
+          url: "",
+        };
+
+  const [ubuntuSystems, setUbuntuSystems] = useState<
+    {
+      arches: string[];
+      osystem: string;
+      release: string;
+    }[]
+  >([]);
+  const [otherSystems, setOtherSystems] = useState<
+    {
+      arch: string;
+      os: string;
+      release: string;
+      subArch: string;
+    }[]
+  >([]);
+
+  useEffect(() => {
+    if (sources && resources && otherImages) {
+      const downloadableImages = getDownloadableImages(
+        sources.releases,
+        sources.arches,
+        otherImages
+      );
+      const syncedImages = getSyncedImages(downloadableImages, resources);
+
+      const ubuntuList: {
+        arches: string[];
+        osystem: string;
+        release: string;
+      }[] = [];
+      const otherList: {
+        arch: string;
+        os: string;
+        release: string;
+        subArch: string;
+      }[] = [];
+      Object.entries(
+        syncedImages as Record<string, { label: string; value: string }[]>
+      ).forEach(([key, images]) => {
+        const [osystem] = key.split("-", 1);
+
+        if (osystem === "Ubuntu" || osystem === "Centos") {
+          const arches = images.map((image) => image.label);
+          const release = images[0].value.split("-")[1];
+          ubuntuList.push({
+            arches,
+            osystem: osystem.toLowerCase(),
+            release,
+          });
+        } else {
+          const [os, release, arch, subArch] = images[0].value.split("-");
+          otherList.push({
+            arch,
+            os,
+            release,
+            subArch,
+          });
+        }
+      });
+      setUbuntuSystems(ubuntuList);
+      setOtherSystems(otherList);
+    }
+  }, [sources, resources, otherImages]);
+
+  useEffect(() => {
+    dispatch(bootResourceActions.poll({ continuous: false }));
+    dispatch(configActions.fetch());
+    return () => {
+      dispatch(bootResourceActions.pollStop());
+      dispatch(bootResourceActions.cleanup());
+    };
+  }, [dispatch]);
 
   return (
     <ContentSection variant="narrow">
@@ -66,35 +159,53 @@ const ChangeSource = (): ReactElement => {
             Image import is in progress, cannot change source settings.
           </Notification>
         )}
-        <FormikForm<ChangeSourceValues>
-          allowUnchanged
-          aria-label="Choose source"
-          cleanup={cleanup}
-          errors={errors as APIError}
-          initialValues={{
-            keyring_data: "",
-            keyring_filename: "",
-            source_type: BootResourceSourceType.MAAS_IO,
-            url: "",
-            autoSync: autoImport || false,
-          }}
-          onSubmit={(values) => {
-            dispatch(cleanup());
-            dispatch(bootResourceActions.fetch(values));
-            dispatch(
-              configActions.update({
-                boot_images_auto_import: values.autoSync,
-              })
-            );
-          }}
-          saved={saved}
-          saving={saving}
-          submitDisabled={!canChangeSource}
-          submitLabel="Save"
-          validationSchema={ChangeSourceSchema}
-        >
-          <ChangeSourceFields />
-        </FormikForm>
+        {!pollingSources && (
+          <FormikForm<ChangeSourceValues>
+            allowUnchanged
+            aria-label="Choose source"
+            cleanup={cleanup}
+            errors={errors as APIError}
+            initialValues={{
+              ...source,
+              autoSync: autoImport || false,
+            }}
+            onSubmit={(values) => {
+              dispatch(cleanup());
+              dispatch(bootResourceActions.fetch(values));
+              dispatch(
+                configActions.update({
+                  boot_images_auto_import: values.autoSync,
+                })
+              );
+              dispatch(
+                bootResourceActions.saveUbuntu({
+                  keyring_data: values.keyring_data,
+                  keyring_filename: values.keyring_filename,
+                  source_type: values.source_type,
+                  url: values.url,
+                  osystems: ubuntuSystems,
+                })
+              );
+              dispatch(bootResourceActions.saveUbuntuSuccess());
+              dispatch(
+                bootResourceActions.saveOther({
+                  images: otherSystems.map(
+                    ({ arch, os, release, subArch = "" }) =>
+                      `${os}/${arch}/${subArch}/${release}`
+                  ),
+                })
+              );
+              dispatch(bootResourceActions.saveOtherSuccess());
+            }}
+            saved={saved}
+            saving={saving}
+            submitDisabled={!canChangeSource}
+            submitLabel="Save"
+            validationSchema={ChangeSourceSchema}
+          >
+            <ChangeSourceFields />
+          </FormikForm>
+        )}
       </ContentSection.Content>
     </ContentSection>
   );
