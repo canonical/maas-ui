daabd9595e414c456eb8d5b34facec65860c7cf0
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-06-03 15:57:51 +0300

    tests(preferences): fixed SSL tests

--- src/app/preferences/views/SSLKeys/components/DeleteSSLKey/DeleteSSLKey.test.tsx
+++ src/app/preferences/views/SSLKeys/components/DeleteSSLKey/DeleteSSLKey.test.tsx
@@ -1,6 +1,5 @@
 import DeleteSSLKey from "./DeleteSSLKey";
 
-import { Label as SSLKeyListLabels } from "@/app/preferences/views/SSLKeys/views/SSLKeyList";
 import { sslKeyResolvers } from "@/testing/resolvers/sslKeys";
 import {
   screen,
@@ -14,21 +13,15 @@ setupMockServer(sslKeyResolvers.deleteSslKey.handler());
 
 describe("DeleteSSLKey", () => {
   it("can show a delete confirmation", () => {
-    renderWithBrowserRouter(<DeleteSSLKey />, {
-      route: "/account/prefs/ssl-keys/1/delete",
-      routePattern: "/account/prefs/ssl-keys/:id/delete",
-    });
-    expect(screen.getByRole("form", { name: SSLKeyListLabels.DeleteConfirm }));
+    renderWithBrowserRouter(<DeleteSSLKey closeForm={vi.fn()} id={1} />);
+    expect(screen.getByRole("form", { name: "Confirm SSL key deletion" }));
     expect(
       screen.getByText(/Are you sure you want to delete this SSL key?/i)
     ).toBeInTheDocument();
   });
 
   it("can delete an SSL key", async () => {
-    renderWithBrowserRouter(<DeleteSSLKey />, {
-      route: "/account/prefs/ssl-keys/1/delete",
-      routePattern: "/account/prefs/ssl-keys/:id/delete",
-    });
+    renderWithBrowserRouter(<DeleteSSLKey closeForm={vi.fn()} id={1} />);
 
     await userEvent.click(screen.getByRole("button", { name: "Delete" }));
     await waitFor(() => {
