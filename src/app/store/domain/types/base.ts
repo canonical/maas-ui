12f5e05d8690b1f1f047657c451d486bdb302a8d
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-06-16 11:18:36 +0300

    refactor(settings): Remove auth and user stores MAASENG-4620 (#5731)

--- src/app/store/domain/types/base.ts
+++ src/app/store/domain/types/base.ts
@@ -1,10 +1,10 @@
 import type { RecordType } from "./enum";
 
+import type { UserResponse } from "@/app/apiclient";
 import type { APIError } from "@/app/base/types";
 import type { TimestampedModel } from "@/app/store/types/model";
 import type { BaseNode, NodeType } from "@/app/store/types/node";
 import type { GenericState } from "@/app/store/types/state";
-import type { User } from "@/app/store/user/types";
 
 export type DomainResource = {
   dnsdata_id: number | null;
@@ -15,7 +15,7 @@ export type DomainResource = {
   rrtype: RecordType;
   system_id: BaseNode["system_id"] | null;
   ttl: number | null;
-  user_id: User["id"] | null;
+  user_id: UserResponse["id"] | null;
 };
 
 export type BaseDomain = TimestampedModel & {
--- src/app/store/user/types/index.ts
+++
@@ -1,5 +0,0 @@
-export type { CreateParams, UpdateParams } from "./actions";
-
-export type { AuthState, User, UserState } from "./base";
-
-export { UserMeta } from "./enum";
