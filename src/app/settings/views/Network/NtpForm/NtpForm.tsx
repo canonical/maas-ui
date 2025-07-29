063e32460202d5d0c941ed7e82e0420240490dc4
Nick De Villiers <nick.devilliers@canonical.com>
2025-07-29 14:25:06 +0100

    feat(settings): Migrate NTP settings to API v3 MAASENG-5038

--- src/app/settings/views/Network/NtpForm/NtpForm.tsx
+++ src/app/settings/views/Network/NtpForm/NtpForm.tsx
@@ -1,15 +1,17 @@
-import { useEffect } from "react";
-
 import { ContentSection } from "@canonical/maas-react-components";
-import { Spinner } from "@canonical/react-components";
-import { useDispatch, useSelector } from "react-redux";
+import { Notification, Spinner } from "@canonical/react-components";
 import * as Yup from "yup";
 
+import {
+  useBulkSetConfigurations,
+  useConfigurations,
+} from "@/app/api/query/configurations";
 import FormikField from "@/app/base/components/FormikField";
 import FormikForm from "@/app/base/components/FormikForm";
 import { useWindowTitle } from "@/app/base/hooks";
+import { getConfigsFromResponse } from "@/app/settings/utils";
 import { configActions } from "@/app/store/config";
-import configSelectors from "@/app/store/config/selectors";
+import { ConfigNames } from "@/app/store/config/types";
 
 const NtpSchema = Yup.object().shape({
   ntp_external_only: Yup.boolean().required(),
@@ -17,25 +19,33 @@ const NtpSchema = Yup.object().shape({
 });
 
 const NtpForm = (): React.ReactElement => {
-  const dispatch = useDispatch();
-  const updateConfig = configActions.update;
-
-  const loaded = useSelector(configSelectors.loaded);
-  const loading = useSelector(configSelectors.loading);
-  const saved = useSelector(configSelectors.saved);
-  const saving = useSelector(configSelectors.saving);
-  const errors = useSelector(configSelectors.errors);
-
-  const ntpExternalOnly = useSelector(configSelectors.ntpExternalOnly);
-  const ntpServers = useSelector(configSelectors.ntpServers);
+  const { data, error, isPending } = useConfigurations({
+    query: { name: [ConfigNames.NTP_EXTERNAL_ONLY, ConfigNames.NTP_SERVERS] },
+  });
+  const updateConfig = useBulkSetConfigurations();
 
   useWindowTitle("NTP");
 
-  useEffect(() => {
-    if (!loaded) {
-      dispatch(configActions.fetch());
-    }
-  }, [dispatch, loaded]);
+  if (isPending) {
+    return <Spinner text="Loading..." />;
+  }
+
+  if (error) {
+    return (
+      <Notification
+        severity="negative"
+        title="Error while fetching configurations:"
+      >
+        {error.message}
+      </Notification>
+    );
+  }
+
+  const { ntp_external_only: ntpExternalOnly, ntp_servers: ntpServers } =
+    getConfigsFromResponse(data.items, [
+      ConfigNames.NTP_EXTERNAL_ONLY,
+      ConfigNames.NTP_SERVERS,
+    ]);
 
   return (
     <ContentSection variant="narrow">
@@ -43,42 +53,55 @@ const NtpForm = (): React.ReactElement => {
         NTP
       </ContentSection.Title>
       <ContentSection.Content>
-        {loading && <Spinner text="Loading..." />}
-        {loaded && (
-          <FormikForm
-            cleanup={configActions.cleanup}
-            errors={errors}
-            initialValues={{
-              ntp_external_only: ntpExternalOnly ?? false,
-              ntp_servers: ntpServers ?? "",
-            }}
-            onSaveAnalytics={{
-              action: "Saved",
-              category: "Network settings",
-              label: "NTP form",
-            }}
-            onSubmit={(values, { resetForm }) => {
-              dispatch(updateConfig(values));
-              resetForm({ values });
-            }}
-            saved={saved}
-            saving={saving}
-            validationSchema={NtpSchema}
-          >
-            <FormikField
-              help="NTP servers, specified as IP addresses or hostnames delimited by commas and/or spaces, to be used as time references for MAAS itself, the machines MAAS deploys, and devices that make use of MAAS's DHCP services."
-              label="Addresses of NTP servers"
-              name="ntp_servers"
-              type="text"
-            />
-            <FormikField
-              help="Configure all region controller hosts, rack controller hosts, and subsequently deployed machines to refer directly to the configured external NTP servers. Otherwise only region controller hosts will be configured to use those external NTP servers, rack contoller hosts will in turn refer to the regions' NTP servers, and deployed machines will refer to the racks' NTP servers."
-              label="Use external NTP servers only"
-              name="ntp_external_only"
-              type="checkbox"
-            />
-          </FormikForm>
-        )}
+        <FormikForm
+          cleanup={configActions.cleanup}
+          errors={updateConfig.error}
+          initialValues={{
+            ntp_external_only: ntpExternalOnly ?? false,
+            ntp_servers: ntpServers ?? "",
+          }}
+          onSaveAnalytics={{
+            action: "Saved",
+            category: "Network settings",
+            label: "NTP form",
+          }}
+          onSubmit={(values, { resetForm }) => {
+            updateConfig.mutate(
+              {
+                body: {
+                  configurations: [
+                    {
+                      name: "ntp_external_only",
+                      value: values.ntp_external_only,
+                    },
+                    { name: "ntp_servers", value: values.ntp_servers },
+                  ],
+                },
+              },
+              {
+                onSuccess: () => {
+                  resetForm({ values });
+                },
+              }
+            );
+          }}
+          saved={updateConfig.isSuccess}
+          saving={updateConfig.isPending}
+          validationSchema={NtpSchema}
+        >
+          <FormikField
+            help="NTP servers, specified as IP addresses or hostnames delimited by commas and/or spaces, to be used as time references for MAAS itself, the machines MAAS deploys, and devices that make use of MAAS's DHCP services."
+            label="Addresses of NTP servers"
+            name="ntp_servers"
+            type="text"
+          />
+          <FormikField
+            help="Configure all region controller hosts, rack controller hosts, and subsequently deployed machines to refer directly to the configured external NTP servers. Otherwise only region controller hosts will be configured to use those external NTP servers, rack contoller hosts will in turn refer to the regions' NTP servers, and deployed machines will refer to the racks' NTP servers."
+            label="Use external NTP servers only"
+            name="ntp_external_only"
+            type="checkbox"
+          />
+        </FormikForm>
       </ContentSection.Content>
     </ContentSection>
   );
