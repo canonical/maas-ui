c844e921d53d07c4e4f67a3e30ab4d56014d09fc
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-08-27 14:23:38 +0300

    refactor(settings): migrated license keys to generic side panel context and generic table

---
+++ src/app/settings/views/LicenseKeys/views/LicenseKeyList.tsx
@@ -0,0 +1,25 @@
+import { ContentSection } from "@canonical/maas-react-components";
+
+import PageContent from "@/app/base/components/PageContent";
+import { useWindowTitle } from "@/app/base/hooks";
+import LicenseKeyTable from "@/app/settings/views/LicenseKeys/components/LicenseKeyTable/LicenseKeyTable";
+
+const LicenseKeyList = (): React.ReactElement => {
+  useWindowTitle("License keys");
+
+  return (
+    <PageContent
+      sidePanelContent={null}
+      sidePanelTitle={null}
+      useNewSidePanelContext={true}
+    >
+      <ContentSection>
+        <ContentSection.Content>
+          <LicenseKeyTable />
+        </ContentSection.Content>
+      </ContentSection>
+    </PageContent>
+  );
+};
+
+export default LicenseKeyList;
