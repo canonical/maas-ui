94072096a156d03d0577744325827fa4d0ce3be0
Nick De Villiers <nick.devilliers@canonical.com>
2025-10-23 13:44:46 +0100

    how do I keep making this worse?

--- src/app/preferences/views/Details/Details.test.tsx
+++ src/app/preferences/views/Details/Details.test.tsx
@@ -2,7 +2,7 @@ import { waitFor } from "@testing-library/react";
 
 import { Details, Label as DetailsLabels } from "./Details";
 
-import { Labels as UserFormLabels } from "@/app/settings/views/UserManagement/constants";
+import { Labels as UserFormLabels } from "@/app/settings/views/UserManagement/views/UsersList/constants";
 import type { RootState } from "@/app/store/root/types";
 import * as factory from "@/testing/factories";
 import { authResolvers } from "@/testing/resolvers/auth";
