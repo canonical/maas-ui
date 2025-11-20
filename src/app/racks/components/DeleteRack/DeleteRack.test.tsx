760efde4d5cb43373cac0d9750b2078f5e289b3a
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-11-20 15:59:39 +0300

    tests: test fixes due to added GET queries

--- src/app/racks/components/DeleteRack/DeleteRack.test.tsx
+++ src/app/racks/components/DeleteRack/DeleteRack.test.tsx
@@ -8,24 +8,27 @@ import {
   userEvent,
   screen,
   waitFor,
+  waitForLoading,
 } from "@/testing/utils";
 
-const mockServer = setupMockServer(rackResolvers.deleteRack.handler());
+const mockServer = setupMockServer(
+  rackResolvers.getRack.handler(),
+  rackResolvers.deleteRack.handler()
+);
 const { mockClose } = await mockSidePanel();
 
 describe("DeleteRack", () => {
   it("calls closeForm on cancel click", async () => {
     renderWithProviders(<DeleteRack id={2} />);
-
+    await waitForLoading();
     await userEvent.click(screen.getByRole("button", { name: "Cancel" }));
     expect(mockClose).toHaveBeenCalled();
   });
 
   it("calls delete rack on save click", async () => {
     renderWithProviders(<DeleteRack id={2} />);
-
+    await waitForLoading();
     await userEvent.click(screen.getByRole("button", { name: /Delete/i }));
-
     await waitFor(() => {
       expect(rackResolvers.deleteRack.resolved).toBeTruthy();
     });
@@ -35,11 +38,9 @@ describe("DeleteRack", () => {
     mockServer.use(
       rackResolvers.deleteRack.error({ code: 400, message: "Uh oh!" })
     );
-
     renderWithProviders(<DeleteRack id={2} />);
-
+    await waitForLoading();
     await userEvent.click(screen.getByRole("button", { name: "Delete" }));
-
     await waitFor(() => {
       expect(screen.getByText(/Uh oh!/i)).toBeInTheDocument();
     });
