039e8cf53b886f6b687ff099a5a003a30d750b21
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2025-12-07 21:34:21 +0300

    fix: removed disable checks

--- cypress/e2e/with-users/login/login.spec.ts
+++ cypress/e2e/with-users/login/login.spec.ts
@@ -16,10 +16,8 @@ context("Login page", () => {
   });
 
   it("logs in and redirects to the intro", () => {
-    cy.findByRole("button", { name: "Next" }).should("be.disabled");
     cy.get("input[name='username']").type(Cypress.env("username"));
     cy.findByRole("button", { name: /Next/ }).click();
-    cy.findByRole("button", { name: "Login" }).should("be.disabled");
     cy.get("input[name='password']").type(Cypress.env("password"));
     cy.get("button[type='submit']").click();
     cy.location("pathname").should("eq", generateMAASURL("/intro"));
