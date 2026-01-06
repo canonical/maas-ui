c3805871c88bafab9e7718a43590652bdfba90d5
Neh Joshi <neh.joshi@canonical.com>
2026-01-06 09:58:30 +0000

    refactor(base): create-volume-group to generic table MAASENG-5677 (#5893)
    - Refactored `<CreateVolumeGroup />` to use `GenericTable`
    - Created a custom `useCreateVolumeGroupColumns` hook
    - Refactored tests
    
    Resolves [MAASENG-5677](https://warthogs.atlassian.net/browse/MAASENG-5677)

--- src/app/base/components/node/StorageTables/AvailableStorageTable/BulkActions/CreateVolumeGroup/CreateVolumeGroup.test.tsx
+++ src/app/base/components/node/StorageTables/AvailableStorageTable/BulkActions/CreateVolumeGroup/CreateVolumeGroup.test.tsx
@@ -9,39 +9,8 @@ import {
   within,
 } from "@/testing/utils";
 
-describe("CreateVolumeGroup", () => {
-  it("sets the initial name correctly", () => {
-    const vgs = [
-      factory.nodeDisk({ type: DiskTypes.VOLUME_GROUP }),
-      factory.nodeDisk({ type: DiskTypes.VOLUME_GROUP }),
-    ];
-    const physicalDisk = factory.nodeDisk({
-      partitions: null,
-      type: DiskTypes.PHYSICAL,
-    });
-    const state = factory.rootState({
-      machine: factory.machineState({
-        items: [
-          factory.machineDetails({
-            disks: [...vgs, physicalDisk],
-            system_id: "abc123",
-          }),
-        ],
-        statuses: factory.machineStatuses({
-          abc123: factory.machineStatus(),
-        }),
-      }),
-    });
-    renderWithProviders(
-      <CreateVolumeGroup selected={[physicalDisk]} systemId="abc123" />,
-      { state }
-    );
-
-    // Two volume groups already exist so the next one should be vg2
-    expect(screen.getByRole("textbox", { name: "Name" })).toHaveValue("vg2");
-  });
-
-  it("shows the details of the selected storage devices", () => {
+describe("CreateVolumeGroupForm", () => {
+  describe("CreateVolumeGroupForm Table", () => {
     const [selectedDisk, selectedPartition] = [
       factory.nodeDisk({
         name: "floppy",
@@ -62,73 +31,121 @@ describe("CreateVolumeGroup", () => {
         }),
       }),
     });
-
-    renderWithProviders(
-      <CreateVolumeGroup
-        selected={[selectedDisk, selectedPartition]}
-        systemId="abc123"
-      />,
-      { state }
-    );
-
-    const rows = screen.getAllByRole("row");
-    expect(rows).toHaveLength(3);
-    expect(within(rows[1]).getAllByRole("gridcell")[0]).toHaveTextContent(
-      selectedDisk.name
-    );
-    expect(within(rows[2]).getAllByRole("gridcell")[0]).toHaveTextContent(
-      selectedPartition.name
-    );
-    expect(screen.getByText(new RegExp(selectedPartition.name, "i")));
-  });
-
-  it("correctly dispatches an action to create a volume group", async () => {
-    const [selectedDisk, selectedPartition] = [
-      factory.nodeDisk({ partitions: null, type: DiskTypes.PHYSICAL }),
-      factory.nodePartition({ filesystem: null }),
-    ];
-    const disks = [
-      selectedDisk,
-      factory.nodeDisk({ partitions: [selectedPartition] }),
-    ];
-    const state = factory.rootState({
-      machine: factory.machineState({
-        items: [factory.machineDetails({ disks: disks, system_id: "abc123" })],
-        statuses: factory.machineStatuses({
-          abc123: factory.machineStatus(),
-        }),
-      }),
+    it("displays the columns correctly", () => {
+      renderWithProviders(
+        <CreateVolumeGroup selected={[]} systemId="abc123" />,
+        { state }
+      );
+      ["Name", "Size", "Device Type"].forEach((column) => {
+        expect(
+          screen.getByRole("columnheader", {
+            name: new RegExp(`^${column}`, "i"),
+          })
+        ).toBeInTheDocument();
+      });
     });
-    const { store } = renderWithProviders(
-      <CreateVolumeGroup
-        selected={[selectedDisk, selectedPartition]}
-        systemId="abc123"
-      />,
-      { state }
-    );
+    it("shows the details of the selected storage devices in the table", () => {
+      renderWithProviders(
+        <CreateVolumeGroup
+          selected={[selectedDisk, selectedPartition]}
+          systemId="abc123"
+        />,
+        { state }
+      );
 
-    await userEvent.click(
-      screen.getByRole("button", { name: "Create volume group" })
-    );
+      const rows = screen.getAllByRole("row");
+      expect(rows).toHaveLength(3);
+      expect(within(rows[1]).getAllByRole("cell")[0]).toHaveTextContent(
+        selectedDisk.name
+      );
+      expect(within(rows[2]).getAllByRole("cell")[0]).toHaveTextContent(
+        selectedPartition.name
+      );
+      expect(screen.getByText(new RegExp(selectedPartition.name, "i")));
+    });
+  });
+  describe("CreateVolumeGroupForm Details and Actions", () => {
+    it("sets the initial name correctly", () => {
+      const vgs = [
+        factory.nodeDisk({ type: DiskTypes.VOLUME_GROUP }),
+        factory.nodeDisk({ type: DiskTypes.VOLUME_GROUP }),
+      ];
+      const physicalDisk = factory.nodeDisk({
+        partitions: null,
+        type: DiskTypes.PHYSICAL,
+      });
+      const state = factory.rootState({
+        machine: factory.machineState({
+          items: [
+            factory.machineDetails({
+              disks: [...vgs, physicalDisk],
+              system_id: "abc123",
+            }),
+          ],
+          statuses: factory.machineStatuses({
+            abc123: factory.machineStatus(),
+          }),
+        }),
+      });
+      renderWithProviders(
+        <CreateVolumeGroup selected={[physicalDisk]} systemId="abc123" />,
+        { state }
+      );
 
-    expect(
-      store
-        .getActions()
-        .find((action) => action.type === "machine/createVolumeGroup")
-    ).toEqual({
-      meta: {
-        method: "create_volume_group",
-        model: "machine",
-      },
-      payload: {
-        params: {
-          block_devices: [selectedDisk.id],
-          name: "vg0",
-          partitions: [selectedPartition.id],
-          system_id: "abc123",
+      // Two volume groups already exist so the next one should be vg2
+      expect(screen.getByRole("textbox", { name: "Name" })).toHaveValue("vg2");
+    });
+
+    it("correctly dispatches an action to create a volume group", async () => {
+      const [selectedDisk, selectedPartition] = [
+        factory.nodeDisk({ partitions: null, type: DiskTypes.PHYSICAL }),
+        factory.nodePartition({ filesystem: null }),
+      ];
+      const disks = [
+        selectedDisk,
+        factory.nodeDisk({ partitions: [selectedPartition] }),
+      ];
+      const state = factory.rootState({
+        machine: factory.machineState({
+          items: [
+            factory.machineDetails({ disks: disks, system_id: "abc123" }),
+          ],
+          statuses: factory.machineStatuses({
+            abc123: factory.machineStatus(),
+          }),
+        }),
+      });
+      const { store } = renderWithProviders(
+        <CreateVolumeGroup
+          selected={[selectedDisk, selectedPartition]}
+          systemId="abc123"
+        />,
+        { state }
+      );
+
+      await userEvent.click(
+        screen.getByRole("button", { name: "Create volume group" })
+      );
+
+      expect(
+        store
+          .getActions()
+          .find((action) => action.type === "machine/createVolumeGroup")
+      ).toEqual({
+        meta: {
+          method: "create_volume_group",
+          model: "machine",
         },
-      },
-      type: "machine/createVolumeGroup",
+        payload: {
+          params: {
+            block_devices: [selectedDisk.id],
+            name: "vg0",
+            partitions: [selectedPartition.id],
+            system_id: "abc123",
+          },
+        },
+        type: "machine/createVolumeGroup",
+      });
     });
   });
 });
