4197f44d8fa7878b16f64c21334c9046e77f733b
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2026-01-13 21:53:32 +0300

    feat(images): removed old sync uses, added slient polling cleanup to logout

--- src/app/base/hooks/logout.tsx
+++ src/app/base/hooks/logout.tsx
@@ -1,11 +1,13 @@
 import { useDispatch } from "react-redux";
 
+import { resetSilentPolling } from "@/app/api/query/imageSync";
 import { statusActions } from "@/app/store/status";
 
 export const useLogout = () => {
   const dispatch = useDispatch();
 
   return () => {
+    resetSilentPolling();
     localStorage.removeItem("maas-config");
     dispatch(statusActions.logout());
   };
