752297b9936e75441c7356d0ed45561a46793427
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2026-02-02 14:34:07 +0300

    tests(networkDiscovery): fixed cypress tests

--- cypress/e2e/with-users/dashboard/dashboard.spec.ts
+++ cypress/e2e/with-users/dashboard/dashboard.spec.ts
@@ -7,21 +7,8 @@ context("Network Discovery", () => {
   });
 
   it("renders the correct heading", () => {
-    cy.get("[data-testid='section-header-title']").contains(
+    cy.get("[data-testid='main-toolbar-heading']").contains(
       "Network discovery"
     );
-  });
-
-  it("displays the discoveries tab by default", () => {
-    const tab = ".p-tabs__link[aria-selected='true']";
-    cy.get(tab).should("exist");
-    cy.get(tab).contains("discover");
-  });
-
-  it("can display the configuration tab", () => {
-    cy.get(".p-tabs__link:contains(Configuration)").click();
-    const tab = ".p-tabs__link[aria-selected='true']";
-    cy.get(tab).should("exist");
-    cy.get(tab).contains("Configuration");
   });
 });
