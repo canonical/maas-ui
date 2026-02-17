3a7869508dfc76acfdaa1f3141cbcd8bd5de7197
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2026-02-17 18:26:26 +0300

    refactor(kvm): LXD hosts table to GenericTable MAASENG-5684 (#5932)

--- src/app/kvm/views/KVMList/KVMList.tsx
+++ src/app/kvm/views/KVMList/KVMList.tsx
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router";
 
 import KVMListHeader from "./KVMListHeader";
-import LxdTable from "./LxdTable";
+import LXDHostsTable from "./LXDHostsTable";
 import VirshTable from "./components/VirshTable/VirshTable";
 
 import PageContent from "@/app/base/components/PageContent/PageContent";
@@ -52,7 +52,7 @@ const KVMList = (): ReactElement => {
   } else if (showingLXD && hasLXDs) {
     content = (
       <Strip className="u-no-padding--bottom" data-testid="lxd-table" shallow>
-        <LxdTable />
+        <LXDHostsTable />
       </Strip>
     );
   } else if (showingVirsh && hasVirsh) {
