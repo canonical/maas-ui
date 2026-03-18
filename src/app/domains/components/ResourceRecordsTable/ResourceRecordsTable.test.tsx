06f4c8bdd81a856702473f2a31fcba81dc73e395
Nick De Villiers <nick.devilliers@canonical.com>
2026-03-18 16:22:04 +0000

    fix(domains): show rrtype and rrdata in resource records table (#5965)
    - Display `rrtype` instead of `node_type`
    - Display `rrdata` instead of `data`

--- src/app/domains/components/ResourceRecordsTable/ResourceRecordsTable.test.tsx
+++ src/app/domains/components/ResourceRecordsTable/ResourceRecordsTable.test.tsx
@@ -30,7 +30,7 @@ describe("ResourceRecordsTable", () => {
           dnsdata_id: 15,
           dnsresource_id: 100,
           node_type: 4,
-          rrdata: "",
+          rrdata: "192.168.1.1",
           rrtype: RecordType.A,
         },
       ],
@@ -63,8 +63,9 @@ describe("ResourceRecordsTable", () => {
     const row = within(screen.getAllByRole("row")[1]);
 
     expect(row.getAllByRole("cell")[0]).toHaveTextContent("abc");
-    expect(row.getAllByRole("cell")[1]).toHaveTextContent("4");
+    expect(row.getAllByRole("cell")[1]).toHaveTextContent(RecordType.A);
     expect(row.getAllByRole("cell")[2]).toHaveTextContent("20");
+    expect(row.getAllByRole("cell")[3]).toHaveTextContent("192.168.1.1");
   });
 
   it("renders a link in the name column when id is auto-generated", () => {
