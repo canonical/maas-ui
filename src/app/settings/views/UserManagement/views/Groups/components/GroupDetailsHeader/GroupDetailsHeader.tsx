8a298641e386b4105e08d474341857cacf1bf0eb
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2026-04-17 18:15:49 +0300

    feat(groups): action grouping and back to list

--- src/app/settings/views/UserManagement/views/Groups/components/GroupDetailsHeader/GroupDetailsHeader.tsx
+++ src/app/settings/views/UserManagement/views/Groups/components/GroupDetailsHeader/GroupDetailsHeader.tsx
@@ -7,6 +7,7 @@ import { useGroupStatistics } from "@/app/api/query/groups";
 import type { UserGroupResponse } from "@/app/apiclient";
 import SectionHeader from "@/app/base/components/SectionHeader";
 import { useSidePanel } from "@/app/base/side-panel-context";
+import urls from "@/app/settings/urls";
 import DeleteGroup from "@/app/settings/views/UserManagement/views/Groups/components/DeleteGroup";
 import EditGroup from "@/app/settings/views/UserManagement/views/Groups/components/EditGroup";
 
@@ -35,55 +36,59 @@ const GroupDetailsHeader = ({
         <ContextualMenu
           hasToggleIcon
           links={[
-            {
-              children: "Add entitlement...",
-              onClick: () => {
-                openSidePanel({
-                  component: EditGroup,
-                  title: "Add entitlement",
-                  props: {
-                    id: group!.id,
-                  },
-                });
+            [
+              {
+                children: "Add entitlement...",
+                onClick: () => {
+                  openSidePanel({
+                    component: EditGroup,
+                    title: "Add entitlement",
+                    props: {
+                      id: group!.id,
+                    },
+                  });
+                },
               },
-            },
-            {
-              children: "Add member...",
-              onClick: () => {
-                openSidePanel({
-                  component: EditGroup,
-                  title: "Add member",
-                  props: {
-                    id: group!.id,
-                  },
-                });
+              {
+                children: "Add member...",
+                onClick: () => {
+                  openSidePanel({
+                    component: EditGroup,
+                    title: "Add member",
+                    props: {
+                      id: group!.id,
+                    },
+                  });
+                },
               },
-            },
-            {
-              children: "Edit...",
-              onClick: () => {
-                openSidePanel({
-                  component: EditGroup,
-                  title: "Edit group",
-                  props: {
-                    id: group!.id,
-                  },
-                });
+            ],
+            [
+              {
+                children: "Edit...",
+                onClick: () => {
+                  openSidePanel({
+                    component: EditGroup,
+                    title: "Edit group",
+                    props: {
+                      id: group!.id,
+                    },
+                  });
+                },
               },
-            },
-            {
-              children: "Delete...",
-              onClick: () => {
-                openSidePanel({
-                  component: DeleteGroup,
-                  title: "Delete group",
-                  props: {
-                    id: group!.id,
-                    user_count: statistics?.items[0]?.user_count ?? 0,
-                  },
-                });
+              {
+                children: "Delete...",
+                onClick: () => {
+                  openSidePanel({
+                    component: DeleteGroup,
+                    title: "Delete group",
+                    props: {
+                      id: group!.id,
+                      user_count: statistics?.items[0]?.user_count ?? 0,
+                    },
+                  });
+                },
               },
-            },
+            ],
           ]}
           position="right"
           toggleAppearance="positive"
@@ -105,7 +110,18 @@ const GroupDetailsHeader = ({
           to: `${urlBase}/members`,
         },
       ]}
-      title={group ? group.name : ""}
+      title={
+        <>
+          {group ? group.name : "Group"}
+          <Link
+            className="u-nudge-right"
+            style={{ fontSize: "1rem" }}
+            to={urls.userManagement.groups}
+          >
+            &lsaquo; Back to all groups
+          </Link>
+        </>
+      }
     />
   );
 };
