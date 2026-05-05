91332758124051c56cd58c1eb4e639e43566edb1
Neh Joshi <neh.joshi@canonical.com>
2026-05-05 12:07:47 +0100

    chore: add e2e test for adding DNS records MAASENG-6452 (#6007)
    - Added an end-to-end verifying that new DNS records can be added and are correctly hyperlinked
    
    Resolves [MAASENG-6452](https://warthogs.atlassian.net/browse/MAASENG-6452)

--- cypress/support/step_definitions/common/navigation.steps.ts
+++ cypress/support/step_definitions/common/navigation.steps.ts
@@ -2,6 +2,9 @@ import { Given, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { routes } from "../../../constants";
 import { generateMAASURL } from "../../../e2e/utils";
 
+const escapeRegExp = (value: string) =>
+  value.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
 Given(/^the user navigates to the (.+) page$/, (page: string) => {
   const path = routes[page];
   cy.visit(generateMAASURL(path));
@@ -14,5 +17,14 @@ Then(/^the user is redirected to the (.+) page$/, (page: string) => {
 });
 
 Then("the pathname should equal {string}", (expectedPath: string) => {
-  cy.location("pathname").should("eq", generateMAASURL(expectedPath));
+  const expectedFullPath = generateMAASURL(expectedPath);
+
+  if (expectedPath.includes("*")) {
+    const pattern = `^${escapeRegExp(expectedFullPath).replace(/\\\*/g, "[^/]+")}$`;
+    // "*" matches one path segment, e.g. "/device/*/details" matches "/device/abc123/details"
+    cy.location("pathname").should("match", new RegExp(pattern));
+    return;
+  }
+
+  cy.location("pathname").should("eq", expectedFullPath);
 });
