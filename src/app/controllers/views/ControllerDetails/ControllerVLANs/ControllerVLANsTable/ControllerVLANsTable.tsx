0e7cdb7393f4a075053f5c77b3f0cc0a456ccab4
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2026-05-21 15:42:19 +0300

    refactor(controllers): ControllerVLANsTable to GenericTable MAASENG-5680 (#6041)

--- src/app/controllers/views/ControllerDetails/ControllerVLANs/ControllerVLANsTable/ControllerVLANsTable.tsx
+++ src/app/controllers/views/ControllerDetails/ControllerVLANs/ControllerVLANsTable/ControllerVLANsTable.tsx
@@ -1,95 +1,35 @@
-import { useMemo } from "react";
+import type { ReactElement } from "react";
 
-import { ModularTable } from "@canonical/react-components";
-import type { ColumnWithLooseAccessor } from "react-table";
+import { GenericTable } from "@canonical/maas-react-components";
 
-import { columnLabels, ControllerVLANsColumns } from "./constants";
 import { useControllerVLANsTable } from "./hooks";
-import type { ControllerTableData } from "./types";
 
-import ControllerLink from "@/app/base/components/ControllerLink";
-import FabricLink from "@/app/base/components/FabricLink";
-import SubnetLink from "@/app/base/components/SubnetLink";
-import VLANLink from "@/app/base/components/VLANLink";
+import useControllerVLANsTableColumns from "@/app/controllers/views/ControllerDetails/ControllerVLANs/ControllerVLANsTable/useControllerVLANsTableColumns/useControllerVLANsTableColumns";
 import type { Controller, ControllerMeta } from "@/app/store/controller/types";
-import type { Subnet } from "@/app/store/subnet/types";
 
-type Props = {
+import "./_index.scss";
+
+type ControllerVLANsTableProps = {
   systemId: Controller[ControllerMeta.PK];
 };
 
-const ControllerVLANsTable = ({ systemId }: Props): React.ReactElement => {
+const ControllerVLANsTable = ({
+  systemId,
+}: ControllerVLANsTableProps): ReactElement => {
   const { data, loaded } = useControllerVLANsTable({ systemId });
 
+  const columns = useControllerVLANsTableColumns();
+
   return (
-    <ModularTable
+    <GenericTable
       aria-label="Controller VLANs"
       className="controller-vlans-table"
-      columns={useMemo(
-        () =>
-          [
-            {
-              Header: columnLabels[ControllerVLANsColumns.FABRIC],
-              accessor: ControllerVLANsColumns.FABRIC,
-              Cell: ({
-                value,
-              }: {
-                value: ControllerTableData[ControllerVLANsColumns.FABRIC];
-              }) => <FabricLink id={value?.id} />,
-            },
-            {
-              Header: columnLabels[ControllerVLANsColumns.VLAN],
-              accessor: ControllerVLANsColumns.VLAN,
-              Cell: ({
-                value,
-              }: {
-                value: ControllerTableData[ControllerVLANsColumns.VLAN];
-              }) => <VLANLink id={value?.id} />,
-            },
-            {
-              Header: columnLabels[ControllerVLANsColumns.DHCP],
-              accessor: ControllerVLANsColumns.DHCP,
-            },
-            {
-              Header: columnLabels[ControllerVLANsColumns.SUBNET],
-              accessor: ControllerVLANsColumns.SUBNET,
-              Cell: ({
-                value,
-              }: {
-                value: ControllerTableData[ControllerVLANsColumns.SUBNET];
-              }) => (
-                <>
-                  {value?.map(({ id }: Subnet) => (
-                    <div key={id}>
-                      <SubnetLink id={id} />
-                    </div>
-                  ))}
-                </>
-              ),
-            },
-            {
-              Header: columnLabels[ControllerVLANsColumns.PRIMARY_RACK],
-              accessor: ControllerVLANsColumns.PRIMARY_RACK,
-              Cell: ({
-                value,
-              }: {
-                value: ControllerTableData[ControllerVLANsColumns.PRIMARY_RACK];
-              }) => <ControllerLink systemId={value} />,
-            },
-            {
-              Header: columnLabels[ControllerVLANsColumns.SECONDARY_RACK],
-              accessor: ControllerVLANsColumns.SECONDARY_RACK,
-              Cell: ({
-                value,
-              }: {
-                value: ControllerTableData[ControllerVLANsColumns.SECONDARY_RACK];
-              }) => <ControllerLink systemId={value} />,
-            },
-          ] as ColumnWithLooseAccessor[],
-        []
-      )}
+      columns={columns}
       data={data}
-      emptyMsg={loaded ? "No VLANs found" : "Loading..."}
+      isLoading={!loaded}
+      noData="No VLANs found."
+      sorting={[{ id: "fabric", desc: true }]}
+      variant="full-height"
     />
   );
 };
---
+++ src/app/controllers/views/ControllerDetails/ControllerVLANs/ControllerVLANsTable/useControllerVLANsTableColumns/useControllerVLANsTableColumns.tsx
@@ -0,0 +1,94 @@
+import { useMemo } from "react";
+
+import type { ColumnDef } from "@tanstack/react-table";
+
+import ControllerLink from "@/app/base/components/ControllerLink";
+import FabricLink from "@/app/base/components/FabricLink";
+import SubnetLink from "@/app/base/components/SubnetLink";
+import VLANLink from "@/app/base/components/VLANLink";
+import type { ControllerTableData } from "@/app/controllers/views/ControllerDetails/ControllerVLANs/ControllerVLANsTable/types";
+import type { Subnet } from "@/app/store/subnet/types";
+
+type ControllerVLANsColumnDef = ColumnDef<
+  ControllerTableData,
+  Partial<ControllerTableData>
+>;
+
+const useControllerVLANsTableColumns = (): ControllerVLANsColumnDef[] => {
+  return useMemo(
+    () => [
+      {
+        id: "fabric",
+        header: "Fabric",
+        accessorKey: "fabric_id",
+        enableSorting: false,
+        cell: ({
+          row: {
+            original: { fabric_id },
+          },
+        }) => <FabricLink id={fabric_id} />,
+      },
+      {
+        id: "vlan",
+        header: "VLAN",
+        accessorKey: "vlan_id",
+        enableSorting: false,
+        cell: ({
+          row: {
+            original: { vlan_id },
+          },
+        }) => <VLANLink id={vlan_id} />,
+      },
+      {
+        id: "dhcp",
+        header: "DHCP",
+        accessorKey: "dhcp",
+        enableSorting: false,
+      },
+      {
+        id: "subnets",
+        header: "Subnets",
+        accessorKey: "subnets",
+        enableSorting: false,
+        cell: ({
+          row: {
+            original: { subnet },
+          },
+        }) => (
+          <>
+            {subnet?.map(({ id }: Subnet) => (
+              <div key={id}>
+                <SubnetLink id={id} />
+              </div>
+            ))}
+          </>
+        ),
+      },
+      {
+        id: "primary_rack",
+        header: "Primary rack",
+        accessorKey: "primary_rack",
+        enableSorting: false,
+        cell: ({
+          row: {
+            original: { primary_rack },
+          },
+        }) => <ControllerLink systemId={primary_rack} />,
+      },
+      {
+        id: "secondary_rack",
+        header: "Secondary rack",
+        accessorKey: "secondary_rack",
+        enableSorting: false,
+        cell: ({
+          row: {
+            original: { secondary_rack },
+          },
+        }) => <ControllerLink systemId={secondary_rack} />,
+      },
+    ],
+    []
+  );
+};
+
+export default useControllerVLANsTableColumns;
