7b826f24605516164369ceec40c581aa224a49aa
Neh Joshi <neh.joshi@canonical.com>
2026-05-25 14:23:34 +0100

    chore: add cucumber tests for no-users MAASENG-5921 (#6042)
    - Migrated all the no-users cypress tests to the cucumber model
    
    Resolves [MAASENG-5921](https://warthogs.atlassian.net/browse/MAASENG-5921)

--- cypress/e2e/no-users/local-docs.spec.ts
+++
@@ -1,8 +0,0 @@
-context("Local documentation", () => {
-  beforeEach(() => {
-    cy.visit(`${Cypress.env("BASENAME")}/docs`);
-  });
-  it("displays a correct heading", () => {
-    cy.findByRole("heading", { name: /MAAS documentation/i }).should("exist");
-  });
-});
