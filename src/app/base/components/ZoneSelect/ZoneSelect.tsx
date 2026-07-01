5109216830c8a875b93a67ef6579defb9ed1c4f1
nehjoshi5 <neh.joshi@canonical.com>
2026-07-01 10:20:20 +0100

    fix(machines): use name for zone update request

--- src/app/base/components/ZoneSelect/ZoneSelect.tsx
+++ src/app/base/components/ZoneSelect/ZoneSelect.tsx
@@ -3,14 +3,13 @@ import type { HTMLProps } from "react";
 import { Select } from "@canonical/react-components";
 
 import { useZones } from "@/app/api/query/zones";
-import type { ZoneResponse } from "@/app/apiclient";
 import FormikField from "@/app/base/components/FormikField";
 
 type Props = HTMLProps<HTMLSelectElement> & {
   disabled?: boolean;
   label?: string;
   name: string;
-  valueKey?: keyof ZoneResponse;
+  valueKey?: "id" | "name";
 };
 
 export enum Label {
@@ -37,7 +36,7 @@ export const ZoneSelect = ({
         ...(zones.data?.items?.map((zone) => ({
           key: `zone-${zone.id}`,
           label: zone.name,
-          value: zone.id,
+          value: zone[valueKey ?? "id"],
         })) || []),
       ]}
       {...props}
