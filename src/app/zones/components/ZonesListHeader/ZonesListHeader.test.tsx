b72252350fe0ab9098d128db7529e3b29f652153
nehjoshi5 <neh.joshi@canonical.com>
2026-09-02 09:45:34 +0100

    fix(zones): prevent actions when the CAN_EDIT_* entitlement is missing

--- src/app/zones/components/ZonesListHeader/ZonesListHeader.test.tsx
+++ src/app/zones/components/ZonesListHeader/ZonesListHeader.test.tsx
@@ -1,20 +1,46 @@
 import ZonesListHeader from "./ZonesListHeader";
 
+import { authResolvers } from "@/testing/resolvers/auth";
 import {
   userEvent,
   screen,
   renderWithProviders,
   mockSidePanel,
+  setupMockServer,
+  waitFor,
 } from "@/testing/utils";
 
+const mockServer = setupMockServer(
+  authResolvers.getCurrentUser.handler(),
+  authResolvers.getMeEntitlements.handler()
+);
 const { mockOpen } = await mockSidePanel();
 
 describe("ZonesListHeader", () => {
   it("displays the form when Add AZ is clicked", async () => {
     renderWithProviders(<ZonesListHeader />);
 
+    await waitFor(() => {
+      expect(
+        screen.getByRole("button", { name: "Add AZ" })
+      ).not.toBeAriaDisabled();
+    });
+
     await userEvent.click(screen.getByRole("button", { name: "Add AZ" }));
 
     expect(mockOpen).toHaveBeenCalled();
+  });
+
+  it("disables the Add AZ button without permissions", async () => {
+    mockServer.use(authResolvers.getMeEntitlements.handler([]));
+    renderWithProviders(<ZonesListHeader />);
+
+    await waitFor(() => {
+      expect(screen.getByRole("button", { name: "Add AZ" })).toBeAriaDisabled();
+    });
+
+    await userEvent.click(screen.getByRole("button", { name: "Add AZ" }));
+
+    expect(mockOpen).not.toHaveBeenCalled();
   });
 });
