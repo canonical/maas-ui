7f6833ab312c7e3afc30467aeba43d3486a8b4e4
Neh Joshi <neh.joshi@canonical.com>
2026-09-07 08:54:16 +0100

    fix(machines): update machines list when a new machine is manually added (#6140)
    - Previously, from the `/machines` route, when you added a new machine using `Add Hardware` > `Machine`, the machines table would show stale values, and the new machine wasn't visible until you refreshed the page
    - This PR fixes that by invalidating `machineActions` queries, forcing a re-fetch of the machines list

--- src/app/machines/components/MachineForms/AddMachine/AddMachineForm/AddMachineForm.tsx
+++ src/app/machines/components/MachineForms/AddMachine/AddMachineForm/AddMachineForm.tsx
@@ -161,6 +161,7 @@ export const AddMachineForm = (): ReactElement => {
             dispatch(machineActions.create(params));
           }}
           onSuccess={() => {
+            dispatch(machineActions.invalidateQueries());
             if (!secondarySubmit) {
               closeSidePanel();
             }
